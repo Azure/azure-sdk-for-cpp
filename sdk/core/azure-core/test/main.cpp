dc624e5ff935ccfb08640addce765c763ad04cb5
Rick Winter <rick.winter@microsoft.com>
2020-02-20 12:08:32 -0800

    Add core scaffolding

---
+++ sdk/core/azure-core/test/main.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#include <azure.hpp>
+
+#include <stdlib.h>
+
+namespace azure {
+    namespace core {
+        int main() {
+            return 0;
+        }
+    }
+}
