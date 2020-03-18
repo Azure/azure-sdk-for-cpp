0f0c419fe810696cdcbd22b7b7a58487fd7079b4
Victor Vazquez <vhvb1989@gmail.com>
2020-03-18 17:26:11 +0000

    Adding http namespace

---
+++ sdk/core/azure-core/src/http/az_http_request.cpp
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#include <contract.hpp>
+
+namespace azure
+{
+namespace core
+{
+namespace http
+{
+class az_http_request
+{
+
+private:
+  /* data */
+public:
+  az_http_request();
+  ~az_http_request();
+};
+
+az_http_request::az_http_request() {}
+
+az_http_request::~az_http_request() {}
+
+} // namespace http
+} // namespace core
+} // namespace azure
