f579fa72acc4749488c5d41af84f242da898b9f5
Victor Vazquez <vhvb1989@gmail.com>
2020-03-24 21:33:01 +0000

    tests for request

--- sdk/core/azure-core/test/main.cpp
+++ sdk/core/azure-core/test/main.cpp
@@ -1,16 +1,29 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
 
-#include <azure.hpp>
+#include <http/http.hpp>
+
+#include "gtest/gtest.h"
 
 #include <stdlib.h>
 
-namespace azure
-{
-namespace core
-{
+using namespace azure::core;
 
-int main() { return 0; }
+TEST(Http_Request, getters)
+{
+  std::string http_method = "GET";
+  std::string url = "GET";
+  http::Request req(http_method, url);
 
-} // namespace core
-} // namespace azure
+  // EXPECT_PRED works better than just EQ because it will print values in log
+  EXPECT_PRED2([](string a, string b) { return a == b; }, req.getMethod(), http_method);
+  EXPECT_PRED2([](string a, string b) { return a == b; }, req.getUrl(), url);
+  EXPECT_PRED2([](string a, string b) { return a == b; }, req.getBody(), "");
+
+  std::string body = "a body";
+  http::Request req_with_body(http_method, url, body);
+
+  EXPECT_PRED2([](string a, string b) { return a == b; }, req_with_body.getMethod(), http_method);
+  EXPECT_PRED2([](string a, string b) { return a == b; }, req_with_body.getUrl(), url);
+  EXPECT_PRED2([](string a, string b) { return a == b; }, req_with_body.getBody(), body);
+}
