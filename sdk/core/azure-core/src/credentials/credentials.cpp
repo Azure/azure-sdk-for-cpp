a9145731939cf75e63859675081531df9b351679
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2020-03-30 17:24:36 -0700

    Add tests

--- sdk/core/azure-core/src/credentials/credentials.cpp
+++ sdk/core/azure-core/src/credentials/credentials.cpp
@@ -6,11 +6,7 @@
 #include <credentials/credentials.hpp>
 #include <string>
 
-namespace azure
-{
-
-namespace credentials
-{
+using namespace azure::core::credentials;
 
 Credential::Credential() = default;
 Credential::~Credential() noexcept = default;
@@ -19,7 +15,7 @@ Credential& Credential::operator=(Credential const&) = default;
 
 std::string const& Credential::GetScopes() const
 {
-  static std::string empty;
+  static std::string const empty;
   return empty;
 }
 
@@ -30,8 +26,32 @@ TokenCredential::~TokenCredential() noexcept = default;
 TokenCredential::TokenCredential(TokenCredential const&) = default;
 TokenCredential& TokenCredential::operator=(TokenCredential const&) = default;
 
-std::string const& TokenCredential::GetScopes() { return this->scopes; }
-void TokenCredential::SetScopes(std::string const& scopes) { this->_scopes = scopes; }
+std::string const& TokenCredential::GetScopes() const { return this->_scopes; }
+
+void TokenCredential::SetScopes(std::string const& scopes)
+{
+  // TODO: mutex
+  this->_token = std::string();
+  this->_expiresAt = std::chrono::system_clock::time_point();
+
+  this->_scopes = scopes;
+}
+
+std::string const& TokenCredential::GetToken() const { return this->_token; }
+
+std::chrono::system_clock::time_point const& TokenCredential::GetTokenExpiration() const
+{
+  return this->_expiresAt;
+}
+
+void TokenCredential::SetToken(
+    std::string const& token,
+    std::chrono::system_clock::time_point const& expiration)
+{
+  // TODO: mutex
+  this->_token = token;
+  this->_expiresAt = expiration;
+}
 
 ClientSecretCredential::ClientSecretCredential(
     std::string const& tenantId,
@@ -48,9 +68,3 @@ ClientSecretCredential& ClientSecretCredential::operator=(ClientSecretCredential
 std::string const& ClientSecretCredential::GetTenantId() const { return this->_tenantId; }
 std::string const& ClientSecretCredential::GetClientId() const { return this->_clientId; }
 std::string const& ClientSecretCredential::GetClientSecret() const { return this->_clientSecret; }
-
-}; // namespace credentials
-
-} // namespace azure
-
-} // namespace azure
