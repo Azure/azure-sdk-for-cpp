27273290288cf9805e8c4bd2aa9116c53dcfbf23
Tank Tang <kat@microsoft.com>
2020-05-15 10:34:49 +0800

    Adding test framework for azure storage.

---
+++ sdk/storage/test/test_base.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#include "test_base.hpp"
+
+namespace Azure  {  namespace Storage  {  namespace Test  {
+
+constexpr const char* TestUtility::k_STANDARD_STORAGE_CONNECTION_STRING;
+constexpr const char* TestUtility::k_PREMIUM_STORAGE_CONNECTION_STRING;
+constexpr const char* TestUtility::k_BLOB_STORAGE_CONNECTION_STRING;
+constexpr const char* TestUtility::k_PREMIUM_FILE_CONNECTION_STRING;
+constexpr const char* TestUtility::k_ADLS_GEN2_CONNECTION_STRING;
+constexpr const char* TestUtility::k_ACCOUNT_SAS;
+
+}}} // namespace Azure::Storage::Test
