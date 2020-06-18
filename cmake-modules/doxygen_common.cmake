7d2bbe750f6ae4cd38a161124607bd643bae08fe
Daniel Jurek <djurek@microsoft.com>
2020-06-18 16:43:07 -0700

    Use cmake to generate documentation

---
+++ cmake-modules/doxygen_common.cmake
@@ -0,0 +1,22 @@
+function(generate_documentation PROJECT_NAME PROJECT_VERSION)
+    if(BUILD_DOCUMENTATION)
+        find_package(Doxygen REQUIRED doxygen)
+
+        set(DOXYGEN_PROJECT_NAME ${PROJECT_NAME})
+        set(DOXYGEN_PROJECT_NUMBER ${PROJECT_VERSION})
+
+        set(DOXYGEN_OUTPUT_DIRECTORY docs)
+        set(DOXYGEN_LAYOUT_FILE ${CMAKE_SOURCE_DIR}/eng/docs/api/assets/DoxygenLayout.xml)
+        set(DOXYGEN_RECURSIVE YES)
+        set(DOXYGEN_USE_MDFILE_AS_MAINPAGE ./README.md)
+        set(DOXYGEN_INLINE_SOURCES YES)
+        set(DOXYGEN_IGNORE_PREFIX az_ AZ_)
+        set(DOXYGEN_HTML_HEADER ${CMAKE_SOURCE_DIR}/eng/docs/api/assets/header.html)
+        set(DOXYGEN_HTML_FOOTER ${CMAKE_SOURCE_DIR}/eng/docs/api/assets/footer.html)
+        set(DOXYGEN_HTML_STYLESHEET ${CMAKE_SOURCE_DIR}/eng/docs/api/assets/style.css)
+        set(DOXYGEN_GENERATE_LATEX NO)
+
+        doxygen_add_docs(${PROJECT_NAME}-docs
+            COMMENT "Generate documentation for ${TARGET_NAME}")
+    endif()
+endfunction()
