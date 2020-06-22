f0378b4c775f5ce55661877b28c6eb11d6706064
Billy Robert O'Neal III <bion@microsoft.com>
2020-06-21 19:43:14 -0700

    Add locale invariant string equal function in support of the 'extensible enum' guideline.

--- sdk/core/azure-core/inc/azure.hpp
+++ sdk/core/azure-core/inc/azure.hpp
@@ -3,12 +3,13 @@
 
 #pragma once
 
+#include <string>
 #include <internal/contract.hpp>
 
 #define AZURE_UNREFERENCED_PARAMETER(x) ((void) (x));
 
 namespace Azure { namespace Core {
 
-
+bool LocaleInvariantCaseInsensitiveEqual(const std::string& lhs, const std::string& rhs) noexcept;
 
 }} // namespace Azure::Core
