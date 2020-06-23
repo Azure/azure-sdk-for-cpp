4fa03186f8488a71360182b18de1f7b8974586ba
Billy Robert O'Neal III <bion@microsoft.com>
2020-06-22 17:53:14 -0700

    Add more test cases.

--- sdk/core/azure-core/test/string.cpp
+++ sdk/core/azure-core/test/string.cpp
@@ -9,6 +9,14 @@ using namespace Azure::Core;
 
 TEST(String, invariantCompare)
 {
+  EXPECT_TRUE(LocaleInvariantCaseInsensitiveEqual("", ""));
+  EXPECT_TRUE(LocaleInvariantCaseInsensitiveEqual("a", "a"));
+  EXPECT_TRUE(LocaleInvariantCaseInsensitiveEqual("a", "A"));
+  EXPECT_TRUE(LocaleInvariantCaseInsensitiveEqual("aa", "AA"));
+  EXPECT_TRUE(LocaleInvariantCaseInsensitiveEqual("aA", "AA"));
   EXPECT_TRUE(LocaleInvariantCaseInsensitiveEqual("abc", "ABC"));
+  EXPECT_FALSE(LocaleInvariantCaseInsensitiveEqual("", "a"));
+  EXPECT_FALSE(LocaleInvariantCaseInsensitiveEqual("a", ""));
+  EXPECT_FALSE(LocaleInvariantCaseInsensitiveEqual("a", "aA"));
   EXPECT_FALSE(LocaleInvariantCaseInsensitiveEqual("abc", "abcd"));
 }
