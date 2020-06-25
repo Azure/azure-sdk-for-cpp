2c26832defe01b649f77f195b8f80f24bd88faf8
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2020-06-24 17:14:14 -0700

    Use + instead of std::advance

--- sdk/core/azure-core/src/credentials/credentials.cpp
+++ sdk/core/azure-core/src/credentials/credentials.cpp
@@ -135,13 +135,12 @@ AccessToken ClientSecretCredential::GetToken(
 
     expiresInSeconds -= 2 * 60;
     auto const responseBodyBegin = responseBody.begin();
-    std::string const tokenString(
-        std::advance(responseBodyBegin, tokenBegin), std::advance(responseBodyBegin, tokenEnd));
 
-    auto const expiration = std::chrono::system_clock::now()
-        + std::chrono::seconds(expiresInSeconds < 0 ? 0 : expiresInSeconds);
-
-    return {tokenString, expiration};
+    return {
+        std::string(responseBodyBegin + tokenBegin, responseBodyBegin + tokenEnd),
+        std::chrono::system_clock::now()
+            + std::chrono::seconds(expiresInSeconds < 0 ? 0 : expiresInSeconds),
+    };
   }
   catch (AuthenticationException const&)
   {
