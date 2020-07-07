8b98d0e1497080025c235570990bdff914b7b6fd
Daniel Jurek <djurek@microsoft.com>
2020-07-07 12:45:27 -0700

    Add fixes for tagging repo

--- eng/common/scripts/artifact-metadata-parsing.ps1
+++ eng/common/scripts/artifact-metadata-parsing.ps1
@@ -347,6 +347,38 @@ function ParseCArtifact($pkg, $workingDirectory) {
   }
 }
 
+function ParseCppArtifact($pkg, $workingDirectory) {
+  $packageInfo = Get-Content -Raw -Path $pkg | ConvertFrom-JSON
+  $packageArtifactLocation = (Get-ItemProperty $pkg).Directory.FullName
+  $releaseNotes = ""
+  $readmeContent = ""
+
+  $pkgVersion = $packageInfo.version
+  $pkgName = $packageInfo.name
+
+  $changeLogLoc = @(Get-ChildItem -Path $packageArtifactLocation -Recurse -Include "CHANGELOG.md")[0]
+  if ($changeLogLoc)
+  {
+    $releaseNotes = Get-ChangeLogEntryAsString -ChangeLogLocation $changeLogLoc -VersionString $pkgVersion
+  }
+
+  $readmeContentLoc = @(Get-ChildItem -Path $packageArtifactLocation -Recurse -Include "README.md")[0]
+  if ($readmeContentLoc) {
+    $readmeContent = Get-Content -Raw $readmeContentLoc
+  }
+
+  return New-Object PSObject -Property @{
+    PackageId      = $pkgName
+    PackageVersion = $pkgVersion
+    # Artifact info is always considered deployable for now becasue it is not
+    # deployed anywhere. Dealing with duplicate tags happens downstream in
+    # CheckArtifactShaAgainstTagsList
+    Deployable     = $true
+    ReleaseNotes   = $releaseNotes
+  }
+}
+
+
 # Returns the pypi publish status of a package id and version.
 function IsPythonPackageVersionPublished($pkgId, $pkgVersion) {
   try {
@@ -422,6 +454,10 @@ function VerifyPackages($pkgRepository, $artifactLocation, $workingDirectory, $a
     }
     "C" {
       $ParsePkgInfoFn = "ParseCArtifact"
+      $packagePattern = "*.json"
+    }
+    "CPP" {
+      $ParsePkgInfoFn = "ParseCppArtifact"
       $packagePattern = "*.json"
     }
     default {
