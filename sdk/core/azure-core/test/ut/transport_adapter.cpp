60265d65aff7fa042e23ed769fe0e618feed485e
Victor Vazquez <vhvb1989@gmail.com>
2020-07-14 22:05:48 +0000

    adding tests for request types get, put, patch, delete and head

--- sdk/core/azure-core/test/ut/transport_adapter.cpp
+++ sdk/core/azure-core/test/ut/transport_adapter.cpp
@@ -44,4 +44,101 @@ namespace Azure { namespace Core { namespace Test {
         expectedResponseBodySize + 6
             + 13); // header length is 6 (data) + 13 (formating) -> `    "123": "456"\r\n,`
   }
+
+  TEST_F(TransportAdapter, getLoop)
+  {
+    std::string host("http://httpbin.org/get");
+    auto expectedResponseBodySize
+        = 199; // May fail in the future if Server change the response size.
+
+    auto request = Azure::Core::Http::Request(Azure::Core::Http::HttpMethod::Get, host);
+
+    // loop sending request
+    for (auto i = 0; i < 20; i++)
+    {
+      auto response = pipeline.Send(context, request);
+      EXPECT_TRUE(response->GetStatusCode() == Azure::Core::Http::HttpStatusCode::Ok);
+      auto body = response->GetBodyStream();
+      EXPECT_EQ(body->Length(), expectedResponseBodySize);
+      auto bodyVector = Azure::Core::Http::BodyStream::ReadToEnd(context, *body);
+      EXPECT_EQ(expectedResponseBodySize, bodyVector.size());
+    }
+  }
+
+  TEST_F(TransportAdapter, head)
+  {
+    std::string host("http://httpbin.org/get");
+    auto expectedResponseBodySize = 0;
+
+    auto request = Azure::Core::Http::Request(Azure::Core::Http::HttpMethod::Head, host);
+    auto response = pipeline.Send(context, request);
+    EXPECT_TRUE(response->GetStatusCode() == Azure::Core::Http::HttpStatusCode::Ok);
+    auto body = response->GetBodyStream();
+    EXPECT_EQ(body->Length(), expectedResponseBodySize);
+    auto bodyVector = Azure::Core::Http::BodyStream::ReadToEnd(context, *body);
+    // Head response should be 0
+    EXPECT_EQ(0, bodyVector.size());
+    // Check content-length header to be greater than 0
+    auto headers = response->GetHeaders();
+    int64_t contentLengthHeader = std::stoull(headers["content-length"]);
+    EXPECT_TRUE(contentLengthHeader > 0);
+  }
+
+  TEST_F(TransportAdapter, put)
+  {
+    std::string host("http://httpbin.org/put");
+    auto expectedResponseBodySize = 262;
+
+    // PUT 1MB
+    auto requestBodyVector = std::vector<uint8_t>(1024 * 1024, 'x');
+    auto bodyRequest = Azure::Core::Http::MemoryBodyStream(requestBodyVector);
+    auto request
+        = Azure::Core::Http::Request(Azure::Core::Http::HttpMethod::Put, host, &bodyRequest);
+    auto response = pipeline.Send(context, request);
+    EXPECT_TRUE(response->GetStatusCode() == Azure::Core::Http::HttpStatusCode::Ok);
+
+    auto body = response->GetBodyStream();
+    EXPECT_EQ(body->Length(), expectedResponseBodySize);
+    auto bodyVector = Azure::Core::Http::BodyStream::ReadToEnd(context, *body);
+    EXPECT_EQ(expectedResponseBodySize, bodyVector.size());
+  }
+
+  TEST_F(TransportAdapter, deleteRequest)
+  {
+    std::string host("http://httpbin.org/delete");
+    auto expectedResponseBodySize = 265;
+
+    // Delete with 1MB payload
+    auto requestBodyVector = std::vector<uint8_t>(1024 * 1024, 'x');
+    auto bodyRequest = Azure::Core::Http::MemoryBodyStream(requestBodyVector);
+    auto request
+        = Azure::Core::Http::Request(Azure::Core::Http::HttpMethod::Delete, host, &bodyRequest);
+    auto response = pipeline.Send(context, request);
+    EXPECT_TRUE(response->GetStatusCode() == Azure::Core::Http::HttpStatusCode::Ok);
+
+    auto body = response->GetBodyStream();
+    EXPECT_EQ(body->Length(), expectedResponseBodySize);
+    auto bodyVector = Azure::Core::Http::BodyStream::ReadToEnd(context, *body);
+    EXPECT_EQ(expectedResponseBodySize, bodyVector.size());
+  }
+
+  TEST_F(TransportAdapter, patch)
+  {
+    std::string host("http://httpbin.org/patch");
+    auto expectedResponseBodySize = 264;
+
+    // Patch with 1kb payload
+    auto requestBodyVector = std::vector<uint8_t>(1024, 'x');
+    auto bodyRequest = Azure::Core::Http::MemoryBodyStream(requestBodyVector);
+    auto request
+        = Azure::Core::Http::Request(Azure::Core::Http::HttpMethod::Patch, host, &bodyRequest);
+    auto response = pipeline.Send(context, request);
+    EXPECT_TRUE(response->GetStatusCode() == Azure::Core::Http::HttpStatusCode::Ok);
+
+    auto body = response->GetBodyStream();
+    EXPECT_EQ(body->Length(), expectedResponseBodySize);
+    auto bodyVector = Azure::Core::Http::BodyStream::ReadToEnd(context, *body);
+    EXPECT_EQ(expectedResponseBodySize, bodyVector.size());
+  }
+
 }}} // namespace Azure::Core::Test
