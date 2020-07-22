b0e7020eaaec319ba65c4adc9434eb75aee578c0
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2020-07-21 23:43:40 -0700

    Attempt to fix name mangling warning

--- sdk/core/azure-core/src/logging/logging.cpp
+++ sdk/core/azure-core/src/logging/logging.cpp
@@ -33,47 +33,45 @@ LogListener GetLogListener(LogClassification classification) noexcept
 }
 } // namespace
 
-namespace Azure { namespace Core { namespace Logging {
-  void SetLogListener(LogListener logListener)
+void Azure::Core::Logging::SetLogListener(LogListener logListener)
+{
+  std::lock_guard<std::mutex> loggerLock(g_loggerMutex);
+  g_logListener = std::move(logListener);
+}
+
+void Azure::Core::Logging::ResetLogListener()
+{
+  std::lock_guard<std::mutex> loggerLock(g_loggerMutex);
+  g_logListener = {};
+}
+
+void Azure::Core::Logging::SetLogClassifications(std::set<LogClassification> logClassifications)
+{
+  std::lock_guard<std::mutex> loggerLock(g_loggerMutex);
+
+  g_logClassifications = std::move(logClassifications);
+  g_allClassificationsEnabled = false;
+}
+
+void Azure::Core::Logging::ResetLogClassifications()
+{
+  std::lock_guard<std::mutex> loggerLock(g_loggerMutex);
+
+  g_allClassificationsEnabled = true;
+  g_logClassifications = {};
+}
+
+bool Azure::Core::Logging::Details::ShouldWrite(LogClassification classification) noexcept
+{
+  return GetLogListener(classification) != nullptr;
+}
+
+void Azure::Core::Logging::Details::Write(
+    LogClassification classification,
+    std::string const& message) noexcept
+{
+  if (auto logListener = GetLogListener(classification))
   {
-    std::lock_guard<std::mutex> loggerLock(g_loggerMutex);
-    g_logListener = std::move(logListener);
+    logListener(classification, message);
   }
-
-  void ResetLogListener()
-  {
-    std::lock_guard<std::mutex> loggerLock(g_loggerMutex);
-    g_logListener = {};
-  }
-
-  void SetLogClassifications(std::set<LogClassification> logClassifications)
-  {
-    std::lock_guard<std::mutex> loggerLock(g_loggerMutex);
-
-    g_logClassifications = std::move(logClassifications);
-    g_allClassificationsEnabled = false;
-  }
-
-  void ResetLogClassifications()
-  {
-    std::lock_guard<std::mutex> loggerLock(g_loggerMutex);
-
-    g_allClassificationsEnabled = true;
-    g_logClassifications = {};
-  }
-
-  namespace Details {
-    bool ShouldWrite(LogClassification classification) noexcept
-    {
-      return GetLogListener(classification) != nullptr;
-    }
-
-    void Write(LogClassification classification, std::string const& message) noexcept
-    {
-      if (auto logListener = GetLogListener(classification))
-      {
-        logListener(classification, message);
-      }
-    }
-  } // namespace Details
-}}} // namespace Azure::Core::Logging
+}
