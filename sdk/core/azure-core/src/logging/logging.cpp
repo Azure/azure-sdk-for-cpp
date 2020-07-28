3f6be943e06d648f6c24f10128d40787938548f5
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2020-07-28 14:12:36 -0700

    Another way to initialize statics

--- sdk/core/azure-core/src/logging/logging.cpp
+++ sdk/core/azure-core/src/logging/logging.cpp
@@ -28,11 +28,8 @@ LogListener GetLogListener(LogClassification const& classification)
 }
 } // namespace
 
-LogClassifications const Azure::Core::Logging::LogClassification::All
-    = LogClassification::LogClassifications(true);
-
-LogClassifications const Azure::Core::Logging::LogClassification::None
-    = LogClassification::LogClassifications(false);
+LogClassifications const Azure::Core::Logging::LogClassification::All(LogClassifications(true));
+LogClassifications const Azure::Core::Logging::LogClassification::None(LogClassifications(false));
 
 void Azure::Core::Logging::SetLogListener(LogListener logListener)
 {
