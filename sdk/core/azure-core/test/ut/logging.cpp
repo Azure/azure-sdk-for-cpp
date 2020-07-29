2035bb5b59f18690a9c4de95956d05f7e7f306bc
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2020-07-28 22:07:15 -0700

    Expected list fixes

--- sdk/core/azure-core/test/ut/logging.cpp
+++ sdk/core/azure-core/test/ut/logging.cpp
@@ -48,41 +48,42 @@ TEST(Logging, allClassifications)
   EXPECT_EQ(logRecorder.Actual, expected);
 }
 
-TEST(Logging, filteredClassifications){
-    LogArguments const Values{{Http::LogClassification::Request, "Request"},
-                              {Http::LogClassification::Request, "Request"},
-                              {Http::LogClassification::Response, "Response"},
-                              {Http::LogClassification::Request, "Request"},
-                              Http::LogClassification::Retry,
-                              "Retry"},
-};
-
-LogRecorder logRecorder;
-Logging::SetLogListener(logRecorder.LogListener);
-
-auto const AllowedClassification1 = Http::LogClassification::Request;
-auto const AllowedClassification2 = Http::LogClassification::Retry;
-Logging::SetLogClassifications({AllowedClassification1, AllowedClassification2});
-
-LogArguments expected;
-for (auto value : Values)
+TEST(Logging, filteredClassifications)
 {
-  if (value.first == AllowedClassification1 || value.first == AllowedClassification2)
+  LogArguments const Values{
+      {Http::LogClassification::Request, "Request"},
+      {Http::LogClassification::Request, "Request"},
+      {Http::LogClassification::Response, "Response"},
+      {Http::LogClassification::Request, "Request"},
+      {Http::LogClassification::Retry, "Retry"},
+  };
+
+  LogRecorder logRecorder;
+  Logging::SetLogListener(logRecorder.LogListener);
+
+  auto const AllowedClassification1 = Http::LogClassification::Request;
+  auto const AllowedClassification2 = Http::LogClassification::Retry;
+  Logging::SetLogClassifications({AllowedClassification1, AllowedClassification2});
+
+  LogArguments expected;
+  for (auto value : Values)
   {
-    expected.add(value);
+    if (value.first == AllowedClassification1 || value.first == AllowedClassification2)
+    {
+      expected.push_back(value);
+    }
   }
-}
 
-for (auto value : Values)
-{
-  EXPECT_EQ(
-      Logging::Details::ShouldWrite(value.first),
-      (value.first == AllowedClassification1 || value.first == AllowedClassification2));
+  for (auto value : Values)
+  {
+    EXPECT_EQ(
+        Logging::Details::ShouldWrite(value.first),
+        (value.first == AllowedClassification1 || value.first == AllowedClassification2));
 
-  Logging::Details::Write(value.first, value.second);
-}
+    Logging::Details::Write(value.first, value.second);
+  }
 
-EXPECT_EQ(logRecorder.Actual, expected);
+  EXPECT_EQ(logRecorder.Actual, expected);
 }
 
 TEST(Logging, noClassifications)
