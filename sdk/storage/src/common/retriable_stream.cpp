3484a88efae1104821f46156b03dabbc92fdaf8c
Victor Vazquez <vhvb1989@gmail.com>
2020-07-30 05:40:17 +0000

    use structs for retryOptions and stream options

--- sdk/storage/src/common/retriable_stream.cpp
+++ sdk/storage/src/common/retriable_stream.cpp
@@ -10,21 +10,27 @@ namespace Azure { namespace Storage {
 
   int64_t RetriableStream::Read(Context& context, uint8_t* buffer, int64_t count)
   {
+    if (this->m_options.DoInjectError)
+    {
+      throw std::runtime_error("do we say something here for testing?");
+    }
+
     char const* error;
-    for (int64_t intent = 0; intent < this->m_maxRetries; intent++)
+    for (int64_t intent = 0; intent < this->m_options.MaxRetryRequests; intent++)
     {
       // check if we need to get inner stream
       if (this->m_inner == nullptr)
       {
         // Get a new inner bodyStream that start from last offset and size m_count (remaining)
-        this->m_inner = this->m_retryFn(context, this->m_offset, this->m_count);
+        // if this fails, throw should bubble up
+        this->m_inner = this->m_retryFn(context, this->m_retryInfo);
       }
       try
       {
         auto const readBytes = this->m_inner->Read(context, buffer, count);
         // update offset and content
-        this->m_offset += readBytes;
-        this->m_count -= readBytes;
+        this->m_retryInfo.Offset += readBytes;
+        this->m_retryInfo.Count -= readBytes;
         return readBytes;
       }
       catch (std::exception const& e)
