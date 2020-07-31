64060540dc5494874c4d2e4312223e2a6a854d66
Victor Vazquez <vhvb1989@gmail.com>
2020-07-31 21:00:37 +0000

    Download body on error response codes from request

--- sdk/core/azure-core/src/http/transport_policy.cpp
+++ sdk/core/azure-core/src/http/transport_policy.cpp
@@ -16,13 +16,19 @@ std::unique_ptr<RawResponse> TransportPolicy::Send(
    * Call the transport and return
    */
   auto response = m_transport->Send(ctx, request);
-  if (request.  IsDownloadViaStream())
+  auto statusCode = static_cast<typename std::underlying_type<Http::HttpStatusCode>::type>(
+      response->GetStatusCode());
+
+  if (request.IsDownloadViaStream() && statusCode < 300)
   { // special case to return a response with BodyStream to read directly from socket
+    // Return only if response is valid (less than 300)
     return response;
   }
 
   // default behavior for all request is to download body content to Response
   // If ReadToEnd fail, retry policy will eventually call this again
+  // Using DownloadViaStream and getting an error code would also get to here to download error from
+  // body
   auto bodyStream = response->GetBodyStream();
   response->SetBody(BodyStream::ReadToEnd(ctx, *bodyStream));
   // BodyStream is moved out of response. This makes transport implementation to clean any active
