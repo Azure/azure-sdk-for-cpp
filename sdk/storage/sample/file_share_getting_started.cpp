1b4ea7ac7062629f73e2c2a0161fec947f3a3a68
Kan Tang <kat@microsoft.com>
2020-08-02 11:23:50 +0800

    Added file protocol layer. (#367)
    * Added file protocol layer.
    
    * Resolved some comments.

---
+++ sdk/storage/sample/file_share_getting_started.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#include "datalake/datalake.hpp"
+#include "samples_common.hpp"
+#include <iostream>
+
+SAMPLE(FileShareGettingStarted, FileShareGettingStarted)
+void FileShareGettingStarted() {}
