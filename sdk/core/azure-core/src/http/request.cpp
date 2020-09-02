1a223ef2b891e8bdc8a2df81f8cbdd1d1287a43a
Victor Vazquez <vhvb1989@gmail.com>
2020-09-02 08:14:14 +0000

    fix for curl adapter sending request path

--- sdk/core/azure-core/src/http/request.cpp
+++ sdk/core/azure-core/src/http/request.cpp
@@ -59,8 +59,8 @@ std::string Request::GetHTTPMessagePreBody() const
   std::string httpRequest(HttpMethodToString(this->m_method));
   // origin-form. TODO: parse Url to split host from path and use it here instead of empty
   // HTTP version harcoded to 1.0
-  auto const url = this->m_url.ToString();
-  httpRequest += " " + url + " HTTP/1.1\r\n";
+  auto const url = this->m_url.GetPath();
+  httpRequest += " /" + url + " HTTP/1.1\r\n";
   // headers
   for (auto header : this->GetHeaders())
   {
