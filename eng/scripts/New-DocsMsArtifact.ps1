aa59d34af84d9f62fc73cec8dd1ef1cd9aec8207
Daniel Jurek <djurek@microsoft.com>
2020-09-03 17:11:08 -0700

    Consolidate archetype-sdk-client.yml docs.ms generation steps

--- eng/scripts/New-DocsMsArtifact.ps1
+++ eng/scripts/New-DocsMsArtifact.ps1
@@ -5,9 +5,12 @@ Param (
     [Parameter(Mandatory=$True)]
     [string] $ServiceDirectory,
     [Parameter(Mandatory=$True)]
-    [string] $PackageName
+    [string] $PackageName,
+    [string] $TargetFolder = "$RepoRoot/build/sdk/$ServiceDirectory/$PackageName/docs/docs.ms"
 )
 
+npm install -g moxygen
+
 $docsPath = "$RepoRoot/build/sdk/$ServiceDirectory/$PackageName/docs"
-New-Item -ItemType directory -Path "$docsPath/docs.ms" -Force
-moxygen --anchors --output "$docsPath/docs.ms/api-docs.md" "$docsPath/xml"
+New-Item -ItemType directory -Path $TargetFolder -Force
+moxygen --anchors --output "$TargetFolder/api-docs.md" "$docsPath/xml"
