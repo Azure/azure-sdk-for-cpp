50235a4e34e41f7c90b0696acc482f68ddf87213
Victor Vazquez <vhvb1989@gmail.com>
2020-09-10 22:52:39 +0000

    use function instead of macro

--- sdk/core/azure-core/src/http/curl/curl.cpp
+++ sdk/core/azure-core/src/http/curl/curl.cpp
@@ -8,16 +8,22 @@
 #include <string>
 #include <thread>
 
-// Macro for setting libcurl handle options
-#define LIBCURL_SET_OPT(HANDLE, OPT, VALUE, ERROR) \
-  { \
-    auto result = curl_easy_setopt(HANDLE, OPT, VALUE); \
-    if (result != CURLE_OK) \
-    { \
-      throw Azure::Core::Http::TransportException( \
-          ERROR + ". " + std::string(curl_easy_strerror(result))); \
-    } \
+namespace {
+template <typename T>
+inline void setLibcurlOption(
+    CURL* handle,
+    CURLoption option,
+    T value,
+    std::string const& errorMessage)
+{
+  auto result = curl_easy_setopt(handle, option, value);
+  if (result != CURLE_OK)
+  {
+    throw Azure::Core::Http::TransportException(
+        errorMessage + ". " + std::string(curl_easy_strerror(result)));
   }
+}
+} // namespace
 
 using namespace Azure::Core::Http;
 
@@ -879,27 +885,27 @@ std::unique_ptr<CurlConnection> CurlConnectionPool::GetCurlConnection(Request& r
   auto newConnection = std::make_unique<CurlConnection>(host);
 
   // Libcurl setup before open connection (url, connet_only, timeout)
-  LIBCURL_SET_OPT(
+  setLibcurlOption(
       newConnection->GetHandle(),
       CURLOPT_URL,
       request.GetUrl().GetAbsoluteUrl().data(),
-      Details::c_DefaultFailedToGetNewConnectionTemplate + host)
+      Details::c_DefaultFailedToGetNewConnectionTemplate + host);
 
-  LIBCURL_SET_OPT(
+  setLibcurlOption(
       newConnection->GetHandle(),
       CURLOPT_CONNECT_ONLY,
       1L,
-      Details::c_DefaultFailedToGetNewConnectionTemplate + host)
+      Details::c_DefaultFailedToGetNewConnectionTemplate + host);
 
   // curl_easy_setopt(newConnection->GetHandle(), CURLOPT_VERBOSE, 1L);
   // Set timeout to 24h. Libcurl will fail uploading on windows if timeout is:
   // timeout >= 25 days. Fails as soon as trying to upload any data
   // 25 days < timeout > 1 days. Fail on huge uploads ( > 1GB)
-  LIBCURL_SET_OPT(
+  setLibcurlOption(
       newConnection->GetHandle(),
       CURLOPT_TIMEOUT,
       60L * 60L * 24L,
-      Details::c_DefaultFailedToGetNewConnectionTemplate + host)
+      Details::c_DefaultFailedToGetNewConnectionTemplate + host);
 
   auto result = curl_easy_perform(newConnection->GetHandle());
   if (result != CURLE_OK)
