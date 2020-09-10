8d999a353c4173fdc22ebd8d3ab8f8ca93176c14
Victor Vazquez <vhvb1989@gmail.com>
2020-09-10 18:16:53 +0000

    throw transport exception when getting new connection fails

--- sdk/core/azure-core/src/http/curl/curl.cpp
+++ sdk/core/azure-core/src/http/curl/curl.cpp
@@ -8,6 +8,17 @@
 #include <string>
 #include <thread>
 
+// Macro for setting libcurl handle options
+#define LIBCURL_SET_OPT(HANDLE, OPT, VALUE, ERROR) \
+  { \
+    auto result = curl_easy_setopt(HANDLE, OPT, VALUE); \
+    if (result != CURLE_OK) \
+    { \
+      throw Azure::Core::Http::TransportException( \
+          ERROR + ". " + std::string(curl_easy_strerror(result))); \
+    } \
+  }
+
 using namespace Azure::Core::Http;
 
 // To wait for a socket to be ready to be read/write
@@ -868,39 +879,32 @@ std::unique_ptr<CurlConnection> CurlConnectionPool::GetCurlConnection(Request& r
   auto newConnection = std::make_unique<CurlConnection>(host);
 
   // Libcurl setup before open connection (url, connet_only, timeout)
-  auto result = curl_easy_setopt(
-      newConnection->GetHandle(), CURLOPT_URL, request.GetUrl().GetAbsoluteUrl().data());
-  if (result != CURLE_OK)
-  {
-    throw std::runtime_error(
-        Details::c_DefaultFailedToGetNewConnectionTemplate + host + ". "
-        + std::string(curl_easy_strerror(result)));
-  }
+  LIBCURL_SET_OPT(
+      newConnection->GetHandle(),
+      CURLOPT_URL,
+      request.GetUrl().GetAbsoluteUrl().data(),
+      Details::c_DefaultFailedToGetNewConnectionTemplate + host)
 
-  result = curl_easy_setopt(newConnection->GetHandle(), CURLOPT_CONNECT_ONLY, 1L);
-  if (result != CURLE_OK)
-  {
-    throw std::runtime_error(
-        Details::c_DefaultFailedToGetNewConnectionTemplate + host + ". "
-        + std::string(curl_easy_strerror(result)));
-  }
+  LIBCURL_SET_OPT(
+      newConnection->GetHandle(),
+      CURLOPT_CONNECT_ONLY,
+      1L,
+      Details::c_DefaultFailedToGetNewConnectionTemplate + host)
 
   // curl_easy_setopt(newConnection->GetHandle(), CURLOPT_VERBOSE, 1L);
   // Set timeout to 24h. Libcurl will fail uploading on windows if timeout is:
   // timeout >= 25 days. Fails as soon as trying to upload any data
   // 25 days < timeout > 1 days. Fail on huge uploads ( > 1GB)
-  result = curl_easy_setopt(newConnection->GetHandle(), CURLOPT_TIMEOUT, 60L * 60L * 24L);
-  if (result != CURLE_OK)
-  {
-    throw std::runtime_error(
-        Details::c_DefaultFailedToGetNewConnectionTemplate + host + ". "
-        + std::string(curl_easy_strerror(result)));
-  }
+  LIBCURL_SET_OPT(
+      newConnection->GetHandle(),
+      CURLOPT_TIMEOUT,
+      60L * 60L * 24L,
+      Details::c_DefaultFailedToGetNewConnectionTemplate + host)
 
-  result = curl_easy_perform(newConnection->GetHandle());
+  auto result = curl_easy_perform(newConnection->GetHandle());
   if (result != CURLE_OK)
   {
-    throw std::runtime_error(
+    throw Http::TransportException(
         Details::c_DefaultFailedToGetNewConnectionTemplate + host + ". "
         + std::string(curl_easy_strerror(result)));
   }
