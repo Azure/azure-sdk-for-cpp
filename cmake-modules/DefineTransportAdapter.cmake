350d4e4fc097f1e2e5d631434ad79eaabe7534ca
Victor Vazquez <vhvb1989@gmail.com>
2020-10-07 21:01:21 +0000

    remove extra layer variables from CMake for transpot adapter options

--- cmake-modules/DefineTransportAdapter.cmake
+++ cmake-modules/DefineTransportAdapter.cmake
@@ -9,21 +9,21 @@
 #  Defines `BUILD_WIN_HTTP_TRANSPORT_ADAPTER` and `BUILD_CURL_HTTP_TRANSPORT_ADAPTER` for source code
 
 if (WIN32 OR MINGW OR MSYS OR CYGWIN)
-  if(${CURL_TRANSPORT_OPT_NAME})
+  if(BUILD_CURL_TRANSPORT)
     add_compile_definitions(BUILD_CURL_HTTP_TRANSPORT_ADAPTER)
   endif()
   # If explicitly adding WIN_HTTP or did not add CURL
-  if(${WIN_TRANSPORT_OPT_NAME} OR NOT ${CURL_TRANSPORT_OPT_NAME})
+  if(BUILD_WIN_HTTP_TRANSPORT OR NOT BUILD_CURL_TRANSPORT)
     add_compile_definitions(BUILD_WIN_HTTP_TRANSPORT_ADAPTER)
-    SET(${WIN_TRANSPORT_OPT_NAME} ON)
+    SET(BUILD_WIN_HTTP_TRANSPORT ON)
   endif()
 elseif(UNIX)
-    if(${WIN_TRANSPORT_OPT_NAME})
+    if(BUILD_WIN_HTTP_TRANSPORT)
         message(FATAL_ERROR "Win HTTP transport adapter is not supported for Unix platforms")
     endif()
     # regardless if CURL transport is set or not, it is always added for UNIX as default
     add_compile_definitions(BUILD_CURL_HTTP_TRANSPORT_ADAPTER)
-    SET(${CURL_TRANSPORT_OPT_NAME} ON)
+    SET(BUILD_CURL_TRANSPORT ON)
 else()
     message(FATAL_ERROR "Unsupported platform")
 endif ()
