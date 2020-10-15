3a79d5df0760de4d9e0318ac936b60bdccb9419b
Rick Winter <rick.winter@microsoft.com>
2020-10-15 09:49:47 -0700

    Use quotes for files relative to the package

--- sdk/identity/azure-identity/src/client_secret_credential.cpp
+++ sdk/identity/azure-identity/src/client_secret_credential.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
 
-#include <azure/identity/client_secret_credential.hpp>
+#include "azure/identity/client_secret_credential.hpp"
 
 #include <azure/core/http/curl/curl.hpp>
 #include <azure/core/http/pipeline.hpp>
