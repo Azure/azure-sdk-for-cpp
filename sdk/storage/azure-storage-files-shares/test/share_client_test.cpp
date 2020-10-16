8ae0c2e6cd238a79b9c80db5e64284004897c2b4
Tank Tang <kat@microsoft.com>
2020-10-16 14:23:45 +0800

    Resolve comments.

--- sdk/storage/azure-storage-files-shares/test/share_client_test.cpp
+++ sdk/storage/azure-storage-files-shares/test/share_client_test.cpp
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
 
-#include "share_client_test.hpp"
 #include "azure/storage/common/crypt.hpp"
+#include "share_client_test.hpp"
 
 #include <algorithm>
 
