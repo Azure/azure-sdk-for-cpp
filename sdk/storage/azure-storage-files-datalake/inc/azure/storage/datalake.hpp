c46bef8a75a286d05752774f90ecf1bd920b05be
Jinming Hu <jinmhu@microsoft.com>
2020-10-22 10:36:06 +0800

    move blob.hpp

--- sdk/storage/azure-storage-files-datalake/inc/azure/storage/datalake.hpp
+++ sdk/storage/azure-storage-files-datalake/inc/azure/storage/datalake.hpp
@@ -7,4 +7,5 @@
 #include "azure/storage/files/datalake/datalake_file_client.hpp"
 #include "azure/storage/files/datalake/datalake_file_system_client.hpp"
 #include "azure/storage/files/datalake/datalake_path_client.hpp"
+#include "azure/storage/files/datalake/datalake_sas_builder.hpp"
 #include "azure/storage/files/datalake/datalake_service_client.hpp"
