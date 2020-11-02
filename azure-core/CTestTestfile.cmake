bc898fd58fb62767e8ba6f4146ea912ef669ee6b
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2020-11-02 10:49:48 -0800

    Remove unwanted change

--- azure-core/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/antk/source/repos/azure-sdk-for-cpp/sdk/core/azure-core
-# Build directory: C:/Users/antk/source/repos/azure-sdk-for-cpp/out/build/x64-DebugWithTests/sdk/core/azure-core
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
