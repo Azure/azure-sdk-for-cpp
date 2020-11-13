731b3ce81748d3ef93034428f3832f18aff1a01c
Ahson Khan <ahkha@microsoft.com>
2020-11-12 17:13:23 -0800

    Add a simplified identity header.

---
+++ sdk/identity/azure-identity/inc/azure/identity.hpp
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include "azure/identity/client_secret_credential.hpp"
+#include "azure/identity/environment_credential.hpp"
+#include "azure/identity/version.hpp"
