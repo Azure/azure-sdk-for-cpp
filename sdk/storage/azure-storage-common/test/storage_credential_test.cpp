a716e094fe4c17f872181cc755e13843f53ead8d
Gabor Gyimesi <gamezbird@gmail.com>
2020-11-16 02:58:01 +0100

    Fix default EndpointSuffix when parsing connection string (#981)

---
+++ sdk/storage/azure-storage-common/test/storage_credential_test.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#include "azure/storage/common/storage_credential.hpp"
+#include "test_base.hpp"
+
+namespace Azure { namespace Storage { namespace Test {
+
+  TEST(StorageCredentialTest, DefaultHostCorrect)
+  {
+    EXPECT_EQ(Azure::Storage::Details::ParseConnectionString(
+            "DefaultEndpointsProtocol=https;AccountName=testaccount;AccountKey=testkey").BlobServiceUri.GetHost(), "testaccount.blob.core.windows.net");
+  }
+
+}}} // namespace Azure::Storage::Test
