9c02c93146bdb8da6e503c903bc3ab11dd561e22
Jinming Hu <jinmhu@microsoft.com>
2020-11-16 10:21:39 +0800

    credential test

--- sdk/storage/azure-storage-common/test/storage_credential_test.cpp
+++ sdk/storage/azure-storage-common/test/storage_credential_test.cpp
@@ -8,8 +8,18 @@ namespace Azure { namespace Storage { namespace Test {
 
   TEST(StorageCredentialTest, DefaultHostCorrect)
   {
-    EXPECT_EQ(Azure::Storage::Details::ParseConnectionString(
-            "DefaultEndpointsProtocol=https;AccountName=testaccount;AccountKey=testkey").BlobServiceUri.GetHost(), "testaccount.blob.core.windows.net");
+    EXPECT_EQ(
+        Azure::Storage::Details::ParseConnectionString(
+            "DefaultEndpointsProtocol=https;AccountName=testaccount;AccountKey=testkey")
+            .BlobServiceUri.GetHost(),
+        "testaccount.blob.core.windows.net");
+
+    EXPECT_EQ(
+        Azure::Storage::Details::ParseConnectionString(
+            "DefaultEndpointsProtocol=https;AccountName=targettest;AccountKey=testkey;"
+            "EndpointSuffix=core.windows.net")
+            .BlobServiceUri.GetHost(),
+        "testaccount.blob.core.windows.net");
   }
 
 }}} // namespace Azure::Storage::Test
