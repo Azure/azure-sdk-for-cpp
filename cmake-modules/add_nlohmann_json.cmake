bc7508df7549c560b27d6dcc6891f8e8f2bd94b9
Victor Vazquez <victor.vazquez@microsoft.com>
2020-11-19 17:18:42 -0800

    Apply suggestions from code review
    Co-authored-by: Ahson Khan <ahkha@microsoft.com>

--- cmake-modules/add_nlohmann_json.cmake
+++ cmake-modules/add_nlohmann_json.cmake
@@ -8,7 +8,7 @@
 # Azure Core can expose the Nlohmann Json header by fetching a release version and including the header to the include path.
 # 
 # Azure Core will try to find the json header first. This is for an application that is already using Nlohmann header.
-# When it is not found it, Azure Core will featch it.
+# When it is not found it, Azure Core will fetch it.
 
 # Storage requires 3.8 version for using `contains` feature
 find_package(nlohmann_json 3.8.0 CONFIG)
@@ -16,15 +16,15 @@ if (NOT nlohmann_json_FOUND)
 
     include(FetchContent)
 
-    # realese code only. This save us from getting the entire nlohmann source code.
+    # release code only. This save us from getting the entire nlohmann source code.
     FetchContent_Declare(json
         GIT_REPOSITORY https://github.com/ArthurSonzogni/nlohmann_json_cmake_fetchcontent
         GIT_TAG v3.8.0)
 
     FetchContent_GetProperties(json)
-    if(NOT json_POPULATED)
-    FetchContent_Populate(json)
-    add_subdirectory(${json_SOURCE_DIR} ${json_BINARY_DIR} EXCLUDE_FROM_ALL)
+    if (NOT json_POPULATED)
+      FetchContent_Populate(json)
+      add_subdirectory(${json_SOURCE_DIR} ${json_BINARY_DIR} EXCLUDE_FROM_ALL)
     endif()
 
 endif()
