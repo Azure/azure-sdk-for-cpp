98fff95bcedf2cd06fa883a2c2fb88fc864f1604
Victor Vazquez <vhvb1989@gmail.com>
2020-12-02 01:27:39 -0800

    test using original repo

--- cmake-modules/add_nlohmann_json.cmake
+++ cmake-modules/add_nlohmann_json.cmake
@@ -8,15 +8,15 @@
 
 # Storage requires 3.6.0 version for using `contains` feature
 # Using QUIET to avoid warning message if lib is not found
-find_package(nlohmann_json 3.6.0 EXACT QUIET)
+find_package(nlohmann_json 3.9.0 EXACT QUIET)
 if (NOT nlohmann_json_FOUND)
 
     include(FetchContent)
 
     # release code only. This save us from getting the entire nlohmann source code.
     FetchContent_Declare(json
-        GIT_REPOSITORY https://github.com/ArthurSonzogni/nlohmann_json_cmake_fetchcontent
-        GIT_TAG v3.8.0)
+        GIT_REPOSITORY https://github.com/nlohmann/json
+        GIT_TAG v3.9.0)
 
     FetchContent_GetProperties(json)
     if (NOT json_POPULATED)
