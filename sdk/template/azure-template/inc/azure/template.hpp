828bb4098a4b5dd41e4bd5057f9f35a24ce3d6b3
Anton Kolesnyk <41349689+antkmsft@users.noreply.github.com>
2020-12-09 04:02:29 -0800

    Make changes towards making libraries individually buildable (#1108)

---
+++ sdk/template/azure-template/inc/azure/template.hpp
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include "azure/template/template_client.hpp"
+#include "azure/template/version.hpp"
