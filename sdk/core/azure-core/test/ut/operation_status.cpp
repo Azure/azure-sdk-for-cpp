594454ee970c5e5c309ef70af8c806036e09feb4
Rick Winter <rick.winter@microsoft.com>
2020-12-16 15:11:16 -0800

    Fix liscense

--- sdk/core/azure-core/test/ut/operation_status.cpp
+++ sdk/core/azure-core/test/ut/operation_status.cpp
@@ -1,6 +1,7 @@
-// ************ CUSTOMER CODE **********************
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
 
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
 #include <azure/core/operation_status.hpp>
 
