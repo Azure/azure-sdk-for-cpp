51feb56f7e51ee58af4954e8a82976345ea94081
Victor Vazquez <vhvb1989@gmail.com>
2020-12-18 23:22:05 +0000

    Make curl connection pool to be out of the transport adapter tests

--- sdk/core/azure-core/src/http/curl/curl_session_private.hpp
+++ sdk/core/azure-core/src/http/curl/curl_session_private.hpp
@@ -22,7 +22,7 @@
 #ifdef TESTING_BUILD
 // Define the class name that reads from ConnectionPool private members
 namespace Azure { namespace Core { namespace Test {
-  class TransportAdapter_connectionPoolTest_Test;
+  class CurlConnectionPool_connectionPoolTest_Test;
 }}} // namespace Azure::Core::Test
 #endif
 
@@ -42,7 +42,7 @@ namespace Azure { namespace Core { namespace Http {
   class CurlSession : public BodyStream {
 #ifdef TESTING_BUILD
     // Give access to private to this tests class
-    friend class Azure::Core::Test::TransportAdapter_connectionPoolTest_Test;
+    friend class Azure::Core::Test::CurlConnectionPool_connectionPoolTest_Test;
 #endif
   private:
     /**
