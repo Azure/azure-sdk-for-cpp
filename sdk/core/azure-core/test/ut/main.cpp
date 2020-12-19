ef60cb59a5814496a37271bac92661696c4fc7a9
Victor Vazquez <vhvb1989@gmail.com>
2020-12-19 00:21:26 +0000

    Fix for test cases when not using libcurl

--- sdk/core/azure-core/test/ut/main.cpp
+++ sdk/core/azure-core/test/ut/main.cpp
@@ -1,14 +1,23 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
 
-#include <curl/curl.h>
 #include <gtest/gtest.h>
+
+#if defined(BUILD_CURL_HTTP_TRANSPORT_ADAPTER)
+  #include <curl/curl.h>
+#endif
 
 int main(int argc, char** argv)
 {
+  #if defined(BUILD_CURL_HTTP_TRANSPORT_ADAPTER)
   curl_global_init(CURL_GLOBAL_ALL);
+  #endif
+
   testing::InitGoogleTest(&argc, argv);
   auto r = RUN_ALL_TESTS();
+  
+#if defined(BUILD_CURL_HTTP_TRANSPORT_ADAPTER)
   curl_global_cleanup();
+#endif
   return r;
 }
