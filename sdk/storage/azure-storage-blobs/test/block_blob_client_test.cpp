f1d4ba96512d6ad78a2f57f707231ff36eb0fc95
JinmingHu <jinmhu@microsoft.com>
2021-01-06 19:07:23 +0800

    Apply suggestions from code review

--- sdk/storage/azure-storage-blobs/test/block_blob_client_test.cpp
+++ sdk/storage/azure-storage-blobs/test/block_blob_client_test.cpp
@@ -810,12 +810,12 @@ namespace Azure { namespace Storage { namespace Test {
     Blobs::DeleteBlobOptions deleteOptions;
     EXPECT_THROW(blobClient.Delete(deleteOptions), StorageException);
     deleteOptions.DeleteSnapshots = Blobs::Models::DeleteSnapshotsOption::OnlySnapshots;
-    blobClient.Delete(deleteOptions);
+    EXPECT_NO_THROW(blobClient.Delete(deleteOptions));
     EXPECT_NO_THROW(blobClient.GetProperties());
     EXPECT_THROW(blobClient.WithSnapshot(s1).GetProperties(), StorageException);
     auto s2 = blobClient.CreateSnapshot()->Snapshot;
     deleteOptions.DeleteSnapshots = Blobs::Models::DeleteSnapshotsOption::IncludeSnapshots;
-    blobClient.Delete(deleteOptions);
+    EXPECT_NO_THROW(blobClient.Delete(deleteOptions));
     EXPECT_THROW(blobClient.GetProperties(), StorageException);
     EXPECT_THROW(blobClient.WithSnapshot(s2), StorageException);
   }
