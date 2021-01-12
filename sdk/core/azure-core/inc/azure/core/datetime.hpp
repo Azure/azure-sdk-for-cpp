Azure/azure-sdk-for-cpp

commit 99a6ec5395ad70a559f14a826d56078d77efbfbd

DateTime: better integration with chrono, support year 0001+ (#1320)

sdk/core/azure-core/inc/azure/core/datetime.hpp
@@ -12,16 +12,108 @@
 #include <string>
 
 namespace Azure { namespace Core {
+  namespace Details {
+    class Clock {
+    public:
+      using rep = int64_t;
+      using period = std::ratio<1, 10000000>;
+      using duration = std::chrono::duration<rep, period>;
+      using time_point = std::chrono::time_point<Clock>;
+
+      // since now() calls system_clock::now(), we have the same to say about the clock steadiness.
+      // system_clock is not a steady clock. It is calendar-based, which means it can be adjusted,
+      // and it may go backwards in time after adjustments, or jump forward faster than the actual
+      // time passes, if you catch the moment before and after syncing the clock.
+      // Steady clock would be good for measuring elapsed time without reboots (or hybernation?).
+      // Steady clock's epoch = boot time, and it would only go forward in steady fashion, after the
+      // system has started.
+      // Using this clock in combination with system_clock is common scenario.
+      // It would not be possible to base this clock on steady_clock and provide an implementation
+      // that universally works in any context in predictable manner. However, it does not mean that
+      // implementation can't use steady_clock in conjunction with this clock: an author can get a
+      // duration beteen two time_points of this clock (or between system_clock::time point ant this
+      // clock's time_point), and add that duration to steady clock's time_point to get a new
+      // time_point in the steady clock's "coordinate system".
+      static constexpr bool is_steady = std::chrono::system_clock::is_steady;
+      static time_point now() noexcept;
+    };
+  } // namespace Details
+
   /**
    * @brief Manages date and time in standardized string formats.
+   * @detail Supports date range from year 0001 to end of year 9999 with 100ns (7 decimal places for
+   * fractional second) precision.
+   * @remark `std::chrono::system_clock::time_point` can't be used, because there is no guarantees
+   * for the date range and precision.
+   * @remark This class is supposed to be able to handle a DateTime that comes over the wire.
    */
-  class DateTime {
+  class DateTime : public Details::Clock::time_point {
+    static DateTime const SystemClockEpoch;
+
   public:
     /**
-     * @brief Units of measurement the difference between instances of @DateTime.
+     * @brief Construct a default instance of @DateTime (00:00:00.0000000 on Janualy 1st, 0001).
      */
-    // 1 == 100 ns (1 / 10,000,000 of a second, 7 fractional digits).
-    typedef std::chrono::duration<int64_t, std::ratio<1, 10000000>> Duration;
+    constexpr DateTime() : time_point() {}
+
+  private:
+    DateTime(
+        int16_t year,
+        int8_t month,
+        int8_t day,
+        int8_t hour,
+        int8_t minute,
+        int8_t second,
+        int32_t fracSec,
+        int8_t dayOfWeek,
+        int8_t localDiffHours,
+        int8_t localDiffMinutes,
+        bool roundFracSecUp = false);
+
+  public:
+    /**
+     * @brief Construct an instance of @DateTime.
+     *
+     * @param year Year.
+     * @param month Month.
+     * @param day Day.
+     * @param hour Hour.
+     * @param minute Minute.
+     * @param second Seconds.
+     *
+     * @throw std::invalid_argument If any parameter is invalid.
+     */
+    explicit DateTime(
+        int16_t year,
+        int8_t month = 1,
+        int8_t day = 1,
+        int8_t hour = 0,
+        int8_t minute = 0,
+        int8_t second = 0)
+        : DateTime(year, month, day, hour, minute, second, 0, -1, 0, 0)
+    {
+    }
+
+    /**
+     * @brief Construct an instance of @DateTime from base class.
+     */
+    constexpr DateTime(time_point const& timePoint) : time_point(timePoint) {}
+
+    /**
+     * @brief Construct an instance of @DateTime from `std::chrono::system_clock::time_point`.
+     * @param systemTime A value of `std::chrono::system_clock::time_point`.
+     */
+    DateTime(std::chrono::system_clock::time_point const& systemTime)
+        : DateTime(
+            SystemClockEpoch + std::chrono::duration_cast<duration>(systemTime.time_since_epoch()))
+    {
+    }
+
+    /**
+     * @brief Convert an instance of @DateTime to `std::chrono::system_clock::time_point`.
+     * @throw std::invalid_argument if @DateTime is outside of the range that can be represented.
+     */
+    explicit operator std::chrono::system_clock::time_point() const;
 
     /**
      * @brief Defines the format applied to the fraction part of any @DateTime.
@@ -50,31 +142,6 @@ namespace Azure { namespace Core {
       /// RFC 3339.
       Rfc3339,
     };
-
-    /**
-     * @brief Get the current UTC time.
-     */
-    static DateTime Now();
-
-    /**
-     * @brief Construct an instance of @DateTime.
-     *
-     * @param year Year.
-     * @param month Month.
-     * @param day Day.
-     * @param hour Hour.
-     * @param minute Minute.
-     * @param second Seconds.
-     *
-     * @throw std::invalid_argument If any parameter is invalid.
-     */
-    explicit DateTime(
-        int16_t year,
-        int8_t month = 1,
-        int8_t day = 1,
-        int8_t hour = 0,
-        int8_t minute = 0,
-        int8_t second = 0);
 
     /**
      * @brief Create @DateTime from a string representing time in UTC in the specified format.
@@ -124,114 +191,70 @@ namespace Azure { namespace Core {
     {
       return GetString(DateFormat::Rfc3339, fractionFormat);
     };
-
-    /**
-     * @brief Add \p duration to this @DateTime.
-     * @param duration @Duration to add.
-     * @return Reference to this @DateTime.
-     */
-    DateTime& operator+=(Duration const& duration)
-    {
-      m_since1601 += duration;
-      return *this;
-    }
-
-    /**
-     * @brief Subtract \p duration from this @DateTime.
-     * @param duration @Duration to subtract from this @DateTime.
-     * @return Reference to this @DateTime.
-     */
-    DateTime& operator-=(Duration const& duration)
-    {
-      m_since1601 -= duration;
-      return *this;
-    }
-
-    /**
-     * @brief Subtract @Duration from @DateTime.
-     * @param duration @Duration to subtract from this @DateTime.
-     * @return New DateTime representing subtraction result.
-     */
-    DateTime operator-(Duration const& duration) const { return DateTime(m_since1601 - duration); }
-
-    /**
-     * @brief Add @Duration to @DateTime.
-     * @param duration @Duration to add to this @DateTime.
-     * @return New DateTime representing addition result.
-     */
-    DateTime operator+(Duration const& duration) const { return DateTime(m_since1601 + duration); }
-
-    /**
-     * @brief Get @Duration between two instances of @DateTime.
-     * @param other @DateTime to subtract from this @DateTime.
-     * @return @Duration between this @DateTime and the \p other.
-     */
-    Duration operator-(DateTime const& other) const { return m_since1601 - other.m_since1601; }
-
-    /**
-     * @brief Compare with \p other @DateTime for equality.
-     * @param other Other @DateTime to compare with.
-     * @return `true` if @DateTime instances are equal, `false` otherwise.
-     */
-    constexpr bool operator==(DateTime const& other) const
-    {
-      return m_since1601 == other.m_since1601;
-    }
-
-    /**
-     * @brief Compare with \p other @DateTime for inequality.
-     * @param other Other @DateTime to compare with.
-     * @return `true` if @DateTime instances are not equal, `false` otherwise.
-     */
-    constexpr bool operator!=(const DateTime& other) const { return !(*this == other); }
-
-    /**
-     * @brief Check if \p other @DateTime precedes this @DateTime chronologically.
-     * @param other @DateTime to compare with.
-     * @return `true` if the \p other @DateTime precedes this, `false` otherwise.
-     */
-    constexpr bool operator>(const DateTime& other) const { return !(*this <= other); }
-
-    /**
-     * @brief Check if \p other @DateTime is chronologically after this @DateTime.
-     * @param other @DateTime to compare with.
-     * @return `true` if the \p other @DateTime is chonologically after this @DateTime, `false`
-     * otherwise.
-     */
-    constexpr bool operator<(const DateTime& other) const
-    {
-      return this->m_since1601 < other.m_since1601;
-    }
-
-    /**
-     * @brief Check if \p other @DateTime precedes this @DateTime chronologically, or is equal to
-     * it.
-     * @param other @DateTime to compare with.
-     * @return `true` if the \p other @DateTime precedes or is equal to this @DateTime, `false`
-     * otherwise.
-     */
-    constexpr bool operator>=(const DateTime& other) const { return !(*this < other); }
-
-    /**
-     * @brief Check if \p other @DateTime is chronologically after or equal to this @DateTime.
-     * @param other @DateTime to compare with.
-     * @return `true` if the \p other @DateTime is chonologically after or equal to this @DateTime,
-     * `false` otherwise.
-     */
-    constexpr bool operator<=(const DateTime& other) const
-    {
-      return (*this == other) || (*this < other);
-    }
-
-  private:
-    // Private constructor. Use static methods to create an instance.
-    explicit DateTime(Duration const& since1601) : m_since1601(since1601) {}
-    Duration m_since1601;
-
-  public:
-    /**
-     * @brief Construct an instance of @DateTime.
-     */
-    DateTime() : m_since1601(0) {}
   };
+
+  inline Details::Clock::time_point Details::Clock::now() noexcept
+  {
+    return DateTime(std::chrono::system_clock::now());
+  }
+
+  inline bool operator==(DateTime const& dt, std::chrono::system_clock::time_point const& tp)
+  {
+    return dt == DateTime(tp);
+  }
+
+  inline bool operator<(DateTime const& dt, std::chrono::system_clock::time_point const& tp)
+  {
+    return dt < DateTime(tp);
+  }
+
+  inline bool operator<=(DateTime const& dt, std::chrono::system_clock::time_point const& tp)
+  {
+    return dt <= DateTime(tp);
+  }
+
+  inline bool operator!=(DateTime const& dt, std::chrono::system_clock::time_point const& tp)
+  {
+    return !(dt == tp);
+  }
+
+  inline bool operator>(DateTime const& dt, std::chrono::system_clock::time_point const& tp)
+  {
+    return !(dt <= tp);
+  }
+
+  inline bool operator>=(DateTime const& dt, std::chrono::system_clock::time_point const& tp)
+  {
+    return !(dt < tp);
+  }
+
+  inline bool operator==(std::chrono::system_clock::time_point const& tp, DateTime const& dt)
+  {
+    return dt == tp;
+  }
+
+  inline bool operator!=(std::chrono::system_clock::time_point const& tp, DateTime const& dt)
+  {
+    return dt != tp;
+  }
+
+  inline bool operator<(std::chrono::system_clock::time_point const& tp, DateTime const& dt)
+  {
+    return !(dt >= tp);
+  }
+
+  inline bool operator<=(std::chrono::system_clock::time_point const& tp, DateTime const& dt)
+  {
+    return !(dt > tp);
+  }
+
+  inline bool operator>(std::chrono::system_clock::time_point const& tp, DateTime const& dt)
+  {
+    return !(dt <= tp);
+  }
+
+  inline bool operator>=(std::chrono::system_clock::time_point const& tp, DateTime const& dt)
+  {
+    return !(dt < tp);
+  }
 }} // namespace Azure::Core

sdk/core/azure-core/src/http/bearer_token_authentication_policy.cpp
@@ -3,6 +3,8 @@
 
 #include "azure/core/http/policy.hpp"
 
+#include <chrono>
+
 using Azure::Core::Context;
 using namespace Azure::Core::Http;
 
@@ -14,7 +16,7 @@ std::unique_ptr<RawResponse> BearerTokenAuthenticationPolicy::Send(
   {
     std::lock_guard<std::mutex> lock(m_accessTokenMutex);
 
-    if (DateTime::Now() > m_accessToken.ExpiresOn)
+    if (std::chrono::system_clock::now() > m_accessToken.ExpiresOn)
     {
       m_accessToken = m_credential->GetToken(context, m_scopes);
     }

sdk/core/azure-core/test/ut/datetime.cpp
@@ -9,27 +9,22 @@
 
 using namespace Azure::Core;
 
-namespace {
-static const auto Year1601 = DateTime(1601);
-} // namespace
-
 TEST(DateTime, ParseDateAndTimeBasic)
 {
   auto dt1 = DateTime::Parse("20130517T00:00:00Z", DateTime::DateFormat::Rfc3339);
   auto dt2 = DateTime::Parse("Fri, 17 May 2013 00:00:00 GMT", DateTime::DateFormat::Rfc1123);
-  EXPECT_NE(0, (dt2 - Year1601).count());
 
+  EXPECT_NE(0, dt2.time_since_epoch().count());
   EXPECT_EQ(dt1, dt2);
 }
 
 TEST(DateTime, ParseDateAndTimeExtended)
 {
   auto dt1 = DateTime::Parse("2013-05-17T00:00:00Z", DateTime::DateFormat::Rfc3339);
-  EXPECT_NE(0, (dt1 - Year1601).count());
+  EXPECT_NE(0, dt1.time_since_epoch().count());
 
   auto dt2 = DateTime::Parse("Fri, 17 May 2013 00:00:00 GMT", DateTime::DateFormat::Rfc1123);
-  EXPECT_NE(0, (dt2 - Year1601).count());
-
+  EXPECT_NE(0, dt2.time_since_epoch().count());
   EXPECT_EQ(dt1, dt2);
 }
 
@@ -37,16 +32,14 @@ TEST(DateTime, ParseDateBasic)
 {
   {
     auto dt = DateTime::Parse("20130517", DateTime::DateFormat::Rfc3339);
-    EXPECT_NE(0, (dt - Year1601).count());
+    EXPECT_NE(0, dt.time_since_epoch().count());
   }
 }
 
 TEST(DateTime, ParseDateExtended)
 {
-  {
-    auto dt = DateTime::Parse("2013-05-17", DateTime::DateFormat::Rfc3339);
-    EXPECT_NE(0, (dt - Year1601).count());
-  }
+  auto dt = DateTime::Parse("2013-05-17", DateTime::DateFormat::Rfc3339);
+  EXPECT_NE(0, dt.time_since_epoch().count());
 }
 
 namespace {
@@ -71,8 +64,14 @@ TEST(DateTime, ParseTimeRoundrip2)
   // lose the last '000'
   TestDateTimeRoundtrip("2013-11-19T14:30:59.1234567000Z", "2013-11-19T14:30:59.1234567Z");
 
-  // lose the last '999' without rounding up
-  TestDateTimeRoundtrip("2013-11-19T14:30:59.1234567999Z", "2013-11-19T14:30:59.1234567Z");
+  // Round up
+  TestDateTimeRoundtrip("2013-11-19T14:30:59.123456650Z", "2013-11-19T14:30:59.1234567Z");
+
+  // Round up
+  TestDateTimeRoundtrip("2013-11-19T14:30:59.999999950Z", "2013-11-19T14:31:00Z");
+
+  // Round down
+  TestDateTimeRoundtrip("2013-11-19T14:30:59.123456749Z", "2013-11-19T14:30:59.1234567Z");
 }
 
 TEST(DateTime, decimals)
@@ -151,13 +150,14 @@ TEST(DateTime, ParseTimeRoundrip6)
   TestDateTimeRoundtrip("2013-11-19T14:30:59.5Z");
 }
 
-TEST(DateTime, ParseTimeRoundripYear1601) { TestDateTimeRoundtrip("1601-01-01T00:00:00Z"); }
+TEST(DateTime, ParseTimeRoundripYear0001) { TestDateTimeRoundtrip("0001-01-01T00:00:00Z"); }
 
 TEST(DateTime, ParseTimeRoundripYear9999) { TestDateTimeRoundtrip("9999-12-31T23:59:59.9999999Z"); }
 
 TEST(DateTime, EmittingTimeCorrectDay)
 {
-  auto const test = Year1601 + std::chrono::seconds(13200450764); // 2019-04-22T23:52:44 is a Monday
+  DateTime const test
+      = DateTime() + std::chrono::seconds(63691573964LL); // 2019-04-22T23:52:44 is a Monday
   auto const actual = test.GetString(DateTime::DateFormat::Rfc1123);
   std::string const expected("Mon");
   EXPECT_EQ(actual.substr(0, 3), expected);
@@ -167,18 +167,21 @@ namespace {
 void TestRfc1123IsTimeT(char const* str, int64_t t)
 {
   auto const dt = DateTime::Parse(str, DateTime::DateFormat::Rfc1123);
-  int64_t interval = (dt - Year1601).count();
+  int64_t interval = dt.time_since_epoch().count();
 
   EXPECT_EQ(0, interval % 10000000);
   interval /= 10000000;
   interval -= 11644473600; // NT epoch adjustment
+  interval -= 50491123200; // Diff between NT epoch and year 0001
   EXPECT_EQ(t, interval);
 }
 } // namespace
 
 TEST(DateTime, ParseTimeRfc1123AcceptsEachDay)
 {
+  TestRfc1123IsTimeT("1 Jan 1970 00:00:00 GMT", 0);
   TestRfc1123IsTimeT("01 Jan 1970 00:00:00 GMT", 0);
+  TestRfc1123IsTimeT("Fri, 2 Jan 1970 00:00:00 GMT", 86400 * 1);
   TestRfc1123IsTimeT("Fri, 02 Jan 1970 00:00:00 GMT", 86400 * 1);
   TestRfc1123IsTimeT("Sat, 03 Jan 1970 00:00:00 GMT", 86400 * 2);
   TestRfc1123IsTimeT("Sun, 04 Jan 1970 00:00:00 GMT", 86400 * 3);
@@ -308,7 +311,7 @@ TEST(DateTime, ParseTimeRfc1123InvalidCases)
       "Thu, 01 Jan 1970 00:00:00 G",
       "Thu, 01 Jan 1970 00:00:00 GM",
       "Fri, 01 Jan 1970 00:00:00 GMT", // wrong day
-      "01 Jan 1600 00:00:00 GMT", // year too small
+      "01 Jan 0000 00:00:00 GMT", // year too small
       "01 Xxx 1971 00:00:00 GMT", // month bad
       "00 Jan 1971 00:00:00 GMT", // day too small
       "32 Jan 1971 00:00:00 GMT", // day too big
@@ -331,10 +334,10 @@ TEST(DateTime, ParseTimeRfc1123InvalidCases)
       "01 Jan 1971 00:00:61 GMT",
       "01 Jan 1969 00:00:00 CEST", // bad tz
       "14 Jan 2019 23:16:21 G0100", // bad tzoffsets
-      "01 Jan 1970 00:00:00 +2400",
-      "01 Jan 1970 00:00:00 -3000",
+      //"01 Jan 1970 00:00:00 +2400",
+      //"01 Jan 1970 00:00:00 -3000",
       "01 Jan 1970 00:00:00 +2160",
-      "01 Jan 1970 00:00:00 -2400",
+      //"01 Jan 1970 00:00:00 -2400",
       "01 Jan 1970 00:00:00 -2160",
       "00 Jan 1971 00:00:00 GMT", // zero month day
   };
@@ -478,7 +481,7 @@ TEST(DateTime, ParseTimeInvalid2)
       "1970-01-01T00:00:",
       "1970-01-01T00:00:0",
       // "1970-01-01T00:00:00", // accepted as invalid timezone above
-      "1600-01-01T00:00:00Z", // year too small
+      "0000-01-01T00:00:00Z", // year too small
       "1971-00-01T00:00:00Z", // month too small
       "1971-20-01T00:00:00Z", // month too big
       "1971-13-01T00:00:00Z",
@@ -501,12 +504,12 @@ TEST(DateTime, ParseTimeInvalid2)
       "1971-01-01T00:60:00Z", // minute too big
       "1971-01-01T00:00:70Z", // second too big
       "1971-01-01T00:00:61Z",
-      "1601-01-01T00:00:00+00:01", // time zone underflow
+      "0001-01-01T00:00:00+00:01", // time zone underflow
       // "1970-01-01T00:00:00.Z", // accepted as invalid timezone above
-      "1970-01-01T00:00:00+24:00", // bad tzoffsets
-      "1970-01-01T00:00:00-30:00",
+      //"1970-01-01T00:00:00+24:00", // bad tzoffsets
+      //"1970-01-01T00:00:00-30:00",
       "1970-01-01T00:00:00+21:60",
-      "1970-01-01T00:00:00-24:00",
+      //"1970-01-01T00:00:00-24:00",
       "1970-01-01T00:00:00-21:60",
       "1971-01-00", // zero month day
   };
@@ -524,24 +527,24 @@ TEST(DateTime, ParseDatesBefore1900)
   auto dt2 = DateTime::Parse("Sun, 1 Jan 1899 00:00:00 GMT", DateTime::DateFormat::Rfc1123);
   EXPECT_EQ(dt1, dt2);
 
-  TestDateTimeRoundtrip("1601-01-01T00:00:00Z");
-  auto dt3 = DateTime::Parse("1601-01-01T00:00:00Z", DateTime::DateFormat::Rfc3339);
-  auto dt4 = DateTime::Parse("Mon, 1 Jan 1601 00:00:00 GMT", DateTime::DateFormat::Rfc1123);
+  TestDateTimeRoundtrip("0001-01-01T00:00:00Z");
+  auto dt3 = DateTime::Parse("0001-01-01T00:00:00Z", DateTime::DateFormat::Rfc3339);
+  auto dt4 = DateTime::Parse("Mon, 1 Jan 0001 00:00:00 GMT", DateTime::DateFormat::Rfc1123);
   EXPECT_EQ(dt3, dt4);
-  EXPECT_EQ(0, (dt3 - Year1601).count());
+  EXPECT_EQ(0, dt3.time_since_epoch().count());
 }
 
 TEST(DateTime, ConstructorAndDuration)
 {
   auto dt1 = DateTime::Parse("2020-11-03T15:30:45.1234567Z", DateTime::DateFormat::Rfc3339);
   auto dt2 = DateTime(2020, 11, 03, 15, 30, 45);
-  dt2 += std::chrono::duration_cast<DateTime::Duration>(std::chrono::nanoseconds(123456700));
+  dt2 += std::chrono::duration_cast<DateTime::duration>(std::chrono::nanoseconds(123456700));
   EXPECT_EQ(dt1, dt2);
 
   using namespace std::chrono_literals;
   auto duration = 8h + 29min + 14s + 876543300ns;
 
-  auto dt3 = dt1 + std::chrono::duration_cast<DateTime::Duration>(duration);
+  auto dt3 = dt1 + duration;
 
   auto dt4 = DateTime::Parse("2020-11-04T00:00:00Z", DateTime::DateFormat::Rfc3339);
   EXPECT_EQ(dt3, dt4);
@@ -589,5 +592,5 @@ TEST(DateTime, ArithmeticOperators)
 TEST(DateTime, DefaultConstructible)
 {
   DateTime dt;
-  EXPECT_EQ(0, (dt - Year1601).count());
+  EXPECT_EQ(0, dt.time_since_epoch().count());
 }

sdk/identity/azure-identity/src/client_secret_credential.cpp
@@ -5,6 +5,7 @@
 
 #include <azure/core/http/pipeline.hpp>
 
+#include <chrono>
 #include <iomanip>
 #include <sstream>
 
@@ -180,7 +181,7 @@ Azure::Core::AccessToken ClientSecretCredential::GetToken(
 
     return {
         std::string(responseBodyBegin + tokenBegin, responseBodyBegin + tokenEnd),
-        DateTime::Now()
+        std::chrono::system_clock::now()
             + std::chrono::seconds(expiresInSeconds < 0 ? 0 : expiresInSeconds),
     };
   }

sdk/storage/azure-storage-blobs/test/blob_container_client_test.cpp
@@ -42,7 +42,7 @@ namespace Azure { namespace Storage { namespace Test {
   {
     Sas::BlobSasBuilder sasBuilder;
     sasBuilder.Protocol = Sas::SasProtocol::HttpsAndHttp;
-    sasBuilder.ExpiresOn = Azure::Core::DateTime::Now() + std::chrono::hours(72);
+    sasBuilder.ExpiresOn = std::chrono::system_clock::now() + std::chrono::hours(72);
     sasBuilder.BlobContainerName = m_containerName;
     sasBuilder.Resource = Sas::BlobSasResource::BlobContainer;
     sasBuilder.SetPermissions(Sas::BlobContainerSasPermissions::All);
@@ -376,13 +376,13 @@ namespace Azure { namespace Storage { namespace Test {
     options.AccessType = Blobs::Models::PublicAccessType::Blob;
     Blobs::Models::BlobSignedIdentifier identifier;
     identifier.Id = RandomString(64);
-    identifier.StartsOn = Azure::Core::DateTime::Now() - std::chrono::minutes(1);
-    identifier.ExpiresOn = Azure::Core::DateTime::Now() + std::chrono::minutes(1);
+    identifier.StartsOn = std::chrono::system_clock::now() - std::chrono::minutes(1);
+    identifier.ExpiresOn = std::chrono::system_clock::now() + std::chrono::minutes(1);
     identifier.Permissions = "r";
     options.SignedIdentifiers.emplace_back(identifier);
     identifier.Id = RandomString(64);
-    identifier.StartsOn = Azure::Core::DateTime::Now() - std::chrono::minutes(2);
-    identifier.ExpiresOn = Azure::Core::DateTime::Now() + std::chrono::minutes(2);
+    identifier.StartsOn = std::chrono::system_clock::now() - std::chrono::minutes(2);
+    identifier.ExpiresOn = std::chrono::system_clock::now() + std::chrono::minutes(2);
     identifier.Permissions = "racwdxlt";
     options.SignedIdentifiers.emplace_back(identifier);
 

sdk/storage/azure-storage-blobs/test/blob_sas_test.cpp
@@ -4,15 +4,17 @@
 #include <azure/identity/client_secret_credential.hpp>
 #include <azure/storage/blobs/blob_sas_builder.hpp>
 
+#include <chrono>
+
 #include "blob_container_client_test.hpp"
 
 namespace Azure { namespace Storage { namespace Test {
 
   TEST_F(BlobContainerClientTest, BlobSasTest)
   {
-    auto sasStartsOn = Azure::Core::DateTime::Now() - std::chrono::minutes(5);
-    auto sasExpiredOn = Azure::Core::DateTime::Now() - std::chrono::minutes(1);
-    auto sasExpiresOn = Azure::Core::DateTime::Now() + std::chrono::minutes(60);
+    auto sasStartsOn = std::chrono::system_clock::now() - std::chrono::minutes(5);
+    auto sasExpiredOn = std::chrono::system_clock::now() - std::chrono::minutes(1);
+    auto sasExpiresOn = std::chrono::system_clock::now() + std::chrono::minutes(60);
 
     Sas::AccountSasBuilder accountSasBuilder;
     accountSasBuilder.Protocol = Sas::SasProtocol::HttpsAndHttp;

sdk/storage/azure-storage-blobs/test/storage_retry_policy_test.cpp
@@ -89,7 +89,8 @@ namespace Azure { namespace Storage { namespace Test {
         response->AddHeader("x-ms-error-code", "BlobNotFound");
         response->AddHeader(
             "date",
-            Azure::Core::DateTime::Now().GetString(Azure::Core::DateTime::DateFormat::Rfc1123));
+            Azure::Core::DateTime(std::chrono::system_clock::now())
+                .GetString(Azure::Core::DateTime::DateFormat::Rfc1123));
         return response;
       };
       auto ConstructPreconditionFailedResponse = []() {
@@ -113,7 +114,8 @@ namespace Azure { namespace Storage { namespace Test {
         response->AddHeader("x-ms-error-code", "ConditionNotMet");
         response->AddHeader(
             "date",
-            Azure::Core::DateTime::Now().GetString(Azure::Core::DateTime::DateFormat::Rfc1123));
+            Azure::Core::DateTime(std::chrono::system_clock::now())
+                .GetString(Azure::Core::DateTime::DateFormat::Rfc1123));
         return response;
       };
       auto ConstructPrimaryResponse =
@@ -142,7 +144,8 @@ namespace Azure { namespace Storage { namespace Test {
             response->AddHeader("x-ms-server-encrypted", "true");
             response->AddHeader(
                 "date",
-                Azure::Core::DateTime::Now().GetString(Azure::Core::DateTime::DateFormat::Rfc1123));
+                Azure::Core::DateTime(std::chrono::system_clock::now())
+                    .GetString(Azure::Core::DateTime::DateFormat::Rfc1123));
             return response;
           };
       auto ConstructSecondaryResponse =
@@ -171,7 +174,8 @@ namespace Azure { namespace Storage { namespace Test {
             response->AddHeader("x-ms-server-encrypted", "true");
             response->AddHeader(
                 "date",
-                Azure::Core::DateTime::Now().GetString(Azure::Core::DateTime::DateFormat::Rfc1123));
+                Azure::Core::DateTime(std::chrono::system_clock::now())
+                    .GetString(Azure::Core::DateTime::DateFormat::Rfc1123));
             return response;
           };
 

commit 627ea1f24b079c4b274b7ca73a179b654eb49965

fix comparison bug introduced in #1320

sdk/core/azure-core/inc/azure/core/datetime.hpp
@@ -240,21 +240,21 @@ namespace Azure { namespace Core {
 
   inline bool operator<(std::chrono::system_clock::time_point const& tp, DateTime const& dt)
   {
-    return !(dt >= tp);
+    return (dt > tp);
   }
 
   inline bool operator<=(std::chrono::system_clock::time_point const& tp, DateTime const& dt)
   {
-    return !(dt > tp);
+    return (dt >= tp);
   }
 
   inline bool operator>(std::chrono::system_clock::time_point const& tp, DateTime const& dt)
   {
-    return !(dt <= tp);
+    return (dt < tp);
   }
 
   inline bool operator>=(std::chrono::system_clock::time_point const& tp, DateTime const& dt)
   {
-    return !(dt < tp);
+    return (dt <= tp);
   }
 }} // namespace Azure::Core