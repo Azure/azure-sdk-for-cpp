e76cefd0eee52b67240d56786e4597ac48b674d8
Jinming Hu <jinmhu@microsoft.com>
2021-01-26 12:54:07 +0800

    operation<T>

---
+++ sdk/storage/azure-storage-blobs/src/blob_responses.cpp
@@ -0,0 +1,61 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#include "azure/storage/blobs/blob_responses.hpp"
+
+#include "azure/storage/blobs/blob_client.hpp"
+
+namespace Azure { namespace Storage { namespace Blobs { namespace Models {
+
+  std::unique_ptr<Azure::Core::Http::RawResponse> StartCopyBlobResult::PollInternal(
+      Azure::Core::Context& context)
+  {
+    unused(context);
+
+    auto response = m_blobClient->GetProperties();
+    if (!response->CopyStatus.HasValue())
+    {
+      m_status = Azure::Core::OperationStatus::Failed;
+    }
+    else if (response->CopyStatus.GetValue() == CopyStatus::Pending)
+    {
+      m_status = Azure::Core::OperationStatus::Running;
+    }
+    else if (response->CopyStatus.GetValue() == CopyStatus::Success)
+    {
+      m_status = Azure::Core::OperationStatus::Succeeded;
+    }
+    else
+    {
+      m_status = Azure::Core::OperationStatus::Failed;
+    }
+    m_pollResult = *response;
+    return response.ExtractRawResponse();
+  }
+
+  Azure::Core::Response<GetBlobPropertiesResult> StartCopyBlobResult::PollUntilDoneInternal(
+      Azure::Core::Context& context,
+      std::chrono::milliseconds period)
+  {
+    while (true)
+    {
+      auto rawResponse = PollInternal(context);
+
+      if (m_status == Azure::Core::OperationStatus::Succeeded)
+      {
+        return Azure::Core::Response<GetBlobPropertiesResult>(m_pollResult, std::move(rawResponse));
+      }
+      else if (m_status == Azure::Core::OperationStatus::Failed)
+      {
+        throw StorageException("Operation failed");
+      }
+      else if (m_status == Azure::Core::OperationStatus::Cancelled)
+      {
+        throw StorageException("Operation was cancelled");
+      }
+
+      std::this_thread::sleep_for(period);
+    };
+  }
+
+}}}} // namespace Azure::Storage::Blobs::Models
