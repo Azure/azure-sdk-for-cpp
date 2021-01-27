56b011dcbc575e2c010a44eb95081192acf9f5a7
Victor Vazquez <vhvb1989@gmail.com>
2021-01-27 22:19:50 +0000

    disble secure warnings for windows to make getenv work

--- sdk/keyvault/azure-security-keyvault-keys/test/key_client_test_live.cpp
+++ sdk/keyvault/azure-security-keyvault-keys/test/key_client_test_live.cpp
@@ -1,6 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
 
+#if defined(_MSC_VER)
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
 #include "gtest/gtest.h"
 
 #include "key_client_base_test.hpp"
@@ -11,7 +15,7 @@
 
 using namespace Azure::Security::KeyVault::Keys::Test;
 
-TEST_F(KeyVaultClientTest, SendRequestDefault)
+TEST_F(KeyVaultClientTest, GetKey)
 {
   Azure::Security::KeyVault::Keys::KeyClient keyClient(m_keyVaultUrl, m_credential);
   // Assuming and RS Key exists in the KeyVault Account.
