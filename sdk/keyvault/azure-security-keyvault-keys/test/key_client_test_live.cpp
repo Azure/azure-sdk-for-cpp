73776aae3683dc919056fcbb0acf167abb85efde
Victor Vazquez <vhvb1989@gmail.com>
2021-01-28 01:31:58 +0000

    create key live tests

--- sdk/keyvault/azure-security-keyvault-keys/test/key_client_test_live.cpp
+++ sdk/keyvault/azure-security-keyvault-keys/test/key_client_test_live.cpp
@@ -46,56 +46,60 @@ TEST_F(KeyVaultClientTest, CreateKey)
   }
 }
 
-// TEST_F(KeyVaultClientTest, DISABLED_CreateKeyWithOptions)
-// {
-//   auto credential = std::make_shared<Azure::Identity::ClientSecretCredential>(
-//       "tenantId", "clientId", "secrectId");
-//   Azure::Security::KeyVault::Keys::KeyClient keyClient(m_keyVaultUrl, m_credential);
+TEST_F(KeyVaultClientTest, CreateKeyWithOptions)
+{
+  Azure::Security::KeyVault::Keys::KeyClient keyClient(m_keyVaultUrl, m_credential);
+  std::string keyName("createKeyWithOptions");
 
-//   Azure::Security::KeyVault::Keys::CreateKeyOptions options;
-//   options.KeyOperations.push_back(Azure::Security::KeyVault::Keys::KeyOperation::Sign());
-//   options.KeyOperations.push_back(Azure::Security::KeyVault::Keys::KeyOperation::Verify());
+  Azure::Security::KeyVault::Keys::CreateKeyOptions options;
+  options.KeyOperations.push_back(Azure::Security::KeyVault::Keys::KeyOperation::Sign());
+  options.KeyOperations.push_back(Azure::Security::KeyVault::Keys::KeyOperation::Verify());
+  {
+    auto keyResponse
+        = keyClient.CreateKey(keyName, Azure::Security::KeyVault::Keys::KeyTypeEnum::Ec, options);
+    auto keyVaultKey = keyResponse.ExtractValue();
 
-//   auto r = keyClient.CreateKey("myKeyWithOptions2", KeyTypeEnum::Ec, options);
-//   auto keyVaultKey = r.ExtractValue();
+    EXPECT_EQ(keyVaultKey.Name(), keyName);
+    EXPECT_EQ(keyVaultKey.GetKeyType(), Azure::Security::KeyVault::Keys::KeyTypeEnum::Ec);
+    auto& keyOperations = keyVaultKey.KeyOperations();
+    EXPECT_EQ(keyOperations.size(), 2);
 
-//   EXPECT_EQ(keyVaultKey.Name(), "myKeyWithOptions2");
-//   EXPECT_EQ(keyVaultKey.GetKeyType(), KeyTypeEnum::Ec);
-//   auto& keyOperations = keyVaultKey.KeyOperations();
-//   EXPECT_EQ(keyOperations.size(), 2);
+    auto findOperation = [keyOperations](Azure::Security::KeyVault::Keys::KeyOperation op) {
+      for (Azure::Security::KeyVault::Keys::KeyOperation operation : keyOperations)
+      {
+        if (operation.ToString() == op.ToString())
+        {
+          return true;
+        }
+      }
+      return false;
+    };
+    EXPECT_PRED1(findOperation, Azure::Security::KeyVault::Keys::KeyOperation::Sign());
+    EXPECT_PRED1(findOperation, Azure::Security::KeyVault::Keys::KeyOperation::Verify());
+  }
+}
 
-//   auto findOperation = [keyOperations](KeyOperation op) {
-//     for (KeyOperation operation : keyOperations)
-//     {
-//       if (operation.ToString() == op.ToString())
-//       {
-//         return true;
-//       }
-//     }
-//     return false;
-//   };
-//   EXPECT_PRED1(findOperation, KeyOperation::Sign());
-//   EXPECT_PRED1(findOperation, KeyOperation::Verify());
-// }
+TEST_F(KeyVaultClientTest, CreateKeyWithTags)
+{
+  Azure::Security::KeyVault::Keys::KeyClient keyClient(m_keyVaultUrl, m_credential);
+  std::string keyName("myKeyWithOptionsTags");
 
-// TEST_F(KeyVaultClientTest, DISABLED_CreateKeyWithTags)
-// {
-//   auto credential = std::make_shared<Azure::Identity::ClientSecretCredential>(
-//       "tenantId", "clientId", "secrectId");
-//   Azure::Security::KeyVault::Keys::KeyClient keyClient(m_keyVaultUrl, m_credential);
+  Azure::Security::KeyVault::Keys::CreateKeyOptions options;
+  options.Tags.emplace("one", "value=1");
+  options.Tags.emplace("two", "value=2");
 
-//   CreateKeyOptions options;
-//   options.Tags.emplace("one", "value=1");
-//   options.Tags.emplace("two", "value=2");
+  {
+    auto keyResponse
+        = keyClient.CreateKey(keyName, Azure::Security::KeyVault::Keys::KeyTypeEnum::Rsa, options);
+    auto keyVaultKey = keyResponse.ExtractValue();
 
-//   auto r = keyClient.CreateKey("myKeyWithOptionsTags", KeyTypeEnum::Rsa, options);
-//   auto keyVaultKey = r.ExtractValue();
+    EXPECT_EQ(keyVaultKey.Name(), keyName);
+    EXPECT_EQ(keyVaultKey.GetKeyType(), Azure::Security::KeyVault::Keys::KeyTypeEnum::Rsa);
 
-//   EXPECT_EQ(keyVaultKey.Name(), "myKeyWithOptionsTags");
-//   EXPECT_EQ(keyVaultKey.GetKeyType(), KeyTypeEnum::Rsa);
-
-//   auto findTag = [keyVaultKey](std::string key, std::string value) {
-//     // Will throw if key is not found
-//     auto v = keyVaultKey.Properties.Tags.at(key);
-//     return value == v;
-//   };
+    auto findTag = [keyVaultKey](std::string key, std::string value) {
+      // Will throw if key is not found
+      auto v = keyVaultKey.Properties.Tags.at(key);
+      return value == v;
+    };
+  }
+}
