d0a6f7e8f82dfdbbc12d397efd43dee4fbd07cc4
Kan Tang <kat@microsoft.com>
2021-02-03 18:23:37 +0800

    Refined returned properties for list/download API in DataLake. (#1580)
    * Refined returned properties for list/download API in DataLake.
    
    * Refine readme
    
    * Resolve review comments

--- sdk/storage/azure-storage-files-datalake/src/datalake_service_client.cpp
+++ sdk/storage/azure-storage-files-datalake/src/datalake_service_client.cpp
@@ -43,34 +43,34 @@ namespace Azure { namespace Storage { namespace Files { namespace DataLake {
       {
         Models::FileSystemItem fileSystem;
         fileSystem.Name = std::move(item.Name);
-        fileSystem.ETag = std::move(item.Details.ETag);
-        fileSystem.LastModified = std::move(item.Details.LastModified);
-        fileSystem.Metadata = std::move(item.Details.Metadata);
+        fileSystem.Details.ETag = std::move(item.Details.ETag);
+        fileSystem.Details.LastModified = std::move(item.Details.LastModified);
+        fileSystem.Details.Metadata = std::move(item.Details.Metadata);
         if (item.Details.AccessType == Blobs::Models::PublicAccessType::BlobContainer)
         {
-          fileSystem.AccessType = Models::PublicAccessType::FileSystem;
+          fileSystem.Details.AccessType = Models::PublicAccessType::FileSystem;
         }
         else if (item.Details.AccessType == Blobs::Models::PublicAccessType::Blob)
         {
-          fileSystem.AccessType = Models::PublicAccessType::Path;
+          fileSystem.Details.AccessType = Models::PublicAccessType::Path;
         }
         else if (item.Details.AccessType == Blobs::Models::PublicAccessType::None)
         {
-          fileSystem.AccessType = Models::PublicAccessType::None;
+          fileSystem.Details.AccessType = Models::PublicAccessType::None;
         }
         else
         {
-          fileSystem.AccessType = Models::PublicAccessType(item.Details.AccessType.Get());
+          fileSystem.Details.AccessType = Models::PublicAccessType(item.Details.AccessType.Get());
         }
-        fileSystem.HasImmutabilityPolicy = item.Details.HasImmutabilityPolicy;
-        fileSystem.HasLegalHold = item.Details.HasLegalHold;
+        fileSystem.Details.HasImmutabilityPolicy = item.Details.HasImmutabilityPolicy;
+        fileSystem.Details.HasLegalHold = item.Details.HasLegalHold;
         if (item.Details.LeaseDuration.HasValue())
         {
-          fileSystem.LeaseDuration
+          fileSystem.Details.LeaseDuration
               = Models::LeaseDurationType((item.Details.LeaseDuration.GetValue().Get()));
         }
-        fileSystem.LeaseState = Models::LeaseStateType(item.Details.LeaseState.Get());
-        fileSystem.LeaseStatus = Models::LeaseStatusType(item.Details.LeaseStatus.Get());
+        fileSystem.Details.LeaseState = Models::LeaseStateType(item.Details.LeaseState.Get());
+        fileSystem.Details.LeaseStatus = Models::LeaseStatusType(item.Details.LeaseStatus.Get());
 
         fileSystems.emplace_back(std::move(fileSystem));
       }
