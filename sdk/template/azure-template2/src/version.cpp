c46a77bdfb8fcdae75c5c8ca7e039d03e98cf7a5
Daniel Jurek <djurek@microsoft.com>
2021-02-08 10:43:55 -0800

    Add azure-template2 to validate publishing multiple packages to vcpkg

---
+++ sdk/template/azure-template2/src/version.cpp
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#include <azure/template2/version.hpp>
+#include <sstream>
+#include <string>
+
+using namespace Azure::Template::Details;
+
+const std::string Version::PreRelease = secret;
+
+std::string Version::VersionString()
+{
+  static const std::string versionString = [] {
+    std::string version;
+    std::stringstream ss;
+    std::string dot = ".";
+
+    ss << Version::Major << dot << Version::Minor << dot << Version::Patch;
+
+    if (!Version::PreRelease.empty())
+      ss << "-" << Version::PreRelease;
+
+    return ss.str();
+  }();
+
+  return versionString;
+}
---
+++ sdk/template/azure-template2/vcpkg/portfile.cmake
@@ -0,0 +1,22 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# SPDX-License-Identifier: MIT
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO Azure/azure-sdk-for-cpp
+    REF azure-template_@AZ_LIBRARY_VERSION@
+    SHA512 1
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}/sdk/template/azure-template/
+    PREFER_NINJA
+    OPTIONS
+        -DWARNINGS_AS_ERRORS=OFF
+)
+
+vcpkg_install_cmake()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+vcpkg_fixup_cmake_targets()
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+vcpkg_copy_pdbs()
