3ae6902ae2d218c2558b1e5f0e73ac39d09af0b2
Ahson Khan <ahkha@microsoft.com>
2021-02-09 11:47:40 -0800

    Renamed `Crc64` to `Crc64Hash` and change it to derive from the `Azure::Core::Cryptography::Hash` class.

--- sdk/storage/azure-storage-common/inc/azure/storage/common/crypt.hpp
+++ sdk/storage/azure-storage-common/inc/azure/storage/common/crypt.hpp
@@ -7,32 +7,21 @@
 #include <string>
 #include <vector>
 
+#include "azure/core/cryptography/hash.hpp"
 #include <azure/core/base64.hpp>
 
 namespace Azure { namespace Storage {
 
-  class Crc64 {
+  class Crc64Hash : public Azure::Core::Cryptography::Hash {
   public:
-    void Update(const uint8_t* data, std::size_t length);
-    void Concatenate(const Crc64& other);
-
-    std::vector<uint8_t> Digest() const;
-
-    static std::vector<uint8_t> Hash(const uint8_t* data, std::size_t length)
-    {
-      Crc64 instance;
-      instance.Update(data, length);
-      return instance.Digest();
-    }
-
-    static std::vector<uint8_t> Hash(const std::string& data)
-    {
-      return Hash(reinterpret_cast<const uint8_t*>(data.data()), data.length());
-    }
+    void Concatenate(const Crc64Hash& other);
 
   private:
     uint64_t m_context = 0ULL;
     uint64_t m_length = 0ULL;
+
+    void OnAppend(const uint8_t* data, std::size_t length) override;
+    std::vector<uint8_t> OnFinal(const uint8_t* data, std::size_t length) override;
   };
 
   namespace Details {
