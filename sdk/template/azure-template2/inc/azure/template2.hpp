5e9e4bffa8c74aeb5a7e52965db7c6c9a59a4f93
Daniel Jurek <djurek@microsoft.com>
2021-02-09 14:03:21 -0800

    Revert "Revert "Revert template2""
    This reverts commit 1721a626e3c2e2b8c43dfe4a2fedf5a891c58793.

--- sdk/template/azure-template2/inc/azure/template2.hpp
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// SPDX-License-Identifier: MIT
-
-#pragma once
-
-#include "azure/template2/dll_import_export.hpp"
-#include "azure/template2/template_client.hpp"
-#include "azure/template2/version.hpp"
