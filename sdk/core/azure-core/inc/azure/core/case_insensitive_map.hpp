4d6bfe16e34003f57a966583bce4b9cd6b43ac93
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2021-02-24 01:13:41 -0800

    Remove Allocator template parameter - we can add it later if we need it, currently no need to commit to having it there

--- sdk/core/azure-core/inc/azure/core/case_insensitive_map.hpp
+++ sdk/core/azure-core/inc/azure/core/case_insensitive_map.hpp
@@ -19,16 +19,8 @@ namespace Azure { namespace Core {
    * @brief A type alias of `std::map<std::string, T>` with case-insensitive key comparison.
    *
    * @tparam T Type of values being stored in the map.
-   *
-   * @tparam[optional] STL container allocator type
-   * (default is `std::map<std::string, T>::allocator_type`).
    */
-  template <
-      typename T,
-      typename Allocator
-      = std::map<std::string, T, typename Internal::Strings::CaseInsensitiveComparator>::
-          allocator_type>
-  using CaseInsensitiveMap
-      = std::map<std::string, T, Internal::Strings::CaseInsensitiveComparator, Allocator>;
+  template <typename T>
+  using CaseInsensitiveMap = std::map<std::string, T, Internal::Strings::CaseInsensitiveComparator>;
 
 }} // namespace Azure::Core
