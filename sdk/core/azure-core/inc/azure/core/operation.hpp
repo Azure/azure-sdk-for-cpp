f2194f770455160cc730d9b87b0ee5040a83fedc
Rick Winter <rick.winter@microsoft.com>
2021-03-05 20:17:41 -0800

    Update sdk/core/azure-core/inc/azure/core/operation.hpp
    Co-authored-by: Ahson Khan <ahkha@microsoft.com>

--- sdk/core/azure-core/inc/azure/core/operation.hpp
+++ sdk/core/azure-core/inc/azure/core/operation.hpp
@@ -55,7 +55,7 @@ namespace Azure { namespace Core {
      * @brief Get the raw HTTP response.
      * @return A pointer to #Azure::Core::Http::RawResponse or null if the RawResponse does not
      * exist.
-     * @note Does not give up ownership of the RawResponse
+     * @note Does not give up ownership of the RawResponse.
      */
     virtual Azure::Core::Http::RawResponse* GetRawResponse() const = 0;
 
