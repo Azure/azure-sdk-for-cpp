e6cdd529863b84d252e25731ea45b7e07b5cfb52
Ahson Khan <ahkha@microsoft.com>
2021-03-10 22:34:25 -0800

    Revert "_internal::Json => Json::_internal (#1834)"
    This reverts commit 2562df6682afb542b68a6fce680a57c2cdf3348e.

--- sdk/core/perf/src/program.cpp
+++ sdk/core/perf/src/program.cpp
@@ -70,14 +70,14 @@ inline void PrintOptions(
 {
   {
     std::cout << std::endl << "=== Global Options ===" << std::endl;
-    Azure::Core::Json::_internal::json optionsAsJson = options;
+    Azure::Core::_internal::Json::json optionsAsJson = options;
     std::cout << ReplaceAll(optionsAsJson.dump(), ",", ",\n") << std::endl;
   }
 
   if (testOptions.size() > 0)
   {
     std::cout << std::endl << "=== Test Options ===" << std::endl;
-    Azure::Core::Json::_internal::json optionsAsJson;
+    Azure::Core::_internal::Json::json optionsAsJson;
     for (auto option : testOptions)
     {
       try
