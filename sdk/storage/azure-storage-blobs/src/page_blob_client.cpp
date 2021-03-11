4e2236858d51fb10759a40d90c158d634740d608
Ahson Khan <ahkha@microsoft.com>
2021-03-10 22:32:45 -0800

    Revert "Rename the Range type to HttpRange within the Azure::Core::Http namespace. (#1835)"
    This reverts commit 2a1ea1f890ecfea0c804a762569dca2e300298f9.

--- sdk/storage/azure-storage-blobs/src/page_blob_client.cpp
+++ sdk/storage/azure-storage-blobs/src/page_blob_client.cpp
@@ -160,7 +160,7 @@ namespace Azure { namespace Storage { namespace Blobs {
   Azure::Response<Models::UploadPageBlobPagesFromUriResult> PageBlobClient::UploadPagesFromUri(
       int64_t destinationOffset,
       std::string sourceUri,
-      Azure::Core::Http::HttpRange sourceRange,
+      Azure::Core::Http::Range sourceRange,
       const UploadPageBlobPagesFromUriOptions& options,
       const Azure::Core::Context& context) const
   {
@@ -188,7 +188,7 @@ namespace Azure { namespace Storage { namespace Blobs {
   }
 
   Azure::Response<Models::ClearPageBlobPagesResult> PageBlobClient::ClearPages(
-      Azure::Core::Http::HttpRange range,
+      Azure::Core::Http::Range range,
       const ClearPageBlobPagesOptions& options,
       const Azure::Core::Context& context) const
   {
