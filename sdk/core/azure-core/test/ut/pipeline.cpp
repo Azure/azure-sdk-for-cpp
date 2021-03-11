0e384bb79a30be0011ddc600d6ddb3a3872d4643
Ahson Khan <ahkha@microsoft.com>
2021-03-10 23:05:39 -0800

    Revert "Move http policies to Azure::Core::Http::Policies (#1844)"
    This reverts commit c7ba56f837f29e8317c2ec22d108f3183300b73e.

--- sdk/core/azure-core/test/ut/pipeline.cpp
+++ sdk/core/azure-core/test/ut/pipeline.cpp
@@ -10,9 +10,8 @@
 TEST(Pipeline, createPipeline)
 {
   // Construct pipeline without exception
-  std::vector<std::unique_ptr<Azure::Core::Http::Policies::HttpPolicy>> policies;
-  policies.push_back(
-      std::make_unique<Azure::Core::Http::Policies::TelemetryPolicy>("test", "test"));
+  std::vector<std::unique_ptr<Azure::Core::Http::HttpPolicy>> policies;
+  policies.push_back(std::make_unique<Azure::Core::Http::TelemetryPolicy>("test", "test"));
 
   EXPECT_NO_THROW(Azure::Core::Http::_internal::HttpPipeline pipeline(policies));
 }
@@ -20,7 +19,7 @@ TEST(Pipeline, createPipeline)
 TEST(Pipeline, createEmptyPipeline)
 {
   // throw invalid arg for empty policies
-  std::vector<std::unique_ptr<Azure::Core::Http::Policies::HttpPolicy>> policies;
+  std::vector<std::unique_ptr<Azure::Core::Http::HttpPolicy>> policies;
   EXPECT_THROW(
       Azure::Core::Http::_internal::HttpPipeline pipeline(policies), std::invalid_argument);
 }
@@ -28,9 +27,8 @@ TEST(Pipeline, createEmptyPipeline)
 TEST(Pipeline, clonePipeline)
 {
   // Construct pipeline without exception and clone
-  std::vector<std::unique_ptr<Azure::Core::Http::Policies::HttpPolicy>> policies;
-  policies.push_back(
-      std::make_unique<Azure::Core::Http::Policies::TelemetryPolicy>("test", "test"));
+  std::vector<std::unique_ptr<Azure::Core::Http::HttpPolicy>> policies;
+  policies.push_back(std::make_unique<Azure::Core::Http::TelemetryPolicy>("test", "test"));
 
   Azure::Core::Http::_internal::HttpPipeline pipeline(policies);
   EXPECT_NO_THROW(Azure::Core::Http::_internal::HttpPipeline pipeline2(pipeline));
@@ -40,7 +38,7 @@ TEST(Pipeline, refrefPipeline)
 {
   // Construct pipeline without exception
   EXPECT_NO_THROW(Azure::Core::Http::_internal::HttpPipeline pipeline(
-      std::vector<std::unique_ptr<Azure::Core::Http::Policies::HttpPolicy>>(1)));
+      std::vector<std::unique_ptr<Azure::Core::Http::HttpPolicy>>(1)));
 }
 
 TEST(Pipeline, refrefEmptyPipeline)
@@ -48,6 +46,6 @@ TEST(Pipeline, refrefEmptyPipeline)
   // Construct pipeline with invalid exception with move constructor
   EXPECT_THROW(
       Azure::Core::Http::_internal::HttpPipeline pipeline(
-          std::vector<std::unique_ptr<Azure::Core::Http::Policies::HttpPolicy>>(0)),
+          std::vector<std::unique_ptr<Azure::Core::Http::HttpPolicy>>(0)),
       std::invalid_argument);
 }
