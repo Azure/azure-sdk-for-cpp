1352b4b7a7198f58c82bec2b534f4d6b7805bc9e
Victor Vazquez <vhvb1989@gmail.com>
2021-03-17 18:04:18 +0000

    update arg name

--- sdk/core/azure-core/inc/azure/core/internal/json/json_optional.hpp
+++ sdk/core/azure-core/inc/azure/core/internal/json/json_optional.hpp
@@ -25,47 +25,48 @@ namespace Azure { namespace Core { namespace Json { namespace _internal {
   {
     /**
      * @brief If the optional key \p key is present in the json node \p jsonKey set the value of \p
-     * source.
+     * destination.
      *
-     * @remark If the key is not in the json node, the \p source is not modified.
+     * @remark If the key is not in the json node, the \p destination is not modified.
      *
      * @param jsonKey The json node to review.
      * @param key The key name for the optional property.
-     * @param source The value to update if the key name property is in the json node.
+     * @param destination The value to update if the key name property is in the json node.
      */
     template <class T>
     static inline void SetIfExists(
-        Azure::Nullable<T>& source,
+        Azure::Nullable<T>& destination,
         Azure::Core::Json::_internal::json const& jsonKey,
         std::string const& key) noexcept
     {
       if (jsonKey.contains(key))
       {
-        source = jsonKey[key].get<T>();
+        destination = jsonKey[key].get<T>();
       }
     }
 
     /**
      * @brief If the optional key \p key is present in the json node \p jsonKey set the value of \p
-     * source.
+     * destination.
      *
-     * @remark If the key is not in the json node, the \p source is not modified.
+     * @remark If the key is not in the json node, the \p destination is not modified.
      *
      * @param jsonKey The json node to review.
      * @param key The key name for the optional property.
-     * @param source The value to update if the key name property is in the json node.
-     * @param decorator A optional function to update the json value before updating the \p source.
+     * @param destination The value to update if the key name property is in the json node.
+     * @param decorator A optional function to update the json value before updating the \p
+     * destination.
      */
     template <class T, class V>
     static inline void SetIfExists(
-        Azure::Nullable<V>& source,
+        Azure::Nullable<V>& destination,
         Azure::Core::Json::_internal::json const& jsonKey,
         std::string const& key,
         std::function<V(T value)> decorator) noexcept
     {
       if (jsonKey.contains(key))
       {
-        source = decorator(jsonKey[key].get<T>());
+        destination = decorator(jsonKey[key].get<T>());
       }
     }
   };
