8a6b9ea68b525517d2befec58e5ed06a0b5b1256
Victor Vazquez <vhvb1989@gmail.com>
2021-03-19 00:17:15 +0000

    ignore sigpipe

--- sdk/core/azure-core/test/ut/main.cpp
+++ sdk/core/azure-core/test/ut/main.cpp
@@ -5,12 +5,16 @@
 
 #if defined(BUILD_CURL_HTTP_TRANSPORT_ADAPTER)
 #include <curl/curl.h>
+#include <signal.h>
 #endif
 
 int main(int argc, char** argv)
 {
 #if defined(BUILD_CURL_HTTP_TRANSPORT_ADAPTER)
   curl_global_init(CURL_GLOBAL_ALL);
+  // OpenSSL signals SIGPIPE when trying to clean an HTTPS closed connection.
+  // End users need to decide if SIGPIPE should be ignored or not.
+  signal(SIGPIPE, SIG_IGN);
 #endif
 
   testing::InitGoogleTest(&argc, argv);
