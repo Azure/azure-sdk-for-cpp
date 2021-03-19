0bddbdf4dcb3ac57cef97c3e69c0a3af0a24a365
Victor Vazquez <vhvb1989@gmail.com>
2021-03-19 06:38:46 +0000

    fix for windows

--- sdk/core/azure-core/test/ut/main.cpp
+++ sdk/core/azure-core/test/ut/main.cpp
@@ -12,9 +12,11 @@ int main(int argc, char** argv)
 {
 #if defined(BUILD_CURL_HTTP_TRANSPORT_ADAPTER)
   curl_global_init(CURL_GLOBAL_ALL);
+#if defined(AZ_PLATFORM_POSIX)
   // OpenSSL signals SIGPIPE when trying to clean an HTTPS closed connection.
   // End users need to decide if SIGPIPE should be ignored or not.
   signal(SIGPIPE, SIG_IGN);
+#endif
 #endif
 
   testing::InitGoogleTest(&argc, argv);
