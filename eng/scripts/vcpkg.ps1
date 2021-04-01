7033b3adf3692105d367b291081ba4cd0c54d443
Daniel Jurek <djurek@microsoft.com>
2021-04-01 14:09:50 -0700

    Add Write-Host to logs

--- eng/scripts/vcpkg.ps1
+++ eng/scripts/vcpkg.ps1
@@ -16,16 +16,24 @@ Param (
 $initialDirectory = Get-Location
 
 try {
+    Write-Host "git clone https://github.com/Microsoft/vcpkg $VcpkgPath"
     git clone https://github.com/Microsoft/vcpkg $VcpkgPath
+    Write-Host "Set-Location $VcpkgPath"
     Set-Location $VcpkgPath
+    Write-Host "git fetch --tags"
     git fetch --tags
+    Write-Host "git checkout $Ref"
     git checkout $Ref
 
     if ($IsWindows) {
+        Write-Host ".\bootstrap-vcpkg.bat"
         .\bootstrap-vcpkg.bat
+        Write-Host ".\vcpkg.exe install $Dependencies.Split(' ')"
         .\vcpkg.exe install $Dependencies.Split(' ')
     } else {
+        Write-Host "./bootstrap-vcpkg.sh"
         ./bootstrap-vcpkg.sh
+        Write-Host "./vcpkg install $Dependencies.Split(' ')"
         ./vcpkg install $Dependencies.Split(' ')
     }
 } finally {
