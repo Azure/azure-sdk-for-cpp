9ded0b195893b3f88a8f5e50a07a792d05a6c9b7
Daniel Jurek <djurek@microsoft.com>
2021-04-01 14:48:27 -0700

    Documentation and variable naming

--- eng/scripts/Update-VcpkgNightlyBranch.ps1
+++ eng/scripts/Update-VcpkgNightlyBranch.ps1
@@ -1,6 +1,27 @@
+<#
+.SYNOPSIS
+Updates a Target branch with changes from a Main branch and a Source branch
+
+.DESCRIPTION
+Updat the long-lived nightly branch with changes from Main and a given Source 
+branch. Changes are first merged from the Main branch and then from the Source 
+branch. In both cases conflicts are resolved in favor of the branch which is 
+being merged.
+
+.PARAMETER SourceBranchName
+Name of the branch with new changes to merge into the Target Branch
+
+.PARAMETER TargetBranchName
+Name of the long-lived branch receiving the changes
+
+.PARAMETER MainBranchName
+Name of the "Main" branch to merge into the Target Branch
+
+#>
+
 param(
-    [string] $SourceBranch,
-    [string] $TargetBranch,
+    [string] $SourceBranchName,
+    [string] $TargetBranchName,
     [string] $MainBranchName
 )
 
@@ -14,12 +35,12 @@ if ((Get-Command git | Measure-Object).Count -eq 0) {
 Write-Host "git fetch --all"
 git fetch --all
 
-# Fail quickly if the $SourceBranch of nightly changes cannot be found
-Write-Host "git checkout $SourceBranch"
-git checkout $SourceBranch
+# Fail quickly if the $SourceBranchName of nightly changes cannot be found
+Write-Host "git checkout $SourceBranchName"
+git checkout $SourceBranchName
 
 if ($LASTEXITCODE -ne 0) { 
-    Write-Error "Cannot find branch: $SourceBranch, nothing to merge"
+    Write-Error "Cannot find branch: $SourceBranchName, nothing to merge"
     exit 1
 }
 
@@ -27,13 +48,13 @@ if ($LASTEXITCODE -ne 0) {
 Write-Host "git branch --show-current"
 $currentBranch = git branch --show-current
 
-if ($currentBranch -ne $TargetBranch) {
-    Write-Host "git checkout $TargetBranch"
-    git checkout $TargetBranch
+if ($currentBranch -ne $TargetBranchName) {
+    Write-Host "git checkout $TargetBranchName"
+    git checkout $TargetBranchName
 
     if ($LASTEXITCODE -ne 0) { 
-        Write-Host "git checkout -b $TargetBranch"
-        git checkout -b $TargetBranch
+        Write-Host "git checkout -b $TargetBranchName"
+        git checkout -b $TargetBranchName
     }
 }
 
@@ -41,14 +62,14 @@ Write-Host "git merge $MainBranchName --strategy-option=theirs"
 git merge $MainBranchName --strategy-option=theirs
 
 if ($LASTEXITCODE -ne 0) { 
-    Write-Error "Could not merge $MainBranchName into $TargetBranch"
+    Write-Error "Could not merge $MainBranchName into $TargetBranchName"
     exit 1
 }
 
-Write-Host "git merge $SourceBranch --strategy-option=theirs" 
-git merge $SourceBranch --strategy-option=theirs
+Write-Host "git merge $SourceBranchName --strategy-option=theirs" 
+git merge $SourceBranchName --strategy-option=theirs
 
 if ($LASTEXITCODE -ne 0) {
-    Write-Error "Could not merge $SourceBranch into $TargetBranch" 
+    Write-Error "Could not merge $SourceBranchName into $TargetBranchName" 
     exit 1
 }
