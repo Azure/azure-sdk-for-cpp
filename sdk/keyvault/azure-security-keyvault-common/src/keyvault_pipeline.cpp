b54bad660a2a0e64b207bbd10acc74cdbf1dd74d
Victor Vazquez <vhvb1989@gmail.com>
2021-04-14 23:56:41 +0000

    minor

--- sdk/keyvault/azure-security-keyvault-common/src/keyvault_pipeline.cpp
+++ sdk/keyvault/azure-security-keyvault-common/src/keyvault_pipeline.cpp
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
 
-#include <azure/core/exception.hpp>
 #include <azure/core/http/http.hpp>
 
 #include "azure/keyvault/common/internal/keyvault_pipeline.hpp"
