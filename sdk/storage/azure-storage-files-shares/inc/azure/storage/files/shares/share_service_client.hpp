e5fed3146fb9b5d8963e9085ad7b82ff28d73a1d
Jinming Hu <jinmhu@microsoft.com>
2021-04-15 10:35:38 +0800

    fix clang-format

--- sdk/storage/azure-storage-files-shares/inc/azure/storage/files/shares/share_service_client.hpp
+++ sdk/storage/azure-storage-files-shares/inc/azure/storage/files/shares/share_service_client.hpp
@@ -67,8 +67,8 @@ namespace Azure { namespace Storage { namespace Files { namespace Shares {
     std::string GetUrl() const { return m_serviceUrl.GetAbsoluteUrl(); }
 
     /**
-     * @brief Returns a paginated collection of the shares in the storage account. Enumerating the shares may
-     * make multiple requests to the service while fetching all the values.
+     * @brief Returns a paginated collection of the shares in the storage account. Enumerating the
+     * shares may make multiple requests to the service while fetching all the values.
      * @param options Optional parameters to list the shares.
      * @param context Context for cancelling long running operations.
      * @return ListSharesPagedResponse describing the shares in this storage account.
