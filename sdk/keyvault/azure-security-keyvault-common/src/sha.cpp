7f2bd07ef369dcc83362fc24f50c428f60cffe8f
Victor Vazquez <vhvb1989@gmail.com>
2021-04-24 23:49:18 -0700

    Windows bcrypt implementation

--- sdk/keyvault/azure-security-keyvault-common/src/sha.cpp
+++ sdk/keyvault/azure-security-keyvault-common/src/sha.cpp
@@ -113,3 +113,130 @@ Azure::Security::KeyVault::SHA512::SHA512()
 }
 
 #endif
+
+#if defined(AZ_PLATFORM_WINDOWS)
+namespace {
+struct AlgorithmProviderInstance
+{
+  BCRYPT_ALG_HANDLE Handle;
+  std::size_t ContextSize;
+  std::size_t HashLength;
+
+  AlgorithmProviderInstance(LPCWSTR hashAlgorithm)
+  {
+    NTSTATUS status = BCryptOpenAlgorithmProvider(&Handle, hashAlgorithm, nullptr, 0);
+    if (!BCRYPT_SUCCESS(status))
+    {
+      throw std::runtime_error("BCryptOpenAlgorithmProvider failed");
+    }
+    DWORD objectLength = 0;
+    DWORD dataLength = 0;
+    status = BCryptGetProperty(
+        Handle,
+        BCRYPT_OBJECT_LENGTH,
+        reinterpret_cast<PBYTE>(&objectLength),
+        sizeof(objectLength),
+        &dataLength,
+        0);
+    if (!BCRYPT_SUCCESS(status))
+    {
+      throw std::runtime_error("BCryptGetProperty failed");
+    }
+    ContextSize = objectLength;
+    DWORD hashLength = 0;
+    status = BCryptGetProperty(
+        Handle,
+        BCRYPT_HASH_LENGTH,
+        reinterpret_cast<PBYTE>(&hashLength),
+        sizeof(hashLength),
+        &dataLength,
+        0);
+    if (!BCRYPT_SUCCESS(status))
+    {
+      throw std::runtime_error("BCryptGetProperty failed");
+    }
+    HashLength = hashLength;
+  }
+
+  ~AlgorithmProviderInstance() { BCryptCloseAlgorithmProvider(Handle, 0); }
+};
+
+class SHAWithBCrypt : public Azure::Core::Cryptography::Hash {
+private:
+  std::string m_buffer;
+  BCRYPT_HASH_HANDLE m_hashHandle = nullptr;
+  std::size_t m_hashLength = 0;
+
+  std::vector<uint8_t> OnFinal(const uint8_t* data, std::size_t length) override
+  {
+    OnAppend(data, length);
+    
+    std::vector<uint8_t> hash;
+    hash.resize(m_hashLength);
+    NTSTATUS status = BCryptFinishHash(
+        m_hashHandle,
+        reinterpret_cast<PUCHAR>(&hash[0]),
+        static_cast<ULONG>(hash.size()),
+        0);
+    if (!BCRYPT_SUCCESS(status))
+    {
+      throw std::runtime_error("BCryptFinishHash failed");
+    }
+    return hash;
+  }
+
+  void OnAppend(const uint8_t* data, std::size_t length) override
+  {
+    NTSTATUS status = BCryptHashData(
+        m_hashHandle,
+        reinterpret_cast<PBYTE>(const_cast<uint8_t*>(data)),
+        static_cast<ULONG>(length),
+        0);
+    if (!BCRYPT_SUCCESS(status))
+    {
+      throw std::runtime_error("BCryptHashData failed");
+    }
+  }
+
+public:
+  SHAWithBCrypt(LPCWSTR hashAlgorithm)
+  {
+    AlgorithmProviderInstance algorithmProvider(hashAlgorithm);
+
+    m_buffer.resize(algorithmProvider.ContextSize);
+    m_hashLength = algorithmProvider.HashLength;
+
+    NTSTATUS status = BCryptCreateHash(
+        algorithmProvider.Handle,
+        &m_hashHandle,
+        reinterpret_cast<PUCHAR>(&m_buffer[0]),
+        static_cast<ULONG>(m_buffer.size()),
+        nullptr,
+        0,
+        0);
+    if (!BCRYPT_SUCCESS(status))
+    {
+      throw std::runtime_error("BCryptCreateHash failed");
+    }
+  }
+
+  ~SHAWithBCrypt() { BCryptDestroyHash(m_hashHandle); }
+};
+
+} // namespace
+
+Azure::Security::KeyVault::SHA256::SHA256()
+    : m_portableImplementation(std::make_unique<SHAWithBCrypt>(BCRYPT_SHA256_ALGORITHM))
+{
+}
+
+Azure::Security::KeyVault::SHA384::SHA384()
+    : m_portableImplementation(std::make_unique<SHAWithBCrypt>(BCRYPT_SHA384_ALGORITHM))
+{
+}
+
+Azure::Security::KeyVault::SHA512::SHA512()
+    : m_portableImplementation(std::make_unique<SHAWithBCrypt>(BCRYPT_SHA512_ALGORITHM))
+{
+}
+#endif
