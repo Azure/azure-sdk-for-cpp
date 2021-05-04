86066c18b9e6e4023efa95238a0199f66c3aac62
Victor Vazquez <vhvb1989@gmail.com>
2021-05-04 21:00:13 +0000

    Run asserts even on Production

--- sdk/core/azure-core/inc/azure/core/azure_assert.hpp
+++ sdk/core/azure-core/inc/azure/core/azure_assert.hpp
@@ -5,14 +5,17 @@
  * @file
  * @brief Provide assert macros to use as pre-conditions.
  *
- * @remark Asserts are turned off is `NDEBUG` is defined
+ * @remark Asserts are turned ON even if `NDEBUG` is defined.
  *
  */
 
 #pragma once
 
-// uncomment next line to disable asserts
-// #define NDEBUG
+#if defined(NDEBUG)
+#define AZURE_SDK_INTERNAL_IMPLEMENTATION_OVERRIDE_NDEBUG
+#undef NDEBUG
+#endif
+
 #include <cassert>
 #include <stdexcept>
 
@@ -20,4 +23,8 @@
 #define AZURE_ASSERT_FALSE(exp) assert(!(exp))
 #define AZURE_ASSERT_MSG(exp, msg) assert(((void)msg, exp))
 #define AZURE_UNREACHABLE_CODE AZURE_ASSERT_MSG(false, "Unreachable code was access")
-#define AZURE_NOT_IMPLEMENTED throw std::runtime_error("Not implemented");
+#define AZURE_NOT_IMPLEMENTED AZURE_ASSERT_MSG(false, "Not implemented")
+
+#if defined(AZURE_SDK_INTERNAL_IMPLEMENTATION_OVERRIDE_NDEBUG)
+#define NDEBUG
+#endif
