6d63fead6d1ef0e82b49fd96f823048b963f7bbb
Victor Vazquez <vhvb1989@gmail.com>
2021-05-05 22:48:05 +0000

    wait

--- sdk/core/azure-core/inc/azure/core/azure_assert.hpp
+++ sdk/core/azure-core/inc/azure/core/azure_assert.hpp
@@ -3,7 +3,7 @@
 
 /**
  * @file
- * @brief Provide assert macros to use as pre-conditions.
+ * @brief Provide assert macros to use with pre-conditions.
  *
  * @remark Asserts are turned ON even if `NDEBUG` is defined.
  *
@@ -12,19 +12,34 @@
 #pragma once
 
 #if defined(NDEBUG)
-#define AZURE_SDK_INTERNAL_IMPLEMENTATION_OVERRIDE_NDEBUG
-#undef NDEBUG
-#endif
+
+/*
+ * NDEBUG = defined = Build is on Release
+ * Define AZURE_ASSERT to call abort directly on exp == false
+ */
+
+#define AZURE_ASSERT(exp) \
+  if (!(exp)) \
+  { \
+    std::abort(); \
+  }
+
+#define AZURE_ASSERT_MSG(exp, msg) AZURE_ASSERT(exp)
+
+#else
+
+/*
+ * NDEBUG = NOT defined = Build is on Debug
+ * Define AZURE_ASSERT to call assert to provide better debug experience.
+ */
 
 #include <cassert>
-#include <stdexcept>
 
-#define AZURE_ASSERT(exp) assert(exp)
-#define AZURE_ASSERT_FALSE(exp) assert(!(exp))
-#define AZURE_ASSERT_MSG(exp, msg) assert(((void)msg, exp))
+#define AZURE_ASSERT(exp) assert((exp))
+#define AZURE_ASSERT_MSG(exp, msg) assert(((void)msg, (exp)))
+
+#endif
+
+#define AZURE_ASSERT_FALSE(exp) AZURE_ASSERT(!(exp))
 #define AZURE_UNREACHABLE_CODE AZURE_ASSERT_MSG(false, "Unreachable code was access")
 #define AZURE_NOT_IMPLEMENTED AZURE_ASSERT_MSG(false, "Not implemented")
-
-#if defined(AZURE_SDK_INTERNAL_IMPLEMENTATION_OVERRIDE_NDEBUG)
-#define NDEBUG
-#endif
