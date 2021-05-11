b661b3ef5e1cb9df1f7cdf4d3ddf15c0d9d7a407
Ahson Khan <ahson_ahmedk@yahoo.com>
2021-05-10 17:08:59 -0700

    Testing CI and storage live tests to see if they pass

--- sdk/core/azure-core/src/context.cpp
+++ sdk/core/azure-core/src/context.cpp
@@ -11,6 +11,7 @@ Azure::DateTime Azure::Core::Context::GetDeadline() const
 {
   // Contexts form a tree. Here, we walk from a node all the way back to the root in order to find
   // the earliest deadline value.
+  // Random change.
   auto result = DateTime::max();
   for (auto ptr = m_contextSharedState; ptr; ptr = ptr->Parent)
   {
