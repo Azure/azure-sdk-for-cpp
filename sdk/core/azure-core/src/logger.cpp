3e63108819f4f07148a3a6c9a6fa566d206793d1
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2021-05-17 12:26:32 -0700

    Update one more place

--- sdk/core/azure-core/src/logger.cpp
+++ sdk/core/azure-core/src/logger.cpp
@@ -14,7 +14,8 @@ using namespace Azure::Core::Diagnostics::_internal;
 
 namespace {
 static std::shared_timed_mutex g_logListenerMutex;
-static Logger::Listener g_logListener(_detail::EnvironmentLogLevelListener::GetLogListener());
+static std::function<void(Logger::Level level, std::string const& message)> g_logListener(
+    _detail::EnvironmentLogLevelListener::GetLogListener());
 } // namespace
 
 std::atomic<bool> Log::g_isLoggingEnabled(
