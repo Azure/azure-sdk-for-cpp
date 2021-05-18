ff126f1eb7f95417b50cb3d45768d41de00526e1
Ahson Khan <ahson_ahmedk@yahoo.com>
2021-05-18 13:49:53 -0700

    Explore removing use of `int` within header files to align with guidelines. (#2276)
    Part of https://github.com/Azure/azure-sdk-for-cpp/issues/2226
    
    Trying to focus mainly on headers, depending on the conclusion, we'd want to make cpp file changes as well, but that can be done later.
    
    Assuming this is the guideline we want:
    https://azure.github.io/azure-sdk/cpp_implementation.html#integer-sizes
    
    ![image](https://user-images.githubusercontent.com/6527137/118341184-00d52580-b4d3-11eb-8e96-937cb5899c92.png)
    
    Some places that we still use `int` is for things like unix file desriptor or where APIs like BIO_read from OpenSSL returns an int. Are those exceptions OK?
    https://github.com/Azure/azure-sdk-for-cpp/blob/0b64ab3845d6850bc1666337f21640899e732ef5/sdk/core/azure-core/inc/azure/core/io/body_stream.hpp#L166
    https://github.com/Azure/azure-sdk-for-cpp/blob/308e5363adaa00ace2f06ff0f1a31747a0a28062/sdk/core/azure-core/src/base64.cpp#L87

--- sdk/keyvault/azure-security-keyvault-common/src/private/package_version.hpp
+++ sdk/keyvault/azure-security-keyvault-common/src/private/package_version.hpp
@@ -8,6 +8,8 @@
 
 #pragma once
 
+#include <cstdint>
+
 #define AZURE_SECURITY_KEYVAULT_COMMON_VERSION_MAJOR 4
 #define AZURE_SECURITY_KEYVAULT_COMMON_VERSION_MINOR 0
 #define AZURE_SECURITY_KEYVAULT_COMMON_VERSION_PATCH 0
@@ -25,13 +27,13 @@ namespace Azure { namespace Security { namespace KeyVault { namespace Common { n
   class PackageVersion final {
   public:
     /// Major numeric identifier.
-    static constexpr int Major = AZURE_SECURITY_KEYVAULT_COMMON_VERSION_MAJOR;
+    static constexpr int32_t Major = AZURE_SECURITY_KEYVAULT_COMMON_VERSION_MAJOR;
 
     /// Minor numeric identifier.
-    static constexpr int Minor = AZURE_SECURITY_KEYVAULT_COMMON_VERSION_MINOR;
+    static constexpr int32_t Minor = AZURE_SECURITY_KEYVAULT_COMMON_VERSION_MINOR;
 
     /// Patch numeric identifier.
-    static constexpr int Patch = AZURE_SECURITY_KEYVAULT_COMMON_VERSION_PATCH;
+    static constexpr int32_t Patch = AZURE_SECURITY_KEYVAULT_COMMON_VERSION_PATCH;
 
     /// Indicates whether the SDK is in a pre-release state.
     static constexpr bool IsPreRelease
