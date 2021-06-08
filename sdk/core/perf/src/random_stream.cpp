70ef82b154cfa5e6170bdebe53f2f7961d559470
Victor Vazquez <vhvb1989@gmail.com>
2021-06-08 22:56:20 +0000

    random stream

---
+++ sdk/core/perf/src/random_stream.cpp
@@ -0,0 +1,83 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#include <azure/core/platform.hpp>
+
+#include "azure/perf/random_stream.hpp"
+
+#if defined(AZ_PLATFORM_WINDOWS)
+#if !defined(WIN32_LEAN_AND_MEAN)
+#define WIN32_LEAN_AND_MEAN
+#endif
+#if !defined(NOMINMAX)
+#define NOMINMAX
+#endif
+#endif
+
+static thread_local std::mt19937_64 random_generator(std::random_device{}());
+
+namespace {
+
+static uint8_t RandomChar()
+{
+  const uint8_t charset[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  std::uniform_int_distribution<size_t> distribution(0, sizeof(charset) - 2);
+  return charset[distribution(random_generator)];
+}
+
+void RandomBuffer(uint8_t* buffer, size_t length)
+{
+  uint8_t* start_addr = buffer;
+  uint8_t* end_addr = buffer + length;
+
+  const size_t rand_int_size = sizeof(uint64_t);
+  while (uintptr_t(start_addr) % rand_int_size != 0 && start_addr < end_addr)
+  {
+    *(start_addr++) = RandomChar();
+  }
+
+  std::uniform_int_distribution<uint64_t> distribution(0ULL, std::numeric_limits<uint64_t>::max());
+  while (start_addr + rand_int_size <= end_addr)
+  {
+    *reinterpret_cast<uint64_t*>(start_addr) = distribution(random_generator);
+    start_addr += rand_int_size;
+  }
+  while (start_addr < end_addr)
+  {
+    *(start_addr++) = RandomChar();
+  }
+}
+
+} // namespace
+
+Azure::Perf::RandomStream::CircularStream::CircularStream(size_t size)
+    : m_buffer(std::make_unique<std::vector<uint8_t>>(1024 * 1024)), m_length(size),
+      m_memoryStream(*m_buffer)
+{
+  RandomBuffer(m_buffer->data(), 1024 * 1024);
+}
+
+size_t Azure::Perf::RandomStream::CircularStream::OnRead(
+    uint8_t* buffer,
+    size_t count,
+    Azure::Core::Context const& context)
+{
+  size_t available = m_length - m_totalRead;
+  if (available == 0)
+  {
+    return 0;
+  }
+
+  size_t toRead = std::min(count, available);
+  auto read = m_memoryStream.Read(buffer, toRead, context);
+
+  // Circurl implementation. Rewind the stream every time we reach the end
+  if (read == 0) // No more bytes to read from.
+  {
+    m_memoryStream.Rewind();
+    read = m_memoryStream.Read(buffer, toRead, context);
+  }
+
+  m_totalRead += read;
+  return read;
+}
---
+++ sdk/core/perf/test/src/random_stream_test.cpp
@@ -0,0 +1,44 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#include <gtest/gtest.h>
+
+#include <azure/perf/random_stream.hpp>
+
+TEST(circular_stream, basic)
+{
+  size_t const totalSize = 1024 * 1024 * 3; // should give 5 loops
+  size_t const chunk = 1024 * 1024;
+  auto r_stream = Azure::Perf::RandomStream::Create(totalSize);
+  uint8_t buffer[chunk];
+  uint8_t buffer2[chunk];
+
+  // 1st read
+  auto count = r_stream->Read(buffer, chunk, Azure::Core::Context::ApplicationContext);
+  EXPECT_EQ(count, chunk);
+
+  // 2nd read
+  count = r_stream->Read(buffer2, chunk, Azure::Core::Context::ApplicationContext);
+  EXPECT_EQ(count, chunk);
+  for (size_t i = 0; i != chunk; i++)
+  {
+    EXPECT_EQ(buffer[i], buffer2[i]);
+  }
+
+  // 3nd read
+  count = r_stream->Read(buffer, chunk, Azure::Core::Context::ApplicationContext);
+  EXPECT_EQ(count, chunk);
+  for (size_t i = 0; i != chunk; i++)
+  {
+    EXPECT_EQ(buffer[i], buffer2[i]);
+  }
+
+  // 4nd read
+  count = r_stream->Read(buffer, chunk, Azure::Core::Context::ApplicationContext);
+  EXPECT_EQ(count, 0);
+  // should not change buffer
+  for (size_t i = 0; i != chunk; i++)
+  {
+    EXPECT_EQ(buffer[i], buffer2[i]);
+  }
+}
