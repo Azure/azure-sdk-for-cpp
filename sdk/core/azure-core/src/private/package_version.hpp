03024b1a336cf2ea1bd22204b90b064e146bd687
Daniel Jurek <djurek@microsoft.com>
2021-06-18 10:59:58 -0700

    Test CI after default branch rename

--- sdk/core/azure-core/src/private/package_version.hpp
+++ sdk/core/azure-core/src/private/package_version.hpp
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
-
+// Do not merge. Validating PR behavior.
 /**
  * @file
  * @brief Provides version information.
