e2fba127ded52e30c9597e61eb7d01907fc52748
Jinming Hu <jinmhu@microsoft.com>
2021-07-01 14:38:22 +0800

    Add queue service protocol layer

---
+++ sdk/storage/azure-storage-queues/CHANGELOG.md
@@ -0,0 +1,7 @@
+# Release History
+
+## 12.0.0-beta.1 (Unreleased)
+
+### New Features
+
+- Nothing yet.
---
+++ sdk/storage/azure-storage-queues/sample/queue_getting_started.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#include <iostream>
+
+#include <azure/storage/queues.hpp>
+
+#include "samples_common.hpp"
+
+SAMPLE(QueuesGettingStarted, QueuesGettingStarted)
+void QueuesGettingStarted()
+{
+}
