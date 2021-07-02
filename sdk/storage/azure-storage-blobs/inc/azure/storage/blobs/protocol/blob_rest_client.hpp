32328eec7501e87ca9183b504c1782956889bb83
Jinming Hu <jinmhu@microsoft.com>
2021-07-02 13:38:28 +0800

    remove unused headers from blob protocol layer

--- sdk/storage/azure-storage-blobs/inc/azure/storage/blobs/protocol/blob_rest_client.hpp
+++ sdk/storage/azure-storage-blobs/inc/azure/storage/blobs/protocol/blob_rest_client.hpp
@@ -3,11 +3,8 @@
 
 #pragma once
 
-#include <cstring>
-#include <limits>
-#include <map>
-#include <set>
-#include <stdexcept>
+#include <chrono>
+#include <memory>
 #include <string>
 #include <type_traits>
 #include <vector>
@@ -17,7 +14,6 @@
 #include <azure/core/etag.hpp>
 #include <azure/core/http/http.hpp>
 #include <azure/core/internal/http/pipeline.hpp>
-#include <azure/core/internal/strings.hpp>
 #include <azure/core/nullable.hpp>
 #include <azure/core/response.hpp>
 #include <azure/storage/common/crypt.hpp>
