3fc96e21bf51f737f3d8909b29cbf89d19abd4cf
Victor Vazquez <vhvb1989@gmail.com>
2021-07-09 22:47:33 +0000

    rename header

--- sdk/keyvault/azure-security-keyvault-keys/inc/azure/keyvault/key_vault_keys.hpp
+++ sdk/keyvault/azure-security-keyvault-keys/inc/azure/keyvault/key_vault_keys.hpp
@@ -32,5 +32,5 @@
 #include "azure/keyvault/keys/key_properties.hpp"
 #include "azure/keyvault/keys/key_type.hpp"
 #include "azure/keyvault/keys/key_vault_key.hpp"
-#include "azure/keyvault/keys/list_keys_result.hpp"
+#include "azure/keyvault/keys/list_keys_responses.hpp"
 #include "azure/keyvault/keys/recover_deleted_key_operation.hpp"
