efbb9935668daae66939d90971328754edc95bf5
George Arama <50641385+gearama@users.noreply.github.com>
2021-07-12 16:20:00 -0700

    first pass

---
+++ sdk/keyvault/azure-security-keyvault-secrets/src/private/package_version.hpp
@@ -0,0 +1,60 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+/**
+ * @file
+ * @brief Provides version information.
+ */
+
+#pragma once
+
+#define AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_MAJOR 1
+#define AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_MINOR 0
+#define AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_PATCH 0
+#define AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_PRERELEASE "1"
+
+#define AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_ITOA_HELPER(i) #i
+#define AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_ITOA(i) \
+  AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_ITOA_HELPER(i)
+
+namespace Azure { namespace Template { namespace _detail {
+  /**
+   * @brief Provides version information.
+   */
+  class PackageVersion final {
+  public:
+    /// Major numeric identifier.
+    static constexpr int Major = AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_MAJOR;
+
+    /// Minor numeric identifier.
+    static constexpr int Minor = AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_MINOR;
+
+    /// Patch numeric identifier.
+    static constexpr int Patch = AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_PATCH;
+
+    /// Indicates whether the SDK is in a pre-release state.
+    static constexpr bool IsPreRelease
+        = sizeof(AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_PRERELEASE) != sizeof("");
+
+    /**
+     * @brief The version in string format used for telemetry following the `semver.org` standard
+     * (https://semver.org).
+     */
+    static constexpr const char* ToString()
+    {
+      return IsPreRelease
+          ? AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_ITOA(AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_MAJOR) "." AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_ITOA(
+              AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_MINOR) "." AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_ITOA(AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_PATCH) "-" AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_PRERELEASE
+          : AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_ITOA(AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_MAJOR) "." AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_ITOA(
+              AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_MINOR) "." AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_ITOA(AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_PATCH);
+    }
+  };
+}}} // namespace Azure::Template::_detail
+
+#undef AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_ITOA_HELPER
+#undef AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_ITOA
+
+#undef AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_MAJOR
+#undef AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_MINOR
+#undef AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_PATCH
+#undef AZURE_SECURITY_KEYVAULT_SECRETS_VERSION_PRERELEASE
---
+++ sdk/keyvault/azure-security-keyvault-secrets/src/secret_client.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#include "azure/keyvault/secrets/secret_client.hpp"
+
+#include "private/package_version.hpp"
+
+#include <string>
+
+using namespace Azure::Template;
+using namespace Azure::Template::_detail;
+
+std::string TemplateClient::ClientVersion() const { return PackageVersion::ToString(); }
