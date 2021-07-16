175ea60d79f8880d5c68773e687b220c3d6084b2
Ahson Khan <ahkha@microsoft.com>
2021-07-15 17:04:49 -0700

    Fix clang formatting.

--- sdk/keyvault/azure-security-keyvault-keys/src/key_client_paged_responses.cpp
+++ sdk/keyvault/azure-security-keyvault-keys/src/key_client_paged_responses.cpp
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
 
-#include "azure/keyvault/keys/key_client.hpp"
 #include "azure/keyvault/keys/key_client_paged_responses.hpp"
+#include "azure/keyvault/keys/key_client.hpp"
 #include "private/key_constants.hpp"
 #include "private/key_serializers.hpp"
 
