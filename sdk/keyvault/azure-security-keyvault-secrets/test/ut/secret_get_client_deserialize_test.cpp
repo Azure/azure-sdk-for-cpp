a3b10775ff649649be5e5a4868392b1a2ce9a1e7
George Arama <50641385+gearama@users.noreply.github.com>
2021-07-19 20:37:58 -0700

    more tests

--- sdk/keyvault/azure-security-keyvault-secrets/test/ut/secret_get_client_deserialize_test.cpp
+++ sdk/keyvault/azure-security-keyvault-secrets/test/ut/secret_get_client_deserialize_test.cpp
@@ -2,19 +2,19 @@
 // SPDX-License-Identifier: MIT
 
 #include "../src/private/secret_serializers.hpp"
-#include <gtest/gtest.h>
-#include <azure/core/http/policies/policy.hpp>
 #include <azure/core/http/http.hpp>
-#include <azure/keyvault/secrets/secret_client.hpp>
+#include <azure/core/http/policies/policy.hpp>
 #include <azure/keyvault/secrets/keyvault_secret.hpp>
+#include <azure/keyvault/secrets/secret_client.hpp>
+#include <gtest/gtest.h>
 #include <string>
 
 using namespace Azure::Security::KeyVault::Secrets;
 using namespace Azure::Security::KeyVault::Secrets::_detail;
 using namespace Azure::Core::Http::_internal;
 
-
-TEST(SecretClient, GetClientPartial)
+namespace {
+Azure::Core::Http::RawResponse getPartialResponse()
 {
   auto response = Azure::Core::Http::RawResponse(1, 1, Azure::Core::Http::HttpStatusCode::Ok, "OK");
 
@@ -37,20 +37,11 @@ TEST(SecretClient, GetClientPartial)
   response.SetBodyStream(
       std::make_unique<Azure::Core::IO::MemoryBodyStream>(responseBody, sizeof(responseBody) - 1));
 
-  KeyVaultSecret secret = _detail::KeyVaultSecretSerializer::KeyVaultSecretDeserialize(response);
-  EXPECT_EQ(secret.Value, "mysecretvalue");
-  EXPECT_EQ(
-      secret.Id,
-      "https://myvault.vault.azure.net/secrets/mysecretname/4387e9f3d6e14c459867679a90fd0f79");
-  EXPECT_EQ(secret.KeyId.HasValue(), false);
-  EXPECT_EQ(secret.Properties.Name.HasValue(), false);
-  EXPECT_EQ(secret.Properties.Enabled.Value(), true);
-  EXPECT_EQ(secret.Managed, false);
-  EXPECT_EQ(secret.Properties.UpdatedOn.HasValue(), true);
-  EXPECT_EQ(secret.Properties.CreatedOn.HasValue(), true);
+  return response;
 }
 
-TEST(SecretClient, GetClientFull)
+Azure::Core::Http::RawResponse getFullResponse()
+
 {
   auto response = Azure::Core::Http::RawResponse(1, 1, Azure::Core::Http::HttpStatusCode::Ok, "OK");
 
@@ -76,16 +67,93 @@ TEST(SecretClient, GetClientFull)
   response.SetBodyStream(
       std::make_unique<Azure::Core::IO::MemoryBodyStream>(responseBody, sizeof(responseBody) - 1));
 
-  KeyVaultSecret secret = _detail::KeyVaultSecretSerializer::KeyVaultSecretDeserialize(response);
+  return response;
+}
+
+void runPartialExpect(KeyVaultSecret& secret) {
   EXPECT_EQ(secret.Value, "mysecretvalue");
   EXPECT_EQ(
       secret.Id,
       "https://myvault.vault.azure.net/secrets/mysecretname/4387e9f3d6e14c459867679a90fd0f79");
-  EXPECT_EQ(secret.Properties.Name.HasValue(), false);
+  EXPECT_EQ(secret.KeyId.HasValue(), false);
+  EXPECT_EQ(secret.Properties.Enabled.Value(), true);
+  EXPECT_EQ(secret.Managed, false);
+  EXPECT_EQ(secret.Properties.UpdatedOn.HasValue(), true);
+  EXPECT_EQ(secret.Properties.CreatedOn.HasValue(), true);
+}
+
+void runFullExpect(KeyVaultSecret& secret) {
+  EXPECT_EQ(secret.Value, "mysecretvalue");
+  EXPECT_EQ(
+      secret.Id,
+      "https://myvault.vault.azure.net/secrets/mysecretname/4387e9f3d6e14c459867679a90fd0f79");
   EXPECT_EQ(secret.Properties.Enabled.Value(), true);
   EXPECT_EQ(secret.Managed, true);
   EXPECT_EQ(secret.ContentType.Value(), "ct");
   EXPECT_EQ(secret.KeyId.Value(), "kid");
   EXPECT_EQ(secret.Properties.UpdatedOn.HasValue(), true);
   EXPECT_EQ(secret.Properties.CreatedOn.HasValue(), true);
+}
+
+} // namespace
+TEST(SecretClient, GetClientDeserializePartial1)
+{
+  auto response = getPartialResponse();
+
+  KeyVaultSecret secret = _detail::KeyVaultSecretSerializer::KeyVaultSecretDeserialize(response);
+  EXPECT_EQ(secret.Properties.Name.HasValue(), false);
+  runPartialExpect(secret);
+}
+
+TEST(SecretClient, GetClientDeserializePartial2)
+{
+  auto response = getPartialResponse();
+
+  KeyVaultSecret secret
+      = _detail::KeyVaultSecretSerializer::KeyVaultSecretDeserialize("name1", response);
+
+  EXPECT_EQ(secret.Properties.Name.Value(), "name1");
+  runPartialExpect(secret);
+}
+
+TEST(SecretClient, GetClientDeserializePartial3)
+{
+  auto response = getPartialResponse();
+
+  KeyVaultSecret secret = KeyVaultSecret("name2");
+  _detail::KeyVaultSecretSerializer::KeyVaultSecretDeserialize(secret, response);
+
+  EXPECT_EQ(secret.Properties.Name.Value(), "name2");
+  runPartialExpect(secret);
+}
+
+TEST(SecretClient, GetClientdeserializeFull1)
+{
+  auto response = getFullResponse();
+
+  KeyVaultSecret secret = _detail::KeyVaultSecretSerializer::KeyVaultSecretDeserialize(response);
+  EXPECT_EQ(secret.Properties.Name.HasValue(), false);
+  runFullExpect(secret);
+}
+
+TEST(SecretClient, GetClientdeserializeFull2)
+{
+  auto response = getFullResponse();
+
+  KeyVaultSecret secret
+      = _detail::KeyVaultSecretSerializer::KeyVaultSecretDeserialize("name1", response);
+
+  EXPECT_EQ(secret.Properties.Name.Value(), "name1");
+  runFullExpect(secret);
+}
+
+TEST(SecretClient, GetClientdeserializeFull3)
+{
+  auto response = getFullResponse();
+
+  KeyVaultSecret secret = KeyVaultSecret("name2");
+  _detail::KeyVaultSecretSerializer::KeyVaultSecretDeserialize(secret, response);
+
+  EXPECT_EQ(secret.Properties.Name.Value(), "name2");
+  runFullExpect(secret);
 }
