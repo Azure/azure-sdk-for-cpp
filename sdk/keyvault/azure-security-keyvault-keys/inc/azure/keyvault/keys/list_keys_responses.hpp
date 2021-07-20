497d727eb8e1130e48d658bae592b967d217eec3
Victor Vazquez <vhvb1989@gmail.com>
2021-07-19 22:51:55 -0700

    fix - copy token on move-construct pagedResponse

--- sdk/keyvault/azure-security-keyvault-keys/inc/azure/keyvault/keys/list_keys_responses.hpp
+++ sdk/keyvault/azure-security-keyvault-keys/inc/azure/keyvault/keys/list_keys_responses.hpp
@@ -54,7 +54,8 @@ namespace Azure { namespace Security { namespace KeyVault { namespace Keys {
         std::unique_ptr<Azure::Core::Http::RawResponse> rawResponse,
         std::shared_ptr<KeyClient> keyClient,
         std::string const& keyName = std::string())
-        : m_keyName(keyName), m_keyClient(keyClient), Items(std::move(keyProperties.Items))
+        : PagedResponse(std::move(keyProperties)), m_keyName(keyName),
+          m_keyClient(keyClient), Items(std::move(keyProperties.Items))
     {
       RawResponse = std::move(rawResponse);
     }
@@ -101,7 +102,8 @@ namespace Azure { namespace Security { namespace KeyVault { namespace Keys {
         DeletedKeyPagedResponse&& deletedKeyProperties,
         std::unique_ptr<Azure::Core::Http::RawResponse> rawResponse,
         std::shared_ptr<KeyClient> keyClient)
-        : m_keyClient(keyClient), Items(std::move(deletedKeyProperties.Items))
+        : PagedResponse(std::move(deletedKeyProperties)), m_keyClient(keyClient),
+          Items(std::move(deletedKeyProperties.Items))
     {
       RawResponse = std::move(rawResponse);
     }
