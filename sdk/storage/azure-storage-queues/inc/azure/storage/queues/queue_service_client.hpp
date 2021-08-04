063899c837174b794e7f806cef69ed64e26b3ffa
Jinming Hu <jinmhu@microsoft.com>
2021-08-04 14:05:00 +0800

    clang-format

--- sdk/storage/azure-storage-queues/inc/azure/storage/queues/queue_service_client.hpp
+++ sdk/storage/azure-storage-queues/inc/azure/storage/queues/queue_service_client.hpp
@@ -21,7 +21,6 @@ namespace Azure { namespace Storage { namespace Queues {
    */
   class QueueServiceClient final {
   public:
-
     /**
      * @brief Initializes a new instance of QueueServiceClient.
      *
@@ -37,7 +36,7 @@ namespace Azure { namespace Storage { namespace Queues {
 
     /**
      * @brief Initializes a new instance of QueueServiceClient.
-     * 
+     *
      * @param serviceUrl A URL referencing the queue that includes the name of the account.
      * @param credential The shared key credential used to sign requests.
      * @param options Optional client options that define the transport pipeline policies for
@@ -50,7 +49,7 @@ namespace Azure { namespace Storage { namespace Queues {
 
     /**
      * @brief Initializes a new instance of QueueServiceClient.
-     * 
+     *
      * @param serviceUrl A URL referencing the queue that includes the name of the account.
      * @param credential The token credential used to sign requests.
      * @param options Optional client options that define the transport pipeline policies for
@@ -63,7 +62,7 @@ namespace Azure { namespace Storage { namespace Queues {
 
     /**
      * @brief Initializes a new instance of QueueServiceClient.
-     * 
+     *
      * @param serviceUrl A URL referencing the queue that includes the name of the account.
      * @param options Optional client options that define the transport pipeline policies for
      * authentication, retries, etc., that are applied to every request.
@@ -75,7 +74,7 @@ namespace Azure { namespace Storage { namespace Queues {
     /**
      * @brief Creates a new QueueClient object with the same URL as this QueueServiceClient.
      * The new QueueClient uses the same request policy pipeline as this QueueServiceClient.
-     * 
+     *
      * @param queueName The name of the queue to reference.
      * @return A new QueueClient instance.
      */
@@ -83,7 +82,7 @@ namespace Azure { namespace Storage { namespace Queues {
 
     /**
      * @brief Gets the queue service's primary URL endpoint..
-     * 
+     *
      * @return The queue service's primary URL endpoint.
      */
     std::string GetUrl() const { return m_serviceUrl.GetAbsoluteUrl(); }
@@ -92,7 +91,7 @@ namespace Azure { namespace Storage { namespace Queues {
      * @brief Returns a sequence of queues in the storage account. Enumerating the queues may make
      * multiple requests to the service while fetching all the values. Queue names are returned in
      * lexicographic order.
-     * 
+     *
      * @param options Optional parameters to execute this function.
      * @param context Context for cancelling long running operations.
      * @return A ListQueuesPagedResponse describing the queues in the storage account.
@@ -103,7 +102,7 @@ namespace Azure { namespace Storage { namespace Queues {
 
     /**
      * @brief Sets the properties of the queue service.
-     * 
+     *
      * @param properties The queue service properties.
      * @param options Optional parameters to execute this function.
      * @param context Context for cancelling long running operations.
@@ -116,7 +115,7 @@ namespace Azure { namespace Storage { namespace Queues {
 
     /**
      * @brief Gets the properties of a storage account's queue service.
-     * 
+     *
      * @param options Optional parameters to execute this function.
      * @param context Context for cancelling long running operations.
      * @return A QueueServiceProperties describing the service properties.
@@ -129,7 +128,7 @@ namespace Azure { namespace Storage { namespace Queues {
      * @brief Retrieves statistics related to replication for the Queue service. It is only
      * available on the secondary location endpoint when read-access geo-redundant replication is
      * enabled for the storage account.
-     * 
+     *
      * @param options Optional parameters to execute this function.
      * @param context Context for cancelling long running operations.
      * @return A ServiceStatistics describing the service replication statistics.
@@ -156,7 +155,7 @@ namespace Azure { namespace Storage { namespace Queues {
 
     /**
      * @brief Marks the specified queue for deletion.
-     * 
+     *
      * @param queueName The name of the queue to delete.
      * @param options Optional parameters to execute this function.
      * @param context Context for cancelling long running operations.
