4b426a16a2dba51338572a19c0763edc7ef09d48
Jinming Hu <jinmhu@microsoft.com>
2021-08-04 14:03:49 +0800

    add some doc

--- sdk/storage/azure-storage-queues/inc/azure/storage/queues/queue_client.hpp
+++ sdk/storage/azure-storage-queues/inc/azure/storage/queues/queue_client.hpp
@@ -13,84 +13,247 @@
 
 namespace Azure { namespace Storage { namespace Queues {
 
+  /**
+   * The QueueClient allows you to manipulate Azure Storage queues and their messages.
+   */
   class QueueClient final {
   public:
+    /**
+     * @brief Initializes a new instance of QueueClient.
+     *
+     * @param connectionString A connection string includes the authentication information required
+     * for your application to access data in an Azure Storage account at runtime.
+     * @param queueName The name of the queue.
+     * @param options Optional client options that define the transport pipeline policies for
+     * authentication, retries, etc., that are applied to every request.
+     * @return A new QueueClient instance.
+     */
     static QueueClient CreateFromConnectionString(
         const std::string& connectionString,
         const std::string& queueName,
         const QueueClientOptions& options = QueueClientOptions());
 
+    /**
+     * @brief Initializes a new instance of QueueClient.
+     *
+     * @param queueUrl A url referencing the queue that includes the name of the account and the
+     * name of the queue.
+     * @param credential The shared key credential used to sign requests.
+     * @param options Optional client options that define the transport pipeline policies for
+     * authentication, retries, etc., that are applied to every request.
+     */
     explicit QueueClient(
         const std::string& queueUrl,
         std::shared_ptr<StorageSharedKeyCredential> credential,
         const QueueClientOptions& options = QueueClientOptions());
 
+    /**
+     * @brief Initializes a new instance of QueueClient.
+     *
+     * @param queueUrl A url referencing the queue that includes the name of the account and the
+     * name of the queue.
+     * @param credential The token credential used to sign requests.
+     * @param options Optional client options that define the transport pipeline policies for
+     * authentication, retries, etc., that are applied to every request.
+     */
     explicit QueueClient(
         const std::string& queueUrl,
         std::shared_ptr<Core::Credentials::TokenCredential> credential,
         const QueueClientOptions& options = QueueClientOptions());
 
+    /**
+     * @brief Initializes a new instance of QueueClient.
+     *
+     * @param queueUrl A url referencing the queue that includes the name of the account and the
+     * name of the queue.
+     * @param options Optional client options that define the transport pipeline policies for
+     * authentication, retries, etc., that are applied to every request.
+     */
     explicit QueueClient(
         const std::string& queueUrl,
         const QueueClientOptions& options = QueueClientOptions());
 
+    /**
+     * @brief Gets the queue's primary URL endpoint.
+     *
+     * @return The queue's primary URL endpoint.
+     */
     std::string GetUrl() const { return m_queueUrl.GetAbsoluteUrl(); }
 
+    /**
+     * @brief Creates a new queue under the specified account. If the queue with the same name
+     * already exists and the metadata is identical to the one of the existing queue, the operation
+     * is successful and the value of CreateQueueResult.Created is false. If the metadata doesn't
+     * match the one of the existing queue, the operation fails.
+     *
+     * @param options Optional parameters to execute this function.
+     * @param context Context for cancelling long running operations.
+     * @return A CreateQueueResult describing the newly created blob container.
+     */
     Azure::Response<Models::CreateQueueResult> Create(
         const CreateQueueOptions& options = CreateQueueOptions(),
         const Azure::Core::Context& context = Azure::Core::Context()) const;
 
+    /**
+     * @brief Creates a new queue under the specified account. If the queue with the same name
+     * already exists, it is not changed.
+     *
+     * @param options Optional parameters to execute this function.
+     * @param context Context for cancelling long running operations.
+     * @return A CreateQueueResult describing the newly created queue if the queue doesn't exist.
+     * CreateQueueResult.Created is false if the queue already exists.
+     */
     Azure::Response<Models::CreateQueueResult> CreateIfNotExists(
         const CreateQueueOptions& options = CreateQueueOptions(),
         const Azure::Core::Context& context = Azure::Core::Context()) const;
 
+    /**
+     * @brief Marks the specified queue for deletion.
+     *
+     * @param options Optional parameters to execute this function.
+     * @param context Context for cancelling long running operations.
+     * @return A DeleteQueueResult if successful.
+     */
     Azure::Response<Models::DeleteQueueResult> Delete(
         const DeleteQueueOptions& options = DeleteQueueOptions(),
         const Azure::Core::Context& context = Azure::Core::Context()) const;
 
+    /**
+     * @brief Marks the specified queue for deletion if it exists.
+     *
+     * @param options Optional parameters to execute this function.
+     * @param context Context for cancelling long running operations.
+     * @return A DeleteQueueResult if successful.
+     */
     Azure::Response<Models::DeleteQueueResult> DeleteIfExists(
         const DeleteQueueOptions& options = DeleteQueueOptions(),
         const Azure::Core::Context& context = Azure::Core::Context()) const;
 
+    /**
+     * @brief Returns all user-defined metadata and system properties for the specified queue.
+     *
+     * @param options Optional parameters to execute this function.
+     * @param context Context for cancelling long running operations.
+     * @return A QueueProperties describing the queue and its properties.
+     */
     Azure::Response<Models::QueueProperties> GetProperties(
         const GetQueuePropertiesOptions& options = GetQueuePropertiesOptions(),
         const Azure::Core::Context& context = Azure::Core::Context()) const;
 
+    /**
+     * @brief Sets one or more user-defined name-value pairs for the specified queue.
+     *
+     * @param metadata Custom metadata to set for this queue.
+     * @param options Optional parameters to execute this function.
+     * @param context Context for cancelling long running operations.
+     * @return A SetQueueMetadataResult if successful.
+     */
     Azure::Response<Models::SetQueueMetadataResult> SetMetadata(
         Metadata metadata,
         const SetQueueMetadataOptions& options = SetQueueMetadataOptions(),
         const Azure::Core::Context& context = Azure::Core::Context()) const;
 
+    /**
+     * @brief Gets details about any stored access policies specified on the queue that may be used
+     * with SAS.
+     *
+     * @param options Optional parameters to execute this function.
+     * @param context Context for cancelling long running operations.
+     * @return A QueueAccessPolicy describing the queue's access policy.
+     */
     Azure::Response<Models::QueueAccessPolicy> GetAccessPolicy(
         const GetQueueAccessPolicyOptions& options = GetQueueAccessPolicyOptions(),
         const Azure::Core::Context& context = Azure::Core::Context()) const;
 
+    /**
+     * @brief Sets stored access policies for the queue that may be used with SAS.
+     *
+     * @param signedIdentifiers Stored access policies that can be used to provide fine grained
+     * control over queue permissions.
+     * @param options Optional parameters to execute this function.
+     * @param context Context for cancelling long running operations.
+     * @return A SetQueueAccessPolicyResult describing the updated queue.
+     */
     Azure::Response<Models::SetQueueAccessPolicyResult> SetAccessPolicy(
         std::vector<Models::SignedIdentifier> signedIdentifiers,
         const SetQueueAccessPolicyOptions& options = SetQueueAccessPolicyOptions(),
         const Azure::Core::Context& context = Azure::Core::Context()) const;
 
+    /**
+     * @brief Adds a new message to the back of a queue. The visibility timeout specifies how long
+     * the message should be invisible to dequeue and peek operations.
+     *
+     * @param messageText Message in plain text.
+     * @param options Optional parameters to execute this function.
+     * @param context Context for cancelling long running operations.
+     * @return A SendMessageResult describing the result.
+     */
     Azure::Response<Models::SendMessageResult> SendMessage(
         std::string messageText,
         const SendMessageOptions& options = SendMessageOptions(),
         const Azure::Core::Context& context = Azure::Core::Context()) const;
 
+    /**
+     * @brief Receives one message from the front of the queue. Throws if there's no message
+     * available.
+     *
+     * @param options Optional parameters to execute this function.
+     * @param context Context for cancelling long running operations.
+     * @return A queue message.
+     */
     Azure::Response<Models::QueueMessage> ReceiveMessage(
         const ReceiveMessageOptions& options = ReceiveMessageOptions(),
         const Azure::Core::Context& context = Azure::Core::Context()) const;
 
+    /**
+     * @brief Receives one or more messages from the front of the queue. Returns empty collection if
+     * there's not message available.
+     *
+     * @param options Optional parameters to execute this function.
+     * @param context Context for cancelling long running operations.
+     * @return A vector of queue messages.
+     */
     Azure::Response<std::vector<Models::QueueMessage>> ReceiveMessages(
         const ReceiveMessagesOptions& options = ReceiveMessagesOptions(),
         const Azure::Core::Context& context = Azure::Core::Context()) const;
 
+    /**
+     * @brief Retrieves one message from the front of the queue but does not alter the visibility of
+     * the message. Throws if there's no message available.
+     *
+     * @param options Optional parameters to execute this function.
+     * @param context Context for cancelling long running operations.
+     * @return A peeked queue message.
+     */
     Azure::Response<Models::PeekedQueueMessage> PeekMessage(
         const PeekMessageOptions& options = PeekMessageOptions(),
         const Azure::Core::Context& context = Azure::Core::Context()) const;
 
+    /**
+     * @brief Retrieves one or more messages from the front of the queue but does not alter the
+     * visibility of the message. Returns empty collection if there's not message available.
+     *
+     * @param options Optional parameters to execute this function.
+     * @param context Context for cancelling long running operations.
+     * @return A vector of queue messages.
+     */
     Azure::Response<std::vector<Models::PeekedQueueMessage>> PeekMessages(
         const PeekMessagesOptions& options = PeekMessagesOptions(),
         const Azure::Core::Context& context = Azure::Core::Context()) const;
 
+    /**
+     * @brief Changes a message's visibility timeout and/or contents.
+     *
+     * @param messageId ID of the message to update.
+     * @param popReceipt Specifies the valid pop receipt value returned from an earlier call.
+     * @param visibilityTimeout Specifies the new visibility timeout value, in seconds, relative to
+     * server time. The new value must be larger than or equal to 0, and cannot be larger than 7
+     * days. The visibility timeout of a message cannot be set to a value later than the expiry
+     * time. A message can be updated until it has been deleted or has expired.
+     * @param options Optional parameters to execute this function.
+     * @param context Context for cancelling long running operations.
+     * @return An UpdateMessageResult describing the updated message.
+     */
     Azure::Response<Models::UpdateMessageResult> UpdateMessage(
         const std::string& messageId,
         const std::string& popReceipt,
@@ -98,12 +261,28 @@ namespace Azure { namespace Storage { namespace Queues {
         const UpdateMessageOptions& options = UpdateMessageOptions(),
         const Azure::Core::Context& context = Azure::Core::Context()) const;
 
+    /**
+     * @brief Permanently removes the specified message from the queue.
+     *
+     * @param messageId ID of the message to delete.
+     * @param popReceipt A valid pop receipt value returned from an earlier call.
+     * @param options Optional parameters to execute this function.
+     * @param context Context for cancelling long running operations.
+     * @return A DeleteMessageResult describing the result.
+     */
     Azure::Response<Models::DeleteMessageResult> DeleteMessage(
         const std::string& messageId,
         const std::string& popReceipt,
         const DeleteMessageOptions& options = DeleteMessageOptions(),
         const Azure::Core::Context& context = Azure::Core::Context()) const;
 
+    /**
+     * @brief Deletes all messages from the queue.
+     *
+     * @param options Optional parameters to execute this function.
+     * @param context Context for cancelling long running operations.
+     * @return A ClearMessagesResult describing the result.
+     */
     Azure::Response<Models::ClearMessagesResult> ClearMessages(
         const ClearMessagesOptions& options = ClearMessagesOptions(),
         const Azure::Core::Context& context = Azure::Core::Context()) const;
--- sdk/storage/azure-storage-queues/inc/azure/storage/queues/queue_service_client.hpp
+++ sdk/storage/azure-storage-queues/inc/azure/storage/queues/queue_service_client.hpp
@@ -15,52 +15,153 @@
 
 namespace Azure { namespace Storage { namespace Queues {
 
+  /**
+   * The QueueServiceClient allows you to manipulate Azure Storage service resources and queues. The
+   * storage account provides the top-level namespace for the Queue service.
+   */
   class QueueServiceClient final {
   public:
+
+    /**
+     * @brief Initializes a new instance of QueueServiceClient.
+     *
+     * @param connectionString A connection string includes the authentication information required
+     * for your application to access data in an Azure Storage account at runtime.
+     * @param options Optional client options that define the transport pipeline policies for
+     * authentication, retries, etc., that are applied to every request.
+     * @return A new QueueServiceClient instance.
+     */
     static QueueServiceClient CreateFromConnectionString(
         const std::string& connectionString,
         const QueueClientOptions& options = QueueClientOptions());
 
+    /**
+     * @brief Initializes a new instance of QueueServiceClient.
+     * 
+     * @param serviceUrl A URL referencing the queue that includes the name of the account.
+     * @param credential The shared key credential used to sign requests.
+     * @param options Optional client options that define the transport pipeline policies for
+     * authentication, retries, etc., that are applied to every request.
+     */
     explicit QueueServiceClient(
         const std::string& serviceUrl,
         std::shared_ptr<StorageSharedKeyCredential> credential,
         const QueueClientOptions& options = QueueClientOptions());
 
+    /**
+     * @brief Initializes a new instance of QueueServiceClient.
+     * 
+     * @param serviceUrl A URL referencing the queue that includes the name of the account.
+     * @param credential The token credential used to sign requests.
+     * @param options Optional client options that define the transport pipeline policies for
+     * authentication, retries, etc., that are applied to every request.
+     */
     explicit QueueServiceClient(
         const std::string& serviceUrl,
         std::shared_ptr<Core::Credentials::TokenCredential> credential,
         const QueueClientOptions& options = QueueClientOptions());
 
+    /**
+     * @brief Initializes a new instance of QueueServiceClient.
+     * 
+     * @param serviceUrl A URL referencing the queue that includes the name of the account.
+     * @param options Optional client options that define the transport pipeline policies for
+     * authentication, retries, etc., that are applied to every request.
+     */
     explicit QueueServiceClient(
         const std::string& serviceUrl,
         const QueueClientOptions& options = QueueClientOptions());
 
+    /**
+     * @brief Creates a new QueueClient object with the same URL as this QueueServiceClient.
+     * The new QueueClient uses the same request policy pipeline as this QueueServiceClient.
+     * 
+     * @param queueName The name of the queue to reference.
+     * @return A new QueueClient instance.
+     */
     QueueClient GetQueueClient(const std::string& queueName) const;
 
+    /**
+     * @brief Gets the queue service's primary URL endpoint..
+     * 
+     * @return The queue service's primary URL endpoint.
+     */
     std::string GetUrl() const { return m_serviceUrl.GetAbsoluteUrl(); }
 
+    /**
+     * @brief Returns a sequence of queues in the storage account. Enumerating the queues may make
+     * multiple requests to the service while fetching all the values. Queue names are returned in
+     * lexicographic order.
+     * 
+     * @param options Optional parameters to execute this function.
+     * @param context Context for cancelling long running operations.
+     * @return A ListQueuesPagedResponse describing the queues in the storage account.
+     */
     ListQueuesPagedResponse ListQueues(
         const ListQueuesOptions& options = ListQueuesOptions(),
         const Azure::Core::Context& context = Azure::Core::Context()) const;
 
+    /**
+     * @brief Sets the properties of the queue service.
+     * 
+     * @param properties The queue service properties.
+     * @param options Optional parameters to execute this function.
+     * @param context Context for cancelling long running operations.
+     * @return A SetServicePropertiesResult on successfully setting the properties.
+     */
     Azure::Response<Models::SetServicePropertiesResult> SetProperties(
         Models::QueueServiceProperties properties,
         const SetServicePropertiesOptions& options = SetServicePropertiesOptions(),
         const Azure::Core::Context& context = Azure::Core::Context()) const;
 
+    /**
+     * @brief Gets the properties of a storage account's queue service.
+     * 
+     * @param options Optional parameters to execute this function.
+     * @param context Context for cancelling long running operations.
+     * @return A QueueServiceProperties describing the service properties.
+     */
     Azure::Response<Models::QueueServiceProperties> GetProperties(
         const GetServicePropertiesOptions& options = GetServicePropertiesOptions(),
         const Azure::Core::Context& context = Azure::Core::Context()) const;
 
+    /**
+     * @brief Retrieves statistics related to replication for the Queue service. It is only
+     * available on the secondary location endpoint when read-access geo-redundant replication is
+     * enabled for the storage account.
+     * 
+     * @param options Optional parameters to execute this function.
+     * @param context Context for cancelling long running operations.
+     * @return A ServiceStatistics describing the service replication statistics.
+     */
     Azure::Response<Models::ServiceStatistics> GetStatistics(
         const GetQueueServiceStatisticsOptions& options = GetQueueServiceStatisticsOptions(),
         const Azure::Core::Context& context = Azure::Core::Context()) const;
 
+    /**
+     * @brief Creates a new queue under the specified account. If the queue with the same name
+     * already exists and the metadata is identical to the one of the existing queue, the operation
+     * is successful. If the metadata doesn't match the one of the existing queue, the operation
+     * fails.
+     *
+     * @param queueName The name of the queue to create.
+     * @param options Optional parameters to execute this function.
+     * @param context Context for cancelling long running operations.
+     * @return A QueueClient referencing the newly created container.
+     */
     Azure::Response<QueueClient> CreateQueue(
         const std::string& queueName,
         const CreateQueueOptions& options = CreateQueueOptions(),
         const Azure::Core::Context& context = Azure::Core::Context()) const;
 
+    /**
+     * @brief Marks the specified queue for deletion.
+     * 
+     * @param queueName The name of the queue to delete.
+     * @param options Optional parameters to execute this function.
+     * @param context Context for cancelling long running operations.
+     * @return A DeleteQueueResult if successful.
+     */
     Azure::Response<Models::DeleteQueueResult> DeleteQueue(
         const std::string& queueName,
         const DeleteQueueOptions& options = DeleteQueueOptions(),
