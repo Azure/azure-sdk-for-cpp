67c5be711e577006e907bb0ab25cb5b6833478bd
George Arama <50641385+gearama@users.noreply.github.com>
2021-08-06 12:28:34 -0700

    file format

--- sdk/keyvault/azure-security-keyvault-secrets/test/sample/test_app.cpp
+++ sdk/keyvault/azure-security-keyvault-secrets/test/sample/test_app.cpp
@@ -4,43 +4,43 @@
 #define _CRT_SECURE_NO_WARNINGS
 #endif
 
+#include "azure/keyvault/keyvault_secrets.hpp"
 #include <azure/identity.hpp>
-#include <azure/keyvault/keyvault_secrets.hpp>
 
 using namespace Azure::Security::KeyVault::Secrets;
 
 int main()
 {
-	auto tenantId = std::getenv("AZURE_TENANT_ID");
-	auto clientId = std::getenv("AZURE_CLIENT_ID");
-	auto clientSecret = std::getenv("AZURE_CLIENT_SECRET");
-	auto credential
-		= std::make_shared<Azure::Identity::ClientSecretCredential>(tenantId, clientId, clientSecret);
+  auto tenantId = std::getenv("AZURE_TENANT_ID");
+  auto clientId = std::getenv("AZURE_CLIENT_ID");
+  auto clientSecret = std::getenv("AZURE_CLIENT_SECRET");
+  auto credential
+      = std::make_shared<Azure::Identity::ClientSecretCredential>(tenantId, clientId, clientSecret);
 
-	SecretClient secretClient(std::getenv("AZURE_KEYVAULT_URL"), credential);
-	// just a response, with a secret
-	// auto response = secretClient.GetSecret("testSecret");
-	// response.Value.Properties.ContentType = "weqeq";
-	// GetSecretOptions options;
+  SecretClient secretClient(std::getenv("AZURE_KEYVAULT_URL"), credential);
+  // just a response, with a secret
+  // auto response = secretClient.GetSecret("testSecret");
+  // response.Value.Properties.ContentType = "weqeq";
+  // GetSecretOptions options;
 
-	// response = secretClient.UpdateSecretProperties(
-	//     response.Value.Name, response.Value.Properties.Version, response.Value.Properties);
+  // response = secretClient.UpdateSecretProperties(
+  //     response.Value.Name, response.Value.Properties.Version, response.Value.Properties);
 
-	// just a response, with a secret
-	// auto response3 = secretClient.GetDeletedSecret("someSecret");
+  // just a response, with a secret
+  // auto response3 = secretClient.GetDeletedSecret("someSecret");
 
-	// auto response4 = secretClient.BackupSecret("someSecret2");
-	// auto response5 = secretClient.RestoreSecretBackup(response4.Value.Secret);
+  // auto response4 = secretClient.BackupSecret("someSecret2");
+  // auto response5 = secretClient.RestoreSecretBackup(response4.Value.Secret);
 
-	//auto response = secretClient.PurgeDeletedSecret("someSecret3");
+  // auto response = secretClient.PurgeDeletedSecret("someSecret3");
 
-	// auto response4 = secretClient.BackupSecret("someSecret2");
-	// auto response5 = secretClient.RestoreSecretBackup(response4.Value.Secret);
-	//GetPropertiesOfSecretsOptions options;
-	//options.MaxResults = 1;
-	// auto r1 = secretClient.GetPropertiesOfSecrets(options);
-	// auto r2 = secretClient.GetPropertiesOfSecretsVersions(r1.Items[0].Name);
-	auto r3 = secretClient.GetDeletedSecrets();
-	// r1.MoveToNextPage();
-	return 0;
+  // auto response4 = secretClient.BackupSecret("someSecret2");
+  // auto response5 = secretClient.RestoreSecretBackup(response4.Value.Secret);
+  // GetPropertiesOfSecretsOptions options;
+  // options.MaxResults = 1;
+  // auto r1 = secretClient.GetPropertiesOfSecrets(options);
+  // auto r2 = secretClient.GetPropertiesOfSecretsVersions(r1.Items[0].Name);
+  auto r3 = secretClient.GetDeletedSecrets();
+  // r1.MoveToNextPage();
+  return 0;
 }
