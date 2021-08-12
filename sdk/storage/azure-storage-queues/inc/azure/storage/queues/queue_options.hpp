43e3426c7ac383a78da35f308c4dbfdf5758e8d8
Jinming Hu <jinmhu@microsoft.com>
2021-08-12 13:46:29 +0800

    add file doxygen

--- sdk/storage/azure-storage-queues/inc/azure/storage/queues/queue_options.hpp
+++ sdk/storage/azure-storage-queues/inc/azure/storage/queues/queue_options.hpp
@@ -1,5 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
+/**
+ * @file
+ * @brief Defines Queue client options and operation options.
+ *
+ */
 
 #pragma once
 
--- sdk/storage/azure-storage-queues/inc/azure/storage/queues/queue_responses.hpp
+++ sdk/storage/azure-storage-queues/inc/azure/storage/queues/queue_responses.hpp
@@ -1,5 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
+/**
+ * @file
+ * @brief Defines Queue operation responses.
+ *
+ */
 
 #pragma once
 
