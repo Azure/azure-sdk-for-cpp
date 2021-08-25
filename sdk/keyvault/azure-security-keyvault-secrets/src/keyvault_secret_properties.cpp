bd22c7e2901dbdd24c5deb6b09d972c7048149ca
George Arama <50641385+gearama@users.noreply.github.com>
2021-08-25 13:04:12 -0700

    add new constructor

---
+++ sdk/keyvault/azure-security-keyvault-secrets/src/keyvault_secret_properties.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+/**
+ * @file
+ * @brief Declares SecretProperties.
+ *
+ */
+
+#include "azure/keyvault/secrets/keyvault_secret.hpp"
+#include "private/secret_serializers.hpp"
+
+using namespace Azure::Security::KeyVault::Secrets;
+
+SecretProperties::SecretProperties(Azure::Core::Url const& url)
+{
+  _detail::SecretSerializer::ParseIDUrl(*this, url.GetAbsoluteUrl());
+};
