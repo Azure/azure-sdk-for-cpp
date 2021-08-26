9bcfc247b1b7fc9d3a6388b0db5e6b157e4b9908
George Arama <50641385+gearama@users.noreply.github.com>
2021-08-26 11:19:37 -0700

    Secrets test (#2764)
    * playback tests
    
    * two running tests
    
    * more tests and some redaction
    
    * formatting
    
    * formatting
    
    * fix bb and remove unused includes
    
    * macos
    
    * annoying issues
    
    * offline client
    
    * format
    
    * pr comment
    
    * reset the env to original after the client is gone.
    
    * oops
    
    * testing
    
    * formats
    
    * remove default resource for secrets, not needed
    
    * code coverage
    
    * build issue

---
+++ sdk/keyvault/azure-security-keyvault-secrets/test/ut/secret_client_base_test.hpp
@@ -0,0 +1,221 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+/**
+ * @file
+ * @brief The base class to construct and init a Key Vault client.
+ *
+ */
+#include <gtest/gtest.h>
+
+#include <azure/core/test/test_base.hpp>
+#include <azure/identity/client_secret_credential.hpp>
+#include <azure/keyvault/keyvault_secrets.hpp>
+
+using namespace std::chrono_literals;
+
+namespace Azure { namespace Security { namespace KeyVault { namespace Secrets { namespace _test {
+  /**
+   * @brief Client Secret Credential authenticates with the Azure services using a Tenant ID, Client
+   * ID and a client secret.
+   *
+   */
+  class TestClientSecretCredential final : public Core::Credentials::TokenCredential {
+  public:
+    Core::Credentials::AccessToken GetToken(
+        Core::Credentials::TokenRequestContext const& tokenRequestContext,
+        Core::Context const& context) const override
+    {
+      Core::Credentials::AccessToken accessToken;
+      accessToken.Token = "magicToken";
+      accessToken.ExpiresOn = DateTime::max();
+
+      if (context.IsCancelled() || tokenRequestContext.Scopes.size() == 0)
+      {
+        accessToken.ExpiresOn = DateTime::min();
+      }
+
+      return accessToken;
+    }
+  };
+  class KeyVaultSecretClientTest : public Azure::Core::Test::TestBase,
+                                   public ::testing::WithParamInterface<int> {
+  private:
+    std::unique_ptr<Azure::Security::KeyVault::Secrets::SecretClient> m_client;
+    std::string GetEnv(const std::string& name, std::string const& defaultValue = std::string())
+    {
+#if defined(_MSC_VER)
+#pragma warning(push)
+#pragma warning(disable : 4996)
+      const char* ret = std::getenv(name.data());
+#pragma warning(pop)
+#else
+      const char* ret = std::getenv(name.data());
+#endif
+
+      if (!ret)
+      {
+        if (!defaultValue.empty())
+        {
+          return defaultValue;
+        }
+
+        throw std::runtime_error(
+            name + " is required to run the tests but not set as an environment variable.");
+      }
+
+      return std::string(ret);
+    }
+
+  protected:
+    int m_testPollingTimeOutMinutes = 20;
+    std::chrono::minutes m_testPollingIntervalMinutes = std::chrono::minutes(1);
+    std::shared_ptr<Azure::Identity::ClientSecretCredential> m_credential;
+    std::shared_ptr<TestClientSecretCredential> m_testCredential;
+    std::string m_keyVaultUrl;
+    std::string m_keyVaultHsmUrl;
+    std::chrono::milliseconds m_defaultWait;
+
+    Azure::Security::KeyVault::Secrets::SecretClient const& GetClientForTest(
+        std::string const& testName)
+    {
+      //_putenv_s("AZURE_TEST_MODE", "PLAYBACK");
+      // keep this here to quickly switch between test modes
+      InitializeClient();
+      // set the interceptor for the current test
+      m_testContext.RenameTest(testName);
+      return *m_client;
+    }
+
+    // Create
+    void InitializeClient()
+    {
+      // Init interceptor from PlayBackRecorder
+      std::string recordingPath(AZURE_TEST_RECORDING_DIR);
+      recordingPath.append("/recordings");
+      Azure::Core::Test::TestBase::SetUpBase(recordingPath);
+
+      std::string tenantId = GetEnv("AZURE_TENANT_ID", "tenant");
+      std::string clientId = GetEnv("AZURE_CLIENT_ID", "client");
+      std::string secretId = GetEnv("AZURE_CLIENT_SECRET", "secret");
+
+      m_keyVaultUrl = GetEnv("AZURE_KEYVAULT_URL", "https://REDACTED.vault.azure.net/");
+      m_keyVaultHsmUrl = GetEnv("AZURE_KEYVAULT_HSM_URL", "https://REDACTED.vault.azure.net/");
+
+      // Create default client for the test
+      SecretClientOptions options;
+      // Replace default transport adapter for playback
+      if (m_testContext.IsPlaybackMode())
+      {
+        options.Transport.Transport = m_interceptor->GetPlaybackClient();
+      }
+      // Insert Recording policy when Record mode is on (non playback and non LiveMode)
+      else if (!m_testContext.IsLiveMode())
+      {
+        // AZURE_TEST_RECORDING_DIR is exported by CMAKE
+        options.PerRetryPolicies.push_back(m_interceptor->GetRecordPolicy());
+      }
+
+      if (m_testContext.IsPlaybackMode())
+      { // inject fake token client here if it's test
+        m_testCredential = std::make_shared<TestClientSecretCredential>();
+        m_client = std::make_unique<SecretClient>(m_keyVaultUrl, m_testCredential, options);
+        // we really dont need to wait for results
+        m_defaultWait = 1ms;
+      }
+      else
+      {
+        m_credential = std::make_shared<Azure::Identity::ClientSecretCredential>(
+            tenantId, clientId, secretId);
+        m_client = std::make_unique<SecretClient>(m_keyVaultUrl, m_credential, options);
+        m_defaultWait = 30s;
+      }
+
+      // When running live tests, service can return 429 error response if the client is sending
+      // multiple requests per second. This can happen if the network is fast and tests are running
+      // without any delay between them.
+      auto avoidTestThrottled = GetEnv("AZURE_KEYVAULT_AVOID_THROTTLED", "0");
+
+      if (avoidTestThrottled != "0")
+      {
+        std::cout << "- Wait to avoid server throttled..." << std::endl;
+        // 10 sec should be enough to prevent from 429 error
+        std::this_thread::sleep_for(std::chrono::seconds(10));
+      }
+    }
+
+  public:
+    template <class T>
+    static inline void CheckValidResponse(
+        Azure::Response<T>& response,
+        Azure::Core::Http::HttpStatusCode expectedCode = Azure::Core::Http::HttpStatusCode::Ok)
+    {
+      auto const& rawResponse = response.RawResponse;
+      EXPECT_EQ(
+          static_cast<typename std::underlying_type<Azure::Core::Http::HttpStatusCode>::type>(
+              rawResponse->GetStatusCode()),
+          static_cast<typename std::underlying_type<Azure::Core::Http::HttpStatusCode>::type>(
+              expectedCode));
+    }
+
+    static inline std::string GetUniqueName() { return Azure::Core::Uuid::CreateUuid().ToString(); }
+
+    static inline void CleanUpKeyVault(SecretClient const& secretClient)
+    {
+
+      std::vector<DeletedSecret> deletedSecrets;
+      for (auto secretResponse = secretClient.GetDeletedSecrets(); secretResponse.HasPage();
+           secretResponse.MoveToNextPage())
+      {
+        for (auto& secret : secretResponse.Items)
+        {
+          deletedSecrets.emplace_back(secret);
+        }
+      }
+      if (deletedSecrets.size() > 0)
+      {
+        for (auto& deletedSecret : deletedSecrets)
+        {
+          secretClient.PurgeDeletedSecret(deletedSecret.Name);
+        }
+        // Wait for purge is completed
+        std::this_thread::sleep_for(std::chrono::minutes(1));
+      }
+    }
+
+    static inline void RemoveAllSecretsFromVault(
+        SecretClient const& secretClient,
+        bool waitForPurge = true)
+    {
+      std::vector<DeleteSecretOperation> deletedSecrets;
+      GetPropertiesOfSecretsOptions options;
+      for (auto secretResponse = secretClient.GetPropertiesOfSecrets(); secretResponse.HasPage();
+           secretResponse.MoveToNextPage())
+      {
+        for (auto& secret : secretResponse.Items)
+        {
+          deletedSecrets.emplace_back(secretClient.StartDeleteSecret(secret.Name));
+        }
+      }
+      if (deletedSecrets.size() > 0)
+      {
+        std::cout << std::endl
+                  << "Cleaning vault. " << deletedSecrets.size()
+                  << " Will be deleted and purged now...";
+        for (auto& deletedSecret : deletedSecrets)
+        {
+          auto readyToPurgeSecret = deletedSecret.PollUntilDone(std::chrono::minutes(1));
+          secretClient.PurgeDeletedSecret(readyToPurgeSecret.Value.Name);
+          std::cout << std::endl << "Deleted and purged secret: " + readyToPurgeSecret.Value.Name;
+        }
+        std::cout << std::endl << "Complete purge operation.";
+        // Wait for purge is completed
+        if (waitForPurge)
+        {
+          std::this_thread::sleep_for(std::chrono::minutes(1));
+        }
+      }
+    }
+  };
+
+}}}}} // namespace Azure::Security::KeyVault::Secrets::_test
--- sdk/keyvault/azure-security-keyvault-secrets/test/ut/secret_client_test.cpp
+++ sdk/keyvault/azure-security-keyvault-secrets/test/ut/secret_client_test.cpp
@@ -1,16 +1,14 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
 
-#include "azure/core/context.hpp"
 #include "azure/identity/client_secret_credential.hpp"
-#include "azure/keyvault/secrets/secret_client.hpp"
-
-#include <exception>
+#include "secret_client_base_test.hpp"
+#include <cstddef>
 #include <gtest/gtest.h>
-#include <memory>
 
+using namespace std::chrono_literals;
 using namespace Azure::Security::KeyVault::Secrets;
-
+using namespace Azure::Security::KeyVault::Secrets::_test;
 TEST(SecretClient, InitClient)
 {
   auto credential
@@ -51,4 +49,281 @@ TEST(SecretClient, GetUrl)
   auto url = "vaultUrl";
   SecretClient secretClient(url, credential);
   EXPECT_EQ(url, secretClient.GetUrl());
+}
+
+TEST_F(KeyVaultSecretClientTest, FirstCreateTest)
+{
+  auto secretName = "FirstCreateTest";
+  auto const& client
+      = GetClientForTest(::testing::UnitTest::GetInstance()->current_test_info()->name());
+
+  {
+    auto secretResponse = client.SetSecret(secretName, "secretValue");
+    CheckValidResponse(secretResponse);
+    auto secret = secretResponse.Value;
+    EXPECT_EQ(secret.Name, secretName);
+  }
+  {
+    // Now get the key
+    auto secretResponse = client.GetSecret(secretName);
+    CheckValidResponse(secretResponse);
+    auto secret = secretResponse.Value;
+    EXPECT_EQ(secret.Name, secretName);
+  }
+  {
+    auto operation = client.StartDeleteSecret(secretName);
+    operation.PollUntilDone(m_defaultWait);
+    auto deletedSecretResponse = client.GetDeletedSecret(secretName);
+    CheckValidResponse(deletedSecretResponse);
+    auto secret = deletedSecretResponse.Value;
+    EXPECT_EQ(secret.Name, secretName);
+  }
+  {
+    auto purgedResponse = client.PurgeDeletedSecret(secretName);
+    CheckValidResponse(purgedResponse, Azure::Core::Http::HttpStatusCode::NoContent);
+  }
+}
+
+TEST_F(KeyVaultSecretClientTest, SecondCreateTest)
+{
+  auto secretName = "SecondCreateTest";
+  auto const& client
+      = GetClientForTest(::testing::UnitTest::GetInstance()->current_test_info()->name());
+  std::string version1;
+  std::string version2;
+  {
+    auto secretResponse = client.SetSecret(secretName, "secretValue");
+    CheckValidResponse(secretResponse);
+    auto secret = secretResponse.Value;
+    version1 = secret.Properties.Version;
+    EXPECT_EQ(secret.Name, secretName);
+  }
+  {
+    auto secretResponse = client.SetSecret(secretName, "secretValue2");
+    CheckValidResponse(secretResponse);
+    auto secret = secretResponse.Value;
+    version2 = secret.Properties.Version;
+    EXPECT_EQ(secret.Name, secretName);
+  }
+  {
+    auto secretResponse = client.GetPropertiesOfSecretsVersions(secretName);
+    EXPECT_EQ(secretResponse.Items.size(), size_t(2));
+    EXPECT_TRUE(
+        secretResponse.Items[0].Version == version1 || secretResponse.Items[0].Version == version2);
+    EXPECT_TRUE(
+        secretResponse.Items[1].Version == version1 || secretResponse.Items[1].Version == version2);
+  }
+  {
+    auto operation = client.StartDeleteSecret(secretName);
+    operation.PollUntilDone(m_defaultWait);
+    auto deletedSecretResponse = client.GetDeletedSecret(secretName);
+    CheckValidResponse(deletedSecretResponse);
+    auto secret = deletedSecretResponse.Value;
+    EXPECT_EQ(secret.Name, secretName);
+  }
+  {
+    auto purgedResponse = client.PurgeDeletedSecret(secretName);
+    CheckValidResponse(purgedResponse, Azure::Core::Http::HttpStatusCode::NoContent);
+  }
+}
+
+TEST_F(KeyVaultSecretClientTest, UpdateTest)
+{
+  auto secretName = "UpdateTest";
+  SecretProperties properties;
+  auto const& client
+      = GetClientForTest(::testing::UnitTest::GetInstance()->current_test_info()->name());
+  {
+    auto secretResponse = client.SetSecret(secretName, "secretValue");
+    CheckValidResponse(secretResponse);
+    auto secret = secretResponse.Value;
+    EXPECT_EQ(secret.Name, secretName);
+  }
+  {
+    // Now get the key
+    auto secretResponse = client.GetSecret(secretName);
+    CheckValidResponse(secretResponse);
+    auto secret = secretResponse.Value;
+    properties = secret.Properties;
+    EXPECT_EQ(secret.Name, secretName);
+  }
+  {
+    properties.ContentType = "xyz";
+    UpdateSecretPropertiesOptions options;
+    auto props = properties;
+    auto secretResponse = client.UpdateSecretProperties(properties);
+    CheckValidResponse(secretResponse);
+    auto secret = secretResponse.Value;
+    EXPECT_EQ(secret.Name, secretName);
+    EXPECT_EQ(secret.Properties.ContentType.Value(), properties.ContentType.Value());
+  }
+  {
+    auto operation = client.StartDeleteSecret(secretName);
+    operation.PollUntilDone(m_defaultWait);
+    auto deletedSecretResponse = client.GetDeletedSecret(secretName);
+    CheckValidResponse(deletedSecretResponse);
+    auto secret = deletedSecretResponse.Value;
+    EXPECT_EQ(secret.Name, secretName);
+  }
+  {
+    auto purgedResponse = client.PurgeDeletedSecret(secretName);
+    CheckValidResponse(purgedResponse, Azure::Core::Http::HttpStatusCode::NoContent);
+  }
+}
+
+TEST_F(KeyVaultSecretClientTest, BackupRestore)
+{
+  auto secretName = "BackupRestore";
+  BackupSecretResult backupData;
+  auto const& client
+      = GetClientForTest(::testing::UnitTest::GetInstance()->current_test_info()->name());
+
+  {
+    auto secretResponse = client.SetSecret(secretName, "secretValue");
+    CheckValidResponse(secretResponse);
+    auto secret = secretResponse.Value;
+    EXPECT_EQ(secret.Name, secretName);
+  }
+  {
+    auto backup = client.BackupSecret(secretName);
+    CheckValidResponse(backup);
+    backupData = backup.Value;
+  }
+  {
+    auto operation = client.StartDeleteSecret(secretName);
+    operation.PollUntilDone(m_defaultWait);
+    auto deletedSecretResponse = client.GetDeletedSecret(secretName);
+    CheckValidResponse(deletedSecretResponse);
+    auto secret = deletedSecretResponse.Value;
+    EXPECT_EQ(secret.Name, secretName);
+  }
+  {
+    auto purgedResponse = client.PurgeDeletedSecret(secretName);
+    CheckValidResponse(purgedResponse, Azure::Core::Http::HttpStatusCode::NoContent);
+    std::this_thread::sleep_for(m_defaultWait);
+  }
+  {
+    auto restore = client.RestoreSecretBackup(backupData);
+    CheckValidResponse(restore);
+    auto restored = restore.Value;
+    EXPECT_EQ(restored.Name, secretName);
+  }
+  {
+    auto operation = client.StartDeleteSecret(secretName);
+    operation.PollUntilDone(m_defaultWait);
+    auto deletedSecretResponse = client.GetDeletedSecret(secretName);
+    CheckValidResponse(deletedSecretResponse);
+    auto secret = deletedSecretResponse.Value;
+    EXPECT_EQ(secret.Name, secretName);
+  }
+  {
+    auto purgedResponse = client.PurgeDeletedSecret(secretName);
+    CheckValidResponse(purgedResponse, Azure::Core::Http::HttpStatusCode::NoContent);
+  }
+}
+
+TEST_F(KeyVaultSecretClientTest, Recover)
+{
+  auto secretName = "Recover";
+  std::vector<uint8_t> backupData;
+  auto const& client
+      = GetClientForTest(::testing::UnitTest::GetInstance()->current_test_info()->name());
+
+  {
+    auto secretResponse = client.SetSecret(secretName, "secretValue");
+    CheckValidResponse(secretResponse);
+    auto secret = secretResponse.Value;
+    EXPECT_EQ(secret.Name, secretName);
+  }
+  {
+    auto operation = client.StartDeleteSecret(secretName);
+    operation.PollUntilDone(m_defaultWait);
+    EXPECT_EQ(operation.GetResumeToken(), secretName);
+    EXPECT_EQ(operation.HasValue(), true);
+    auto operationResult = operation.Value();
+    auto deletedSecretResponse = client.GetDeletedSecret(secretName);
+    CheckValidResponse(deletedSecretResponse);
+    auto secret = deletedSecretResponse.Value;
+    EXPECT_EQ(secret.Name, secretName);
+    EXPECT_EQ(operationResult.Name, secretName);
+    EXPECT_EQ(operation.GetRawResponse().GetStatusCode(), Azure::Core::Http::HttpStatusCode::Ok);
+  }
+  {
+    auto operation = client.StartRecoverDeletedSecret(secretName);
+    operation.PollUntilDone(m_defaultWait);
+    EXPECT_EQ(operation.GetResumeToken(), secretName);
+    EXPECT_EQ(operation.HasValue(), true);
+    auto operationResult = operation.Value();
+    auto restoredSecret = client.GetSecret(secretName);
+    auto secret = restoredSecret.Value;
+    EXPECT_EQ(secret.Name, secretName);
+    EXPECT_EQ(operationResult.Name, secretName);
+    EXPECT_EQ(operation.GetRawResponse().GetStatusCode(), Azure::Core::Http::HttpStatusCode::Ok);
+  }
+  {
+    auto operation = client.StartDeleteSecret(secretName);
+    operation.PollUntilDone(m_defaultWait);
+    auto deletedSecretResponse = client.GetDeletedSecret(secretName);
+    CheckValidResponse(deletedSecretResponse);
+    auto secret = deletedSecretResponse.Value;
+    EXPECT_EQ(secret.Name, secretName);
+  }
+  {
+    auto purgedResponse = client.PurgeDeletedSecret(secretName);
+    CheckValidResponse(purgedResponse, Azure::Core::Http::HttpStatusCode::NoContent);
+  }
+}
+TEST_F(KeyVaultSecretClientTest, GetProperties)
+{
+  auto secretName = "GetProperties";
+  auto secretName2 = "GetProperties2";
+
+  auto const& client
+      = GetClientForTest(::testing::UnitTest::GetInstance()->current_test_info()->name());
+  {
+    auto secretResponse = client.SetSecret(secretName, "secretValue");
+    CheckValidResponse(secretResponse);
+    auto secret = secretResponse.Value;
+    EXPECT_EQ(secret.Name, secretName);
+  }
+  {
+    auto secretResponse = client.SetSecret(secretName2, "secretValue2");
+    CheckValidResponse(secretResponse);
+    auto secret = secretResponse.Value;
+    EXPECT_EQ(secret.Name, secretName2);
+  }
+  {
+    auto secretResponse = client.GetPropertiesOfSecrets();
+    EXPECT_EQ(secretResponse.Items.size(), size_t(2));
+    EXPECT_TRUE(
+        secretResponse.Items[0].Name == secretName || secretResponse.Items[0].Name == secretName2);
+    EXPECT_TRUE(
+        secretResponse.Items[1].Name == secretName || secretResponse.Items[1].Name == secretName2);
+  }
+  {
+    auto operation = client.StartDeleteSecret(secretName);
+    operation.PollUntilDone(m_defaultWait);
+  }
+  {
+    auto operation = client.StartDeleteSecret(secretName2);
+    operation.PollUntilDone(m_defaultWait);
+  }
+  {
+    auto deletedResponse = client.GetDeletedSecrets();
+    EXPECT_EQ(deletedResponse.Items.size(), size_t(2));
+    EXPECT_TRUE(
+        deletedResponse.Items[0].Name == secretName
+        || deletedResponse.Items[0].Name == secretName2);
+    EXPECT_TRUE(
+        deletedResponse.Items[1].Name == secretName
+        || deletedResponse.Items[1].Name == secretName2);
+  }
+  {
+    auto purgedResponse = client.PurgeDeletedSecret(secretName);
+    CheckValidResponse(purgedResponse, Azure::Core::Http::HttpStatusCode::NoContent);
+  }
+  {
+    auto purgedResponse = client.PurgeDeletedSecret(secretName2);
+    CheckValidResponse(purgedResponse, Azure::Core::Http::HttpStatusCode::NoContent);
+  }
 }
