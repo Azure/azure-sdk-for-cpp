3a2eb724c6ae76de9c3b07e8458637f0df2ccf05
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2021-09-03 16:09:52 -0700

    --exclude-throw-branches

--- sdk/core/azure-core/test/ut/azure_assert.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// SPDX-License-Identifier: MIT
-
-#include <azure/core/azure_assert.hpp>
-
-#include <gtest/gtest.h>
-
-TEST(AzureAssert, AzureNoReturnPath) { ASSERT_DEATH(AzureNoReturnPath("test"), ""); }
