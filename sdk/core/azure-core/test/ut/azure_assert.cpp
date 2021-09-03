e573d3d526a3f0026a261944c55730d30c36efc9
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2021-09-03 14:45:14 -0700

    Add unit test for azure assert

---
+++ sdk/core/azure-core/test/ut/azure_assert.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#include <azure/core/azure_assert.hpp>
+
+#include <gtest/gtest.h>
+
+TEST(AzureAssert, AzureNoReturnPath) { ASSERT_DEATH(AzureNoReturnPath("test")); }
