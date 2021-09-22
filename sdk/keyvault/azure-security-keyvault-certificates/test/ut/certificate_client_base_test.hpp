5875df86af0f62c5e645cf7ab8fb827c23fe3fd1
George Arama <50641385+gearama@users.noreply.github.com>
2021-09-21 18:44:55 -0700

    PR issues

--- sdk/keyvault/azure-security-keyvault-certificates/test/ut/certificate_client_base_test.hpp
+++ sdk/keyvault/azure-security-keyvault-certificates/test/ut/certificate_client_base_test.hpp
@@ -83,7 +83,7 @@ namespace Azure {
     Azure::Security::KeyVault::Certificates::CertificateClient const& GetClientForTest(
         std::string const& testName)
     {
-      _putenv_s("AZURE_TEST_MODE", "PLAYBACK");
+      // used to test/dev purposes _putenv_s("AZURE_TEST_MODE", "PLAYBACK");
       InitializeClient();
       // set the interceptor for the current test
       m_testContext.RenameTest(testName);
