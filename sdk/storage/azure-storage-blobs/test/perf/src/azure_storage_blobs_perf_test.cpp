8ff60cba3c2e3e93cd0f4c8a2b3ad7cf6b867fa4
Victor Vazquez <vhvb1989@gmail.com>
2021-10-22 00:59:01 +0000

    make libcurl depend on compilation

--- sdk/storage/azure-storage-blobs/test/perf/src/azure_storage_blobs_perf_test.cpp
+++ sdk/storage/azure-storage-blobs/test/perf/src/azure_storage_blobs_perf_test.cpp
@@ -6,7 +6,11 @@
 #include "azure/storage/blobs/test/download_blob_from_sas.hpp"
 #include "azure/storage/blobs/test/download_blob_pipeline_only.hpp"
 #include "azure/storage/blobs/test/download_blob_test.hpp"
-#include "azure/storage/blobs/test/download_blob_trasport_only.hpp"
+
+#if defined(BUILD_CURL_HTTP_TRANSPORT_ADAPTER)
+#include "azure/storage/blobs/test/download_blob_transport_only.hpp"
+#endif
+
 #include "azure/storage/blobs/test/list_blob_test.hpp"
 #include "azure/storage/blobs/test/upload_blob_test.hpp"
 
@@ -14,13 +18,17 @@ int main(int argc, char** argv)
 {
 
   // Create the test list
-  std::vector<Azure::Perf::TestMetadata> tests{
-      Azure::Storage::Blobs::Test::DownloadBlob::GetTestMetadata(),
-      Azure::Storage::Blobs::Test::UploadBlob::GetTestMetadata(),
-      Azure::Storage::Blobs::Test::ListBlob::GetTestMetadata(),
-      Azure::Storage::Blobs::Test::DownloadBlobSas::GetTestMetadata(),
-      Azure::Storage::Blobs::Test::DownloadBlobWithTransportOnly::GetTestMetadata(),
-      Azure::Storage::Blobs::Test::DownloadBlobWithPipelineOnly::GetTestMetadata()};
+  std::vector<Azure::Perf::TestMetadata> tests
+  {
+    Azure::Storage::Blobs::Test::DownloadBlob::GetTestMetadata(),
+        Azure::Storage::Blobs::Test::UploadBlob::GetTestMetadata(),
+        Azure::Storage::Blobs::Test::ListBlob::GetTestMetadata(),
+        Azure::Storage::Blobs::Test::DownloadBlobSas::GetTestMetadata(),
+#if defined(BUILD_CURL_HTTP_TRANSPORT_ADAPTER)
+        Azure::Storage::Blobs::Test::DownloadBlobWithTransportOnly::GetTestMetadata(),
+#endif
+        Azure::Storage::Blobs::Test::DownloadBlobWithPipelineOnly::GetTestMetadata()
+  };
 
   Azure::Perf::Program::Run(Azure::Core::Context::ApplicationContext, tests, argc, argv);
 
