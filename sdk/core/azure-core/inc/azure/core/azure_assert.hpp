82eefa65204701023bb7cbe3c672c56bd225e084
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2021-12-14 16:39:23 -0800

    Macro rename compatibility layer

---
+++ sdk/core/azure-core/inc/azure/core/azure_assert.hpp
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#include "azure/core/internal/azure_assert.hpp"
+
+#define AZURE_ASSERT(exp) _azure_ASSERT(exp)
+#define AZURE_ASSERT_MSG(exp, msg) _azure_ASSERT_MSG(exp, msg)
+#define AZURE_ASSERT_FALSE(exp) _azure_ASSERT_FALSE(exp)
+#define AZURE_UNREACHABLE_CODE() _azure_UNREACHABLE_CODE()
+#define AZURE_NOT_IMPLEMENTED() _azure_NOT_IMPLEMENTED()
