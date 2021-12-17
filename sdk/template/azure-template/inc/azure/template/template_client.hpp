a76802c1722a0f82fed9ff1ff822130efae165d4
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2021-12-16 20:16:45 -0800

    Include header change

--- sdk/template/azure-template/inc/azure/template/template_client.hpp
+++ sdk/template/azure-template/inc/azure/template/template_client.hpp
@@ -10,7 +10,7 @@ namespace Azure { namespace Template {
   class TemplateClient final {
   public:
     std::string ClientVersion() const;
-    int32_t GetValue() const;
+    int GetValue(int key) const;
   };
 
 }} // namespace Azure::Template
