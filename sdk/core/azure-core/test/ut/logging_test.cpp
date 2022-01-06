00dca3cf85b9708ed8860bac0b1525614c9912b2
Victor Vazquez <vhvb1989@gmail.com>
2022-01-06 21:22:20 +0000

    big logs testing

--- sdk/core/azure-core/test/ut/logging_test.cpp
+++ sdk/core/azure-core/test/ut/logging_test.cpp
@@ -206,9 +206,13 @@ namespace {
 struct LoggerTestParameter
 {
   std::string Suffix;
+  uint64_t LogLines;
 };
 
 class LoggerTest : public testing::TestWithParam<LoggerTestParameter> {
+
+protected:
+  virtual void SetUp() override {}
 };
 
 std::string GetSuffix(const testing::TestParamInfo<LoggerTest::ParamType>& info)
@@ -216,8 +220,63 @@ std::string GetSuffix(const testing::TestParamInfo<LoggerTest::ParamType>& info)
   return info.param.Suffix;
 }
 
-std::vector<LoggerTestParameter> GetTestParameters();
+std::vector<LoggerTestParameter> GetTestParameters()
+{
+  std::vector<LoggerTestParameter> parameters;
+  parameters.emplace_back(LoggerTestParameter{"halfMillion", 500000U});
+  parameters.emplace_back(LoggerTestParameter{"oneMillion", 1000000U});
+  return parameters;
+}
 
-INSTANTIATE_TEST_SUITE_P(withParam, LoggerTest, GetTestParameters(), GetSuffix);
+// cspell:disable
+constexpr static const char LogSample[]
+    = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt "
+      "ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation "
+      "ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in "
+      "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur "
+      "sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est "
+      "laborum.";
+// cspell:enable
 
 } // namespace
+
+using Azure::Core::Diagnostics::Logger;
+using Azure::Core::Diagnostics::_internal::Log;
+
+TEST_P(LoggerTest, defaultLogger)
+{
+  LoggerTestParameter parameter(GetParam());
+
+  for (uint64_t counter = 0; counter < parameter.LogLines; counter++)
+  {
+    Log::Write(Logger::Level::Informational, LogSample);
+  }
+}
+
+#include <thread>
+#include <vector>
+
+TEST_P(LoggerTest, parallelLog)
+{
+  LoggerTestParameter parameter(GetParam());
+
+  std::vector<std::thread> workers;
+
+  auto routine = [parameter]() {
+    for (uint64_t counter = 0; counter < parameter.LogLines; counter++)
+    {
+      Log::Write(Logger::Level::Informational, LogSample);
+    }
+  };
+
+  for (auto j = 0; j < 5; j++)
+  {
+    workers.emplace_back(std::thread(routine));
+  }
+  for (auto& w : workers)
+  {
+    w.join();
+  }
+}
+
+INSTANTIATE_TEST_SUITE_P(withParam, LoggerTest, testing::ValuesIn(GetTestParameters()), GetSuffix);
