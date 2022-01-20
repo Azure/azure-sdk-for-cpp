b0d872293575bbbd4db25bbde4f387088968ad1a
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2022-01-20 13:46:56 -0800

    Update console log listener test for UWP

--- sdk/core/azure-core/test/ut/environment_log_level_listener_test.cpp
+++ sdk/core/azure-core/test/ut/environment_log_level_listener_test.cpp
@@ -1,6 +1,18 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
 
+#include <azure/core/platform.hpp>
+
+#if defined(AZ_PLATFORM_WINDOWS)
+#if !defined(WIN32_LEAN_AND_MEAN)
+#define WIN32_LEAN_AND_MEAN
+#endif
+#if !defined(NOMINMAX)
+#define NOMINMAX
+#endif
+
+#include <windows.h>
+#endif
 
 #include <gtest/gtest.h>
 #include <private/environment_log_level_listener.hpp>
@@ -10,20 +22,36 @@ using Azure::Core::Diagnostics::_detail::EnvironmentLogLevelListener;
 
 namespace {
 
+constexpr bool IsUwp =
+#if (!defined(WINAPI_PARTITION_DESKTOP) || WINAPI_PARTITION_DESKTOP) // See azure/core/platform.hpp
+                                                                     // for explanation.
+    false
+#else
+    true
+#endif
+    ;
+
 std::string environmentVariable = "AZURE_LOG_LEVEL";
 
 void SetLogLevel(std::string const& value)
 {
+#if (!defined(WINAPI_PARTITION_DESKTOP) || WINAPI_PARTITION_DESKTOP) // See azure/core/platform.hpp
+                                                                     // for explanation.
 #if defined(_MSC_VER)
   static_cast<void>(_putenv((environmentVariable + "=" + value).c_str()));
 #else
   static_cast<void>(setenv(environmentVariable.c_str(), value.c_str(), 1));
+#endif
+#else
+  static_cast<void>(value); // On UWP, there is no environment.
 #endif
 }
 
 } // namespace
 class EnvironmentLogLevelListenerTest : public testing::Test {
 protected:
+#if (!defined(WINAPI_PARTITION_DESKTOP) || WINAPI_PARTITION_DESKTOP) // See azure/core/platform.hpp
+                                                                     // for explanation.
   void SetUp() override
   {
 #if defined(_MSC_VER)
@@ -43,6 +71,8 @@ protected:
 
 private:
   std::string m_previousValue;
+
+#endif
 };
 
 TEST_F(EnvironmentLogLevelListenerTest, LogLevelDefault)
@@ -63,16 +93,16 @@ TEST_F(EnvironmentLogLevelListenerTest, LogLevelError)
   EnvironmentLogLevelListener::SetInitialized(false);
   SetLogLevel("error");
   auto level = EnvironmentLogLevelListener::GetLogLevel(Logger::Level::Verbose);
-  EXPECT_EQ(level, Logger::Level::Error);
+  EXPECT_EQ(level, IsUwp ? Logger::Level::Verbose : Logger::Level::Error);
 
   EnvironmentLogLevelListener::SetInitialized(false);
   SetLogLevel("err");
   level = EnvironmentLogLevelListener::GetLogLevel(Logger::Level::Verbose);
-  EXPECT_EQ(level, Logger::Level::Error);
+  EXPECT_EQ(level, IsUwp ? Logger::Level::Verbose : Logger::Level::Error);
 
   SetLogLevel("4");
   level = EnvironmentLogLevelListener::GetLogLevel(Logger::Level::Verbose);
-  EXPECT_EQ(level, Logger::Level::Error);
+  EXPECT_EQ(level, IsUwp ? Logger::Level::Verbose : Logger::Level::Error);
 }
 
 TEST_F(EnvironmentLogLevelListenerTest, LogLevelWarning)
@@ -80,17 +110,17 @@ TEST_F(EnvironmentLogLevelListenerTest, LogLevelWarning)
   EnvironmentLogLevelListener::SetInitialized(false);
   SetLogLevel("warning");
   auto level = EnvironmentLogLevelListener::GetLogLevel(Logger::Level::Verbose);
-  EXPECT_EQ(level, Logger::Level::Warning);
+  EXPECT_EQ(level, IsUwp ? Logger::Level::Verbose : Logger::Level::Warning);
 
   EnvironmentLogLevelListener::SetInitialized(false);
   SetLogLevel("warn");
   level = EnvironmentLogLevelListener::GetLogLevel(Logger::Level::Verbose);
-  EXPECT_EQ(level, Logger::Level::Warning);
+  EXPECT_EQ(level, IsUwp ? Logger::Level::Verbose : Logger::Level::Warning);
 
   EnvironmentLogLevelListener::SetInitialized(false);
   SetLogLevel("3");
   level = EnvironmentLogLevelListener::GetLogLevel(Logger::Level::Verbose);
-  EXPECT_EQ(level, Logger::Level::Warning);
+  EXPECT_EQ(level, IsUwp ? Logger::Level::Verbose : Logger::Level::Warning);
 }
 
 TEST_F(EnvironmentLogLevelListenerTest, LogLevelInformational)
@@ -98,40 +128,40 @@ TEST_F(EnvironmentLogLevelListenerTest, LogLevelInformational)
   EnvironmentLogLevelListener::SetInitialized(false);
   SetLogLevel("informational");
   auto level = EnvironmentLogLevelListener::GetLogLevel(Logger::Level::Verbose);
-  EXPECT_EQ(level, Logger::Level::Informational);
+  EXPECT_EQ(level, IsUwp ? Logger::Level::Verbose : Logger::Level::Informational);
 
   EnvironmentLogLevelListener::SetInitialized(false);
   SetLogLevel("info");
   level = EnvironmentLogLevelListener::GetLogLevel(Logger::Level::Verbose);
-  EXPECT_EQ(level, Logger::Level::Informational);
+  EXPECT_EQ(level, IsUwp ? Logger::Level::Verbose : Logger::Level::Informational);
 
   EnvironmentLogLevelListener::SetInitialized(false);
   SetLogLevel("information");
   level = EnvironmentLogLevelListener::GetLogLevel(Logger::Level::Verbose);
-  EXPECT_EQ(level, Logger::Level::Informational);
+  EXPECT_EQ(level, IsUwp ? Logger::Level::Verbose : Logger::Level::Informational);
 
   EnvironmentLogLevelListener::SetInitialized(false);
   SetLogLevel("2");
   level = EnvironmentLogLevelListener::GetLogLevel(Logger::Level::Verbose);
-  EXPECT_EQ(level, Logger::Level::Informational);
+  EXPECT_EQ(level, IsUwp ? Logger::Level::Verbose : Logger::Level::Informational);
 }
 
 TEST_F(EnvironmentLogLevelListenerTest, LogLevelVerbose)
 {
   EnvironmentLogLevelListener::SetInitialized(false);
   SetLogLevel("verbose");
-  auto level = EnvironmentLogLevelListener::GetLogLevel(Logger::Level::Verbose);
-  EXPECT_EQ(level, Logger::Level::Verbose);
+  auto level = EnvironmentLogLevelListener::GetLogLevel(Logger::Level::Error);
+  EXPECT_EQ(level, IsUwp ? Logger::Level::Error : Logger::Level::Verbose);
 
   EnvironmentLogLevelListener::SetInitialized(false);
   SetLogLevel("debug");
-  level = EnvironmentLogLevelListener::GetLogLevel(Logger::Level::Verbose);
-  EXPECT_EQ(level, Logger::Level::Verbose);
+  level = EnvironmentLogLevelListener::GetLogLevel(Logger::Level::Error);
+  EXPECT_EQ(level, IsUwp ? Logger::Level::Error : Logger::Level::Verbose);
 
   EnvironmentLogLevelListener::SetInitialized(false);
   SetLogLevel("1");
-  level = EnvironmentLogLevelListener::GetLogLevel(Logger::Level::Verbose);
-  EXPECT_EQ(level, Logger::Level::Verbose);
+  level = EnvironmentLogLevelListener::GetLogLevel(Logger::Level::Error);
+  EXPECT_EQ(level, IsUwp ? Logger::Level::Error : Logger::Level::Verbose);
 }
 
 TEST_F(EnvironmentLogLevelListenerTest, GetLogListenerVerbose)
@@ -145,10 +175,18 @@ TEST_F(EnvironmentLogLevelListenerTest, GetLogListenerVerbose)
   std::string text = buffer.str(); // text will now contain "Bla\n"
   auto listener = EnvironmentLogLevelListener::GetLogListener();
 
-  listener(Logger::Level::Verbose, "message");
-  EXPECT_NE(listener, nullptr);
-  EXPECT_NE(buffer.str().find("DEBUG : message"), std::string::npos);
-  std::cerr.rdbuf(old);
+  if (!IsUwp)
+  {
+    EXPECT_NE(listener, nullptr);
+
+    listener(Logger::Level::Verbose, "message");
+    EXPECT_NE(buffer.str().find("DEBUG : message"), std::string::npos);
+    std::cerr.rdbuf(old);
+  }
+  else
+  {
+    EXPECT_EQ(listener, nullptr);
+  }
 }
 
 TEST_F(EnvironmentLogLevelListenerTest, GetLogListenerError)
@@ -162,10 +200,18 @@ TEST_F(EnvironmentLogLevelListenerTest, GetLogListenerError)
   std::string text = buffer.str(); // text will now contain "Bla\n"
   auto listener = EnvironmentLogLevelListener::GetLogListener();
 
-  listener(Logger::Level::Error, "message");
-  EXPECT_NE(listener, nullptr);
-  EXPECT_NE(buffer.str().find("ERROR : message"), std::string::npos);
-  std::cerr.rdbuf(old);
+  if (!IsUwp)
+  {
+    EXPECT_NE(listener, nullptr);
+
+    listener(Logger::Level::Error, "message");
+    EXPECT_NE(buffer.str().find("ERROR : message"), std::string::npos);
+    std::cerr.rdbuf(old);
+  }
+  else
+  {
+    EXPECT_EQ(listener, nullptr);
+  }
 }
 
 TEST_F(EnvironmentLogLevelListenerTest, GetLogListenerWarning)
@@ -179,10 +225,18 @@ TEST_F(EnvironmentLogLevelListenerTest, GetLogListenerWarning)
   std::string text = buffer.str(); // text will now contain "Bla\n"
   auto listener = EnvironmentLogLevelListener::GetLogListener();
 
-  listener(Logger::Level::Warning, "message");
-  EXPECT_NE(listener, nullptr);
-  EXPECT_NE(buffer.str().find("WARN  : message"), std::string::npos);
-  std::cerr.rdbuf(old);
+  if (!IsUwp)
+  {
+    EXPECT_NE(listener, nullptr);
+
+    listener(Logger::Level::Warning, "message");
+    EXPECT_NE(buffer.str().find("WARN  : message"), std::string::npos);
+    std::cerr.rdbuf(old);
+  }
+  else
+  {
+    EXPECT_EQ(listener, nullptr);
+  }
 }
 
 TEST_F(EnvironmentLogLevelListenerTest, GetLogListenerInformational)
@@ -196,10 +250,18 @@ TEST_F(EnvironmentLogLevelListenerTest, GetLogListenerInformational)
   std::string text = buffer.str(); // text will now contain "Bla\n"
   auto listener = EnvironmentLogLevelListener::GetLogListener();
 
-  listener(Logger::Level::Informational, "message");
-  EXPECT_NE(listener, nullptr);
-  EXPECT_NE(buffer.str().find("INFO  : message"), std::string::npos);
-  std::cerr.rdbuf(old);
+  if (!IsUwp)
+  {
+    EXPECT_NE(listener, nullptr);
+
+    listener(Logger::Level::Informational, "message");
+    EXPECT_NE(buffer.str().find("INFO  : message"), std::string::npos);
+    std::cerr.rdbuf(old);
+  }
+  else
+  {
+    EXPECT_EQ(listener, nullptr);
+  }
 }
 
 TEST_F(EnvironmentLogLevelListenerTest, GetLogListenerUnknown)
@@ -213,8 +275,16 @@ TEST_F(EnvironmentLogLevelListenerTest, GetLogListenerUnknown)
   std::string text = buffer.str(); // text will now contain "Bla\n"
   auto listener = EnvironmentLogLevelListener::GetLogListener();
 
-  listener(static_cast<Logger::Level>(42), "message");
-  EXPECT_NE(listener, nullptr);
-  EXPECT_NE(buffer.str().find("????? : message"), std::string::npos);
-  std::cerr.rdbuf(old);
+  if (!IsUwp)
+  {
+    EXPECT_NE(listener, nullptr);
+
+    listener(static_cast<Logger::Level>(42), "message");
+    EXPECT_NE(buffer.str().find("????? : message"), std::string::npos);
+    std::cerr.rdbuf(old);
+  }
+  else
+  {
+    EXPECT_EQ(listener, nullptr);
+  }
 }
