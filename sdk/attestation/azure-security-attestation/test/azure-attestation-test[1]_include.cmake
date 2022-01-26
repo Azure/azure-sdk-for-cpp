530867c188fb1f0f22ca48a8cf99dd1b3eb30985
Larry Osterman <LarryOsterman@users.noreply.github.com>
2022-01-26 13:41:18 -0800

    Moved some test collateral around to make cspell happier

--- sdk/attestation/azure-security-attestation/test/azure-attestation-test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "G:/Az/LarryO/azure-sdk-for-cpp/sdk/attestation/azure-security-attestation/test/azure-attestation-test[1]_tests.cmake")
-  include("G:/Az/LarryO/azure-sdk-for-cpp/sdk/attestation/azure-security-attestation/test/azure-attestation-test[1]_tests.cmake")
-else()
-  add_test(azure-attestation-test_NOT_BUILT azure-attestation-test_NOT_BUILT)
-endif()
