3985fc84a65c8f296fe7bdf21f7034ddbf0e841f
Larry Osterman <LarryOsterman@users.noreply.github.com>
2022-02-02 13:09:05 -0800

    Added tests

--- sdk/core/azure-core/test/ut/base64_test.cpp
+++ sdk/core/azure-core/test/ut/base64_test.cpp
@@ -125,3 +125,21 @@ TEST(Base64, Roundtrip)
     EXPECT_EQ(Convert::Base64Decode(Convert::Base64Encode(data)), data);
   }
 }
+
+TEST(Base64, InvalidDecode)
+{
+  EXPECT_NO_THROW(Convert::Base64Decode(Convert::Base64Encode(std::vector<uint8_t>{})));
+  EXPECT_NO_THROW(Convert::Base64Decode(""));
+  EXPECT_THROW(Convert::Base64Decode("@#!%"), std::runtime_error);
+  EXPECT_THROW(Convert::Base64Decode("ABCD%GA="), std::runtime_error); // cspell:disable-line
+  EXPECT_THROW(Convert::Base64Decode("ABCDE^A="), std::runtime_error); // cspell:disable-line
+  EXPECT_THROW(Convert::Base64Decode("ABCDEF&="), std::runtime_error); // cspell:disable-line
+
+  EXPECT_THROW(Convert::Base64Decode("ABD%GA=="), std::runtime_error); // cspell:disable-line
+  EXPECT_THROW(Convert::Base64Decode("ABDE^A=="), std::runtime_error); // cspell:disable-line
+  EXPECT_THROW(Convert::Base64Decode("ABDEF&=="), std::runtime_error); // cspell:disable-line
+
+  EXPECT_THROW(Convert::Base64Decode("AD%GA==="), std::runtime_error); // cspell:disable-line
+  EXPECT_THROW(Convert::Base64Decode("ADE^A==="), std::runtime_error); // cspell:disable-line
+  EXPECT_THROW(Convert::Base64Decode("ADEF&==="), std::runtime_error); // cspell:disable-line
+}
