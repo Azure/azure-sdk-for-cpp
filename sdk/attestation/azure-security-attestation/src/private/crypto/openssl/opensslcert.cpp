52a92237b53c803777c8b3134a77e9b717210f0b
Larry Osterman <LarryOsterman@users.noreply.github.com>
2022-02-02 11:21:49 -0800

    std::find_if is C++17, not C++14

--- sdk/attestation/azure-security-attestation/src/private/crypto/openssl/opensslcert.cpp
+++ sdk/attestation/azure-security-attestation/src/private/crypto/openssl/opensslcert.cpp
@@ -71,20 +71,9 @@ namespace Security {
 
         /// Trim whitespace from the start of the string.
         /// @param s string to trim.
-        static inline void ltrim(std::string& s)
-        {
-          s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
-                    return !std::isspace(ch);
-                  }));
-        }
+        static inline void ltrim(std::string& s) { s.erase(s.find_last_not_of(" \t\r\n") + 1); }
 
-        static inline void rtrim(std::string& s)
-        {
-          s.erase(
-              std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !std::isspace(ch); })
-                  .base(),
-              s.end());
-        }
+        static inline void rtrim(std::string& s) { s.erase(0, s.find_first_not_of(" \t\r\n")); }
 
         static inline void trim(std::string& s)
         {
