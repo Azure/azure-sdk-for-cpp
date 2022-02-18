b8f0bf2374aa1e0bf6547378d3b66218b66542ad
George Arama <50641385+gearama@users.noreply.github.com>
2022-02-18 11:36:58 -0800

    ttt

---
+++ sdk/core/azure-core/test/ut/challenge_test.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#include <gtest/gtest.h>
+#include <azure/core/http/policies/policy.hpp>
+
+#include <memory>
+#include <vector>
+
+using namespace Azure::Core;
+
+
+TEST(ChallengeParameters, emptyString) {
+
+}
