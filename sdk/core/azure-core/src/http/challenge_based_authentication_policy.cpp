c925132872145af57a681d3841ab2aad909ebe24
George Arama <50641385+gearama@users.noreply.github.com>
2022-02-18 18:37:17 -0800

    pedantic char

--- sdk/core/azure-core/src/http/challenge_based_authentication_policy.cpp
+++ sdk/core/azure-core/src/http/challenge_based_authentication_policy.cpp
@@ -24,7 +24,7 @@ std::unique_ptr<RawResponse> ChallengeBasedAuthenticationPolicy::Send(
     }
   }
   return result;
-};
+}
 
 std::string ChallengeBasedAuthenticationPolicy::GetRequestAuthority(Request request)
 {
