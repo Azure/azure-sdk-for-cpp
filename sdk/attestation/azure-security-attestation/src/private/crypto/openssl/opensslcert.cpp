d4c98391dfb4da264ebb10b80b8e416584a8cc69
Jinming Hu <jinmhu@microsoft.com>
2022-02-19 23:44:33 +0800

    r

--- sdk/attestation/azure-security-attestation/src/private/crypto/openssl/opensslcert.cpp
+++ sdk/attestation/azure-security-attestation/src/private/crypto/openssl/opensslcert.cpp
@@ -420,7 +420,7 @@ namespace Azure { namespace Security { namespace Attestation { namespace _detail
 
       // Normalize the expiration time based on the adjustments. This will handle
       // wrapping hours, days, etc.
-#if defined(AZ_PLATFORM_WINDOWS)
+#if _WINDOWS
       expirationTime = _mkgmtime(&currentTime); // cspell:disable-line
 #else
       expirationTime = timegm(&currentTime); // cspell:disable-line
