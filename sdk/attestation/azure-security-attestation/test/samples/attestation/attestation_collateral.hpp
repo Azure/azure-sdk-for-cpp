b20864e278a224f63644a672a6342c8129c21d12
Larry Osterman <LarryOsterman@users.noreply.github.com>
2022-03-03 08:33:14 -0800

    Added some comments

--- sdk/attestation/azure-security-attestation/test/samples/attestation/attestation_collateral.hpp
+++ sdk/attestation/azure-security-attestation/test/samples/attestation/attestation_collateral.hpp
@@ -4,11 +4,22 @@
 
 #include <azure/core/base64.hpp>
 
+/** @brief Collateral used by Attestation APIs
+ *
+ * @details this hides the details of the actual base64url encoded values from sample
+ * implementations.
+ */
 class AttestationCollateral {
 public:
+  /// An Open Enclave report which can be used to test.
   static std::vector<uint8_t> OpenEnclaveReport();
+
+  /// An Sgx Enclave report which can be used to test.
   static std::vector<uint8_t> SgxQuote();
+
+  /// Runtime data which is encapsulated in the quote/reports returned.
   static std::vector<uint8_t> RuntimeData();
 
+  /// Minimal Attestation Policy.
   static std::string GetMinimalPolicy();
 };
