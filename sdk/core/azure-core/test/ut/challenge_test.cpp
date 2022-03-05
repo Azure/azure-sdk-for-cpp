bf4faae3493431e7cffda4e285c8f50c9b739151
George Arama <50641385+gearama@users.noreply.github.com>
2022-03-04 19:13:02 -0800

    clang 2

--- sdk/core/azure-core/test/ut/challenge_test.cpp
+++ sdk/core/azure-core/test/ut/challenge_test.cpp
@@ -25,7 +25,9 @@ TEST(ChallengeParameters, validString)
   ChallengeParameters challenge(validData);
 
   EXPECT_EQ(challenge.Schema, Azure::Core::Http::Policies::_detail::BearerName);
-  EXPECT_EQ(challenge.AuthorizationUri.GetPath(), "72f988bf-86f1-41af-91ab-2d7cd011db47/oauth2/v2.0/token");
+  EXPECT_EQ(
+      challenge.AuthorizationUri.GetPath(),
+      "72f988bf-86f1-41af-91ab-2d7cd011db47/oauth2/v2.0/token");
   EXPECT_EQ(challenge.Scopes.size(), size_t(1));
   EXPECT_EQ(challenge.Scopes[0], "https://vault.azure.net/.default");
   EXPECT_EQ(challenge.TenantId, "72f988bf-86f1-41af-91ab-2d7cd011db47");
@@ -38,7 +40,9 @@ TEST(ChallengeParameters, validString2)
       = "Bearer authorization=\"https://login.windows.net/72f988bf-86f1-41af-91ab-2d7cd011db47\"";
   ChallengeParameters challenge(validData);
   EXPECT_EQ(challenge.Schema, Azure::Core::Http::Policies::_detail::BearerName);
-  EXPECT_EQ(challenge.AuthorizationUri.GetPath(), "72f988bf-86f1-41af-91ab-2d7cd011db47/oauth2/v2.0/token");
+  EXPECT_EQ(
+      challenge.AuthorizationUri.GetPath(),
+      "72f988bf-86f1-41af-91ab-2d7cd011db47/oauth2/v2.0/token");
   EXPECT_EQ(challenge.Scopes.size(), size_t(0));
   EXPECT_EQ(challenge.TenantId, "72f988bf-86f1-41af-91ab-2d7cd011db47");
   EXPECT_FALSE(challenge.IsEmpty());
