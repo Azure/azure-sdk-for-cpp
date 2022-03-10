c8aa80fe12940b84d81187eb4613a9dc4554a5e3
Larry Osterman <LarryOsterman@users.noreply.github.com>
2022-03-09 17:44:10 -0800

    cspell

--- sdk/attestation/azure-security-attestation/test/ut/token_test.cpp
+++ sdk/attestation/azure-security-attestation/test/ut/token_test.cpp
@@ -144,11 +144,13 @@ namespace Azure { namespace Security { namespace Attestation { namespace Test {
   TEST(SerializationTests, TestPolicyCertificateManagementBody)
   {
     PolicyCertificateManagementBody body;
+    // cspell: disable
     body.policyCertificate = JsonWebKeySerializer::Deserialize(json::parse(R"({"kty":"RSA",
        "n": "0vx7agoebGcQSuuPiLJXZptN9nndrQmbXEps2aiAFbWhM78LhWx4cbbfAAtVT86zwu1RK7aPFFxuhDR1L6tSoc_BJECPebWKRXjBZCiFV4n3oknjhMstn64tZ_2W-5JsGY4Hc5n9yBXArwl93lqt7_RN5w6Cf0h4QyQ5v-65YGjQR0_FDW2QvzqY368QQMicAtaSqzs8KJZgnYb9c7d0zgdAZHzu6qMQvRL5hajrn1n91CbOpbISD08qNLyrdkt-bFTWhAI4vMQFh6WeZu0fM4lFd2NcRwr3XPksINHaQ-G_xBniIqbw0Ls1jF44-csFCur-kEgU8awapJzKnqDKgw",
        "e":"AQAB",
        "alg":"RS256",
        "kid":"2011-04-29"})"));
+    // cspell: enable
     std::string serializedBody = PolicyCertificateManagementBodySerializer::Serialize(body);
     auto deserializedBody
         = PolicyCertificateManagementBodySerializer::Deserialize(json::parse(serializedBody));
