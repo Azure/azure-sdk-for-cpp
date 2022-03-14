2fd99a9fe84ec80130db8188b0ff323d9fb1ee87
Daniel Jurek <djurek@microsoft.com>
2022-03-14 09:55:25 -0700

    2022-03-14: azure-core-cpp 1.4.0-beta.1

---
+++ eng/scripts/Update-VcpkgBeta.ps1
@@ -0,0 +1,66 @@
+param(
+    [string] $VcpkgBetaFolder,
+    [string] $VcpkgFolder,
+    [string] $ReleaseArtifactSourceDirectory,
+    [string] $GitCommitParameters
+)
+
+$portFolder = "azure-$Service-cpp"
+
+# To ensure a clean synchronization remove all files at the destination.
+# This ensures that files no longer present in the build output do not
+# persist in later versions.
+Remove-Item "$VcpkgBetaFolder/ports/$portFolder" -Recurse -Force
+
+Copy-Item `
+    -Path "$ReleaseArtifactSourceDirectory/vcpkg/port/*" `
+    -Destination "$VcpkgBetaFolder/vcpkg/ports/$portFolder"
+
+$rawPackageInfo = Get-Content -Raw -Path $ReleaseArtifactSourceDirectory/package-info.json
+$packageInfo = ConvertFrom-Json $rawPackageInfo
+
+$portName = "$($packageInfo.name)-cpp"
+
+# TODO: test?
+# vcpkg install azure-core-cpp --overlay-ports=../azure-sdk-vcpkg-betas/ports/
+
+$originalLocation = Get-Location
+try {
+    & $VcpkgFolder/vcpkg format-manifest `
+        --all `
+        --vcpkg-root =. `
+        --x-scripts-root=$VcpkgFolder/scripts
+
+    Write-Host "git add -A"
+    git add -A
+    Write-Host "git $GitCommitParameters commit -m \"$(Get-Date -Format "yyyy-MM-dd" ): $portName $($packageInfo.version)\""
+    git $GitCommitParameters commit -m "$(Get-Date -Format "yyyy-MM-dd" ): $portName $($packageInfo.version)"
+
+    & $VcpkgFolder/vcpkg x-add-version `
+        --all `
+        --vcpkg-root=. `
+        --x-scripts-root=$VcpkgFolder/scripts
+
+    git add -A
+    git commit --amend --no-edit
+
+    $commitHash = git log -1 --format=format:%H
+
+    # Update vcpkg-configuration.json to include this package and set the
+    # baseline
+    $vcpkgConfigPath = "$VcpkgBetaFolder/vcpkg-configuration.json"
+    $rawVcpkgConfig = Get-Content -Raw -Path $vcpkgConfigPath
+    $vcpkgConfig = ConvertFrom-Json $rawVcpkgConfig
+
+
+    $vcpkgConfig.registries[0].baseline = $commitHash
+    if (!($vcpkgConfig.registries[0].packages -contains $portName)) {
+        $vcpkgConfig.registries[0].packages += $portName
+    }
+
+    $vcpkgConfigJson = ConvertTo-Json $vcpkgConfig -Depth 100
+    Set-Content -Path $vcpkgConfigPath -Value $vcpkgConfigJson
+
+} finally {
+    Set-Location $originalLocation
+}
