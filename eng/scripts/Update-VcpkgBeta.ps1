3ec5c9614d5ea99f3639c2ca3438dff3bb803bce
Daniel Jurek <djurek@microsoft.com>
2022-03-14 10:23:09 -0700

    Script that automates updating vcpkg betas

--- eng/scripts/Update-VcpkgBeta.ps1
+++ eng/scripts/Update-VcpkgBeta.ps1
@@ -10,41 +10,47 @@ param(
 # This ensures that files no longer present in the build output do not
 # persist in later versions.
 Remove-Item "$VcpkgBetaFolder/ports/$VcpkgPortName" -Recurse -Force
-New-Item -ItemType Directory -Path "$VcpkgBetaFolder/vcpkg/ports/$VcpkgPortName"
+New-Item -ItemType Directory -Path "$VcpkgBetaFolder/ports/$VcpkgPortName"
 
 Copy-Item `
     -Path "$ReleaseArtifactSourceDirectory/vcpkg/port/*" `
-    -Destination "$VcpkgBetaFolder/vcpkg/ports/$VcpkgPortName"
+    -Destination "$VcpkgBetaFolder/ports/$VcpkgPortName"
 
 $rawPackageInfo = Get-Content -Raw -Path $ReleaseArtifactSourceDirectory/package-info.json
 $packageInfo = ConvertFrom-Json $rawPackageInfo
-
-$portName = "$($packageInfo.name)-cpp"
 
 # TODO: test?
 # vcpkg install azure-core-cpp --overlay-ports=../azure-sdk-vcpkg-betas/ports/
 
 $originalLocation = Get-Location
 try {
+    Set-Location $VcpkgBetaFolder
+
+    Write-Host "$VcpkgFolder/vcpkg format-manifest --all --vcpkg-root=. --x-scripts-root=$VcpkgFolder/scripts"
     & $VcpkgFolder/vcpkg format-manifest `
         --all `
-        --vcpkg-root =. `
+        --vcpkg-root=. `
         --x-scripts-root=$VcpkgFolder/scripts
 
     Write-Host "git add -A"
     git add -A
-    Write-Host "git $GitCommitParameters commit -m \"$(Get-Date -Format "yyyy-MM-dd" ): $portName $($packageInfo.version)\""
-    git $GitCommitParameters commit -m "$(Get-Date -Format "yyyy-MM-dd" ): $portName $($packageInfo.version)"
+    Write-Host "git $GitCommitParameters commit -m \"$(Get-Date -Format "yyyy-MM-dd" ): $VcpkgPortName $($packageInfo.version)\""
+    git $GitCommitParameters commit -m "$(Get-Date -Format "yyyy-MM-dd" ): $VcpkgPortName $($packageInfo.version)"
 
+    Write-Host "$VcpkgFolder/vcpkg x-add-version --all --vcpkg-root=. --x-scripts-root=$VcpkgFolder/scripts"
     & $VcpkgFolder/vcpkg x-add-version `
         --all `
         --vcpkg-root=. `
         --x-scripts-root=$VcpkgFolder/scripts
 
+    Write-Host "git add -A"
     git add -A
+    Write-Host "git commit --amend --no-edit"
     git commit --amend --no-edit
 
-    $commitHash = git log -1 --format=format:%H
+    Write-Host "git log -1 --format=format:%H"
+    $baseHash = git log -1 --format=format:%H
+    Write-Host "New Baseline: $baseHash"
 
     # Update vcpkg-configuration.json to include this package and set the
     # baseline
@@ -53,9 +59,9 @@ try {
     $vcpkgConfig = ConvertFrom-Json $rawVcpkgConfig
 
 
-    $vcpkgConfig.registries[0].baseline = $commitHash
-    if (!($vcpkgConfig.registries[0].packages -contains $portName)) {
-        $vcpkgConfig.registries[0].packages += $portName
+    $vcpkgConfig.registries[0].baseline = $baseHash
+    if (!($vcpkgConfig.registries[0].packages -contains $VcpkgPortName)) {
+        $vcpkgConfig.registries[0].packages += $VcpkgPortName
     }
 
     $vcpkgConfigJson = ConvertTo-Json $vcpkgConfig -Depth 100
