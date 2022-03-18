78588707f6fe7110137d0f77b00c4a12ba36adb7
George Arama <50641385+gearama@users.noreply.github.com>
2022-03-17 17:55:30 -0700

    added storage

--- samples/integration/vcpkg-all-smoke/src/main.cpp
+++ samples/integration/vcpkg-all-smoke/src/main.cpp
@@ -20,9 +20,9 @@
 #include <azure/keyvault/keyvault_keys.hpp>
 #include <azure/keyvault/keyvault_secrets.hpp>
 #include <azure/storage/blobs.hpp>
-#include <azure/storage/queues.hpp>
 #include <azure/storage/files/datalake.hpp>
 #include <azure/storage/files/shares.hpp>
+#include <azure/storage/queues.hpp>
 
 #include <chrono>
 #include <iostream>
@@ -39,21 +39,37 @@ using namespace Azure::Storage::Files::Shares;
 
 int main()
 {
-  auto tenantId = "tenant";
-  auto clientId = "client";
-  auto clientSecret = "secret";
+  const std::string tenantId = "tenant";
+  const std::string clientId = "client";
+  const std::string clientSecret = "secret";
+  const std::string leaseID = "leaseID";
+  const std::string storageUrl = "https://blob.com";
+
   auto credential
       = std::make_shared<Azure::Identity::ClientSecretCredential>(tenantId, clientId, clientSecret);
- // auto connString = "DefaultEndpointsProtocol=https;AccountName=notReal;AccountKey="
- //                   "333333333333333333333333333333333333333333333333333333333333333333333333333333"
- //                   "3333333333;EndpointSuffix=core.windows.net";
-  auto storageUrl = "https://blob.com";
+
   // instantiate the clients
+  // keyvault
   KeyClient keyClient(std::getenv("AZURE_KEYVAULT_URL"), credential);
   SecretClient secretClient(std::getenv("AZURE_KEYVAULT_URL"), credential);
   CertificateClient certificateClient(std::getenv("AZURE_KEYVAULT_URL"), credential);
+
+  // Storage
   BlobClient blobClient(storageUrl);
   QueueClient queueClient(storageUrl);
+
+  DataLakeDirectoryClient directoryClient(storageUrl);
+  DataLakeFileClient fileClient(storageUrl);
+  DataLakeFileSystemClient fileSystemClient(storageUrl);
+  DataLakePathClient pathClient(storageUrl);
+  DataLakeLeaseClient leaseClient(pathClient, leaseID);
+  DataLakeServiceClient serviceClient(storageUrl);
+
+  ShareClient shareClient(storageUrl);
+  ShareDirectoryClient shareDirectoryClient(storageUrl);
+  ShareFileClient shareFileClient(storageUrl);
+  ShareLeaseClient shareLeaseClient(shareFileClient, leaseID);
+  ShareServiceClient shareServiceClient(storageUrl);
 
   return 0;
 }
