b42a3bc294525432e9eb61d683499b223a6aeb7c
Victor Vazquez <vhvb1989@gmail.com>
2022-03-19 04:09:03 +0000

    reuse macro for samples

--- cmake-modules/AzureBuildTargetForCI.cmake
+++ cmake-modules/AzureBuildTargetForCI.cmake
@@ -4,20 +4,21 @@
 # Defines utility functions to create build targets for CI.
 #
 
-macro(create_per_service_target_build target service)
+macro(create_per_service_target_build target service runAsSample)
 
     file(APPEND ${CMAKE_BINARY_DIR}/${service}-targets-build.txt "${target}\n")
 
-endmacro()
-
-macro(add_sample_for_ci_run service sample_binary)
-    SET(binary "${sample_binary}")
-    if(MSVC)
-        SET(binary "${sample_binary}.exe")
+    # the binary should be run as a sample as well
+    if(${runAsSample})
+        SET(binary "${target}")
+        if(MSVC)
+            SET(binary "${target}.exe")
+        endif()
+        # Samples are run on Release mode.
+        if(CMAKE_GENERATOR MATCHES "Visual Studio.*")
+            SET(binary "Release/${binary}")
+        endif()
+        file(APPEND ${CMAKE_BINARY_DIR}/${service}-samples.txt "${CMAKE_CURRENT_BINARY_DIR}/${binary}\n")
     endif()
-    if(CMAKE_GENERATOR MATCHES "Visual Studio.*")
-        SET(binary "Release/${binary}")
-    endif()
-    file(APPEND ${CMAKE_BINARY_DIR}/${service}-samples.txt "${CMAKE_CURRENT_BINARY_DIR}/${binary}\n")
 
 endmacro()
