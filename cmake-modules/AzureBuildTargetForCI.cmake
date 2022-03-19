b706265b8bcbedf20e702be3e4ce5c5ffddef1da
Victor Vazquez <vhvb1989@gmail.com>
2022-03-19 02:15:52 +0000

    changes

--- cmake-modules/AzureBuildTargetForCI.cmake
+++ cmake-modules/AzureBuildTargetForCI.cmake
@@ -9,3 +9,12 @@ macro(create_per_service_target_build target service)
     file(APPEND ${CMAKE_BINARY_DIR}/${service}-targets-build.txt "${target}\n")
 
 endmacro()
+
+macro(add_sample_for_ci_run service sample_binary)
+    SET(binary "${sample_binary}")
+    if(MSVC)
+        SET(binary "${sample_binary}/Release/${sample_binary}.exe")
+    endif()
+    file(APPEND ${CMAKE_BINARY_DIR}/${service}-samples.txt "${CMAKE_CURRENT_BINARY_DIR}/${binary}\n")
+
+endmacro()
