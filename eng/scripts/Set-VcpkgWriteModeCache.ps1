fe40739072c9ca52b2003f25a0ef730b9bf7933c
Daniel Jurek <djurek@microsoft.com>
2022-03-21 14:49:28 -0700

    Correct pathing for module

--- eng/scripts/Set-VcpkgWriteModeCache.ps1
+++ eng/scripts/Set-VcpkgWriteModeCache.ps1
@@ -1,7 +1,7 @@
 param(
     [string] $StorageAccountKey
 )
-. $PSScriptRoot/Helpers/PSModule-Helpers.ps1
+. $PSScriptRoot/../common/scripts/Helpers/PSModule-Helpers.ps1
 
 Install-ModuleIfNotInstalled "Az.Storage" "4.3.0" | Import-Module
 
