83ab5c338ba187e7e19d3db49253f5956455f10b
George Arama <50641385+gearama@users.noreply.github.com>
2022-04-11 15:29:03 -0700

    Update service version for keyvault Secrets to 7.3 (#3540)
    * one commit to rule them all
    
    * Updated service version to 7.3
    
    * support v7.2
    
    * typo

--- sdk/keyvault/azure-security-keyvault-secrets/test/ut/secret_client_test.cpp
+++ sdk/keyvault/azure-security-keyvault-secrets/test/ut/secret_client_test.cpp
@@ -29,11 +29,11 @@ TEST(SecretClient, ServiceVersion)
   auto credential
       = std::make_shared<Azure::Identity::ClientSecretCredential>("tenantID", "AppId", "SecretId");
   {
-    // 7.2
+    // 7.3
     EXPECT_NO_THROW(
-        auto options = SecretClientOptions(ServiceVersion::V7_2);
+        auto options = SecretClientOptions(ServiceVersion::V7_3);
         SecretClient SecretClient("http://account.vault.azure.net", credential, options);
-        EXPECT_EQ(options.Version.ToString(), "7.2"););
+        EXPECT_EQ(options.Version.ToString(), "7.3"););
   }
   {
     // arbitrary version
