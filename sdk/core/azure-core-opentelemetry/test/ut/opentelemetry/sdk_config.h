215aeec46fd2b196dfe3214c0605c1b12d168f18
Larry Osterman <LarryOsterman@users.noreply.github.com>
2022-04-18 09:38:12 -0700

    Removed unnecessary sdk_config.h workaround; set opentelemetry version in vcpkg.json

--- sdk/core/azure-core-opentelemetry/test/ut/opentelemetry/sdk_config.h
+++
@@ -1,9 +0,0 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
-// The OpenTelemetry vcpkg package appears to be missing the file sdk_config.h, this file is a replacement
-// to enable the use of in-memory exporters.
-#pragma once
-
-#include "opentelemetry/config.h"
-#include "opentelemetry/sdk/common/global_log_handler.h"
