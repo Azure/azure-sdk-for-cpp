b4ab09fe463f80015970e1a436e654cc9e454eb7
Larry Osterman <LarryOsterman@users.noreply.github.com>
2022-05-16 11:43:08 -0700

    Added some test cases for code coverage

--- sdk/core/azure-core/test/ut/service_tracing_test.cpp
+++ sdk/core/azure-core/test/ut/service_tracing_test.cpp
@@ -53,10 +53,87 @@ TEST(ServiceTracing, SimpleServiceSpanTests)
   }
 }
 
+// Dummy service tracing class.
+class TestSpan final : public Azure::Core::Tracing::_internal::Span {
+public:
+  TestSpan() : Azure::Core::Tracing::_internal::Span() {}
+  void Azure::Core::Tracing::_internal::Span::End(Azure::Nullable<Azure::DateTime>) override {}
+  void Azure::Core::Tracing::_internal::Span::AddAttributes(
+      const Azure::Core::Tracing::_internal::AttributeSet&) override
+  {
+  }
+  void Azure::Core::Tracing::_internal::Span::AddEvent(const std::exception&) override {}
+  void Azure::Core::Tracing::_internal::Span::AddEvent(const std::string&) override {}
+  void Azure::Core::Tracing::_internal::Span::AddEvent(
+      const std::string&,
+      const Azure::Core::Tracing::_internal::AttributeSet&) override
+  {
+  }
+  void Azure::Core::Tracing::_internal::Span::SetStatus(
+      const Azure::Core::Tracing::_internal::SpanStatus&,
+      const std::string&) override
+  {
+  }
+};
+
+class TestAttributeSet : public Azure::Core::Tracing::_internal::AttributeSet {
+public:
+  TestAttributeSet() : Azure::Core::Tracing::_internal::AttributeSet() {}
+
+  // Inherited via AttributeSet
+  virtual void AddAttribute(std::string const&, bool) override {}
+  virtual void AddAttribute(std::string const&, int32_t) override {}
+  virtual void AddAttribute(std::string const&, int64_t) override {}
+  virtual void AddAttribute(std::string const&, uint64_t) override {}
+  virtual void AddAttribute(std::string const&, double) override {}
+  virtual void AddAttribute(std::string const&, const char*) override {}
+  virtual void AddAttribute(std::string const&, std::string const&) override {}
+};
+class TestTracer final : public Azure::Core::Tracing::_internal::Tracer {
+public:
+  TestTracer(std::string const&, std::string const&) : Azure::Core::Tracing::_internal::Tracer() {}
+  std::shared_ptr<Span> CreateSpan(std::string const&, CreateSpanOptions const&) const override
+  {
+    return std::make_shared<TestSpan>();
+  }
+
+  std::unique_ptr<AttributeSet> CreateAttributeSet() const override
+  {
+    return std::make_unique<TestAttributeSet>();
+  };
+};
+
+class TestTracingProvider final : public Azure::Core::Tracing::TracerProvider {
+public:
+  TestTracingProvider() : TracerProvider() {}
+  ~TestTracingProvider() {}
+  std::shared_ptr<Azure::Core::Tracing::_internal::Tracer> CreateTracer(
+      std::string const& serviceName,
+      std::string const& serviceVersion) const override
+  {
+    return std::make_shared<TestTracer>(serviceName, serviceVersion);
+  };
+};
+
 TEST(ServiceTracing, BasicServiceSpanTests)
 {
   {
     Azure::Core::_internal::ClientOptions clientOptions;
+    Azure::Core::Tracing::_internal::ServiceTracing serviceTrace(
+        clientOptions, "my-service-cpp", "1.0b2");
+
+    auto contextAndSpan = serviceTrace.CreateSpan("My API", {});
+    auto span = std::move(contextAndSpan.second);
+
+    span.End();
+    span.AddEvent("New Event");
+    span.AddEvent(std::runtime_error("Exception"));
+    span.SetStatus(SpanStatus::Error);
+  }
+  {
+    Azure::Core::_internal::ClientOptions clientOptions;
+    auto testTracer = std::make_shared<TestTracingProvider>();
+    clientOptions.Telemetry.TracingProvider = testTracer;
     Azure::Core::Tracing::_internal::ServiceTracing serviceTrace(
         clientOptions, "my-service-cpp", "1.0b2");
 
