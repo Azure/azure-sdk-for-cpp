2ebbf872a6da98ed4aa8e632edf3cea598be233a
Emma Zhu <emmazhu@microsoft.com>
2022-06-05 17:30:05 +0800

    Add directory download with pageable listing

--- sdk/storage/azure-storage-datamovement/samples/datamovement_getting_started.cpp
+++ sdk/storage/azure-storage-datamovement/samples/datamovement_getting_started.cpp
@@ -28,7 +28,7 @@ int main()
   using namespace Azure::Storage;
   using namespace Azure::Storage::Blobs;
 
-  const std::string containerName = "dmcontainer";
+  const std::string containerName = "acontainer111";
   const std::string blobName = "blockblob";
   const std::string localFile = "D:\\work\\tempfile";
   const std::string localDirectory = "sample-localdir";
@@ -38,35 +38,16 @@ int main()
   auto blobContainerClient
       = BlobContainerClient::CreateFromConnectionString(GetConnectionString(), containerName);
   blobContainerClient.CreateIfNotExists();
-  auto blobClient = blobContainerClient.GetBlobClient(blobName);
 
-  auto job1 = m.ScheduleUpload(localFile, blobClient);
-  std::cout << job1.Id << std::endl;
-  std::cout << job1.SourceUrl << " -> " << job1.DestinationUrl << std::endl;
-
-  auto blobFolder = BlobFolder(blobContainerClient, localDirectory);
-  auto job2 = m.ScheduleUploadDirectory(localDirectory, blobFolder);
+  auto blobFolder = BlobFolder(blobContainerClient, "testfolder");
+  auto job2 = m.ScheduleDownloadDirectory(blobFolder, "D:\\work\\testfolder1");
   std::cout << job2.Id << std::endl;
   std::cout << job2.SourceUrl << " -> " << job2.DestinationUrl << std::endl;
 
-  auto job1Status = job1.WaitHandle.get();
-  if (job1Status == JobStatus::Succeeded)
-  {
-    std::cout << "job1 succeeded" << std::endl;
-  }
   auto job2Status = job2.WaitHandle.get();
   if (job2Status == JobStatus::Succeeded)
   {
     std::cout << "job2 succeeded" << std::endl;
-  }
-
-  auto job3 = m.ScheduleDownload(blobClient, localFile + "_2");
-  std::cout << job3.Id << std::endl;
-  std::cout << job3.SourceUrl << " -> " << job3.DestinationUrl << std::endl;
-  auto job3Status = job3.WaitHandle.get();
-  if (job3Status == JobStatus::Succeeded)
-  {
-    std::cout << "job3 succeeded" << std::endl;
   }
   return 0;
 }
