803c1ccd013226875755b753533a10f351332a1b
Larry Osterman <LarryOsterman@users.noreply.github.com>
2022-06-06 16:03:26 -0700

    More branches

--- sdk/template/azure-template/test/ut/template_test.cpp
+++ sdk/template/azure-template/test/ut/template_test.cpp
@@ -16,4 +16,9 @@ TEST(Template, GetValue)
   EXPECT_EQ(templateClient.GetValue(-1), 0);
   EXPECT_EQ(templateClient.GetValue(0), 0);
   EXPECT_EQ(templateClient.GetValue(1), 10);
+  EXPECT_EQ(templateClient.GetValue(22), -100);
+  EXPECT_EQ(templateClient.GetValue(21), 150);
+  EXPECT_EQ(templateClient.GetValue(11), 20);
+  EXPECT_EQ(templateClient.GetValue(14), 1);
+  EXPECT_EQ(templateClient.GetValue(18), 1);
 }
