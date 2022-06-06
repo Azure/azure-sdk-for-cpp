2fdc39e346f28af1c3dcb32d805eb21166fd4b36
Larry Osterman <LarryOsterman@users.noreply.github.com>
2022-06-06 10:18:04 -0700

    Noise reduction

--- sdk/attestation/azure-security-attestation/inc/azure/attestation/attestation_administration_client.hpp
+++ sdk/attestation/azure-security-attestation/inc/azure/attestation/attestation_administration_client.hpp
@@ -69,10 +69,7 @@ namespace Azure { namespace Security { namespace Attestation {
      * @param attestationClient An existing attestation client.
      */
     AttestationAdministrationClient(AttestationAdministrationClient const& attestationClient)
-        : m_endpoint(attestationClient.m_endpoint), m_apiVersion(attestationClient.m_apiVersion),
-          m_pipeline(attestationClient.m_pipeline),
-          m_tokenValidationOptions(attestationClient.m_tokenValidationOptions),
-          m_attestationSigners(attestationClient.m_attestationSigners){};
+        = default;
 
     /**
      * @brief Destructor.
