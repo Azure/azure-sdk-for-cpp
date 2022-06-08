471f9fc433f741b83dd4ce0947fd2c9345ce0204
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2022-06-08 15:44:49 -0700

    Make sample service an object library

--- samples/helpers/service/inc/azure/service/client.hpp
+++ samples/helpers/service/inc/azure/service/client.hpp
@@ -3,9 +3,11 @@
 
 #pragma once
 
+#include <azure/core/context.hpp>
 #include <azure/core/credentials/credentials.hpp>
 
 #include <memory>
+#include <string>
 #include <utility>
 
 namespace Azure { namespace Service {
@@ -26,32 +28,13 @@ namespace Azure { namespace Service {
       static_cast<void>(serviceUrl); // to suppress the "unused variable" warning.
     }
 
-    void DoSomething(const Core::Context& context) const
-    {
-      static_cast<void>(context); // to suppress the "unused variable" warning.
-
-      // This method does nothing, because the purpose of this class is to demonstrate
-      // how Azure::Identity classes can be used with a generic Azure SDK service client.
-      // If we have code here that gets the token, it would be up to the user to set it up to be
-      // valid enough to get a token, which is not critical for the intended demonstration purposes.
-      // And if user runs this, and authentication is unsuccessful, it may draw an unnecessary
-      // attention to an irrelevant (to the demo) point.
-
-      // But an oversimplified logic of what a typical Azure SDK client does is below:
-#if (0)
-      // Every client has its own scope. We use management.azure.com here as an example.
-      Core::Credentials::TokenRequestContext azureServiceClientContext;
-      azureServiceClientContext.Scopes = {"https://management.azure.com/"};
-
-      auto authenticationToken = m_credential->GetToken(azureServiceClientContext, context);
-
-      // Now that it has a token, Client can authorize and DoSomething().
-      // ...
-      // ...
-
-      static_cast<void>(authenticationToken); // to suppress the "unused variable" warning.
-#endif
-    }
+    // This method does nothing, because the purpose of this class is to demonstrate how
+    // Azure::Identity classes can be used with a generic Azure SDK service client. If we have code
+    // here that gets the token, it would be up to the user to set it up to be valid enough to get a
+    // token, which is not critical for the intended demonstration purposes. And if user runs this,
+    // and authentication is unsuccessful, it may draw an unnecessary attention to an irrelevant (to
+    // the demo) point.
+    void DoSomething(const Core::Context& context) const;
   };
 
 }} // namespace Azure::Service
