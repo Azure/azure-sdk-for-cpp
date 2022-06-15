846f60b14fb4a2b4033f4f9b3ff80ef0de85b948
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2022-06-14 20:09:59 -0700

    Update cpp

--- sdk/core/azure-core/src/http/http_sanitizer.cpp
+++ sdk/core/azure-core/src/http/http_sanitizer.cpp
@@ -1,89 +1,86 @@
 
-#include "azure/core/internal/input_sanitizer.hpp"
+#include "azure/core/internal/http/http_sanitizer.hpp"
 #include "azure/core/url.hpp"
 #include <regex>
 #include <sstream>
 
-namespace Azure { namespace Core { namespace _internal {
+namespace {
+std::string const RedactedPlaceholder = "REDACTED";
+}
 
-  const char* InputSanitizer::m_RedactedPlaceholder = "REDACTED";
+using Azure::Core::Http::_internal::HttpSanitizer;
 
-  Azure::Core::Url InputSanitizer::SanitizeUrl(Azure::Core::Url const& url) const
+Azure::Core::Url HttpSanitizer::SanitizeUrl(Azure::Core::Url const& url) const
+{
+  std::ostringstream ss;
+
+  // Sanitize the non-query part of the URL (remove username and password).
+  if (!url.GetScheme().empty())
   {
-    std::ostringstream ss;
+    ss << url.GetScheme() << "://";
+  }
+  ss << url.GetHost();
+  if (url.GetPort() != 0)
+  {
+    ss << ":" << url.GetPort();
+  }
+  if (!url.GetPath().empty())
+  {
+    ss << "/" << url.GetPath();
+  }
 
-    // Sanitize the non-query part of the URL (remove username and password).
-    if (!url.GetScheme().empty())
-    {
-      ss << url.GetScheme() << "://";
-    }
-    ss << url.GetHost();
-    if (url.GetPort() != 0)
-    {
-      ss << ":" << url.GetPort();
-    }
-    if (!url.GetPath().empty())
-    {
-      ss << "/" << url.GetPath();
-    }
+  {
+    auto encodedRequestQueryParams = url.GetQueryParameters();
 
+    std::remove_const<std::remove_reference<decltype(encodedRequestQueryParams)>::type>::type
+        loggedQueryParams;
+
+    if (!encodedRequestQueryParams.empty())
     {
-      auto encodedRequestQueryParams = url.GetQueryParameters();
-
-      std::remove_const<std::remove_reference<decltype(encodedRequestQueryParams)>::type>::type
-          loggedQueryParams;
-
-      if (!encodedRequestQueryParams.empty())
+      auto const& unencodedAllowedQueryParams = m_allowedHttpQueryParameters;
+      if (!unencodedAllowedQueryParams.empty())
       {
-        auto const& unencodedAllowedQueryParams = m_allowedHttpQueryParameters;
-        if (!unencodedAllowedQueryParams.empty())
-        {
-          std::remove_const<std::remove_reference<decltype(unencodedAllowedQueryParams)>::type>::
-              type encodedAllowedQueryParams;
-          std::transform(
-              unencodedAllowedQueryParams.begin(),
-              unencodedAllowedQueryParams.end(),
-              std::inserter(encodedAllowedQueryParams, encodedAllowedQueryParams.begin()),
-              [](std::string const& s) { return Url::Encode(s); });
+        std::remove_const<std::remove_reference<decltype(unencodedAllowedQueryParams)>::type>::type
+            encodedAllowedQueryParams;
+        std::transform(
+            unencodedAllowedQueryParams.begin(),
+            unencodedAllowedQueryParams.end(),
+            std::inserter(encodedAllowedQueryParams, encodedAllowedQueryParams.begin()),
+            [](std::string const& s) { return Url::Encode(s); });
 
-          for (auto const& encodedRequestQueryParam : encodedRequestQueryParams)
-          {
-            if (encodedRequestQueryParam.second.empty()
-                || (encodedAllowedQueryParams.find(encodedRequestQueryParam.first)
-                    != encodedAllowedQueryParams.end()))
-            {
-              loggedQueryParams.insert(encodedRequestQueryParam);
-            }
-            else
-            {
-              loggedQueryParams.insert(
-                  std::make_pair(encodedRequestQueryParam.first, m_RedactedPlaceholder));
-            }
-          }
-        }
-        else
+        for (auto const& encodedRequestQueryParam : encodedRequestQueryParams)
         {
-          for (auto const& encodedRequestQueryParam : encodedRequestQueryParams)
+          if (encodedRequestQueryParam.second.empty()
+              || (encodedAllowedQueryParams.find(encodedRequestQueryParam.first)
+                  != encodedAllowedQueryParams.end()))
+          {
+            loggedQueryParams.insert(encodedRequestQueryParam);
+          }
+          else
           {
             loggedQueryParams.insert(
-                std::make_pair(encodedRequestQueryParam.first, m_RedactedPlaceholder));
+                std::make_pair(encodedRequestQueryParam.first, RedactedPlaceholder));
           }
         }
-
-        ss << Azure::Core::_detail::FormatEncodedUrlQueryParameters(loggedQueryParams);
       }
-    }
-    return Azure::Core::Url(ss.str());
-  }
+      else
+      {
+        for (auto const& encodedRequestQueryParam : encodedRequestQueryParams)
+        {
+          loggedQueryParams.insert(
+              std::make_pair(encodedRequestQueryParam.first, RedactedPlaceholder));
+        }
+      }
 
-  std::string InputSanitizer::SanitizeHeader(std::string const& header, std::string const& value)
-      const
-  {
-    if (m_allowedHttpHeaders.find(header) != m_allowedHttpHeaders.end())
-    {
-      return value;
+      ss << Azure::Core::_detail::FormatEncodedUrlQueryParameters(loggedQueryParams);
     }
-    return m_RedactedPlaceholder;
   }
+  return Azure::Core::Url(ss.str());
+}
 
-}}} // namespace Azure::Core::_internal
+std::string HttpSanitizer::SanitizeHeader(std::string const& header, std::string const& value)
+    const
+{
+  return (m_allowedHttpHeaders.find(header) != m_allowedHttpHeaders.end()) ? value
+                                                                           : RedactedPlaceholder;
+}
