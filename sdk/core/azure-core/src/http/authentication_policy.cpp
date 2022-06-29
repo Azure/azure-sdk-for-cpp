66191d8282808f19149378766162aad5af644294
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2022-06-29 08:38:15 -0700

    Build fix

--- sdk/core/azure-core/src/http/authentication_policy.cpp
+++ sdk/core/azure-core/src/http/authentication_policy.cpp
@@ -49,7 +49,7 @@ void BearerTokenAuthenticationPolicy::AuthenticateAndAuthorizeRequest(
   // Refresh the token in 2 or less minutes before the actual expiration.
   if (std::chrono::system_clock::now() > (m_accessToken.ExpiresOn - std::chrono::minutes(2)))
   {
-    m_accessToken = m_credential->GetToken(tokenRequestContext, context);
+    m_accessToken = m_credential->GetToken(TokenRequestContext, context);
   }
 
   request.SetHeader("authorization", "Bearer " + m_accessToken.Token);
