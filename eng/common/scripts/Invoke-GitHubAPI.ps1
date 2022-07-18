be3f7a17ea9e57a08ae92295900713c3dbd6e8d0
Daniel Jurek <djurek@microsoft.com>
2022-07-18 13:46:13 -0700

    Macro syntax with a variable set earlier

--- eng/common/scripts/Invoke-GitHubAPI.ps1
+++ eng/common/scripts/Invoke-GitHubAPI.ps1
@@ -60,8 +60,6 @@ function Get-GitHubPullRequests {
   if ($Sort) { $uri += "sort=$Sort&" }
   if ($Direction){ $uri += "direction=$Direction&" }
 
-  Write-Host "Get PRs: $uri"
-
   return Invoke-RestMethod `
           -Method GET `
           -Uri $uri `
