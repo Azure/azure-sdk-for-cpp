c5b3adc0a8eabe9e659bfea91822a369b004f5c4
Daniel Jurek <djurek@microsoft.com>
2022-07-18 11:24:10 -0700

    Output GH PR URI

--- eng/common/scripts/Invoke-GitHubAPI.ps1
+++ eng/common/scripts/Invoke-GitHubAPI.ps1
@@ -60,6 +60,8 @@ function Get-GitHubPullRequests {
   if ($Sort) { $uri += "sort=$Sort&" }
   if ($Direction){ $uri += "direction=$Direction&" }
 
+  Write-Host "Get PRs: $uri"
+
   return Invoke-RestMethod `
           -Method GET `
           -Uri $uri `
