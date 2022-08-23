847240cc181bcfee9d09cb4e809389fb3b70fc97
Larry Osterman <LarryOsterman@users.noreply.github.com>
2022-08-23 12:18:31 -0700

    Traces for ubuntu install

--- eng/scripts/Install-WSL.ps1
+++ eng/scripts/Install-WSL.ps1
@@ -1,3 +1,4 @@
+write-host "WSL install of ubuntu."
 wsl --install -d Ubuntu-20.04
 
 [console]::OutputEncoding = New-Object System.Text.UnicodeEncoding
@@ -5,7 +6,9 @@ $wsl = wsl l -v | out-string
 
 write-host $wsl
 while ($wsl -match 'installing') {
+	write-host "Polling ..."
 	start-sleep -seconds 1
 	$wsl = wsl l -v | out-string
 	write-host $wsl
 }
+write-host "Ubuntu installed."
