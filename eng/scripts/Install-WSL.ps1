1662c6e27cfa6be2e17e9f46dc195cae87b0753c
Larry Osterman <LarryOsterman@users.noreply.github.com>
2022-08-23 12:29:39 -0700

    launch ubuntu fix

--- eng/scripts/Install-WSL.ps1
+++ eng/scripts/Install-WSL.ps1
@@ -2,13 +2,13 @@ write-host "WSL install of ubuntu."
 wsl --install -d Ubuntu-20.04
 
 [console]::OutputEncoding = New-Object System.Text.UnicodeEncoding
-$wsl = wsl l -v | out-string
+$wsl = wsl -l -v | out-string
 
 write-host $wsl
 while ($wsl -match 'installing') {
 	write-host "Polling ..."
 	start-sleep -seconds 1
-	$wsl = wsl l -v | out-string
+	$wsl = wsl -l -v | out-string
 	write-host $wsl
 }
 write-host "Ubuntu installed."
