fc8f5a313b641a551d861e6752943b723017a69b
Larry Osterman <LarryOsterman@users.noreply.github.com>
2022-08-23 13:50:02 -0700

    More configuration of wsl

--- eng/scripts/Install-WSL.ps1
+++ eng/scripts/Install-WSL.ps1
@@ -1,12 +1,12 @@
 write-host "WSL install of ubuntu."
 wsl --install -d Ubuntu-20.04
 
+write-host "Launch WSL."
 [console]::OutputEncoding = New-Object System.Text.UnicodeEncoding
 $wsl = wsl -l -v | out-string
 
 write-host $wsl
 while ($wsl -match 'installing') {
-	write-host "Polling ..."
 	start-sleep -seconds 1
 	$wsl = wsl -l -v | out-string
 	write-host $wsl
