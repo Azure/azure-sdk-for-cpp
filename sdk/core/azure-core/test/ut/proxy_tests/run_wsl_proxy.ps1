2225b3e60c1a78af460de180c0c57f7e01a085a0
Larry Osterman <LarryOsterman@users.noreply.github.com>
2022-08-24 10:39:54 -0700

    Moved proxy launch to a script

---
+++ sdk/core/azure-core/test/ut/proxy_tests/run_wsl_proxy.ps1
@@ -0,0 +1,21 @@
+copy localproxy/squid.conf \\wsl$\Ubuntu-20.04\etc\squid\squid.conf
+copy localproxy.passwd/proxypasswd //wsl$/Ubuntu-20.04/tmp/proxypasswd
+copy localproxy.passwd/squid.conf \\wsl$\Ubuntu-20.04\tmp\squid.conf
+
+write-host "Verify contents of /etc/squid and /tmp to confirm copy succeeded."
+wsl ls -l /etc/squid; ls -l /tmp
+
+write-host "Launch anonymous squid server."
+wsl sudo squid -f /etc/squid/squid.conf
+if (!$?) {
+	write-error "Failed to launch anonymous server."
+}
+
+write-host "Launch authenticated squid server."
+wsl sudo squid -f /tmp/squid.conf
+if (!$?) {
+	write-error "Failed to launch authenticated server."
+}
+
+write-host "Dump processes; verify two copies of squid running."
+wsl ps auxf
