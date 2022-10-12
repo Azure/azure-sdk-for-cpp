14e0c8609e0c05481b7387b233234df599296d1e
Larry Osterman <LarryOsterman@users.noreply.github.com>
2022-10-12 10:20:30 -0700

    Update sdk/core/azure-core/src/http/winhttp/win_http_request.hpp
    Co-authored-by: Anton Kolesnyk <41349689+antkmsft@users.noreply.github.com>

--- sdk/core/azure-core/src/http/winhttp/win_http_request.hpp
+++ sdk/core/azure-core/src/http/winhttp/win_http_request.hpp
@@ -23,7 +23,7 @@ namespace Azure { namespace Core { namespace Http { namespace _detail {
   /**
    * @brief An outstanding WinHTTP action. This object is used process asynchronous WinHTTP actions.
    *
-   * The WinHttpRequest object has a WinHttpAction associated it to convert asynchronous winhttp
+   * The WinHttpRequest object has a WinHttpAction associated it to convert asynchronous WinHTTP
    * operations to synchronous operations.
    *
    */
