e488a81a03db403a8ea29400f04cf5c54bfdf04b
Larry Osterman <LarryOsterman@users.noreply.github.com>
2022-10-13 14:06:05 -0700

    Improved discoverability of tests; handle exceptions thrown during test execution (#4021)
    * Improved discoverability of tests on WIndows; handle exceptions thrown during test execution better

--- sdk/core/perf/test/src/perf_test.cpp
+++ sdk/core/perf/test/src/perf_test.cpp
@@ -5,6 +5,7 @@
 
 #include "azure/perf/test/delay_test.hpp"
 #include "azure/perf/test/extended_options_test.hpp"
+#include "azure/perf/test/http_pipeline_get_test.hpp"
 #if defined(BUILD_CURL_HTTP_TRANSPORT_ADAPTER)
 #include "azure/perf/test/curl_http_client_get_test.hpp"
 #endif
@@ -24,7 +25,9 @@ int main(int argc, char** argv)
       Azure::Perf::Test::NoOp::GetTestMetadata(),
       Azure::Perf::Test::ExtendedOptionsTest::GetTestMetadata(),
       Azure::Perf::Test::DelayTest::GetTestMetadata(),
-      Azure::Perf::Test::ExceptionTest::GetTestMetadata()};
+      Azure::Perf::Test::ExceptionTest::GetTestMetadata(),
+      Azure::Perf::Test::HttpPipelineGetTest::GetTestMetadata(),
+  };
 
 #if defined(BUILD_CURL_HTTP_TRANSPORT_ADAPTER)
   tests.emplace_back(Azure::Perf::Test::CurlHttpClientGetTest::GetTestMetadata());
