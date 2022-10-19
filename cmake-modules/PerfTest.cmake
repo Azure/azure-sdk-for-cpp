76273e74a9b2ac8a4bcdd4b9e55f75037a04222e
George Arama <50641385+gearama@users.noreply.github.com>
2022-10-19 14:05:06 -0700

    eewew

--- cmake-modules/PerfTest.cmake
+++ cmake-modules/PerfTest.cmake
@@ -5,7 +5,20 @@ macro(SetPerfDeps PACKAGE VAR_RESULT)
     if(DEFINED ENV{${VAR_TRIGGER}})
         find_package(${PACKAGE} $ENV{${VAR_TRIGGER}} EXACT)
         add_compile_definitions(${VAR_RESULT}="$ENV{${VAR_TRIGGER}}")
+        find_path(WASTORAGE_INCLUDE_DIR /azure/storage/blobs.h)
+        include_directories(${WASTORAGE_INCLUDE_DIR})
+        # Include the headers from the project.
+        target_include_directories(
+          azure-storage-blobs-perf
+            PUBLIC
+            ${WASTORAGE_INCLUDE_DIR}>
+)
     else()
         add_compile_definitions(${VAR_RESULT}="source")
+        target_include_directories(
+          azure-storage-blobs-perf
+          PUBLIC
+          $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/inc>
+        )
     endif()
 endmacro()
