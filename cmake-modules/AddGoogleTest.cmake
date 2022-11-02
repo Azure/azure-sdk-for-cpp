1da78ce13e22efded724d0196b8611c63ecff24d
George Arama <50641385+gearama@users.noreply.github.com>
2022-11-02 15:46:38 -0700

    Gtest timeout fix (#4078)
    * PT1
    
    * typo
    
    * timeout
    
    * gtest_1.12.1
    
    * cspell isnore cmakelistst , it seem to freeze when checking these files
    
    * timeout for cmake generate
    
    * timeout for cmake generate p2
    
    * timeout for cmake generate p3
    
    * timeout for cmake generate p4
    
    * timeout for cmake generate p5
    
    * PR
    
    * node 18
    
    * nppm
    
    * npm
    
    * bug in cspell, freezed on txt files
    
    * AntonComment extra space
    
    * cspell

--- cmake-modules/AddGoogleTest.cmake
+++ cmake-modules/AddGoogleTest.cmake
@@ -15,7 +15,7 @@ if(CMAKE_VERSION VERSION_LESS 3.11)
     download_project(
         PROJ                googletest
 		GIT_REPOSITORY      https://github.com/google/googletest.git
-		GIT_TAG             release-1.11.0
+		GIT_TAG             release-1.12.1
 		UPDATE_DISCONNECTED 1
 		QUIET
     )
@@ -29,7 +29,7 @@ else()
     FetchContent_Declare(
         googletest
         GIT_REPOSITORY      https://github.com/google/googletest.git
-        GIT_TAG             release-1.11.0
+        GIT_TAG             release-1.12.1
         )
     FetchContent_GetProperties(googletest)
     if(NOT googletest_POPULATED)
