0e3731be49a18e8a70c39608193b9178fcf76454
George Arama <50641385+gearama@users.noreply.github.com>
2022-11-30 16:48:39 -0800

    storage recordings

--- sdk/storage/azure-storage-blobs/test/ut/blob_batch_client_test.cpp
+++ sdk/storage/azure-storage-blobs/test/ut/blob_batch_client_test.cpp
@@ -32,7 +32,7 @@ namespace Azure { namespace Storage { namespace Test {
     }
   };
 
-  TEST_F(BlobBatchClientTest, SubmitDeleteBatch)
+  TEST_F(BlobBatchClientTest, SubmitDeleteBatch_LIVEONLY_)
   {
     const std::string testName = GetTestNameLowerCase();
 
@@ -74,7 +74,7 @@ namespace Azure { namespace Storage { namespace Test {
     container2Client.Delete();
   }
 
-  TEST_F(BlobBatchClientTest, SnapshotVersion)
+  TEST_F(BlobBatchClientTest, SnapshotVersion_LIVEONLY_)
   {
     const std::string testName = GetTestNameLowerCase();
 
@@ -161,7 +161,7 @@ namespace Azure { namespace Storage { namespace Test {
     serviceClient.DeleteBlobContainer(containerName);
   }
 
-  TEST_F(BlobBatchClientTest, TokenAuthorization)
+  TEST_F(BlobBatchClientTest, TokenAuthorization_LIVEONLY_)
   {
     const std::string testName = GetTestNameLowerCase();
 
