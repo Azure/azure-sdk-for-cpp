0dc2f8e3bce15da514e2ab6229de75d6c173db28
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2022-12-07 13:15:14 -0800

    Only allow BearerTokenAuthPolicy for HTTPS

--- sdk/core/azure-core/src/http/bearer_token_authentication_policy.cpp
+++ sdk/core/azure-core/src/http/bearer_token_authentication_policy.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
 
+#include "azure/core/credentials/credentials.hpp"
 #include "azure/core/http/policies/policy.hpp"
 
 #include <chrono>
@@ -9,12 +10,19 @@ using Azure::Core::Context;
 using namespace Azure::Core::Http;
 using namespace Azure::Core::Http::Policies;
 using namespace Azure::Core::Http::Policies::_internal;
+using Azure::Core::Credentials::AuthenticationException;
 
 std::unique_ptr<RawResponse> BearerTokenAuthenticationPolicy::Send(
     Request& request,
     NextHttpPolicy nextPolicy,
     Context const& context) const
 {
+  if (request.GetUrl().GetScheme() != "https")
+  {
+    throw AuthenticationException(
+        "Bearer token authentication is not permitted for non TLS protected (https) endpoints.");
+  }
+
   {
     std::lock_guard<std::mutex> lock(m_accessTokenMutex);
 
