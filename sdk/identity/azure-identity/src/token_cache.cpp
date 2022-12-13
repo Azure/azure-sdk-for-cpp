bebffe78a2fb47a5aafa20391d6d858359237456
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2022-12-13 10:15:57 -0800

    text correction

--- sdk/identity/azure-identity/src/token_cache.cpp
+++ sdk/identity/azure-identity/src/token_cache.cpp
@@ -123,7 +123,7 @@ namespace {
 template <
     typename T, // Type
     T L = 0, // Left hand side
-    T R = 1, // Right hand size
+    T R = 1, // Right hand side
     size_t N = 0, // Counter (for array)
     bool X = ((std::numeric_limits<T>::max() - L) < R)> // Condition to stop (integer overflow of T)
 struct SortedFibonacciSequence
