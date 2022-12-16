6cad8e41506d0e4da52a9434d30daaa9b5fdf915
George Arama <50641385+gearama@users.noreply.github.com>
2022-12-15 18:34:00 -0800

    skeletor

---
+++ sdk/keyvault/azure-security-keyvault-administration/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Release History
+
+## 4.0.0-beta.1 (Unrleased)
+
+- initial preview
---
+++ sdk/keyvault/azure-security-keyvault-administration/inc/azure/keyvault/administration.hpp
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+/**
+ * @brief Includes all public headers from Azure Template SDK library.
+ *
+ */
+
+#pragma once
+
+#include "azure/keyvault/administration/dll_import_export.hpp"
+#include "azure/keyvault/administration/rtti.hpp"
