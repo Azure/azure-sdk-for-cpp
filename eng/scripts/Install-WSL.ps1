78952195e82150954747e22b58f0318e5a823a5c
Daniel Jurek <djurek@microsoft.com>
2022-12-30 09:52:48 -0800

    Use Azure Pipelines and workaround for WSL installation

--- eng/scripts/Install-WSL.ps1
+++ eng/scripts/Install-WSL.ps1
@@ -1,8 +1,22 @@
+[console]::OutputEncoding = New-Object System.Text.UnicodeEncoding
+
+# Workaround from https://github.com/actions/runner-images/issues/6844#issuecomment-1367225048
+# At the time of this comment (30-12-2022) this only works on Azure DevOps hosted
+# agents and doesn't work on 1ES images
+Write-Host "wsl --update --web-download"
+wsl --update --web-download | Out-String
+
+# Out-String waits for the process to finish
+Write-Host "wsl --update --web-download | Out-String"
+wsl --update --web-download | Out-String
+
+Write-Host "wsl --version" 
+wsl --version
+
 write-host "WSL install of ubuntu."
-wsl --install -d Ubuntu-20.04
+wsl --install -d Ubuntu-20.04 --web-download
 
 write-host "Launch WSL."
-[console]::OutputEncoding = New-Object System.Text.UnicodeEncoding
 $wsl = wsl -l -v | out-string
 
 write-host $wsl
