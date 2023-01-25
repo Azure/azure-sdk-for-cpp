85e8d4ff8e6707c7ebe16358291795de4f857b9f
Ahson Khan <ahkha@microsoft.com>
2023-01-24 16:55:10 -0800

    Clang format fix.

--- sdk/core/azure-core-tracing-opentelemetry/test/ut/service_support_test.cpp
+++ sdk/core/azure-core-tracing-opentelemetry/test/ut/service_support_test.cpp
@@ -17,9 +17,9 @@
 // The OpenTelemetry headers generate a couple of warnings on MSVC in the OTel 1.2 package, suppress
 // the warnings across the includes.
 #pragma warning(push)
-#pragma warning(disable : 4100) 
-#pragma warning(disable : 4244) 
-#pragma warning(disable : 6323) // Disable "Use of arithmetic operator on Boolean type" warning. 
+#pragma warning(disable : 4100)
+#pragma warning(disable : 4244)
+#pragma warning(disable : 6323) // Disable "Use of arithmetic operator on Boolean type" warning.
 #endif
 #include <opentelemetry/sdk/common/global_log_handler.h>
 #include <opentelemetry/sdk/trace/processor.h>
