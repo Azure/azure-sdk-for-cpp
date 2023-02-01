1baf64a7d3d4bfd1d0db2974ad1103244e1d54f3
Larry Osterman <LarryOsterman@users.noreply.github.com>
2023-02-01 10:26:04 -0800

    Better deprecation disabling 4

--- sdk/core/azure-core/test/ut/pipeline_test.cpp
+++ sdk/core/azure-core/test/ut/pipeline_test.cpp
@@ -59,7 +59,7 @@ TEST(Pipeline, attestationConstructor)
 #pragma warning(disable : 4996)
 #else
 #pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-implementations"
+#pragma message("Disable deprecated declartatins")
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
 #endif
   // Construct pipeline without exception
