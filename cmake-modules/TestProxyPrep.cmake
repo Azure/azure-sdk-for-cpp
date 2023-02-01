573aef7d48ceba8884d44503d13b3abcbe60ad87
George Arama <50641385+gearama@users.noreply.github.com>
2023-01-31 16:31:22 -0800

    capitalization

--- cmake-modules/TestProxyPrep.cmake
+++ cmake-modules/TestProxyPrep.cmake
@@ -6,8 +6,8 @@ macro(SetUpTestProxy subDir)
     # assets.json dir
     add_compile_definitions(AZURE_TEST_ASSETS_DIR="${AZ_ROOT_DIR}/${subDir}/")
     #copy start stop scripts to the bin folder
-    file(COPY ${AZ_ROOT_DIR}/eng/Scripts/Start-TestProxy.ps1
+    file(COPY ${AZ_ROOT_DIR}/eng/scripts/Start-TestProxy.ps1
         DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
-    file(COPY ${AZ_ROOT_DIR}/eng/Scripts/Stop-TestProxy.ps1
+    file(COPY ${AZ_ROOT_DIR}/eng/scripts/Stop-TestProxy.ps1
         DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
 endmacro()
