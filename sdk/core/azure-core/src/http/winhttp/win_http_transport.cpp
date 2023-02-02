f998e3eee24e15a85a477f57ff97f2859a33dcd0
Ahson Khan <ahkha@microsoft.com>
2023-02-01 18:11:47 -0800

    Remove modification to leftover source files.

--- sdk/core/azure-core/src/http/winhttp/win_http_transport.cpp
+++ sdk/core/azure-core/src/http/winhttp/win_http_transport.cpp
@@ -1,4 +1,4 @@
-//  Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
 // cspell:words HCERTIFICATECHAIN PCCERT CCERT HCERTCHAINENGINE HCERTSTORE
 
