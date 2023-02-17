b80157eca0ebb5167c68113fd14a11a7c7bb11e1
Konrad Jamrozik <kojamroz@microsoft.com>
2023-02-16 18:12:20 -0800

    update to win-20xx format

--- eng/scripts/Get-BinarySizes.ps1
+++ eng/scripts/Get-BinarySizes.ps1
@@ -48,11 +48,11 @@ function getTargetOs {
         return $OsVMImage
     }
 
-    if ($OsVMImage -match "^MMS2019Compliant$|^win-2019$") {
+    if ($OsVMImage -match "^windows-2019$|^win-2019$") {
         return "win-2019"
     }
 
-    if ($OsVMImage -match "^MMS2022Compliant$|^win-2022$|^windows-2022$") {
+    if ($OsVMImage -match "^windows-2022$|^win-2022$") {
         return "win-2022"
     }
 
