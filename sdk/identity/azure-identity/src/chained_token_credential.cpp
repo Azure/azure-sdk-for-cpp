f24d709f8c256641904ce505c8173924db5c855d
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2023-03-14 16:04:45 -0700

    Fix iteration

--- sdk/identity/azure-identity/src/chained_token_credential.cpp
+++ sdk/identity/azure-identity/src/chained_token_credential.cpp
@@ -41,7 +41,7 @@ ChainedTokenCredential::ChainedTokenCredential(
       credSourceDetails = " with the following credentials: ";
 
       auto const sourcesSize = m_sources.size();
-      for (size_t i = 0; i < (sourcesSize - 1); ++i)
+      for (size_t i = 0; i < sourcesSize; ++i)
       {
         if (i != 0)
         {
