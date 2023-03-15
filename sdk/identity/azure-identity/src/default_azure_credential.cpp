9f19a28af34824e45e2fdb9d27cad18b0d6183a2
Anton Kolesnyk <41349689+antkmsft@users.noreply.github.com>
2023-03-15 16:18:51 -0700

    Use aka.ms link to Identity troubleshooting (#4449)
    * Use aka.ms link to Identity troubleshooting
    
    * Update default_azure_credential.cpp
    
    * Update default_azure_credential.cpp
    
    ---------
    
    Co-authored-by: Anton Kolesnyk <antkmsft@users.noreply.github.com>

--- sdk/identity/azure-identity/src/default_azure_credential.cpp
+++ sdk/identity/azure-identity/src/default_azure_credential.cpp
@@ -69,7 +69,6 @@ AccessToken DefaultAzureCredential::GetToken(
     throw AuthenticationException(
         "Failed to get token from " + GetCredentialName()
         + ".\nSee Azure::Core::Diagnostics::Logger for details "
-          "(https://github.com/Azure/azure-sdk-for-cpp/tree/main/sdk/"
-          "identity/azure-identity#troubleshooting).");
+          "(https://aka.ms/azsdk/cpp/identity/troubleshooting).");
   }
 }
