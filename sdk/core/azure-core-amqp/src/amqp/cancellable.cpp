ba346c5d50ec0a52b412e2cb3493b308f0c128f1
Larry Osterman <LarryOsterman@users.noreply.github.com>
2023-03-15 14:31:56 -0700

    Fixed name of transfer_instance.cpp

--- sdk/core/azure-core-amqp/src/amqp/cancellable.cpp
+++ sdk/core/azure-core-amqp/src/amqp/cancellable.cpp
@@ -1,7 +1,7 @@
 // Copyright(c) Microsoft Corporation.All rights reserved.
 // SPDX - License - Identifier : MIT
 
-#include "azure/core/amqp/Cancellable.hpp"
+#include "azure/core/amqp/cancellable.hpp"
 #include <memory>
 
 #include <azure_uamqp_c/async_operation.h>
