00215df20f46f9cb42133163a9aaa12f9cb8d3d3
Larry Osterman <LarryOsterman@users.noreply.github.com>
2023-03-15 17:47:58 -0700

    Moved message sender implementation to shared pointer to enable copy and move semantics

--- sdk/core/azure-core-amqp/inc/azure/core/amqp/message_sender.hpp
+++ sdk/core/azure-core-amqp/inc/azure/core/amqp/message_sender.hpp
@@ -21,118 +21,101 @@ namespace Azure { namespace Core { namespace Credentials {
 }}} // namespace Azure::Core::Credentials
 
 namespace Azure { namespace Core { namespace _internal { namespace Amqp {
+    namespace _detail {
+      class MessageSenderImpl;
+    }
 
-  enum class MessageSendResult
-  {
-    Invalid,
-    Ok,
-    Error,
-    Timeout,
-    Cancelled
-  };
-  enum class MessageSenderState
-  {
-    Invalid,
-    Idle,
-    Opening,
-    Open,
-    Closing,
-    Error
-  };
+    enum class MessageSendResult
+    {
+      Invalid,
+      Ok,
+      Error,
+      Timeout,
+      Cancelled
+    };
+    enum class MessageSenderState
+    {
+      Invalid,
+      Idle,
+      Opening,
+      Open,
+      Closing,
+      Error
+    };
 
-  enum class SenderSettleMode
-  {
-    Unsettled,
-    Settled,
-    Mixed
-  };
+    enum class SenderSettleMode
+    {
+      Unsettled,
+      Settled,
+      Mixed
+    };
 
-  struct MessageSenderOptions
-  {
-    std::string Name;
-    SenderSettleMode SenderSettleMode{};
-    std::string SourceAddress;
-    std::vector<std::string> AuthenticationScopes;
-    uint32_t MaxMessageSize{};
-    bool EnableTrace{false};
+    struct MessageSenderOptions
+    {
+      std::string Name;
+      SenderSettleMode SenderSettleMode{};
+      std::string SourceAddress;
+      std::vector<std::string> AuthenticationScopes;
+      uint32_t MaxMessageSize{};
+      bool EnableTrace{false};
 
-    // Copied from Go, not sure if they're needed.
-    std::vector<std::string> Capabilities;
-    LinkDurability Durability{};
-    bool DynamicAddress{false};
-    ExpiryPolicy ExpiryPolicy{};
-    std::chrono::seconds ExpiryTimeout{std::chrono::seconds(0)};
-    bool IgnoreDispositionErrors{false};
+      // Copied from Go, not sure if they're needed.
+      std::vector<std::string> Capabilities;
+      LinkDurability Durability{};
+      bool DynamicAddress{false};
+      ExpiryPolicy ExpiryPolicy{};
+      std::chrono::seconds ExpiryTimeout{std::chrono::seconds(0)};
+      bool IgnoreDispositionErrors{false};
 
-    Azure::Core::Amqp::Models::Value Properties;
+      Azure::Core::Amqp::Models::Value Properties;
 
-    ReceiverSettleMode RequestedReceiverSettleMode{};
+      ReceiverSettleMode RequestedReceiverSettleMode{};
 
-    std::vector<std::string> TargetCapabilities;
-    LinkDurability TargetDurability{};
-    Azure::Core::_internal::Amqp::ExpiryPolicy TargetExpiryPolicy{};
-    std::chrono::seconds TargetExpiryTimeout{std::chrono::seconds(0)};
-  };
+      std::vector<std::string> TargetCapabilities;
+      LinkDurability TargetDurability{};
+      Azure::Core::_internal::Amqp::ExpiryPolicy TargetExpiryPolicy{};
+      std::chrono::seconds TargetExpiryTimeout{std::chrono::seconds(0)};
+    };
 
-  class MessageSender {
-  public:
-    using MessageSendCompleteCallback = std::function<
-        void(MessageSendResult sendResult, Azure::Core::Amqp::Models::Value deliveryState)>;
+    class MessageSender {
+    public:
+      using MessageSendCompleteCallback = std::function<
+          void(MessageSendResult sendResult, Azure::Core::Amqp::Models::Value deliveryState)>;
 
-    MessageSender(
-        Session const& session,
-        std::string const& target,
-        Connection const& connectionToPoll,
-        MessageSenderOptions const& options);
-    MessageSender(
-        Session const& session,
-        std::shared_ptr<ServiceBusSasConnectionStringCredential> credential,
-        std::string const& target,
-        Connection const& connectionToPoll,
-        MessageSenderOptions const& options);
-    MessageSender(
-        Session const& session,
-        std::shared_ptr<Azure::Core::Credentials::TokenCredential> credential,
-        std::string const& target,
-        Connection const& connectionToPoll,
-        MessageSenderOptions const& options);
-    virtual ~MessageSender() noexcept;
+      MessageSender(
+          Session const& session,
+          std::string const& target,
+          Connection const& connectionToPoll,
+          MessageSenderOptions const& options);
+      MessageSender(
+          Session const& session,
+          std::shared_ptr<ServiceBusSasConnectionStringCredential> credential,
+          std::string const& target,
+          Connection const& connectionToPoll,
+          MessageSenderOptions const& options);
+      MessageSender(
+          Session const& session,
+          std::shared_ptr<Azure::Core::Credentials::TokenCredential> credential,
+          std::string const& target,
+          Connection const& connectionToPoll,
+          MessageSenderOptions const& options);
+      virtual ~MessageSender() noexcept;
 
-    MessageSender(MessageSender const&) = delete;
-    MessageSender& operator=(MessageSender const&) = delete;
-    MessageSender(MessageSender&&) noexcept = delete;
-    MessageSender& operator=(MessageSender&&) noexcept = delete;
+      MessageSender(MessageSender const&) = default;
+      MessageSender& operator=(MessageSender const&) = default;
+      MessageSender(MessageSender&&) noexcept = default;
+      MessageSender& operator=(MessageSender&&) noexcept = default;
 
-    void Open();
-    void Close();
-    std::tuple<MessageSendResult, Azure::Core::Amqp::Models::Value> Send(
-        Azure::Core::Amqp::Models::Message const& message);
-    void SendAsync(
-        Azure::Core::Amqp::Models::Message const& message,
-        MessageSendCompleteCallback onSendComplete);
-    void SetTrace(bool traceEnabled);
+      void Open();
+      void Close();
+      std::tuple<MessageSendResult, Azure::Core::Amqp::Models::Value> Send(
+          Azure::Core::Amqp::Models::Message const& message);
+      void SendAsync(
+          Azure::Core::Amqp::Models::Message const& message,
+          MessageSendCompleteCallback onSendComplete);
+      void SetTrace(bool traceEnabled);
 
-  private:
-    static void OnMessageSenderStateChangedFn(
-        void* context,
-        MESSAGE_SENDER_STATE_TAG newState,
-        MESSAGE_SENDER_STATE_TAG oldState);
-
-    virtual void OnStateChanged(MessageSenderState newState, MessageSenderState oldState);
-    void Authenticate(CredentialType type, std::string const& audience, std::string const& token);
-
-    MESSAGE_SENDER_INSTANCE_TAG* m_messageSender{};
-    std::unique_ptr<_detail::Link> m_link;
-
-    Common::AsyncOperationQueue<Azure::Core::Amqp::Models::Message> m_messageQueue;
-    Common::AsyncOperationQueue<MessageSenderState, MessageSenderState> m_stateChangeQueue;
-
-    Connection const& m_connection;
-    Session const& m_session;
-    std::shared_ptr<ConnectionStringCredential> m_connectionCredential;
-    std::shared_ptr<Azure::Core::Credentials::TokenCredential> m_tokenCredential;
-    std::unique_ptr<Cbs> m_claimsBasedSecurity;
-    std::string m_target;
-    MessageSenderOptions m_options;
-  };
+    private:
+      std::shared_ptr<_detail::MessageSenderImpl> m_impl;
+    };
 }}}} // namespace Azure::Core::_internal::Amqp
--- sdk/core/azure-core-amqp/src/amqp/message_sender.cpp
+++ sdk/core/azure-core-amqp/src/amqp/message_sender.cpp
@@ -1,13 +1,14 @@
 // Copyright(c) Microsoft Corporation.All rights reserved.
 // SPDX - License - Identifier : MIT
 
-#include "azure/core/amqp/message_sender.hpp"
 #include "azure/core/amqp/claims_based_security.hpp"
 #include "azure/core/amqp/common/completion_operation.hpp"
 #include "azure/core/amqp/models/amqp_message.hpp"
 #include "azure/core/amqp/models/messaging_values.hpp"
+#include "azure/core/amqp/private/message_sender_impl.hpp"
 #include "azure/core/amqp/session.hpp"
 #include <azure/core/credentials/credentials.hpp>
+
 #include <azure_uamqp_c/message_sender.h>
 #include <memory>
 
@@ -18,7 +19,7 @@ namespace Azure { namespace Core { namespace _internal { namespace Amqp {
       std::string const& target,
       Connection const& connection,
       MessageSenderOptions const& options)
-      : m_session{session}, m_connection{connection}, m_options{options}, m_target{target}
+      : m_impl{std::make_shared<_detail::MessageSenderImpl>(session, target, connection, options)}
   {
   }
 
@@ -28,8 +29,12 @@ namespace Azure { namespace Core { namespace _internal { namespace Amqp {
       std::string const& target,
       Connection const& connection,
       MessageSenderOptions const& options)
-      : m_session{session}, m_connection{connection}, m_options{options}, m_target{target},
-        m_connectionCredential{credential}
+      : m_impl{std::make_shared<_detail::MessageSenderImpl>(
+          session,
+          credential,
+          target,
+          connection,
+          options)}
   {
   }
 
@@ -39,188 +44,252 @@ namespace Azure { namespace Core { namespace _internal { namespace Amqp {
       std::string const& target,
       Connection const& connection,
       MessageSenderOptions const& options)
-      : m_session{session}, m_connection{connection}, m_options{options}, m_target{target},
-        m_tokenCredential{credential}
+      : m_impl{std::make_shared<_detail::MessageSenderImpl>(
+          session,
+          credential,
+          target,
+          connection,
+          options)}
   {
   }
 
-  MessageSender::~MessageSender()
+  void MessageSender::Open() { m_impl->Open(); }
+  void MessageSender::Close() { m_impl->Close(); }
+  std::tuple<MessageSendResult, Azure::Core::Amqp::Models::Value> MessageSender::Send(
+      Azure::Core::Amqp::Models::Message const& message)
   {
-    if (m_messageSender)
-    {
-      messagesender_destroy(m_messageSender);
-      m_messageSender = nullptr;
-    }
+    return m_impl->Send(message);
   }
-
-  MessageSenderState MessageSenderStateFromLowLevel(MESSAGE_SENDER_STATE lowLevel)
-  {
-    switch (lowLevel)
-    {
-      case MESSAGE_SENDER_STATE_CLOSING:
-        return MessageSenderState::Closing;
-      case MESSAGE_SENDER_STATE_ERROR:
-        return MessageSenderState::Error;
-      case MESSAGE_SENDER_STATE_IDLE:
-        return MessageSenderState::Idle;
-      case MESSAGE_SENDER_STATE_INVALID:
-        return MessageSenderState::Invalid;
-      case MESSAGE_SENDER_STATE_OPEN:
-        return MessageSenderState::Open;
-      case MESSAGE_SENDER_STATE_OPENING:
-        return MessageSenderState::Opening;
-      default:
-        throw std::logic_error("Unknown message receiver state.");
-    }
-  }
-
-  void MessageSender::OnMessageSenderStateChangedFn(
-      void* context,
-      MESSAGE_SENDER_STATE newState,
-      MESSAGE_SENDER_STATE oldState)
-  {
-    auto receiver = static_cast<MessageSender*>(const_cast<void*>(context));
-    receiver->OnStateChanged(
-        MessageSenderStateFromLowLevel(newState), MessageSenderStateFromLowLevel(oldState));
-  }
-
-  void MessageSender::OnStateChanged(MessageSenderState newState, MessageSenderState oldState)
-  {
-    m_stateChangeQueue.CompleteOperation(newState, oldState);
-  }
-
-  void MessageSender::Authenticate(
-      CredentialType type,
-      std::string const& audience,
-      std::string const& token)
-  {
-    m_claimsBasedSecurity = std::make_unique<Cbs>(m_session, m_connection);
-    if (m_claimsBasedSecurity->Open() == CbsOpenResult::Ok)
-    {
-      auto result = m_claimsBasedSecurity->PutToken(
-          (type == CredentialType::BearerToken ? CbsTokenType::Jwt : CbsTokenType::Sas),
-          audience,
-          token);
-    }
-    else
-    {
-      throw std::runtime_error("Could not open Claims Based Security.");
-    }
-  }
-
-  void MessageSender::Open()
-  {
-    // If we need to authenticate with either ServiceBus or BearerToken, now is the time to do it.
-    if (m_connectionCredential)
-    {
-      auto sasCredential{std::static_pointer_cast<
-          Azure::Core::_internal::Amqp::ServiceBusSasConnectionStringCredential>(
-          m_connectionCredential)};
-      Authenticate(
-          sasCredential->GetCredentialType(),
-          sasCredential->GetEndpoint() + sasCredential->GetEntityPath(),
-          sasCredential->GenerateSasToken(
-              std::chrono::system_clock::now() + std::chrono::minutes(60)));
-    }
-    else if (m_tokenCredential)
-    {
-      Azure::Core::Credentials::TokenRequestContext requestContext;
-      requestContext.Scopes = m_options.AuthenticationScopes;
-      Azure::Core::Context context;
-
-      Authenticate(
-          CredentialType::BearerToken,
-          m_target,
-          m_tokenCredential->GetToken(requestContext, context).Token);
-    }
-
-    // We cannot create the link until after we've authenticated the connection because the
-    // OnNewLink notification will call into unattached links.
-    m_link = std::make_unique<_detail::Link>(
-        m_session, m_options.Name, _detail::SessionRole::Sender, m_options.SourceAddress, m_target);
-    m_link->SetMaxMessageSize(m_options.MaxMessageSize);
-    m_link->SetSenderSettleMode(m_options.SenderSettleMode);
-
-    m_messageSender
-        = messagesender_create(m_link->Get(), MessageSender::OnMessageSenderStateChangedFn, this);
-
-    if (messagesender_open(m_messageSender))
-    {
-      throw std::runtime_error("Could not open message sender");
-    }
-  }
-
-  void MessageSender::Close()
-  {
-    if (messagesender_close(m_messageSender))
-    {
-      throw std::runtime_error("Could not close message sender");
-    }
-  }
-
-  template <typename CompleteFn> struct WrapSendComplete
-  {
-    static void OnOperation(
-        CompleteFn onComplete,
-        MESSAGE_SEND_RESULT sendResult,
-        AMQP_VALUE disposition)
-    {
-      MessageSendResult result{MessageSendResult::Ok};
-      switch (sendResult)
-      {
-        case MESSAGE_SEND_OK:
-          result = MessageSendResult::Ok;
-          break;
-        case MESSAGE_SEND_CANCELLED:
-          result = MessageSendResult::Cancelled;
-          break;
-        case MESSAGE_SEND_ERROR:
-          result = MessageSendResult::Error;
-          break;
-        case MESSAGE_SEND_TIMEOUT:
-          result = MessageSendResult::Timeout;
-          break;
-      }
-      onComplete(result, disposition);
-    }
-  };
-
   void MessageSender::SendAsync(
       Azure::Core::Amqp::Models::Message const& message,
       MessageSendCompleteCallback onSendComplete)
   {
-    auto operation(std::make_unique<Azure::Core::_internal::Amqp::Common::CompletionOperation<
-                       decltype(onSendComplete),
-                       WrapSendComplete<decltype(onSendComplete)>>>(onSendComplete));
-    auto result = messagesender_send_async(
-        m_messageSender,
-        message,
-        std::remove_pointer<decltype(operation)::element_type>::type::OnOperationFn,
-        operation.release(),
-        0 /*timeout*/);
-    if (result == nullptr)
+    return m_impl->SendAsync(message, onSendComplete);
+  }
+  void MessageSender::SetTrace(bool traceEnabled) { m_impl->SetTrace(traceEnabled); }
+
+  MessageSender::~MessageSender() {}
+
+  namespace _detail {
+
+    MessageSenderImpl::MessageSenderImpl(
+        Session const& session,
+        std::string const& target,
+        Connection const& connection,
+        MessageSenderOptions const& options)
+        : m_session{session}, m_connection{connection}, m_options{options}, m_target{target}
     {
-      throw std::runtime_error("Could not send message");
     }
-  }
 
-  std::tuple<MessageSendResult, Azure::Core::Amqp::Models::Value> MessageSender::Send(
-      Azure::Core::Amqp::Models::Message const& message)
-  {
-    Azure::Core::_internal::Amqp::Common::AsyncOperationQueue<
-        Azure::Core::_internal::Amqp::MessageSendResult,
-        Azure::Core::Amqp::Models::Value>
-        sendCompleteQueue;
-    SendAsync(
-        message,
-        [&](Azure::Core::_internal::Amqp::MessageSendResult sendResult,
-            Azure::Core::Amqp::Models::Value deliveryStatus) {
-          //          std::cout << "Send Complete!" << std::endl;
-          sendCompleteQueue.CompleteOperation(sendResult, deliveryStatus);
-        });
-    //    auto result = sendCompleteQueue.WaitForResult();
-    auto result = sendCompleteQueue.WaitForPolledResult(m_connection);
-    return std::move(*result);
-  }
+    MessageSenderImpl::MessageSenderImpl(
+        Session const& session,
+        std::shared_ptr<ServiceBusSasConnectionStringCredential> credential,
+        std::string const& target,
+        Connection const& connection,
+        MessageSenderOptions const& options)
+        : m_session{session}, m_connection{connection}, m_options{options}, m_target{target},
+          m_connectionCredential{credential}
+    {
+    }
 
+    MessageSenderImpl::MessageSenderImpl(
+        Session const& session,
+        std::shared_ptr<Azure::Core::Credentials::TokenCredential> credential,
+        std::string const& target,
+        Connection const& connection,
+        MessageSenderOptions const& options)
+        : m_session{session}, m_connection{connection}, m_options{options}, m_target{target},
+          m_tokenCredential{credential}
+    {
+    }
+
+    MessageSenderImpl::~MessageSenderImpl()
+    {
+      if (m_messageSender)
+      {
+        messagesender_destroy(m_messageSender);
+        m_messageSender = nullptr;
+      }
+    }
+
+    void MessageSenderImpl::SetTrace(bool traceEnabled)
+    {
+      messagesender_set_trace(m_messageSender, traceEnabled);
+    }
+
+    MessageSenderState MessageSenderStateFromLowLevel(MESSAGE_SENDER_STATE lowLevel)
+    {
+      switch (lowLevel)
+      {
+        case MESSAGE_SENDER_STATE_CLOSING:
+          return MessageSenderState::Closing;
+        case MESSAGE_SENDER_STATE_ERROR:
+          return MessageSenderState::Error;
+        case MESSAGE_SENDER_STATE_IDLE:
+          return MessageSenderState::Idle;
+        case MESSAGE_SENDER_STATE_INVALID:
+          return MessageSenderState::Invalid;
+        case MESSAGE_SENDER_STATE_OPEN:
+          return MessageSenderState::Open;
+        case MESSAGE_SENDER_STATE_OPENING:
+          return MessageSenderState::Opening;
+        default:
+          throw std::logic_error("Unknown message receiver state.");
+      }
+    }
+
+    void MessageSenderImpl::OnMessageSenderStateChangedFn(
+        void* context,
+        MESSAGE_SENDER_STATE newState,
+        MESSAGE_SENDER_STATE oldState)
+    {
+      auto receiver = static_cast<MessageSenderImpl*>(const_cast<void*>(context));
+      receiver->OnStateChanged(
+          MessageSenderStateFromLowLevel(newState), MessageSenderStateFromLowLevel(oldState));
+    }
+
+    void MessageSenderImpl::OnStateChanged(MessageSenderState newState, MessageSenderState oldState)
+    {
+      m_stateChangeQueue.CompleteOperation(newState, oldState);
+    }
+
+    void MessageSenderImpl::Authenticate(
+        CredentialType type,
+        std::string const& audience,
+        std::string const& token)
+    {
+      m_claimsBasedSecurity = std::make_unique<Cbs>(m_session, m_connection);
+      if (m_claimsBasedSecurity->Open() == CbsOpenResult::Ok)
+      {
+        auto result = m_claimsBasedSecurity->PutToken(
+            (type == CredentialType::BearerToken ? CbsTokenType::Jwt : CbsTokenType::Sas),
+            audience,
+            token);
+      }
+      else
+      {
+        throw std::runtime_error("Could not open Claims Based Security.");
+      }
+    }
+
+    void MessageSenderImpl::Open()
+    {
+      // If we need to authenticate with either ServiceBus or BearerToken, now is the time to do
+      // it.
+      if (m_connectionCredential)
+      {
+        auto sasCredential{std::static_pointer_cast<
+            Azure::Core::_internal::Amqp::ServiceBusSasConnectionStringCredential>(
+            m_connectionCredential)};
+        Authenticate(
+            sasCredential->GetCredentialType(),
+            sasCredential->GetEndpoint() + sasCredential->GetEntityPath(),
+            sasCredential->GenerateSasToken(
+                std::chrono::system_clock::now() + std::chrono::minutes(60)));
+      }
+      else if (m_tokenCredential)
+      {
+        Azure::Core::Credentials::TokenRequestContext requestContext;
+        requestContext.Scopes = m_options.AuthenticationScopes;
+        Azure::Core::Context context;
+
+        Authenticate(
+            CredentialType::BearerToken,
+            m_target,
+            m_tokenCredential->GetToken(requestContext, context).Token);
+      }
+
+      // We cannot create the link until after we've authenticated the connection because the
+      // OnNewLink notification will call into unattached links.
+      m_link = std::make_unique<_detail::Link>(
+          m_session,
+          m_options.Name,
+          _detail::SessionRole::Sender,
+          m_options.SourceAddress,
+          m_target);
+      m_link->SetMaxMessageSize(m_options.MaxMessageSize);
+      m_link->SetSenderSettleMode(m_options.SenderSettleMode);
+
+      m_messageSender = messagesender_create(
+          m_link->Get(), MessageSenderImpl::OnMessageSenderStateChangedFn, this);
+
+      if (messagesender_open(m_messageSender))
+      {
+        throw std::runtime_error("Could not open message sender");
+      }
+    }
+
+    void MessageSenderImpl::Close()
+    {
+      if (messagesender_close(m_messageSender))
+      {
+        throw std::runtime_error("Could not close message sender");
+      }
+    }
+
+    template <typename CompleteFn> struct WrapSendComplete
+    {
+      static void OnOperation(
+          CompleteFn onComplete,
+          MESSAGE_SEND_RESULT sendResult,
+          AMQP_VALUE disposition)
+      {
+        MessageSendResult result{MessageSendResult::Ok};
+        switch (sendResult)
+        {
+          case MESSAGE_SEND_OK:
+            result = MessageSendResult::Ok;
+            break;
+          case MESSAGE_SEND_CANCELLED:
+            result = MessageSendResult::Cancelled;
+            break;
+          case MESSAGE_SEND_ERROR:
+            result = MessageSendResult::Error;
+            break;
+          case MESSAGE_SEND_TIMEOUT:
+            result = MessageSendResult::Timeout;
+            break;
+        }
+        onComplete(result, disposition);
+      }
+    };
+
+    void MessageSenderImpl::SendAsync(
+        Azure::Core::Amqp::Models::Message const& message,
+        MessageSendCompleteCallback onSendComplete)
+    {
+      auto operation(std::make_unique<Azure::Core::_internal::Amqp::Common::CompletionOperation<
+                         decltype(onSendComplete),
+                         WrapSendComplete<decltype(onSendComplete)>>>(onSendComplete));
+      auto result = messagesender_send_async(
+          m_messageSender,
+          message,
+          std::remove_pointer<decltype(operation)::element_type>::type::OnOperationFn,
+          operation.release(),
+          0 /*timeout*/);
+      if (result == nullptr)
+      {
+        throw std::runtime_error("Could not send message");
+      }
+    }
+
+    std::tuple<MessageSendResult, Azure::Core::Amqp::Models::Value> MessageSenderImpl::Send(
+        Azure::Core::Amqp::Models::Message const& message)
+    {
+      Azure::Core::_internal::Amqp::Common::AsyncOperationQueue<
+          Azure::Core::_internal::Amqp::MessageSendResult,
+          Azure::Core::Amqp::Models::Value>
+          sendCompleteQueue;
+      SendAsync(
+          message,
+          [&](Azure::Core::_internal::Amqp::MessageSendResult sendResult,
+              Azure::Core::Amqp::Models::Value deliveryStatus) {
+            //          std::cout << "Send Complete!" << std::endl;
+            sendCompleteQueue.CompleteOperation(sendResult, deliveryStatus);
+          });
+      //    auto result = sendCompleteQueue.WaitForResult();
+      auto result = sendCompleteQueue.WaitForPolledResult(m_connection);
+      return std::move(*result);
+    }
+  } // namespace _detail
 }}}} // namespace Azure::Core::_internal::Amqp
