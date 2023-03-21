3fb1e42fa29c42799e132bb1dde71351ee14d22f
Larry Osterman <LarryOsterman@users.noreply.github.com>
2023-03-21 16:13:24 -0700

    Fixed copyright and license text

--- sdk/core/azure-core-amqp/src/amqp/message_sender.cpp
+++ sdk/core/azure-core-amqp/src/amqp/message_sender.cpp
@@ -1,5 +1,5 @@
-// Copyright(c) Microsoft Corporation.All rights reserved.
-// SPDX - License - Identifier : MIT
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-Licence-Identifier: MIT
 
 #include "azure/core/amqp/claims_based_security.hpp"
 #include "azure/core/amqp/common/completion_operation.hpp"
