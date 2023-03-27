ca692fca1d96fc9fe7a0f7fb2c5c1155acadd364
Ahson Khan <ahson_ahmedk@yahoo.com>
2023-03-27 15:19:42 -0700

    Update AddGoogleTest.cmake to v1.13.0

--- cmake-modules/AddGoogleTest.cmake
+++ cmake-modules/AddGoogleTest.cmake
@@ -15,7 +15,7 @@ if(CMAKE_VERSION VERSION_LESS 3.11)
     download_project(
         PROJ                googletest
 		GIT_REPOSITORY      https://github.com/google/googletest.git
-		GIT_TAG             release-1.13.0
+		GIT_TAG             v1.13.0
 		UPDATE_DISCONNECTED 1
 		QUIET
     )
@@ -29,7 +29,7 @@ else()
     FetchContent_Declare(
         googletest
         GIT_REPOSITORY      https://github.com/google/googletest.git
-        GIT_TAG             release-1.13.0
+        GIT_TAG             v1.13.0
         )
     FetchContent_GetProperties(googletest)
     if(NOT googletest_POPULATED)
