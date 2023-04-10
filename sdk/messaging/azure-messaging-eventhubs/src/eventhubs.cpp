e92462356d5248b9988355894252b4e99aac163e
George Arama <50641385+gearama@users.noreply.github.com>
2023-04-10 15:55:17 -0700

    first pass at creating service wireframe

---
+++ sdk/messaging/azure-messaging-eventhubs/src/eventhubs.cpp
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#include <azure/core/exception.hpp>
+#include <azure/core/http/http.hpp>
+#include "azure/messaging/eventhubs.hpp"
+#include "private/package_version.hpp"
