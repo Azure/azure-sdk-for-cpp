a09d3acdcea48d1399ac3cee28fca7f3ecc35f1d
Larry Osterman <LarryOsterman@users.noreply.github.com>
2023-05-15 16:13:49 -0700

    Fixed AMQPS port (#4637)

--- sdk/core/azure-core-amqp/src/amqp/connection.cpp
+++ sdk/core/azure-core-amqp/src/amqp/connection.cpp
@@ -145,7 +145,7 @@ namespace Azure { namespace Core { namespace Amqp { namespace _detail {
       Log::Write(Logger::Level::Informational, "Creating TLS socket connection transport.");
       Azure::Core::Amqp::Network::_internal::TlsTransport transport{
           requestUrl.GetHost(),
-          requestUrl.GetPort() ? requestUrl.GetPort() : static_cast<std::uint16_t>(AmqpPort)};
+          requestUrl.GetPort() ? requestUrl.GetPort() : static_cast<std::uint16_t>(AmqpsPort)};
       m_transport = transport.GetImpl();
     }
     else
