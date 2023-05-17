c40593323873851dfe73d464ec6aa1987dcf7719
Larry Osterman <LarryOsterman@users.noreply.github.com>
2023-05-16 17:39:03 -0700

    gcc9 fixes 3

--- sdk/core/azure-core-amqp/src/amqp/link.cpp
+++ sdk/core/azure-core-amqp/src/amqp/link.cpp
@@ -101,8 +101,8 @@ namespace Azure { namespace Core { namespace Amqp { namespace _detail {
       Models::_internal::MessageTarget const& target)
       : m_session{session}, m_source(source), m_target(target)
   {
-    Models::AmqpValue sourceValue(static_cast<const Models::AmqpValue>(source));
-    Models::AmqpValue targetValue(static_cast<const Models::AmqpValue>(target));
+    Models::AmqpValue sourceValue(source);
+    Models::AmqpValue targetValue(target);
     m_link = link_create(
         *session,
         name.c_str(),
