73a7613103d3335eff86a99e20567e770cdb872b
Larry Osterman <LarryOsterman@users.noreply.github.com>
2023-05-26 13:58:22 -0700

    clang-format

--- sdk/core/azure-core/test/ut/etag_test.cpp
+++ sdk/core/azure-core/test/ut/etag_test.cpp
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // SPDX-License-Identifier: MIT
 #include "test_traits.hpp"
-#include <gtest/gtest.h>
 
 #include <azure/core/etag.hpp>
 
