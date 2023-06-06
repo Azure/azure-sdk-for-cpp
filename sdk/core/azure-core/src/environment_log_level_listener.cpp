ef3df90c9db24d8b9460bed60590817703eb0b16
Larry Osterman <LarryOsterman@users.noreply.github.com>
2023-06-06 13:31:17 -0700

    Added comment about why we don't insert crlf when it's already present

--- sdk/core/azure-core/src/environment_log_level_listener.cpp
+++ sdk/core/azure-core/src/environment_log_level_listener.cpp
@@ -129,6 +129,13 @@ EnvironmentLogLevelListener::GetLogListener()
             << Azure::DateTime(std::chrono::system_clock::now())
                    .ToString(DateTime::DateFormat::Rfc3339, DateTime::TimeFractionFormat::AllDigits)
             << "] " << LogLevelToConsoleString(level) << " : " << message;
+
+        // If the message ends with a new line, flush the stream otherwise insert a new line to
+        // terminate the message.
+        //
+        // If the client of the logger APIs is using the stream form of the logger, then it will
+        // insert a \n character when the client uses std::endl. This check ensures that we don't
+        // insert unnecessary new lines.
         if (!message.empty() && message.back() == '\n')
         {
           *os << std::flush;
