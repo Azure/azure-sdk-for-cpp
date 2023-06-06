ed9f4a6dbdafc10c21eb7f517e8948ecd4300076
Larry Osterman <LarryOsterman@users.noreply.github.com>
2023-06-06 14:32:20 -0700

    ignore return value from strerror_r

--- sdk/core/azure-core-amqp/src/amqp/message_sender.cpp
+++ sdk/core/azure-core-amqp/src/amqp/message_sender.cpp
@@ -210,7 +210,7 @@ namespace Azure { namespace Core { namespace Amqp { namespace _detail {
 #if defined(AZ_PLATFORM_WINDOWS)
       strerror_s(buf, sizeof(buf), err);
 #else
-      strerror_r(err, buf, sizeof(buf));
+      (void)strerror_r(err, buf, sizeof(buf));
 #endif
       throw std::runtime_error(
           "Could not open message sender. errno=" + std::to_string(err) + ", \"" + buf + "\".");
