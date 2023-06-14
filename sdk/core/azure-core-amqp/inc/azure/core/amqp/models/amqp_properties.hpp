5301b06185cc17980d140411af0411bdbdf742ec
Larry Osterman <LarryOsterman@users.noreply.github.com>
2023-06-14 15:40:09 -0700

    clang doesn't understand the @cond and @endcond doxygen tags 3

--- sdk/core/azure-core-amqp/inc/azure/core/amqp/models/amqp_properties.hpp
+++ sdk/core/azure-core-amqp/inc/azure/core/amqp/models/amqp_properties.hpp
@@ -12,6 +12,12 @@
 #include <vector>
 
 struct PROPERTIES_INSTANCE_TAG;
+#if defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6))
+#pragma GCC diagnostic push
+#elif defined(__clang__) // !__clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdocumentation-unknown-command"
+#endif // _MSC_VER
 
 ///  @cond HIDDEN
 
@@ -24,6 +30,13 @@ template <> struct Azure::Core::_internal::UniqueHandleHelper<PROPERTIES_INSTANC
 };
 
 /// @endcond
+
+#if defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6))
+#pragma GCC diagnostic pop
+#elif defined(__clang__) // !__clang__
+#pragma clang diagnostic pop
+#endif // _MSC_VER
+
 namespace Azure { namespace Core { namespace Amqp { namespace Models {
 
   using UniquePropertiesHandle = Azure::Core::_internal::UniqueHandle<PROPERTIES_INSTANCE_TAG>;
