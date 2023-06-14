58d92fff80fdb672f75467471d657c489cb83e56
Larry Osterman <LarryOsterman@users.noreply.github.com>
2023-06-14 15:32:05 -0700

    clang doesn't understand the @cond and @endcond doxygen tags

--- sdk/core/azure-core-amqp/inc/azure/core/amqp/models/amqp_value.hpp
+++ sdk/core/azure-core-amqp/inc/azure/core/amqp/models/amqp_value.hpp
@@ -20,6 +20,13 @@
 #include <vector>
 
 struct AMQP_VALUE_DATA_TAG;
+#if defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6))
+#pragma GCC diagnostic push
+#elif defined(__clang__) // !__clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdocumentation-unknown-command"
+#endif // _MSC_VER
+
 /// @cond INTERNAL
 template <> struct Azure::Core::_internal::UniqueHandleHelper<AMQP_VALUE_DATA_TAG>
 {
@@ -32,6 +39,11 @@ namespace Azure { namespace Core { namespace Amqp { namespace Models { namespace
   using UniqueAmqpValueHandle = Azure::Core::_internal::UniqueHandle<AMQP_VALUE_DATA_TAG>;
 }}}}} // namespace Azure::Core::Amqp::Models::_detail
 /// @endcond
+#if defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6))
+#pragma GCC diagnostic pop
+#elif defined(__clang__) // !__clang__
+#pragma clang diagnostic pop
+#endif // _MSC_VER
 
 namespace Azure { namespace Core { namespace Amqp { namespace Models { namespace _internal {
 
