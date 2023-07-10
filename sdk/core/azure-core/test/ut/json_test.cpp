cf1c01e089c12a362025d08854cd07263c89f438
Ahson Khan <ahson_ahmedk@yahoo.com>
2023-07-10 11:48:09 -0700

    Codifying the behavior of json.contains with nested properties, in the unit test. (#4763)

--- sdk/core/azure-core/test/ut/json_test.cpp
+++ sdk/core/azure-core/test/ut/json_test.cpp
@@ -16,3 +16,31 @@ TEST(Json, create)
 
   EXPECT_EQ(expected, j.dump());
 }
+
+TEST(Json, duplicateName)
+{
+  json jsonRoot = json::parse(R"({"KeyName": 1, "AnotherObject": {"KeyName": 2}})");
+  int value = 0;
+  if (jsonRoot.contains("KeyName"))
+  {
+    value = jsonRoot["KeyName"].get<int>();
+  }
+  EXPECT_EQ(value, 1);
+
+  jsonRoot = json::parse(R"({"AnotherObject": {"KeyName": 2}})");
+  value = 0;
+
+  // The nested KeyName property is considered not found, when at the root.
+  if (jsonRoot.contains("KeyName"))
+  {
+    value = jsonRoot["KeyName"].get<int>();
+  }
+  EXPECT_EQ(value, 0);
+
+  // The nested KeyName property is considered found, when navigating to the nested object first.
+  if (jsonRoot["AnotherObject"].contains("KeyName"))
+  {
+    value = jsonRoot["AnotherObject"]["KeyName"].get<int>();
+  }
+  EXPECT_EQ(value, 2);
+}
