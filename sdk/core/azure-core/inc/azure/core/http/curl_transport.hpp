a0de55100dbe05e961f7ec7746f516fad1800a78
Rick Winter <rick.winter@microsoft.com>
2023-07-28 08:57:50 -0700

    Version of curl unavailable in header

--- sdk/core/azure-core/inc/azure/core/http/curl_transport.hpp
+++ sdk/core/azure-core/inc/azure/core/http/curl_transport.hpp
@@ -53,7 +53,6 @@ namespace Azure { namespace Core { namespace Http {
      */
     bool AllowFailedCrlRetrieval = false;
 
-#if LIBCURL_VERSION_NUM >= 0x074D00 // 7.77.0 
     /**
      * @brief A set of PEM encoded X.509 certificates and CRLs describing the certificates used to
      * validate the server.
@@ -67,7 +66,6 @@ namespace Azure { namespace Core { namespace Http {
      *
      */
     std::string PemEncodedExpectedRootCertificates;
-#endif
   };
 
   /**
