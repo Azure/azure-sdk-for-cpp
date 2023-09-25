fdaf0d73746c651165d778a4adba92efc785d71a
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2023-09-25 10:19:18 -0700

    Clang-format

--- sdk/keyvault/azure-security-keyvault-shared/inc/azure/keyvault/shared/keyvault_challenge_based_authentication_policy.hpp
+++ sdk/keyvault/azure-security-keyvault-shared/inc/azure/keyvault/shared/keyvault_challenge_based_authentication_policy.hpp
@@ -1,16 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-/**
- * @file
- * @brief Key Vault Challenge-Based Authentication Policy.
- *
- */
-
 #pragma once
 
 // This header file is left for compatibility purposes.
-// The name of this header is long, and may cause build error on Windows systems on some installations,
-// with long path and maximum path length of 260 characters.
+// The name of this header is long, and may cause build error on Windows systems on some
+// installations, with long path and maximum path length of 260 characters.
 
 #include <azure/keyvault/shared/keyvault_challenge_based_auth.hpp>
