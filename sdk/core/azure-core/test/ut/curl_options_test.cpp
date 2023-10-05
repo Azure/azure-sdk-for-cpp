b8a2f279371f3580c4a951229c73574d1351d6bf
Ahson Khan <ahkha@microsoft.com>
2023-10-04 19:08:05 -0700

    Revert unit test change that caused an intentional error.

--- sdk/core/azure-core/test/ut/curl_options_test.cpp
+++ sdk/core/azure-core/test/ut/curl_options_test.cpp
@@ -267,7 +267,7 @@ namespace Azure { namespace Core { namespace Test {
 
     std::unique_ptr<Azure::Core::Http::RawResponse> response;
     EXPECT_NO_THROW(response = pipeline.Send(request, Azure::Core::Context::ApplicationContext));
-    EXPECT_NE(response->GetStatusCode(), Azure::Core::Http::HttpStatusCode::Ok);
+    EXPECT_EQ(response->GetStatusCode(), Azure::Core::Http::HttpStatusCode::Ok);
 
     // Clean the connection from the pool *Windows fails to clean if we leave to be clean upon
     // app-destruction
