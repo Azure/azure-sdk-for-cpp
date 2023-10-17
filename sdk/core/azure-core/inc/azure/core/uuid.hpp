5341470eb6114ffcc38d347d7324abd98dca0835
Larry Osterman <LarryOsterman@users.noreply.github.com>
2023-10-17 11:24:58 -0700

    Update sdk/core/azure-core/inc/azure/core/uuid.hpp

--- sdk/core/azure-core/inc/azure/core/uuid.hpp
+++ sdk/core/azure-core/inc/azure/core/uuid.hpp
@@ -13,7 +13,7 @@
 #include <array>
 #include <cstdint> // defines std::uint8_t
 #include <cstring>
-#include <stdint.h> // deprecated, defines uint8_t in global namespace.
+#include <stdint.h> // deprecated, defines uint8_t in global namespace. TODO: Remove in the future when references to uint8_t and friends are removed.
 #include <string>
 
 namespace Azure { namespace Core {
