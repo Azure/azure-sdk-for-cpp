d830c003ad390c20143abaac350e7f628a9a78cc
Larry Osterman <LarryOsterman@users.noreply.github.com>
2023-10-27 12:24:50 -0700

    Removed tracing unified header

--- sdk/core/azure-core-tracing-opentelemetry/inc/azure/core/tracing.hpp
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-#pragma once
-
-#include "azure/core/tracing/opentelemetry/dll_import_export.hpp"
-#include "azure/core/tracing/opentelemetry/internal/apiview.hpp"
-#include "azure/core/tracing/opentelemetry/opentelemetry.hpp"
-#include "azure/core/tracing/opentelemetry/rtti.hpp"
