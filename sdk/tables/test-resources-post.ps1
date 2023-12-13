7351da00438f9cda71130a6eb6e1ef841ba886b7
George Arama <gearama@microsoft.com>
2023-12-13 11:27:10 -0800

    cleanup 2

--- sdk/tables/test-resources-post.ps1
+++ sdk/tables/test-resources-post.ps1
@@ -8,12 +8,6 @@ param(
 
 New-AzStorageEncryptionScope -ResourceGroupName $ResourceGroupName -StorageAccountName $DeploymentOutputs['ACCOUNT_NAME'] -EncryptionScopeName "EncryptionScopeForTest" -StorageEncryption
 
-New-AzStorageEncryptionScope -ResourceGroupName $ResourceGroupName -StorageAccountName $DeploymentOutputs['DATALAKE_ACCOUNT_NAME'] -EncryptionScopeName "EncryptionScopeForTest" -StorageEncryption
-
-Enable-AzStorageBlobDeleteRetentionPolicy -ResourceGroupName $ResourceGroupName -StorageAccountName $DeploymentOutputs['DATALAKE_ACCOUNT_NAME'] -RetentionDays 7
-
-Enable-AzStorageBlobLastAccessTimeTracking -ResourceGroupName $ResourceGroupName -StorageAccountName $DeploymentOutputs['ACCOUNT_NAME'] -PassThru
-
 # This script is used to wait until XCache is refreshed for the service properties (30s), and role assignment takes effect (300s).
 
 Start-Sleep -s 300
