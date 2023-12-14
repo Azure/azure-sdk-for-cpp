2b492f5d6cfa0509958fc7f871d5a41b8635b28d
George Arama <gearama@microsoft.com>
2023-12-14 14:11:08 -0800

    samples

---
+++ sdk/tables/azure-data-tables/samples/tables_service_operations.cpp
@@ -0,0 +1,54 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#include <azure/data/tables.hpp>
+
+#include <cstdio>
+#include <iostream>
+#include <stdexcept>
+#include <thread>
+
+std::string GetConnectionString()
+{
+  const static std::string ConnectionString = "";
+
+  if (!ConnectionString.empty())
+  {
+    return ConnectionString;
+  }
+  const static std::string envConnectionString = std::getenv("STANDARD_STORAGE_CONNECTION_STRING");
+  if (!envConnectionString.empty())
+  {
+    return envConnectionString;
+  }
+  throw std::runtime_error("Cannot find connection string.");
+}
+
+using namespace Azure::Data::Tables;
+const std::string TableName = "sample1";
+
+int main()
+{
+  auto tableServiceClient = TableServicesClient::CreateFromConnectionString(GetConnectionString());
+
+  // list tables
+  auto tables = tableServiceClient.ListTables();
+
+  // print table names
+  for (auto table : tables.Tables)
+  {
+    std::cout << table.TableName << std::endl;
+  }
+
+  // get statistics
+  auto statistics = tableServiceClient.GetStatistics();
+
+  std::cout << statistics.Value.GeoReplication.Status.ToString() << std::endl;
+
+  // get service properties
+  auto serviceProperties = tableServiceClient.GetServiceProperties();
+
+  std::cout << serviceProperties.Value.MinuteMetrics.Version << std::endl;
+
+  return 0;
+}
