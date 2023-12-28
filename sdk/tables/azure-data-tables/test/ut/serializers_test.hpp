25b33fad064cf9c77bd4b283fa313ef6f4a61142
George Arama <gearama@microsoft.com>
2023-12-28 13:12:36 -0800

    clang

--- sdk/tables/azure-data-tables/test/ut/serializers_test.hpp
+++ sdk/tables/azure-data-tables/test/ut/serializers_test.hpp
@@ -9,5 +9,6 @@
 
 namespace Azure { namespace Data { namespace Test {
 
-  class SerializersTest : public Azure::Storage::Test::StorageTest {};
+  class SerializersTest : public Azure::Storage::Test::StorageTest {
+  };
 }}} // namespace Azure::Data::Test
