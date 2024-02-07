50cc74286566e6810a3a51ac26b5cf3b54647da9
George Arama <50641385+gearama@users.noreply.github.com>
2024-02-07 12:20:52 -0800

    Json test (#5324)
    * mroe quotes
    
    * dssf
    
    * sqa
    
    * Json test p1
    
    * qw
    
    * re
    
    * pr build
    
    * cast part 2
    
    * ew
    
    * jj

--- sdk/core/azure-core/test/perf/src/azure_core_perf_test.cpp
+++ sdk/core/azure-core/test/perf/src/azure_core_perf_test.cpp
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 #include "azure/core/test/http_transport_test.hpp"
+#include "azure/core/test/json_test.hpp"
 #include "azure/core/test/nullable_test.hpp"
 #include "azure/core/test/pipeline_test.hpp"
 #include "azure/core/test/uuid_test.hpp"
@@ -16,6 +17,7 @@ int main(int argc, char** argv)
   // Create the test list
   std::vector<Azure::Perf::TestMetadata> tests{
       Azure::Core::Test::HTTPTransportTest::GetTestMetadata(),
+      Azure::Core::Test::JsonTest::GetTestMetadata(),
       Azure::Core::Test::NullableTest::GetTestMetadata(),
       Azure::Core::Test::PipelineTest::GetTestMetadata(),
       Azure::Core::Test::UuidTest::GetTestMetadata()};
