f88dfd9d1ddac88abf89d8494e31aa92162ca85c
Anton Kolesnyk <41349689+antkmsft@users.noreply.github.com>
2024-02-27 14:03:09 -0800

    Format (sort usings alphabetically)

--- sdk/core/azure-core/src/http/bearer_token_authentication_policy.cpp
+++ sdk/core/azure-core/src/http/bearer_token_authentication_policy.cpp
@@ -13,8 +13,8 @@ using Azure::Core::Context;
 using Azure::Core::Credentials::AuthenticationException;
 using Azure::Core::Credentials::TokenRequestContext;
 using Azure::Core::Credentials::_detail::AuthorizationChallengeHelper;
-using Azure::Core::Http::Request;
 using Azure::Core::Http::RawResponse;
+using Azure::Core::Http::Request;
 using Azure::Core::Http::Policies::NextHttpPolicy;
 
 std::unique_ptr<RawResponse> BearerTokenAuthenticationPolicy::Send(
