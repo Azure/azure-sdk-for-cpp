5f9607660c4f700e21717b32061f4ab20d65a682
microzchang <110015819+microzchang@users.noreply.github.com>
2024-02-29 11:07:50 +0800

    Storage/STG93 Update documents for UPN (#5391)
    * update documentation for upn.
    
    * Remove en-us in link.

--- sdk/storage/azure-storage-files-datalake/inc/azure/storage/files/datalake/datalake_options.hpp
+++ sdk/storage/azure-storage-files-datalake/inc/azure/storage/files/datalake/datalake_options.hpp
@@ -322,9 +322,11 @@ namespace Azure { namespace Storage { namespace Files { namespace DataLake {
     /**
      * Valid only when Hierarchical Namespace is enabled for the account. If "true", the user
      * identity values returned in the owner and group fields of each list entry will be transformed
-     * from Azure Active Directory Object IDs to User Principal Names. If "false", the values will
-     * be returned as Azure Active Directory Object IDs. The default value is false. Note that group
-     * and application Object IDs are not translated because they do not have unique friendly names.
+     * from Azure Active Directory Object IDs to User Principal Names. If "false" or not provided,
+     * the values will be returned as Azure Active Directory Object IDs. Note that group and
+     * application Object IDs are not translated because they do not have unique friendly names.
+     * More Details about UserPrincipalName, See
+     * https://learn.microsoft.com/entra/identity/hybrid/connect/plan-connect-userprincipalname#what-is-userprincipalname
      */
     Azure::Nullable<bool> UserPrincipalName;
 
@@ -770,9 +772,11 @@ namespace Azure { namespace Storage { namespace Files { namespace DataLake {
     /**
      * Valid only when Hierarchical Namespace is enabled for the account. If "true", the user
      * identity values returned in the owner and group fields of each list entry will be transformed
-     * from Azure Active Directory Object IDs to User Principal Names. If "false", the values will
-     * be returned as Azure Active Directory Object IDs. The default value is false. Note that group
-     * and application Object IDs are not translated because they do not have unique friendly names.
+     * from Azure Active Directory Object IDs to User Principal Names. If "false" or not provided,
+     * the values will be returned as Azure Active Directory Object IDs. Note that group and
+     * application Object IDs are not translated because they do not have unique friendly names.
+     * More Details about UserPrincipalName, See
+     * https://learn.microsoft.com/entra/identity/hybrid/connect/plan-connect-userprincipalname#what-is-userprincipalname
      */
     Nullable<bool> IncludeUserPrincipalName;
   };
@@ -791,9 +795,11 @@ namespace Azure { namespace Storage { namespace Files { namespace DataLake {
     /**
      * Valid only when Hierarchical Namespace is enabled for the account. If "true", the user
      * identity values returned in the owner and group fields of each list entry will be transformed
-     * from Azure Active Directory Object IDs to User Principal Names. If "false", the values will
-     * be returned as Azure Active Directory Object IDs. The default value is false. Note that group
-     * and application Object IDs are not translated because they do not have unique friendly names.
+     * from Azure Active Directory Object IDs to User Principal Names. If "false" or not provided,
+     * the values will be returned as Azure Active Directory Object IDs. Note that group and
+     * application Object IDs are not translated because they do not have unique friendly names.
+     * More Details about UserPrincipalName, See
+     * https://learn.microsoft.com/entra/identity/hybrid/connect/plan-connect-userprincipalname#what-is-userprincipalname
      */
     Nullable<bool> IncludeUserPrincipalName;
   };
@@ -824,9 +830,11 @@ namespace Azure { namespace Storage { namespace Files { namespace DataLake {
     /**
      * Valid only when Hierarchical Namespace is enabled for the account. If "true", the user
      * identity values returned in the owner and group fields of each list entry will be transformed
-     * from Azure Active Directory Object IDs to User Principal Names. If "false", the values will
-     * be returned as Azure Active Directory Object IDs. The default value is false. Note that group
-     * and application Object IDs are not translated because they do not have unique friendly names.
+     * from Azure Active Directory Object IDs to User Principal Names. If "false" or not provided,
+     * the values will be returned as Azure Active Directory Object IDs. Note that group and
+     * application Object IDs are not translated because they do not have unique friendly names.
+     * More Details about UserPrincipalName, See
+     * https://learn.microsoft.com/entra/identity/hybrid/connect/plan-connect-userprincipalname#what-is-userprincipalname
      */
     Nullable<bool> IncludeUserPrincipalName;
   };
