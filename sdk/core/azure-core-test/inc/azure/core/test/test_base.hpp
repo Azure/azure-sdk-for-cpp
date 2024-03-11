f7b2b7bb9b6f5d3dfa03e1331942cdd7e8b8e72c
George Arama <gearama@microsoft.com>
2024-03-11 14:06:17 -0700

    dsaada

--- sdk/core/azure-core-test/inc/azure/core/test/test_base.hpp
+++ sdk/core/azure-core-test/inc/azure/core/test/test_base.hpp
@@ -162,11 +162,7 @@ namespace Azure { namespace Core { namespace Test {
     // creating
     std::string GetTestNameLowerCase(bool sanitize = true)
     {
-      srand(static_cast<int>(time(0))); // NOLINT(cert-msc51-cpp)
-      std::stringstream string_object;
-      string_object << rand() % 1000; // Insert operation
-      return Azure::Core::_internal::StringExtensions::ToLower(GetTestName(sanitize))
-          + string_object.str();
+      return Azure::Core::_internal::StringExtensions::ToLower(GetTestName(sanitize));
     }
 
     /**
