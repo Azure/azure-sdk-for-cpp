d7a19fbd6c5f4085eae297beb5d6da71be231a4d
Praveen Kuttappan <prmarott@microsoft.com>
2024-03-19 14:20:09 -0400

    Update pipeline step to remove deprecated params in API view script

--- eng/scripts/Create-APIReview.ps1
+++ eng/scripts/Create-APIReview.ps1
@@ -1,44 +1,47 @@
 Param(
     [Parameter(Mandatory=$True)]
-    [string] $ArtifactName,
+    [array] $ArtifactList,
     [Parameter(Mandatory=$True)]
     [string] $OutPath,
     [Parameter(Mandatory=$True)]
-    [string] $ApiviewUri,
-    [Parameter(Mandatory=$True)]
     [string] $ApiKey,
-    [Parameter(Mandatory=$True)]
-    [string] $ApiLabel,
     [Parameter(Mandatory=$True)]
     [string] $SourceBranch,
     [Parameter(Mandatory=$True)]
     [string] $DefaultBranch,
     [Parameter(Mandatory=$True)]
-    [string] $ConfigFileDir,
-    [Parameter(Mandatory=$True)]
     [string] $ParserPath,
     [Parameter(Mandatory=$True)]
-    [string] $SourcePath
+    [string] $ServicePath,
+    [Parameter(Mandatory=$True)]
+    [string] $RepoName,
+    [Parameter(Mandatory=$True)]
+    [string] $BuildId,
 )
 
 Write-Host "$PSScriptRoot"
 . (Join-Path $PSScriptRoot .. common scripts common.ps1)
-$createReviewScript = (Join-Path $PSScriptRoot .. common scripts Create-APIReview.ps1)
 
-$apiviewSettings = Join-Path $SourcePath "ApiViewSettings.json"
-if (!(Test-Path $apiviewSettings))
+foreach ($artifact in $ArtifactList)
 {
-    Write-Host "ApiViewSettings.json file is not found in $($SourcePath). APIView settings file is required to generate API review file."
-    exit 1
+    $ArtifactName = $artifact.name
+    Write-Host "Generating artifact for $ArtifactName"
+    $SourcePath = Join-Path $ServicePath $ArtifactName "inc"
+    $apiviewSettings = Join-Path $SourcePath "ApiViewSettings.json"
+    if (!(Test-Path $apiviewSettings))
+    {
+        Write-Host "ApiViewSettings.json file is not found in $($SourcePath). APIView settings file is required to generate API review file."
+        exit 1
+    }
+
+    Write-Host "Creating API review artifact for $($ArtifactName)"
+    New-Item -ItemType Directory -Path $OutPath/$ArtifactName -force
+    $parentPath = Split-Path $ParserPath  -Parent
+    Write-Host "Contents in $($parentPath)"
+    Get-ChildItem -Path $parentPath -Recurse
+    & $ParserPath -o $OutPath/$ArtifactName/$ArtifactName.json $SourcePath
 }
 
-Write-Host "Creating API review artifact for $($ArtifactName)"
-New-Item -ItemType Directory -Path $OutPath/$ArtifactName -force
-$parentPath = Split-Path $ParserPath  -Parent
-Write-Host "Contents in $($parentPath)"
-Get-ChildItem -Path $parentPath -Recurse
-
-& $ParserPath -o $OutPath/$ArtifactName/$ArtifactName.json $SourcePath
-
-Write-Host "Send request to APIView to create review for $ArtifactName"
-&($createReviewScript) -ArtifactPath $OutPath -APIViewUri $ApiviewUri -APIKey $ApiKey -APILabel $ApiLabel -PackageName $ArtifactName -SourceBranch $SourceBranch -DefaultBranch $DefaultBranch -ConfigFileDir $ConfigFileDir
+$createReviewScript = (Join-Path $PSScriptRoot .. common scripts Create-APIReview.ps1)
+Write-Host "Running script to create review for all artifacts."
+&($createReviewScript) -ArtifactList $ArtifactList -ArtifactPath $OutPath -APIKey $ApiKey -SourceBranch $SourceBranch -DefaultBranch $DefaultBranch -RepoName $RepoName -BuildId $BuildId
