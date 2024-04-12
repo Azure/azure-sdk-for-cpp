d90acb7b6f13ce087341eb20e534a384eb7d30b2
George Arama <gearama@microsoft.com>
2024-04-12 11:10:53 -0700

    typo

--- sdk/storage/azure-storage-files-shares/test/ut/share_file_client_test.cpp
+++ sdk/storage/azure-storage-files-shares/test/ut/share_file_client_test.cpp
@@ -901,7 +901,7 @@ namespace Azure { namespace Storage { namespace Test {
     }
   }
 
-  TEST_F(FileShareFileClientTest, DISABLE_GetRangeListDiffWithRename)
+  TEST_F(FileShareFileClientTest, DISABLED_GetRangeListDiffWithRename)
   {
     size_t rangeSize = 128;
     std::vector<uint8_t> rangeContent = RandomBuffer(rangeSize);
