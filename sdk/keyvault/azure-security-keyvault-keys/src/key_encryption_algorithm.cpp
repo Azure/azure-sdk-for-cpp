d0dc6511a98b0cd158ef937ecc39cfb7d17fafb8
George Arama <gearama@microsoft.com>
2024-05-21 10:36:06 -0700

    need to remove from cpp due to deprecated error

--- sdk/keyvault/azure-security-keyvault-keys/src/key_encryption_algorithm.cpp
+++ sdk/keyvault/azure-security-keyvault-keys/src/key_encryption_algorithm.cpp
@@ -5,23 +5,6 @@
 
 namespace Azure { namespace Security { namespace KeyVault { namespace Keys {
 
-#ifdef _MSC_VER
-#pragma warning(disable : 4996) // disable warning 4996 for deprecated declarations
-#endif
-
-  const KeyEncryptionAlgorithm KeyEncryptionAlgorithm::CKM_RSA_AES_KEY_WRAP(
-      _detail::CKM_RSA_AES_KEY_WRAP_Value);
-  
-  const KeyEncryptionAlgorithm KeyEncryptionAlgorithm::RSA_AES_KEY_WRAP_256(
-      _detail::RSA_AES_KEY_WRAP_256_Value);
-  
-  const KeyEncryptionAlgorithm KeyEncryptionAlgorithm::RSA_AES_KEY_WRAP_384(
-      _detail::RSA_AES_KEY_WRAP_384_Value);
-
-#ifdef _MSC_VER
-#pragma warning(default : 4996) // enable warning 4996 back for deprecated declarations
-#endif
-
   const KeyEncryptionAlgorithm KeyEncryptionAlgorithm::CkmRsaAesKeyWrap(
       _detail::CKM_RSA_AES_KEY_WRAP_Value);
 
