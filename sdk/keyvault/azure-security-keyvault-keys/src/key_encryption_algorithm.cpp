96648c950cd0a46291de021e786f7f04547779b5
George Arama <gearama@microsoft.com>
2024-05-21 10:12:56 -0700

    dasda

--- sdk/keyvault/azure-security-keyvault-keys/src/key_encryption_algorithm.cpp
+++ sdk/keyvault/azure-security-keyvault-keys/src/key_encryption_algorithm.cpp
@@ -1,28 +1,33 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
-#ifdef _MSC_VER
-#pragma warning(disable : 4996) // disable warning 4996 for deprecated declarations
-#endif
 #include "azure/keyvault/keys/key_client_models.hpp"
 #include "private/key_constants.hpp"
 
 namespace Azure { namespace Security { namespace KeyVault { namespace Keys {
 
+#ifdef _MSC_VER
+#pragma warning(disable : 4996) // disable warning 4996 for deprecated declarations
+#endif
+
   const KeyEncryptionAlgorithm KeyEncryptionAlgorithm::CKM_RSA_AES_KEY_WRAP(
       _detail::CKM_RSA_AES_KEY_WRAP_Value);
-  const KeyEncryptionAlgorithm KeyEncryptionAlgorithm::CkmRsaAesKeyWrap(
-      _detail::CKM_RSA_AES_KEY_WRAP_Value);
-
+  
   const KeyEncryptionAlgorithm KeyEncryptionAlgorithm::RSA_AES_KEY_WRAP_256(
       _detail::RSA_AES_KEY_WRAP_256_Value);
-  const KeyEncryptionAlgorithm KeyEncryptionAlgorithm::RsaAesKeyWrap256(
-      _detail::RSA_AES_KEY_WRAP_256_Value);
-
+  
   const KeyEncryptionAlgorithm KeyEncryptionAlgorithm::RSA_AES_KEY_WRAP_384(
       _detail::RSA_AES_KEY_WRAP_384_Value);
-  const KeyEncryptionAlgorithm KeyEncryptionAlgorithm::RsaAesKeyWrap384(
-      _detail::RSA_AES_KEY_WRAP_384_Value);
-}}}} // namespace Azure::Security::KeyVault::Keys
+
 #ifdef _MSC_VER
 #pragma warning(default : 4996) // enable warning 4996 back for deprecated declarations
 #endif
+
+  const KeyEncryptionAlgorithm KeyEncryptionAlgorithm::CkmRsaAesKeyWrap(
+      _detail::CKM_RSA_AES_KEY_WRAP_Value);
+
+  const KeyEncryptionAlgorithm KeyEncryptionAlgorithm::RsaAesKeyWrap256(
+      _detail::RSA_AES_KEY_WRAP_256_Value);
+
+  const KeyEncryptionAlgorithm KeyEncryptionAlgorithm::RsaAesKeyWrap384(
+      _detail::RSA_AES_KEY_WRAP_384_Value);
+}}}} // namespace Azure::Security::KeyVault::Keys
