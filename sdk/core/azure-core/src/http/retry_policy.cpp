e3e178ce56b98ecdb5e1ffb174971a72ff8661f5
Ahson Khan <ahkha@microsoft.com>
2024-05-22 12:03:19 -0700

    Update the RetryPolicy and ShouldRetry customization logic to allow loosen the retry condition.

--- sdk/core/azure-core/src/http/retry_policy.cpp
+++ sdk/core/azure-core/src/http/retry_policy.cpp
@@ -120,7 +120,7 @@ int32_t RetryPolicy::GetRetryCount(Context const& context)
 
 bool RetryPolicy::ShouldRetry(std::unique_ptr<RawResponse> const&, RetryOptions const&) const
 {
-  return true;
+  return false;
 }
 
 std::unique_ptr<RawResponse> RetryPolicy::Send(
@@ -147,17 +147,14 @@ std::unique_ptr<RawResponse> RetryPolicy::Send(
 
       // If we are out of retry attempts, if a response is non-retriable (or simply 200 OK, i.e
       // doesn't need to be retried), then ShouldRetryOnResponse returns false.
-      if (!ShouldRetryOnResponse(*response.get(), m_retryOptions, attempt, retryAfter))
+
+      // Service SDKs can inject custom logic to define whether the request should be retried,
+      // based on the response. The default of `ShouldRetry` is false.
+      if (!ShouldRetryOnResponse(*response.get(), m_retryOptions, attempt, retryAfter)
+          && !ShouldRetry(response, m_retryOptions))
       {
         // If this is the second attempt and StartTry was called, we need to stop it. Otherwise
         // trying to perform same request would use last retry query/headers
-        return response;
-      }
-
-      // Service SDKs can inject custom logic to define whether the request should be retried,
-      // based on the response. The default is true.
-      if (!ShouldRetry(response, m_retryOptions))
-      {
         return response;
       }
     }
