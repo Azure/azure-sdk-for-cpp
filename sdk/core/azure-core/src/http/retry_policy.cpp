9bc6f8b0c1e85020ad99dcb58f896a0e3503cf7f
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2024-07-12 11:32:29 -0700

    Revert "Add a virtual ShouldRetry method to the RetryPolicy for customization. (#5584)"
    This reverts commit ab90ef68b03684def81492e13542b02406160a85.

--- sdk/core/azure-core/src/http/retry_policy.cpp
+++ sdk/core/azure-core/src/http/retry_policy.cpp
@@ -118,11 +118,6 @@ int32_t RetryPolicy::GetRetryCount(Context const& context)
   return *ptr;
 }
 
-bool RetryPolicy::ShouldRetry(std::unique_ptr<RawResponse> const&, RetryOptions const&) const
-{
-  return true;
-}
-
 std::unique_ptr<RawResponse> RetryPolicy::Send(
     Request& request,
     NextHttpPolicy nextPolicy,
@@ -146,18 +141,11 @@ std::unique_ptr<RawResponse> RetryPolicy::Send(
       auto response = nextPolicy.Send(request, retryContext);
 
       // If we are out of retry attempts, if a response is non-retriable (or simply 200 OK, i.e
-      // doesn't need to be retried), then ShouldRetryOnResponse returns false.
+      // doesn't need to be retried), then ShouldRetry returns false.
       if (!ShouldRetryOnResponse(*response.get(), m_retryOptions, attempt, retryAfter))
       {
         // If this is the second attempt and StartTry was called, we need to stop it. Otherwise
         // trying to perform same request would use last retry query/headers
-        return response;
-      }
-
-      // Service SDKs can inject custom logic to define whether the request should be retried,
-      // based on the response. The default is true.
-      if (!ShouldRetry(response, m_retryOptions))
-      {
         return response;
       }
     }
