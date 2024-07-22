d364c4c17f52f364b4dacb76f8a6dbeebfd096ba
microzchang <zchang@microsoft.com>
2024-07-22 12:33:43 +0800

    Add missing ignore

--- sdk/storage/azure-storage-files-datalake/test/ut/datalake_sas_test.cpp
+++ sdk/storage/azure-storage-files-datalake/test/ut/datalake_sas_test.cpp
@@ -818,7 +818,24 @@ namespace Azure { namespace Storage { namespace Test {
       auto sasToken = datalakeSasBuilder.GenerateSasToken(*keyCredential);
       auto signature = Azure::Core::Url::Decode(
           Azure::Core::Url(blobUrl + sasToken).GetQueryParameters().find("sig")->second);
+#if defined(_MSC_VER)
+#pragma warning(push)
+#pragma warning(disable : 4996)
+#elif defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#elif defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#endif
       auto stringToSign = datalakeSasBuilder.GenerateSasStringToSign(*keyCredential);
+#if defined(_MSC_VER)
+#pragma warning(pop)
+#elif defined(__clang__)
+#pragma clang diagnostic pop
+#elif defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif // _MSC_VER
       auto signatureFromStringToSign = Azure::Core::Convert::Base64Encode(_internal::HmacSha256(
           std::vector<uint8_t>(stringToSign.begin(), stringToSign.end()),
           Azure::Core::Convert::Base64Decode(accountKey)));
