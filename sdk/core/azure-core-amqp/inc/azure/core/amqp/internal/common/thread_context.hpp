c48eab5ed97e49dba7e70953d8045fa595c5d42c
Larry Osterman <LarryOsterman@users.noreply.github.com>
2024-09-16 13:15:54 -0700

    clang-format

---
+++ sdk/core/azure-core-amqp/inc/azure/core/amqp/internal/common/thread_context.hpp
@@ -0,0 +1,53 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#pragma once
+
+#if ENABLE_RUST_AMQP
+#include "../rust_amqp/rust_wrapper/rust_amqp_wrapper.h"
+#include "../src/amqp/private/unique_handle.hpp"
+
+#include <azure/core/azure_assert.hpp>
+
+#include <atomic>
+#include <list>
+#include <memory>
+#include <mutex>
+#include <thread>
+
+namespace Azure {
+namespace Core {
+  namespace Amqp {
+    namespace _detail {
+
+      using RustRuntimeContext = RustInterop::RuntimeContext;
+
+      template <> struct UniqueHandleHelper<RustRuntimeContext>
+      {
+        static void FreeRuntimeContext(RustRuntimeContext* obj);
+
+        using type = Core::_internal::BasicUniqueHandle<RustRuntimeContext, FreeRuntimeContext>;
+      };
+    }
+  }
+}
+} // namespace Azure::Core::Amqp::_detail
+
+namespace Azure { namespace Core { namespace Amqp { namespace Common { namespace _detail {
+
+  using UniqueRustRuntimeContext = Azure::Core::Amqp::_detail::UniqueHandleHelper<
+      Azure::Core::Amqp::_detail::RustRuntimeContext>::type;
+  class RustThreadContext final {
+
+    UniqueRustRuntimeContext m_runtimeContext;
+
+  public:
+    RustThreadContext() : m_runtimeContext(Azure::Core::Amqp::_detail::RustInterop::runtime_context_new()) {}
+    Azure::Core::Amqp::_detail::RustRuntimeContext* GetRuntimeContext() const noexcept
+    {
+      return m_runtimeContext.get();
+    }
+  };
+
+}}}}} // namespace Azure::Core::Amqp::Common::_detail
+#endif // ENABLE_RUST_AMQP
---
+++ sdk/core/azure-core-amqp/rust_amqp/rust_wrapper/src/tracing.rs
@@ -0,0 +1,66 @@
+// Copyright (c) Microsoft Corp. All Rights Reserved
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+/*
+cspell: words reqwest repr staticlib dylib brotli gzip
+*/
+
+struct RustTracingSubscriber {
+    tracing_callback: Option<extern "C" fn(_: *const c_char)>,
+}
+
+impl RustTracingSubscriber {
+    fn new() -> Self {
+        Self {
+            tracing_callback: None,
+        }
+    }
+
+    fn set_tracing_callback(&mut self, callback: extern "C" fn(_: *const c_char)) {
+        self.tracing_callback = Some(callback);
+    }
+
+    fn on_event(&self, message: &str) {
+        if let Some(callback) = self.tracing_callback {
+            let c_message = CString::new(message).unwrap();
+            callback(c_message.as_ptr());
+        }
+    }
+}
+
+impl tracing::Subscriber for RustTracingSubscriber {
+    fn enabled(&self, _metadata: &tracing::Metadata) -> bool {
+        true
+    }
+
+    fn new_span(&self, span: &tracing::span::Attributes) -> tracing::span::Id {
+        tracing::span::Id::from_u64(span.metadata().id().into_u64())
+    }
+
+    fn record(&self, _span: &tracing::span::Id, _values: &tracing::span::Record) {}
+
+    fn record_follows_from(&self, _span: &tracing::span::Id, _follows: &tracing::span::Id) {}
+
+    fn event(&self, event: &tracing::Event) {
+        self.on_event(event.metadata().name());
+    }
+
+    fn enter(&self, _span: &tracing::span::Id) {}
+
+    fn exit(&self, _span: &tracing::span::Id) {}
+
+    fn clone_span(&self, _span: &tracing::span::Id) -> tracing::span::Id {
+        tracing::span::Id::from_u64(0)
+    }
+
+    fn drop_span(&self, _span: tracing::span::Id) {}
+}
+
+#[no_mangle]
+pub extern "C" fn register_tracing_callback(callback: extern "C" fn(_: *const c_char)) {
+    let message = CString::new("register_tracing_callback").unwrap();
+    callback(message.as_ptr());
+}
+
+#[no_mangle]
+pub extern "C" fn unregister_tracing_callback() {}
