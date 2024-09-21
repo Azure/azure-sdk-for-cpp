53fc6524036582e401d7a2ddb3e09cedf6b13094
Ahson Khan <ahkha@microsoft.com>
2024-09-21 00:12:15 -0700

    Use code snippets from managed identity credential samples in identity readme doc.

--- sdk/identity/azure-identity/samples/managed_identity_credential.cpp
+++ sdk/identity/azure-identity/samples/managed_identity_credential.cpp
@@ -3,6 +3,7 @@
 
 #include <azure/identity/managed_identity_credential.hpp>
 #include <azure/service/client.hpp>
+#include <azure/storage/blobs.hpp>
 
 #include <iostream>
 
@@ -32,4 +33,61 @@ int main()
   }
 
   return 0;
+}
+
+static void ShowDifferentManagedIdentityApproaches()
+{
+  using namespace Azure::Identity;
+  using namespace Azure::Storage::Blobs;
+
+  std::string blobUrl = "https://myaccount.blob.core.windows.net/mycontainer/myblob";
+  {
+    // @begin_snippet: UserAssignedManagedIdentityViaClientId
+    // When deployed to an Azure host, ManagedIdentityCredential will authenticate the specified
+    // user-assigned managed identity.
+
+    std::string userAssignedClientId = "<your managed identity client ID>";
+    ManagedIdentityCredentialOptions options;
+    options.IdentityId = ManagedIdentityId(ManagedIdentityIdKind::ClientId, userAssignedClientId);
+
+    auto credential = std::make_shared<ManagedIdentityCredential>(options);
+    auto blobClient = BlobClient(blobUrl, credential);
+    // @end_snippet
+  }
+  {
+    // @begin_snippet: UserAssignedManagedIdentityViaResourceId
+    std::string userAssignedResourceId = "<your managed identity resource ID>";
+    ManagedIdentityCredentialOptions options;
+    options.IdentityId
+        = ManagedIdentityId(ManagedIdentityIdKind::ResourceId, userAssignedResourceId);
+
+    auto credential = std::make_shared<ManagedIdentityCredential>(options);
+    auto blobClient = BlobClient(blobUrl, credential);
+    // @end_snippet
+  }
+  {
+    // @begin_snippet: UserAssignedManagedIdentityViaObjectId
+    std::string userAssignedObjectId = "<your managed identity object ID>";
+    ManagedIdentityCredentialOptions options;
+    options.IdentityId = ManagedIdentityId(ManagedIdentityIdKind::ObjectId, userAssignedObjectId);
+
+    auto credential = std::make_shared<ManagedIdentityCredential>(options);
+    auto blobClient = BlobClient(blobUrl, credential);
+    // @end_snippet
+  }
+  {
+    // @begin_snippet: SystemAssignedManagedIdentity
+    ManagedIdentityCredentialOptions options;
+    options.IdentityId = ManagedIdentityId(ManagedIdentityIdKind::SystemAssigned, {});
+
+    auto credential = std::make_shared<ManagedIdentityCredential>(options);
+    auto blobClient = BlobClient(blobUrl, credential);
+    // @end_snippet
+  }
+  {
+    // @begin_snippet: SystemAssignedManagedIdentityBrief
+    auto credential = std::make_shared<ManagedIdentityCredential>();
+    auto blobClient = BlobClient(blobUrl, credential);
+    // @end_snippet
+  }
 }
