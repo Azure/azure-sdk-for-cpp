a55f168776da046350a4c314e390bbbdb63d6036
Larry Osterman <LarryOsterman@users.noreply.github.com>
2024-09-25 16:58:42 -0700

    Oops

--- sdk/core/azure-core-amqp/src/impl/rust_amqp/rust_amqp/azure_core_amqp/src/cbs.rs
+++ sdk/core/azure-core-amqp/src/impl/rust_amqp/rust_amqp/azure_core_amqp/src/cbs.rs
@@ -1,4 +1,4 @@
- // Copyright (c) Microsoft Corporation. All Rights reserved
+// Copyright (c) Microsoft Corporation. All Rights reserved
 // Licensed under the MIT license.
 // cspell: words amqp sasl
 
