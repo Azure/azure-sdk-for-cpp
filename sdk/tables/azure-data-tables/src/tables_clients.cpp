a0546d1a0b22743d56d9f639cba15519818dc640
Ahson Khan <ahkha@microsoft.com>
2024-10-28 15:08:07 -0700

    Rename table client headers and split out the clients into their own files.

--- sdk/tables/azure-data-tables/src/tables_clients.cpp
+++ sdk/tables/azure-data-tables/src/tables_clients.cpp
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-#include "azure/data/tables/tables_clients.hpp"
-
+#include "azure/data/tables/table_client.hpp"
+#include "azure/data/tables/table_service_client.hpp"
 #include "private/policies/service_version_policy.hpp"
 #include "private/policies/shared_key_lite_policy.hpp"
 #include "private/policies/tenant_bearer_token_policy.hpp"
--- sdk/tables/azure-data-tables/test/ut/serializers_test.hpp
+++ sdk/tables/azure-data-tables/test/ut/serializers_test.hpp
@@ -3,11 +3,10 @@
 // Licensed under the MIT License.
 
 #include "../src/private/serializers.hpp"
-#include "azure/data/tables/tables_clients.hpp"
+#include "azure/data/tables/table_client.hpp"
 #include "test/ut/test_base.hpp"
 
 namespace Azure { namespace Data { namespace Test {
 
-  class SerializersTest : public Azure::Storage::Test::StorageTest {
-  };
+  class SerializersTest : public Azure::Storage::Test::StorageTest {};
 }}} // namespace Azure::Data::Test
