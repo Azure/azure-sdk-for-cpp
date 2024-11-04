e8f58edee9fca7b2e3d0f40368dca44e5582b1c3
Larry Osterman <LarryOsterman@users.noreply.github.com>
2024-11-04 13:59:39 -0800

    cspell2

--- sdk/core/azure-core/src/http/winhttp/win_http_transport.cpp
+++ sdk/core/azure-core/src/http/winhttp/win_http_transport.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
-// cspell:words HCERTIFICATECHAIN PCCERT CCERT HCERTCHAINENGINE HCERTSTORE lpsz REFERER hcryptprov ncrypt hcryptkey
+// cspell:words HCERTIFICATECHAIN PCCERT CCERT HCERTCHAINENGINE HCERTSTORE lpsz REFERER hcryptprov
+// ncrypt hcryptkey
 
 #include "azure/core/base64.hpp"
 #include "azure/core/diagnostics/logger.hpp"
