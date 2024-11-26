4e3b9a54869a4e379580acedc30d2a49a22f32ee
George Arama <gearama@microsoft.com>
2024-11-25 16:31:39 -0800

    gdf

--- cmake-modules/AddGoogleTest.cmake
+++ cmake-modules/AddGoogleTest.cmake
@@ -15,7 +15,7 @@ if(CMAKE_VERSION VERSION_LESS 3.11)
     download_project(
         PROJ                googletest
 		GIT_REPOSITORY      https://github.com/google/googletest.git
-		GIT_TAG             v1.15.2
+		GIT_TAG             v1.14.0
 		UPDATE_DISCONNECTED 1
 		QUIET
     )
@@ -29,7 +29,7 @@ else()
     FetchContent_Declare(
         googletest
         GIT_REPOSITORY      https://github.com/google/googletest.git
-        GIT_TAG             v1.15.2
+        GIT_TAG             v1.14.0
         )
     FetchContent_GetProperties(googletest)
     if(NOT googletest_POPULATED)
