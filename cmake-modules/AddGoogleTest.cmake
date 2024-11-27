717fd5f40e3316dd81bf02ecfa2496c9ef7feb77
George Arama <gearama@microsoft.com>
2024-11-27 12:32:03 -0800

    fds

--- cmake-modules/AddGoogleTest.cmake
+++ cmake-modules/AddGoogleTest.cmake
@@ -24,8 +24,22 @@ if(CMAKE_VERSION VERSION_LESS 3.11)
     set(CMAKE_SUPPRESS_DEVELOPER_WARNINGS 1 CACHE BOOL "")
     add_subdirectory(${googletest_SOURCE_DIR} ${googletest_SOURCE_DIR} EXCLUDE_FROM_ALL)
     unset(CMAKE_SUPPRESS_DEVELOPER_WARNINGS)
-else()
+elseif(CMAKE_VERSION VERSION_GREATER_EQUAL 3.31.0)
     include(FetchContent)
+    FetchContent_Declare(
+        googletest
+        GIT_REPOSITORY      https://github.com/google/googletest.git
+        GIT_TAG             release-1.12.1
+        )
+    FetchContent_GetProperties(googletest)
+    if(NOT googletest_POPULATED)
+        FetchContent_MakeAvailable(googletest)
+        set(CMAKE_SUPPRESS_DEVELOPER_WARNINGS 1 CACHE BOOL "")
+        add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR} EXCLUDE_FROM_ALL)
+        unset(CMAKE_SUPPRESS_DEVELOPER_WARNINGS)
+    endif()
+else()
+     include(FetchContent)
     FetchContent_Declare(
         googletest
         GIT_REPOSITORY      https://github.com/google/googletest.git
