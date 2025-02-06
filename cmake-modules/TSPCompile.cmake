07a4af73b724e881baeeaf646c11d19bfd49d7db
George Arama <gearama@microsoft.com>
2025-02-06 13:34:24 -0800

    update to use tsp client

--- cmake-modules/TSPCompile.cmake
+++ cmake-modules/TSPCompile.cmake
@@ -3,69 +3,40 @@
 
 find_package(Git)
 
-macro(GenerateCodeFromTypeSpec TSP_SHA TSP_REPO_PATH TSP_DESTINATION TSP_SERVICE_PATH CODEGEN_SHA CODEGEN_DESTINATION)
+macro(GenerateCodeFromTypeSpec TSP_DESTINATION TSP_SERVICE_PATH CODEGEN_SHA CODEGEN_DESTINATION)
 
     message("\
     GenerateCodeFromTypeSpec using the following params \n\
-    TSP_SHA=${TSP_SHA} \n\
-    TSP_REPO_PATH=${TSP_REPO_PATH} \n\
     TSP_DESTINATION=${TSP_DESTINATION} \n\
     TSP_SERVICE_PATH=${TSP_SERVICE_PATH} \n\
     CODEGEN_SHA=${CODEGEN_SHA} \n\
     CODEGEN_DESTINATION=${CODEGEN_DESTINATION}")
     set(CODEGEN_PATH packages/typespec-cpp)
-    DownloadTSPFiles(${TSP_SHA} ${TSP_REPO_PATH} ${TSP_DESTINATION})
+    DownloadTSPFiles(${TSP_DESTINATION})
     DownloadCodeGenerator(${CODEGEN_SHA} ${CODEGEN_DESTINATION})
-    GenerateCodeFromTSP(${TSP_DESTINATION} ${TSP_REPO_PATH} ${TSP_SERVICE_PATH} ${CODEGEN_DESTINATION} ${CODEGEN_PATH})
+    GenerateCodeFromTSP(${TSP_DESTINATION} ${TSP_SERVICE_PATH} ${CODEGEN_DESTINATION} ${CODEGEN_PATH})
 endmacro()
 
-macro(DownloadTSPFiles TSP_SHA TSP_REPO_PATH TSP_DESTINATION)
+macro(DownloadTSPFiles TSP_DESTINATION)
     set(TSP_REPO "https://github.com/Azure/azure-rest-api-specs.git")
     message ("\
     DownloadTSPFiles using the following params \n\
     TSP_REPO = ${TSP_REPO} \n\
-    TSP_SHA=${TSP_SHA} \n\
-    TSP_REPO_PATH=${TSP_REPO_PATH} \n\
     TSP_DESTINATION=${TSP_DESTINATION}")
 
-    if(Git_FOUND)
-        message("Git found: ${GIT_EXECUTABLE}")
-    else()
-        message(FATAL_ERROR "Git not found")
-    endif()
-    
     set(DOWNLOAD_TSP_FOLDER ${CMAKE_SOURCE_DIR}/build/${TSP_DESTINATION})
-    # if we have the git folder, we don't need to download it again
-    # this also saves times on incremental builds
-    if(NOT EXISTS ${DOWNLOAD_TSP_FOLDER}/.git)
-    message("\
-    First time setting up the repo in \n\
-    ${TSP_DESTINATION}")
-    #make folder
-    make_directory(${DOWNLOAD_TSP_FOLDER})
+    
+    file(COPY ${CMAKE_CURRENT_SOURCE_DIR}/tsp-location.yaml
+    DESTINATION ${DOWNLOAD_TSP_FOLDER})
+    
+    set(SCRIPTS_FOLDER ${CMAKE_SOURCE_DIR}/eng/scripts/typespec/)
 
-    #init git in folder
-    execute_process(COMMAND ${GIT_EXECUTABLE} init
-            WORKING_DIRECTORY ${DOWNLOAD_TSP_FOLDER})
-    #add remote
-    execute_process(COMMAND ${GIT_EXECUTABLE} remote add origin ${TSP_REPO}
-             WORKING_DIRECTORY ${DOWNLOAD_TSP_FOLDER})
-    #set sparse-checkout
-    execute_process(COMMAND ${GIT_EXECUTABLE} sparse-checkout init --cone
-             WORKING_DIRECTORY ${DOWNLOAD_TSP_FOLDER})
-    #set sparse-checkout folder
-    execute_process(COMMAND ${GIT_EXECUTABLE} sparse-checkout set ${TSP_REPO_PATH}
-             WORKING_DIRECTORY ${DOWNLOAD_TSP_FOLDER})
-    else()
-        message("Repo detected at ${TSP_DESTINATION}. Jumping ahead to checkout.") 
-    endif()
-    #fetch
-    execute_process(COMMAND ${GIT_EXECUTABLE} fetch
-         WORKING_DIRECTORY ${DOWNLOAD_TSP_FOLDER})
-    #switch branch
-    execute_process(COMMAND ${GIT_EXECUTABLE} checkout ${TSP_SHA}
-          WORKING_DIRECTORY ${DOWNLOAD_TSP_FOLDER})
- 
+    file(COPY ${SCRIPTS_FOLDER}/Run-Tsp-Update.ps1
+    DESTINATION ${DOWNLOAD_TSP_FOLDER})
+
+    execute_process(COMMAND pwsh Run-Tsp-Update.ps1
+    WORKING_DIRECTORY ${DOWNLOAD_TSP_FOLDER})
+
     if(NOT ${STATUS_CODE} EQUAL 0)
         message(FATAL_ERROR "TSP download failed.")
     endif()
@@ -108,18 +79,17 @@ macro (DownloadCodeGenerator CODEGEN_SHA CODEGEN_DESTINATION)
     endif()
 endmacro()
 
-macro(GenerateCodeFromTSP TSP_DESTINATION TSP_REPO_PATH TSP_SERVICE_PATH CODEGEN_DESTINATION CODEGEN_PATH)
+macro(GenerateCodeFromTSP TSP_DESTINATION TSP_SERVICE_PATH CODEGEN_DESTINATION CODEGEN_PATH)
     message("\
     GenerateCodeFromTSP using the following params \n\
     TSP_DESTINATION=${TSP_DESTINATION}\n\
-    TSP_REPO_PATH=${TSP_REPO_PATH}\n\
     TSP_SERVICE_PATH=${TSP_SERVICE_PATH}\n\
     CODEGEN_DESTINATION=${CODEGEN_DESTINATION}\n\
     CODEGEN_PATH=${CODEGEN_PATH}")
     message("Remember to Download the typspec-cpp emitter from npmjs.org")
     #TODO : https://github.com/Azure/azure-sdk-for-cpp/issues/6071
     set(DOWNLOAD_CODEGEN_FOLDER ${CMAKE_SOURCE_DIR}/build/${CODEGEN_DESTINATION}/${CODEGEN_PATH}/specs/${TSP_SERVICE_PATH})
-    set(DOWNLOAD_TSP_FOLDER ${CMAKE_SOURCE_DIR}/build/${TSP_DESTINATION}/${TSP_REPO_PATH})
+    set(DOWNLOAD_TSP_FOLDER ${CMAKE_SOURCE_DIR}/build/${TSP_DESTINATION}/TempTypeSpecFiles)
     set(TSP_FINAL_LOCATION ${DOWNLOAD_CODEGEN_FOLDER}/${TSP_SERVICE_PATH})
     set(SCRIPTS_FOLDER ${CMAKE_SOURCE_DIR}/eng/scripts/typespec/)
     message("\
