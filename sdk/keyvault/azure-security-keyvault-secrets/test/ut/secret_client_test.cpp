f660f6ea176cce7d03c0c428dfbc413a413dffe7
George Arama <gearama@microsoft.com>
2025-03-12 13:27:01 -0700

    cleanup test files

--- sdk/keyvault/azure-security-keyvault-secrets/test/ut/secret_client_test.cpp
+++ sdk/keyvault/azure-security-keyvault-secrets/test/ut/secret_client_test.cpp
@@ -299,3 +299,14 @@ TEST_F(KeyVaultSecretClientTest, TestGetPropertiesOfSecret)
 
   EXPECT_TRUE(secretProps.size() >= static_cast<size_t>(capacity));
 }
+
+TEST(SecretProperties, FactoryValid)
+{
+  std::string url(
+      "https://myvault.vault.azure.net/secrets/my_secret_name/4387e9f3d6e14c459867679a90fd0f79");
+  SecretProperties props = SecretProperties::CreateFromURL(url);
+  EXPECT_EQ(props.Name, "my_secret_name");
+  EXPECT_EQ(props.Version, "4387e9f3d6e14c459867679a90fd0f79");
+  EXPECT_EQ(props.Id, url);
+  EXPECT_EQ(props.VaultUrl, "https://myvault.vault.azure.net");
+}
