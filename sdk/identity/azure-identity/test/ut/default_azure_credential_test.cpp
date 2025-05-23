697b4699d2302b5776710500065f1cb74f70811f
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2025-05-23 04:57:32 -0700

    Clang-format

--- sdk/identity/azure-identity/test/ut/default_azure_credential_test.cpp
+++ sdk/identity/azure-identity/test/ut/default_azure_credential_test.cpp
@@ -138,7 +138,8 @@ TEST(DefaultAzureCredential, CachingCredential)
   EXPECT_TRUE(c2->WasInvoked);
 }
 
-class LogMessages : public ::testing::TestWithParam<std::string> {};
+class LogMessages : public ::testing::TestWithParam<std::string> {
+};
 
 INSTANTIATE_TEST_SUITE_P(
     DefaultAzureCredential,
