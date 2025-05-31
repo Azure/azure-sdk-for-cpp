b007f228fcc9b17b7ad745dd6957a597ab9f20bb
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2025-05-30 17:31:48 -0700

    Identity samples: make sample service to invoke GetToken()

--- sdk/identity/azure-identity/samples/client_certificate_credential.cpp
+++ sdk/identity/azure-identity/samples/client_certificate_credential.cpp
@@ -18,6 +18,10 @@ int main()
 {
   try
   {
+    // To diagnose, see https://aka.ms/azsdk/cpp/identity/troubleshooting
+    // For example, try setting 'AZURE_LOG_LEVEL' environment variable to 'verbose' before running
+    // this sample to see more details.
+
     // Step 1: Initialize Client Certificate Credential.
     auto clientCertificateCredential
         = std::make_shared<Azure::Identity::ClientCertificateCredential>(
@@ -37,6 +41,24 @@ int main()
     // (invalid credential parameters, insufficient permissions).
     std::cout << "Authentication error: " << exception.what() << std::endl;
     return 1;
+  }
+  catch (const Azure::Core::RequestFailedException& exception)
+  {
+    // Authentication exceptions are thrown as AuthenticationExceptions, client errors are thrown as
+    // RequestFailedExceptions, so it is easier to differentiate whether the request has failed
+    // due to input data, or due to authentication errors.
+    std::cout << "Azure service request error: " << exception.what() << std::endl
+              << "Status: " << static_cast<int>(exception.StatusCode) << " "
+              << exception.ReasonPhrase << std::endl
+              << "Error code: " << exception.ErrorCode << std::endl
+              << "Request ID: " << exception.RequestId << std::endl
+              << "Message: " << exception.Message << std::endl;
+    return 2;
+  }
+  catch (const std::exception& exception)
+  {
+    std::cout << "Unexpected exception thrown: " << exception.what() << std::endl;
+    return 3;
   }
 
   return 0;
--- sdk/identity/azure-identity/samples/environment_credential.cpp
+++ sdk/identity/azure-identity/samples/environment_credential.cpp
@@ -10,6 +10,10 @@ int main()
 {
   try
   {
+    // To diagnose, see https://aka.ms/azsdk/cpp/identity/troubleshooting
+    // For example, try setting 'AZURE_LOG_LEVEL' environment variable to 'verbose' before running
+    // this sample to see more details.
+
     // Step 1: Create an EnvironmentCredential instance.
     // Environment Credential would read its parameters from the environment variables, such as
     // AZURE_TENANT_ID, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET. See documentation for details.
@@ -29,6 +33,24 @@ int main()
     // (invalid credential parameters, insufficient permissions).
     std::cout << "Authentication error: " << exception.what() << std::endl;
     return 1;
+  }
+  catch (const Azure::Core::RequestFailedException& exception)
+  {
+    // Authentication exceptions are thrown as AuthenticationExceptions, client errors are thrown as
+    // RequestFailedExceptions, so it is easier to differentiate whether the request has failed
+    // due to input data, or due to authentication errors.
+    std::cout << "Azure service request error: " << exception.what() << std::endl
+              << "Status: " << static_cast<int>(exception.StatusCode) << " "
+              << exception.ReasonPhrase << std::endl
+              << "Error code: " << exception.ErrorCode << std::endl
+              << "Request ID: " << exception.RequestId << std::endl
+              << "Message: " << exception.Message << std::endl;
+    return 2;
+  }
+  catch (const std::exception& exception)
+  {
+    std::cout << "Unexpected exception thrown: " << exception.what() << std::endl;
+    return 3;
   }
 
   return 0;
