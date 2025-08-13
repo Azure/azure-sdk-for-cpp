Azure/azure-sdk-for-cpp

commit a035ee5f9416ef9188533de40b4ab1c37fb7c0af

Identity: IMDS fail-fast and Cred order change in DAC (and Core support) (#6573)
* Identity: IMDS fail-fast and Cred order change in DAC (and Core support)

* Mac fix and cspell update

* Update unit test and clang-format

* Temporarily update samples to use AzCliCred until recordings are re-recorded

* Revert samples back to use DAC

* Remove SAS auth from Tables template

* Clang-format

* Add support for 'AZURE_POD_IDENTITY_AUTHORITY_HOST', override it for running samples in CI

* Add unit test for AZURE_POD_IDENTITY_AUTHORITY_HOST

* "in milliseconds"

Co-authored-by: Scott Addie <10702007+scottaddie@users.noreply.github.com>

* PR Feedback

* Named constant + comment

---------

Co-authored-by: Anton Kolesnyk <antkmsft@users.noreply.github.com>
Co-authored-by: Scott Addie <10702007+scottaddie@users.noreply.github.com>

sdk/identity/azure-identity/src/managed_identity_source.cpp
@@ -5,9 +5,11 @@
 
 #include "private/identity_log.hpp"
 
+#include <azure/core/http/transport.hpp>
 #include <azure/core/internal/environment.hpp>
 #include <azure/core/platform.hpp>
 
+#include <chrono>
 #include <fstream>
 #include <iterator>
 #include <stdexcept>
@@ -21,12 +23,15 @@ using Azure::Core::_internal::Environment;
 using Azure::Identity::_detail::IdentityLog;
 
 namespace {
-
-// https://learn.microsoft.com/azure/virtual-machines/instance-metadata-service
-// IMDS is a REST API that's available at a well-known, non-routable IP address (169.254.169.254).
-// You can only access it from within the VM. Communication between the VM and IMDS never leaves the
-// host.
-std::string const ImdsEndpoint = "http://169.254.169.254/metadata/identity/oauth2/token";
+// First request for IMDS should not be taking tens of seconds - if IMDS is unavailable, we should
+// fail fast. Among other reasons, this improves user experience when ManagedIdentityCredential is
+// part of DefaultAzureCredential. Especially given that all the service credentials are earlier in
+// the chain than the developer tool credentials, if ManagedIdentityCredential makes a request which
+// takes 30 seconds to time out (host is not available), plus we make 3 retries of that request, and
+// all that to figure out that IMDS is not available before moving on to AzureCliCredential, it will
+// significantly worsen user experience when using DAC. Therefore, we need the timeout below (plus
+// we have logic to not retry that request).
+constexpr std::chrono::milliseconds ImdsFirstRequestConnectionTimeout = std::chrono::seconds{1};
 
 std::string WithSourceAndClientIdMessage(std::string const& credSource, std::string const& clientId)
 {
@@ -496,26 +501,48 @@ std::unique_ptr<ManagedIdentitySource> ImdsManagedIdentitySource::Create(
     std::string const& resourceId,
     Azure::Core::Credentials::TokenCredentialOptions const& options)
 {
+  const std::string ImdsName = "Azure Instance Metadata Service";
+
   IdentityLog::Write(
       IdentityLog::Level::Informational,
-      credName + " will be created"
-          + WithSourceAndClientIdMessage("Azure Instance Metadata Service", clientId)
+      credName + " will be created" + WithSourceAndClientIdMessage(ImdsName, clientId)
           + ".\nSuccessful creation does not guarantee further successful token retrieval.");
 
+  // https://learn.microsoft.com/azure/virtual-machines/instance-metadata-service
+  // IMDS is a REST API that's available at a well-known, non-routable IP address
+  // (169.254.169.254). You can only access it from within the VM. Communication between the VM
+  // and IMDS never leaves the host.
+  // 'AZURE_POD_IDENTITY_AUTHORITY_HOST' environment variable allows user to override the
+  // authority host for IMDS. This is consistent with other language SDKs.
+  Core::Url imdsUrl{"http://169.254.169.254"};
+  constexpr auto ImdsEndpointEnvVarName = "AZURE_POD_IDENTITY_AUTHORITY_HOST";
+  const auto imdsEndpointEnvVarValue = Environment::GetVariable(ImdsEndpointEnvVarName);
+  if (!imdsEndpointEnvVarValue.empty())
+  {
+    IdentityLog::Write(
+        IdentityLog::Level::Verbose,
+        credName + WithSourceAndClientIdMessage(ImdsName, {}) + ": '" + ImdsEndpointEnvVarName
+            + "' environment variable is set, so customized authority host ('"
+            + imdsEndpointEnvVarValue + "') will be used.");
+
+    imdsUrl = Core::Url{imdsEndpointEnvVarValue};
+  }
+  imdsUrl.SetPath("/metadata/identity/oauth2/token");
+
   return std::unique_ptr<ManagedIdentitySource>(
-      new ImdsManagedIdentitySource(clientId, objectId, resourceId, options));
+      new ImdsManagedIdentitySource(clientId, objectId, resourceId, imdsUrl, options));
 }
 
 ImdsManagedIdentitySource::ImdsManagedIdentitySource(
     std::string const& clientId,
     std::string const& objectId,
     std::string const& resourceId,
+    Azure::Core::Url const& imdsUrl,
     Azure::Core::Credentials::TokenCredentialOptions const& options)
     : ManagedIdentitySource(clientId, std::string(), options),
-      m_request(Azure::Core::Http::HttpMethod::Get, Azure::Core::Url(ImdsEndpoint))
+      m_request(Azure::Core::Http::HttpMethod::Get, imdsUrl)
 {
   {
-    using Azure::Core::Url;
     auto& url = m_request.GetUrl();
 
     url.AppendQueryParameter("api-version", "2018-02-01");
@@ -538,6 +565,11 @@ ImdsManagedIdentitySource::ImdsManagedIdentitySource(
   }
 
   m_request.SetHeader("Metadata", "true");
+
+  Core::Credentials::TokenCredentialOptions firstRequestOptions = options;
+  firstRequestOptions.Retry.MaxRetries = 0;
+  m_firstRequestPipeline = std::make_unique<TokenCredentialImpl>(firstRequestOptions);
+  m_firstRequestSucceeded = false;
 }
 
 Azure::Core::Credentials::AccessToken ImdsManagedIdentitySource::GetToken(
@@ -558,7 +590,7 @@ Azure::Core::Credentials::AccessToken ImdsManagedIdentitySource::GetToken(
   // call it later. Therefore, any capture made here will outlive the possible time frame when the
   // lambda might get called.
   return m_tokenCache.GetToken(scopesStr, {}, tokenRequestContext.MinimumExpiration, [&]() {
-    return TokenCredentialImpl::GetToken(context, true, [&]() {
+    std::function<std::unique_ptr<TokenRequest>()> const& createRequest = [&]() {
       auto request = std::make_unique<TokenRequest>(m_request);
 
       if (!scopesStr.empty())
@@ -567,6 +599,28 @@ Azure::Core::Credentials::AccessToken ImdsManagedIdentitySource::GetToken(
       }
 
       return request;
-    });
+    };
+
+    if (!m_firstRequestSucceeded)
+    {
+      std::unique_lock<std::mutex> lock(m_firstRequestMutex);
+      if (!m_firstRequestSucceeded)
+      {
+        const auto token = m_firstRequestPipeline->GetToken(
+            context.WithValue(
+                Core::Http::_internal::HttpConnectionTimeout, ImdsFirstRequestConnectionTimeout),
+            true,
+            createRequest);
+
+        m_firstRequestSucceeded = true;
+
+        lock.unlock();
+        m_firstRequestPipeline.reset();
+
+        return token;
+      }
+    }
+
+    return TokenCredentialImpl::GetToken(context, true, createRequest);
   });
 }

eng/pipelines/templates/jobs/live.tests.yml
@@ -267,6 +267,9 @@ jobs:
         useGlobalConfig: true
       env:
         ${{ insert }}: ${{ parameters.EnvVars }}
+        # Set fake authority host to ensure Managed Identity fail for Default Azure Credential
+        # so "execute samples" step correctly picks up Azure CLI credential.
+        AZURE_POD_IDENTITY_AUTHORITY_HOST: 'FakeAuthorityHost'
 
   - ${{ else }}:
     - bash: |
@@ -292,7 +295,10 @@ jobs:
       displayName: "Run Samples for : ${{ parameters.ServiceDirectory }}"
       condition: and(succeeded(), eq(variables['RunSamples'], '1'))
       env:
-        ${{ insert }}: ${{ parameters.EnvVars }}      
+        ${{ insert }}: ${{ parameters.EnvVars }}
+        # Set fake authority host to ensure Managed Identity fail for Default Azure Credential
+        # so "execute samples" step correctly picks up Azure CLI credential.
+        AZURE_POD_IDENTITY_AUTHORITY_HOST: 'FakeAuthorityHost'
 
   # Make coverage targets (specified in coverage_targets.txt) and assemble
   # coverage report

samples/helpers/service/src/client.cpp
@@ -11,7 +11,7 @@ void Azure::Service::Client::DoSomething(const Azure::Core::Context& context) co
 #if (0)
   // Every client has its own scope. We use management.azure.com here as an example.
   Core::Credentials::TokenRequestContext azureServiceClientContext;
-  azureServiceClientContext.Scopes = {"https://management.azure.com/"};
+  azureServiceClientContext.Scopes = {"https://management.azure.com/.default"};
 
   auto authenticationToken = m_credential->GetToken(azureServiceClientContext, context);
 

sdk/core/azure-core/inc/azure/core/http/transport.hpp
@@ -9,12 +9,22 @@
 #pragma once
 
 #include "azure/core/context.hpp"
+#include "azure/core/dll_import_export.hpp"
 #include "azure/core/http/http.hpp"
 #include "azure/core/http/raw_response.hpp"
 
 #include <memory>
 
 namespace Azure { namespace Core { namespace Http {
+
+  namespace _internal {
+    /**
+     * @brief A context key to use to pass connection timeout (`std::chrono::milliseconds`) to an
+     * HTTP transport.
+     *
+     */
+    AZ_CORE_DLLEXPORT extern const Context::Key HttpConnectionTimeout;
+  } // namespace _internal
 
   /**
    * @brief Base class for all HTTP transport implementations.

sdk/core/azure-core/src/http/curl/curl.cpp
@@ -349,9 +349,20 @@ std::unique_ptr<RawResponse> CurlTransport::Send(Request& request, Context const
   // Create CurlSession to perform request
   Log::Write(Logger::Level::Verbose, LogMsgPrefix + "Creating a new session.");
 
+  auto connectionTimeoutOverride = std::chrono::milliseconds{0};
+  {
+    std::chrono::milliseconds contextConnectionTimeout{0};
+    if (context.TryGetValue(Http::_internal::HttpConnectionTimeout, contextConnectionTimeout)
+        && contextConnectionTimeout.count() > 0)
+    {
+      connectionTimeoutOverride = contextConnectionTimeout;
+    }
+  }
+
   auto session = std::make_unique<CurlSession>(
       request,
-      CurlConnectionPool::g_curlConnectionPool.ExtractOrCreateCurlConnection(request, m_options),
+      CurlConnectionPool::g_curlConnectionPool.ExtractOrCreateCurlConnection(
+          request, m_options, connectionTimeoutOverride, false),
       m_options);
 
   CURLcode performing;
@@ -380,6 +391,7 @@ std::unique_ptr<RawResponse> CurlTransport::Send(Request& request, Context const
         CurlConnectionPool::g_curlConnectionPool.ExtractOrCreateCurlConnection(
             request,
             m_options,
+            connectionTimeoutOverride,
             getConnectionOpenIntent + 1 >= _detail::RequestPoolResetAfterConnectionFailed),
         m_options);
   }
@@ -1386,11 +1398,42 @@ size_t CurlSession::ResponseBufferParser::Parse(
 }
 
 namespace {
+// Calculates the effective timeout value, based on options.ConnectionTimeout and
+// connectionTimeoutOverride. Returns 0 if default.
+long GetConnectionTimeout(
+    CurlTransportOptions const& options,
+    std::chrono::milliseconds connectionTimeoutOverride)
+{
+  auto connectionTimeout
+      = (options.ConnectionTimeout != Azure::Core::Http::_detail::DefaultConnectionTimeout)
+      ? options.ConnectionTimeout
+      : std::chrono::milliseconds{0};
+
+  if (connectionTimeoutOverride.count() > 0)
+  {
+    connectionTimeout = connectionTimeout.count() > 0
+        ? (std::min)(connectionTimeout, connectionTimeoutOverride)
+        : connectionTimeoutOverride;
+  }
+
+  auto connectionTimeoutLong = 0;
+  if (connectionTimeout.count() > 0
+      && connectionTimeout.count() <= std::numeric_limits<long>::max())
+  {
+    connectionTimeoutLong = static_cast<long>(connectionTimeout.count());
+  }
+
+  return connectionTimeoutLong;
+}
+
 // Calculate the connection key.
 // The connection key is a tuple of host, proxy info, TLS info, etc. Basically any characteristics
 // of the connection that should indicate that the connection shouldn't be re-used should be listed
 // the connection key.
-inline std::string GetConnectionKey(std::string const& host, CurlTransportOptions const& options)
+inline std::string GetConnectionKey(
+    std::string const& host,
+    CurlTransportOptions const& options,
+    std::chrono::milliseconds connectionTimeoutOverride)
 {
   std::string key(host);
   key.append(",");
@@ -1427,12 +1470,7 @@ inline std::string GetConnectionKey(std::string const& host, CurlTransportOption
   key.append("0");
 #endif
   key.append(",");
-  // using DefaultConnectionTimeout or 0 result in the same setting
-  key.append(
-      (options.ConnectionTimeout == Azure::Core::Http::_detail::DefaultConnectionTimeout
-       || options.ConnectionTimeout == std::chrono::milliseconds(0))
-          ? "0"
-          : std::to_string(options.ConnectionTimeout.count()));
+  key.append(std::to_string(GetConnectionTimeout(options, connectionTimeoutOverride)));
 
   return key;
 }
@@ -2182,13 +2220,15 @@ int CurlConnection::SslCtxCallback(CURL*, void* sslctx)
 std::unique_ptr<CurlNetworkConnection> CurlConnectionPool::ExtractOrCreateCurlConnection(
     Request& request,
     CurlTransportOptions const& options,
+    std::chrono::milliseconds connectionTimeoutOverride,
     bool resetPool)
 {
   uint16_t port = request.GetUrl().GetPort();
   // Generate a display name for the host being connected to
   std::string const& hostDisplayName = request.GetUrl().GetScheme() + "://"
       + request.GetUrl().GetHost() + (port != 0 ? ":" + std::to_string(port) : "");
-  std::string const connectionKey = GetConnectionKey(hostDisplayName, options);
+  std::string const connectionKey
+      = GetConnectionKey(hostDisplayName, options, connectionTimeoutOverride);
 
   {
     decltype(CurlConnectionPool::g_curlConnectionPool
@@ -2239,7 +2279,8 @@ std::unique_ptr<CurlNetworkConnection> CurlConnectionPool::ExtractOrCreateCurlCo
   // No available connection for the pool for the required host. Create one
   Log::Write(Logger::Level::Verbose, LogMsgPrefix + "Spawn new connection.");
 
-  return std::make_unique<CurlConnection>(request, options, hostDisplayName, connectionKey);
+  return std::make_unique<CurlConnection>(
+      request, options, hostDisplayName, connectionKey, connectionTimeoutOverride);
 }
 
 // Move the connection back to the connection pool. Push it to the front so it becomes the
@@ -2306,7 +2347,8 @@ CurlConnection::CurlConnection(
     Request& request,
     CurlTransportOptions const& options,
     std::string const& hostDisplayName,
-    std::string const& connectionPropertiesKey)
+    std::string const& connectionPropertiesKey,
+    std::chrono::milliseconds connectionTimeoutOverride)
     : m_connectionKey(connectionPropertiesKey)
 {
   m_handle = Azure::Core::_internal::UniqueHandle<CURL>(curl_easy_init());
@@ -2408,15 +2450,17 @@ CurlConnection::CurlConnection(
         + std::string(curl_easy_strerror(result)));
   }
 
-  if (options.ConnectionTimeout != Azure::Core::Http::_detail::DefaultConnectionTimeout)
   {
-    if (!SetLibcurlOption(m_handle, CURLOPT_CONNECTTIMEOUT_MS, options.ConnectionTimeout, &result))
+    const long connectionTimeout = GetConnectionTimeout(options, connectionTimeoutOverride);
+    if (connectionTimeout > 0)
     {
-      throw Azure::Core::Http::TransportException(
-          _detail::DefaultFailedToGetNewConnectionTemplate + hostDisplayName
-          + ". Fail setting connect timeout to: "
-          + std::to_string(options.ConnectionTimeout.count()) + " ms. "
-          + std::string(curl_easy_strerror(result)));
+      if (!SetLibcurlOption(m_handle, CURLOPT_CONNECTTIMEOUT_MS, connectionTimeout, &result))
+      {
+        throw Azure::Core::Http::TransportException(
+            _detail::DefaultFailedToGetNewConnectionTemplate + hostDisplayName
+            + ". Fail setting connect timeout to: " + std::to_string(connectionTimeout) + " ms. "
+            + std::string(curl_easy_strerror(result)));
+      }
     }
   }
 

sdk/core/azure-core/src/http/curl/curl_connection_pool_private.hpp
@@ -16,6 +16,7 @@
 #include <azure/core/http/curl_transport.hpp>
 
 #include <atomic>
+#include <chrono>
 #include <condition_variable>
 #include <list>
 #include <memory>
@@ -77,6 +78,8 @@ namespace Azure { namespace Core { namespace Http { namespace _detail {
      * @param request HTTP request to get #Azure::Core::Http::CurlNetworkConnection for.
      * @param options The connection settings which includes host name and libcurl handle specific
      * configuration.
+     * @param connectionTimeoutOverride If greater than 0, specifies the override value for the
+     * ConnectionTimeout value, specified in options.
      * @param resetPool Request the pool to remove all current connections for the provided
      * options to force the creation of a new connection.
      *
@@ -85,6 +88,7 @@ namespace Azure { namespace Core { namespace Http { namespace _detail {
     std::unique_ptr<CurlNetworkConnection> ExtractOrCreateCurlConnection(
         Request& request,
         CurlTransportOptions const& options,
+        std::chrono::milliseconds connectionTimeoutOverride = std::chrono::milliseconds{0},
         bool resetPool = false);
 
     /**

sdk/core/azure-core/src/http/curl/curl_connection_private.hpp
@@ -196,6 +196,8 @@ namespace Azure { namespace Core {
        * @param request Remote request
        * @param options Connection options.
        * @param hostDisplayName Display name for remote host, used for diagnostics.
+       * @param connectionTimeoutOverride If greater than 0, specifies the override value for the
+       * ConnectionTimeout value, specified in options.
        *
        * @param connectionPropertiesKey CURL connection properties key
        */
@@ -203,7 +205,8 @@ namespace Azure { namespace Core {
           Azure::Core::Http::Request& request,
           Azure::Core::Http::CurlTransportOptions const& options,
           std::string const& hostDisplayName,
-          std::string const& connectionPropertiesKey);
+          std::string const& connectionPropertiesKey,
+          std::chrono::milliseconds connectionTimeoutOverride);
 
       /**
        * @brief Destructor.

sdk/core/azure-core/src/http/http.cpp
@@ -3,6 +3,7 @@
 
 #include "azure/core/http/http.hpp"
 
+#include "azure/core/http/transport.hpp"
 #include "azure/core/internal/strings.hpp"
 #include "azure/core/url.hpp"
 
@@ -12,11 +13,13 @@
 using namespace Azure::Core;
 using namespace Azure::Core::Http;
 
-char const Azure::Core::Http::_internal::HttpShared::ContentType[] = "content-type";
-char const Azure::Core::Http::_internal::HttpShared::ApplicationJson[] = "application/json";
-char const Azure::Core::Http::_internal::HttpShared::Accept[] = "accept";
-char const Azure::Core::Http::_internal::HttpShared::MsRequestId[] = "x-ms-request-id";
-char const Azure::Core::Http::_internal::HttpShared::MsClientRequestId[] = "x-ms-client-request-id";
+const Context::Key Http::_internal::HttpConnectionTimeout{};
+
+char const Http::_internal::HttpShared::ContentType[] = "content-type";
+char const Http::_internal::HttpShared::ApplicationJson[] = "application/json";
+char const Http::_internal::HttpShared::Accept[] = "accept";
+char const Http::_internal::HttpShared::MsRequestId[] = "x-ms-request-id";
+char const Http::_internal::HttpShared::MsClientRequestId[] = "x-ms-client-request-id";
 
 const HttpMethod HttpMethod::Get("GET");
 const HttpMethod HttpMethod::Head("HEAD");

commit 72f327b2c19afffa9df2ce0ef817c13b3dd75804

Fix IMDS token requests for managed identities
This was broken by a035ee5f9416ef9188533de40b4ab1c37fb7c0af, which
accidentally constructed the IMDS URL with a double slash at the start
of the path. This is not properly routed on the server side, leading to
a 404 error with some very misleading XML.

Signed-off-by: James Le Cuirot <jlecuirot@microsoft.com>

sdk/identity/azure-identity/src/managed_identity_source.cpp
@@ -527,7 +527,7 @@ std::unique_ptr<ManagedIdentitySource> ImdsManagedIdentitySource::Create(
 
     imdsUrl = Core::Url{imdsEndpointEnvVarValue};
   }
-  imdsUrl.SetPath("/metadata/identity/oauth2/token");
+  imdsUrl.SetPath("metadata/identity/oauth2/token");
 
   return std::unique_ptr<ManagedIdentitySource>(
       new ImdsManagedIdentitySource(clientId, objectId, resourceId, imdsUrl, options));