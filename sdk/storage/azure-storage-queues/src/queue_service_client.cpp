f29a60d2e08033fa7241e95f93dba122bba91ce8
microzchang <110015819+microzchang@users.noreply.github.com>
2025-08-19 18:53:28 +0800

    Principal Bound Delegation Sas and TQF (#6699)
    * Principal Bound Delegation Sas and TQF
    
    * Disable test cases
    
    * Fix test cases
    
    * Fix cspell
    
    * Fix docs
    
    * Fix clang

--- sdk/storage/azure-storage-queues/src/queue_service_client.cpp
+++ sdk/storage/azure-storage-queues/src/queue_service_client.cpp
@@ -193,4 +193,18 @@ namespace Azure { namespace Storage { namespace Queues {
     return queueClient.Delete(options, context);
   }
 
+  Azure::Response<Models::UserDelegationKey> QueueServiceClient::GetUserDelegationKey(
+      const Azure::DateTime& expiresOn,
+      const GetUserDelegationKeyOptions& options,
+      const Azure::Core::Context& context) const
+  {
+    _detail::ServiceClient::GetServiceUserDelegationKeyOptions protocolLayerOptions;
+    protocolLayerOptions.KeyInfo.Start = options.StartsOn.ToString(
+        Azure::DateTime::DateFormat::Rfc3339, Azure::DateTime::TimeFractionFormat::Truncate);
+    protocolLayerOptions.KeyInfo.Expiry = expiresOn.ToString(
+        Azure::DateTime::DateFormat::Rfc3339, Azure::DateTime::TimeFractionFormat::Truncate);
+    return _detail::ServiceClient::GetUserDelegationKey(
+        *m_pipeline, m_serviceUrl, protocolLayerOptions, context);
+  }
+
 }}} // namespace Azure::Storage::Queues
