397ae78b1e33e3dada492084516d1178e0c628e7
Anton Kolesnyk <41349689+antkmsft@users.noreply.github.com>
2025-10-17 14:51:11 -0700

    Delete cgmanifest.json when being installed via vcpkg, but also update SHAs (#6789)
    * Delete cgmanifest.json when being installed via vcpkg, but also update SHAs
    
    * Update cspell
    
    ---------
    
    Co-authored-by: Anton Kolesnyk <antkmsft@users.noreply.github.com>

--- sdk/appconfiguration/azure-data-appconfiguration/vcpkg/portfile.cmake
+++ sdk/appconfiguration/azure-data-appconfiguration/vcpkg/portfile.cmake
@@ -12,6 +12,9 @@ vcpkg_from_github(
     HEAD_REF main
 )
 
+file(GLOB_RECURSE unused "${SOURCE_PATH}/cgmanifest.json")
+file(REMOVE_RECURSE ${unused})
+
 if(EXISTS "${SOURCE_PATH}/sdk/appconfiguration/azure-data-appconfiguration")
   file(REMOVE_RECURSE "${SOURCE_PATH}/sdk/appconfiguration/_")
   file(REMOVE_RECURSE "${SOURCE_PATH}/sdk/_")
