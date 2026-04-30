e00341c7d51e0a0b7cfad124e79e5f15bd5a0b38
Anton Kolesnyk <41349689+antkmsft@users.noreply.github.com>
2026-04-30 11:45:25 -0700

    Add a change to re-trigger CI

--- cmake-modules/AzureVcpkg.cmake
+++ cmake-modules/AzureVcpkg.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) Microsoft Corporation.
+# Copyright (c) Microsoft Corporation. 
 # Licensed under the MIT License.
 
 # We need to know an absolute path to our repo root to do things like referencing ./LICENSE.txt file.
