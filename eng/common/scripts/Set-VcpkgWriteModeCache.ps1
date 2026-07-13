1c460d05cdb8a341a9a141dda704105caaeaadcb
Daniel Jurek <djurek@microsoft.com>
2026-07-13 15:21:15 -0700

    Disable binary cache

--- eng/common/scripts/Set-VcpkgWriteModeCache.ps1
+++ eng/common/scripts/Set-VcpkgWriteModeCache.ps1
@@ -18,5 +18,5 @@ $vcpkgBinarySourceSas = New-AzStorageContainerSASToken `
 # Ensure redaction of SAS tokens in logs
 Write-Host "##vso[task.setvariable variable=VCPKG_BINARY_SAS_TOKEN;issecret=true;]$vcpkgBinarySourceSas"
 
-Write-Host "Setting vcpkg binary cache to read and write"
-Write-Host "##vso[task.setvariable variable=VCPKG_BINARY_SOURCES_SECRET;issecret=true;]clear;x-azcopy-sas,https://$StorageAccountName.blob.core.windows.net/$StorageContainerName,$vcpkgBinarySourceSas,readwrite"
+Write-Host "Binary cache disabled (clear) to force full from-source builds and validate Terrapin asset coverage"
+Write-Host "##vso[task.setvariable variable=VCPKG_BINARY_SOURCES_SECRET;issecret=true;]clear"
