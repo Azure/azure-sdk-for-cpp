7a7528c2ec8366449daf91c24ba1b7c637b1637a
Larry Osterman <larryo@microsoft.com>
2026-07-31 17:02:13 -0700

    Pin the AMQP test broker hash to protect against changes

--- sdk/core/azure-core-amqp/Test-Setup.ps1
+++ sdk/core/azure-core-amqp/Test-Setup.ps1
@@ -2,11 +2,17 @@
 # Licensed under the MIT License.
 # cspell: ignore JOBID depsfile
 
+
 # Load common ES scripts
 . "$PSScriptRoot\..\..\..\eng\common\scripts\common.ps1"
 
+if ($IsMacOS) {
+  Write-Host "AMQP tests are not supported on macOS. Skipping test setup."
+  exit 0
+}
+
 # Create the test binary *outside* the repo root to avoid polluting the repo.
-$WorkingDirectory = ([System.IO.Path]::Combine($RepoRoot, "../TestArtifacts"))
+$WorkingDirectory = [System.IO.Path]::Combine($RepoRoot, "../TestArtifacts")
 
 # Create the working directory if it does not exist.
 Write-Host "Using Working Directory $WorkingDirectory"
@@ -22,18 +28,23 @@ Push-Location -Path $WorkingDirectory
 # Clone and build the Test Amqp Broker.
 try {
 
+  $repositoryDir = [System.IO.Path]::Combine($WorkingDirectory, "azure-amqp")
+  if (Test-Path $repositoryDir) {
+    Write-Host "Removing previously cloned repository: $repositoryDir"
+    Remove-Item $repositoryDir -Force -Recurse | Out-Null
+  }
+
   $repositoryUrl = "https://github.com/Azure/azure-amqp.git"
-  # We would like to use the "hotfix" branch because that is current, but unfortunately it references System.Net.Security version 4.0.0
-  $repositoryBranch = "master"
-  $cloneCommand = "git clone $repositoryUrl --branch $repositoryBranch"
+  $repositoryHash = "d82a86455c3459c5628bc95b25511f6e8a065598"
+  $cloneCommand = "git clone $repositoryUrl --revision $repositoryHash --depth=1"
 
   Write-Host "Cloning repository from $repositoryUrl..."
   Invoke-LoggedCommand $cloneCommand
 
   Set-Location -Path "./azure-amqp/test/TestAmqpBroker"
 
-  Invoke-LoggedCommand "dotnet build -p RollForward=LatestMajor --framework net8.0"
-  if (!$? -ne 0) {
+  Invoke-LoggedCommand "dotnet build --framework net8.0"
+  if (-not $?) {
     Write-Error "Failed to build TestAmqpBroker."
     exit 1
   }
@@ -41,33 +52,30 @@ try {
   Write-Host "Test broker built successfully."
 
   # now that the Test broker has been built, launch the broker on a local address.
+  $env:TEST_BROKER_ADDRESS = 'amqp://127.0.0.1:25672'
+
   Write-Host "Starting test broker listening on ${env:TEST_BROKER_ADDRESS} ..."
 
+  # Note that we cannot use `dotnet run -f` here because the TestAmqpBroker relies on args[0] being the broker address.
+  # If we use `dotnet run -f`, the first argument is the csproj file.
+  # Instead, we use `dotnet exec` to run the compiled DLL directly.
+  # This allows us to pass the broker address as the first argument.
   Set-Location -Path $WorkingDirectory/azure-amqp/bin/Debug/TestAmqpBroker/net8.0
+  $job = dotnet exec ./TestAmqpBroker.dll ${env:TEST_BROKER_ADDRESS} /headless &
 
-#  $job = dotnet exec ./TestAmqpBroker.dll ${env:TEST_BROKER_ADDRESS} /headless &
-  $Process = Start-Process -NoNewWindow -FilePath "dotnet" -ArgumentList "exec ./TestAmqpBroker.dll ${env:TEST_BROKER_ADDRESS} /headless" -PassThru  -RedirectStandardOutput $WorkingDirectory/test-broker.log -RedirectStandardError $WorkingDirectory/test-broker-error.log
-
-  if (!$? -ne 0) {
-    Write-Error "Failed to start TestAmqpBroker."
-    exit 1
-  }
-
-  $Process
-
-  $env:TEST_BROKER_JOBID = $Process.Id
+  $env:TEST_BROKER_JOBID = $job.Id
 
   Write-Host "Waiting for test broker to start..."
   Start-Sleep -Seconds 3
 
-#  Write-Host "Job Output after wait:"
-#  Receive-Job $job.Id
-#
-#  $job = Get-Job -Id $env:TEST_BROKER_JOBID
-#  if ($job.State -ne "Running") {
-#    Write-Host "Test broker failed to start."
-#    exit 1
-#  }
+  Write-Host "Job Output after wait:"
+  Receive-Job $job.Id
+
+  $job = Get-Job -Id $env:TEST_BROKER_JOBID
+  if ($job.State -ne "Running") {
+    Write-Host "Test broker failed to start."
+    exit 1
+  }
 }
 finally {
   Pop-Location
